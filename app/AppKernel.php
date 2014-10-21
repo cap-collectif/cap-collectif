d13b2eb41de8c32f088acbf4b6040681f76703f2
Laurent Brunet <lbrunet@jolicode.com>
2014-10-21 12:49:30 +0200

    Adding the application bundle **AppBundle**
    GitOrigin-RevId: de5d05c6b8d7a0436718c7ae7784c7f9280eabee

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new CapCollectif\AppBundle\CapCollectifAppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/CapCollectif/AppBundle/CapCollectifAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CapCollectif\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CapCollectifAppBundle extends Bundle
+{
+}
