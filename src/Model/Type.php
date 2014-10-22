b7b63b5fcf1ce4ca4d7cb6e64826bf29da59f0d2
Laurent Brunet <lbrunet@jolicode.com>
2014-10-22 13:02:22 +0200

    create entity Type
    GitOrigin-RevId: 7654efcd3af8f01360ee8aa3b4cd9ced2416f457

---
+++ src/Model/Type.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Type
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Type
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=75)
+     */
+    private $title;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="weight", type="smallint")
+     */
+    private $weight;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Type
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set weight
+     *
+     * @param integer $weight
+     * @return Type
+     */
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    /**
+     * Get weight
+     *
+     * @return integer
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+}
