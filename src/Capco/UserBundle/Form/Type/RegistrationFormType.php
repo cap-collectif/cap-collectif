607e508f29a6720ef42d2ec807acde01c5db3f95
sfavot <sfavot@jolicode.com>
2015-01-26 12:26:31 +0100

    Ref #307 tou
    GitOrigin-RevId: c737b8e3f7d89ef7391181839dc4cfbdafd635df

--- src/Capco/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Capco/UserBundle/Form/Type/RegistrationFormType.php
@@ -4,16 +4,24 @@ namespace Capco\UserBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Capco\AppBundle\SiteParameter\Resolver as SiteParameterResolver;
 
 class RegistrationFormType extends AbstractType
 {
+    private $siteParameterResolver;
+
+    public function __construct(SiteParameterResolver $resolver)
+    {
+        $this->siteParameterResolver = $resolver;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('isTermsAccepted', 'checkbox', array(
-                    'label' => null,
-                    'required' => true,
-                ))
-        ;
+        if ((null != $this->siteParameterResolver->getValue('signin.cgu.name')) && (null != $this->siteParameterResolver->getValue('signin.cgu.link'))) {
+            $builder->add('isTermsAccepted', 'checkbox', array(
+                'label' => null,
+                'required' => true,
+            ));
+        }
     }
 
     public function getParent()
