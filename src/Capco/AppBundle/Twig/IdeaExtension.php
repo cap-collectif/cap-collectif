899230584f20e732e6c46aa1b46297e7914bbb93
spyl94 <spyl94@gmail.com>
2015-03-20 12:50:13 +0100

    add mobile design
    GitOrigin-RevId: 8c59bb61e8392aa5b002580923d80537be65c9ac

---
+++ src/Capco/AppBundle/Twig/IdeaExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Capco\AppBundle\Twig;
+
+
+use Capco\AppBundle\Helper\IdeaHelper;
+
+class IdeaExtension extends \Twig_Extension
+{
+    protected $helper;
+
+    public function __construct(IdeaHelper $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'capco_idea_helper';
+    }
+
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('capco_idea_user_voted', array($this, 'hasVoted')),
+        );
+    }
+
+    public function hasVoted($idea, $user)
+    {
+        return $this->helper->findUserVoteOrCreate($idea, $user)->isConfirmed();
+    }
+}
