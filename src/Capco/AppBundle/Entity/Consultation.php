dd0844f09462d586844b034f6d354d1477b472d4
spyl94 <spyl94@gmail.com>
2015-03-24 16:39:20 +0100

    fix
    GitOrigin-RevId: 478106e87f291404ad8e6ab7d05898d936658238

--- src/Capco/AppBundle/Entity/Consultation.php
+++ src/Capco/AppBundle/Entity/Consultation.php
@@ -163,14 +163,6 @@ class Consultation
     private $Cover;
 
     /**
-     * @var
-     *
-     * @ORM\ManyToOne(targetEntity="Capco\MediaBundle\Entity\Media", cascade={"persist"})
-     * @ORM\JoinColumn(name="image_id", referencedColumnName="id", nullable=true, onDelete="SET NULL")
-     */
-    private $Image;
-
-    /**
      * @ORM\Column(name="video", type="string", nullable = true)
      */
     private $video = null;
@@ -536,22 +528,6 @@ class Consultation
     public function setCover($cover)
     {
         $this->Cover = $cover;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getImage()
-    {
-        return $this->Image;
-    }
-
-    /**
-     * @param mixed $Image
-     */
-    public function setImage($Image)
-    {
-        $this->Image = $Image;
     }
 
     /**
