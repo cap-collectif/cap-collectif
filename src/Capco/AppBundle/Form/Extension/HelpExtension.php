d361860176bfb4172b42b5ad5f67b9b2d9b07907
sfavot <sfavot@jolicode.com>
2015-04-01 13:07:39 +0200

    fix idea
    GitOrigin-RevId: b31c0aeb710acdcaeac4b36b49e038fa50277934

---
+++ src/Capco/AppBundle/Form/Extension/HelpExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Capco\AppBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+class HelpExtension extends AbstractTypeExtension
+{
+    public function getExtendedType()
+    {
+        return 'form';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array('help' => null));
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars = array_merge($view->vars, array('help' => $options['help']));
+    }
+
+
+}
