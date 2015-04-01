1790ee0e0a31fd38a4027c25e3a9e6cb91a4ec5d
sfavot <sfavot@jolicode.com>
2015-04-01 18:17:53 +0200

    Merge pull request #779 from jolicode/feature/fix_idea
    fix idea
    
    GitOrigin-RevId: 6feef494f18a1b5fd1dabedde0200da753dc6af6

---
+++ src/Capco/AppBundle/Validator/Constraints/HasThemeIfActivatedValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Capco\AppBundle\Validator\Constraints;
+
+use Capco\AppBundle\Toggle\Manager;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class HasThemeIfActivatedValidator extends ConstraintValidator
+{
+    private $toggleManager;
+
+    public function __construct(Manager $toggleManager)
+    {
+        $this->toggleManager = $toggleManager;
+    }
+
+    public function validate($object, Constraint $constraint)
+    {
+        if ($this->toggleManager->isActive('themes') && null == $object ) {
+            $this->context->addViolation($constraint->message, array());
+        }
+    }
+}
