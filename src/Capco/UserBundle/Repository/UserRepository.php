695c40342f76c8283a0135580de51c64119fee86
sfavot <sfavot@jolicode.com>
2015-04-17 14:42:05 +0200

    #891 fix modal in idea page
    GitOrigin-RevId: 337a6d507a40a371b6b88a5ef5afa66a23bffe28

--- src/Capco/UserBundle/Repository/UserRepository.php
+++ src/Capco/UserBundle/Repository/UserRepository.php
@@ -9,19 +9,4 @@ use Doctrine\ORM\EntityRepository;
  */
 class UserRepository extends EntityRepository
 {
-    public function findConsultationContributors($consultation)
-    {
-        $qb = $this->createQueryBuilder('u');
-            $qb->select('distinct u.id')
-            ->leftJoin('u.arguments', 'arguments')
-            ->leftJoin('u.opinions', 'opinions')
-            ->leftJoin('arguments.opinion', 'arguments_opinion')
-            ->where($qb->expr()->orX(
-                $qb->expr()->eq('arguments_opinion.Consultation', ':consultation'),
-                $qb->expr()->eq('opinions.Consultation', ':consultation')
-            ))
-            ->setParameter('consultation', $consultation)
-        ;
-        return $qb->getQuery()->getResult();
-    }
 }
