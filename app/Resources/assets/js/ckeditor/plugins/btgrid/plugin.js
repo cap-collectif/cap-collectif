cb3069269af3fc93b40d69ac3f484fe2cf04d1d6
sfavot <sfavot@jolicode.com>
2015-04-30 18:16:14 +0200

    Merge pull request #956 from jolicode/feature/946_improve_ckeditor_grid_tool
    Ref #946 improve btgrid ckeditor plugin
    
    GitOrigin-RevId: f68c1b28078f53b39fbf57a9403d72f3ef6f2383

--- app/Resources/assets/js/ckeditor/plugins/btgrid/plugin.js
+++ app/Resources/assets/js/ckeditor/plugins/btgrid/plugin.js
@@ -1,5 +1,5 @@
 CKEDITOR.plugins.add('btgrid', {
-    lang: 'fr',
+    lang: 'fr, en',
     requires: 'widget,dialog',
     icons: 'btgrid',
     init: function(editor) {
@@ -8,7 +8,7 @@ CKEDITOR.plugins.add('btgrid', {
         CKEDITOR.dialog.add('btgrid', this.path + 'dialogs/btgrid.js');
         editor.addContentsCss( this.path + 'styles/editor.css');
         editor.widgets.add('btgrid', {
-            allowedContent: 'div(!btgrid);div(!row);div(!col-*);div(!content)',
+            allowedContent: 'div(!btgrid);div(!row, !row-*);div(!col-*);div(!content)',
             requiredContent: 'div(btgrid)',
             parts: {
                 btgrid: 'div.btgrid'
@@ -26,6 +26,11 @@ CKEDITOR.plugins.add('btgrid', {
             },
             init: function() {
                 var rowCount = this.element.getChildCount();
+                if (rowCount) {
+                    this.setData('rowCount', rowCount);
+                    var colCount = this.element.getFirst().getChildCount();
+                    this.setData('colCount', colCount);
+                }
                 for (var rowNumber= 1;rowNumber <= rowCount;rowNumber++) {
                     this.createEditable(maxGridColumns, rowNumber);
                 }
@@ -34,26 +39,72 @@ CKEDITOR.plugins.add('btgrid', {
                 if (this.data.colCount) {
                     var colCount = this.data.colCount;
                     var rowCount = this.data.rowCount;
-                    var row = this.parts['btgrid'];
-                    for (var i= 1;i <= rowCount;i++) {
-                        this.createGrid(colCount, row, i);
+                    var block = this.element;
+                    this.updateGrid(rowCount, colCount, block);
+                }
+            },
+            updateGrid: function(rowCount, colCount, block) {
+                var currentRowsCount = block.getChildCount();
+                while (currentRowsCount < rowCount) {
+                    currentRowsCount++;
+                    this.addRow(block, colCount, currentRowsCount);
+                }
+                while (currentRowsCount > rowCount) {
+                    currentRowsCount--;
+                    this.removeRow(block);
+                }
+
+                for (var rowNumber = 1; rowNumber <= rowCount; rowNumber++) {
+                    var row = block.getChild(rowNumber-1);
+                    if (row != null) {
+                        var currentColsCount = row.getChildCount();
+                        while (currentColsCount < colCount) {
+                            currentColsCount++;
+                            this.addCol(row, colCount, currentColsCount);
+                        }
+                        while (currentColsCount > colCount) {
+                            currentColsCount--;
+                            this.removeCol(row);
+                        }
+                        this.updateCol(row, colCount);
+                        this.createEditable(colCount, rowNumber);
                     }
                 }
             },
-            createGrid: function(colCount, row, rowNumber) {
+            addRow: function(block, colCount, rowNumber) {
                 var content = '<div class="row row-' + rowNumber + '">';
                 for (var i = 1; i <= colCount; i++) {
                     content = content + '<div class="col-md-' + maxGridColumns/colCount + '">' +
                     '  <div class="content">' +
-                    '    <p>Col ' + i + ' content area</p>' +
+                    '    <p>Col ' + i + '</p>' +
                     '  </div>' +
                     '</div>';
                 }
-                content =content + '</div>';
-                row.appendHtml(content);
-                this.createEditable(colCount, rowNumber);
+                content = content + '</div>';
+                block.appendHtml(content);
             },
-            createEditable: function(colCount,rowNumber) {
+            removeRow: function(block) {
+                block.getLast().remove();
+            },
+            addCol: function(row, colCount, colNumber) {
+                var content = '<div class="col-md-' + maxGridColumns/colCount + '">' +
+                    '  <div class="content">' +
+                    '    <p>Col ' + colNumber + '</p>' +
+                    '  </div>' +
+                    '</div>';
+                row.appendHtml(content);
+            },
+            removeCol: function(row) {
+                row.getLast().remove();
+            },
+            updateCol: function(row, colCount) {
+                for (var colNumber = 1; colNumber <= row.getChildCount(); colNumber++) {
+                    var col = row.getChild(colNumber-1);
+                    col.setAttribute("class", "");
+                    col.addClass('col-md-' + maxGridColumns/colCount);
+                }
+            },
+            createEditable: function(colCount, rowNumber) {
                 for (var i = 1; i <= colCount; i++) {
                     this.initEditable( 'content'+ rowNumber + i, {
                         selector: '.row-'+ rowNumber +' > div:nth-child('+ i +') div.content'
