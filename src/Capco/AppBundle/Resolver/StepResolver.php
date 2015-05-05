77f88bffad406f5baf4d6236c0ef8fc75ad6b07a
sfavot <sfavot@jolicode.com>
2015-05-05 10:57:03 +0200

    #890 improve consultations
    GitOrigin-RevId: 4a0f30ad557c2e86bb3d50b578935129cde7e73b

--- src/Capco/AppBundle/Resolver/StepResolver.php
+++ src/Capco/AppBundle/Resolver/StepResolver.php
@@ -29,7 +29,7 @@ class StepResolver
             }
         }
 
-        return;
+        return null;
     }
 
     public function getFirstStepLinkForConsultation(Consultation $consultation, $absolute = false)
