f7e60a55814df58d5b1a00c747dcee97ad15b37c
spyl94 <spyl94@gmail.com>
2015-05-22 11:39:19 +0200

    assets install
    GitOrigin-RevId: 3dece18eda23318981affc90b22121db19e4fcbc

--- infrastructure/services/builder/build.sh
+++ infrastructure/services/builder/build.sh
@@ -4,6 +4,7 @@
 cd /capco/
 composer install --prefer-dist --no-interaction --no-scripts
 php vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php
+php app/console assets:install
 
 # Brunch generation / Bower
 npm install
