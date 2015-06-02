84f4074153be453aac71a5eedeab114472ed6bab
sfavot <sfavot@jolicode.com>
2015-06-02 17:47:46 +0200

    Merge pull request #1008 from jolicode/feature/983_fix_popover_buttons
    Ref #983 fix connection popover buttons
    
    GitOrigin-RevId: 33a092055ef71d5f8945a6124e4ea668b5acda2f

--- src/Capco/MediaBundle/Document/Media.php
+++ src/Capco/MediaBundle/Document/Media.php
@@ -8,7 +8,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Capco\MediaBundle\Document;
 
 use Sonata\MediaBundle\Document\BaseMedia as BaseMedia;
