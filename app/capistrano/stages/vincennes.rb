473254aa1c4ad86e7ca4049d1a0d02f2843fbbc9
spyl94 <spyl94@gmail.com>
2015-07-21 17:41:44 +0200

    goodbye capistrano
    GitOrigin-RevId: d93a0b472cbb4010d4e2eb5aea6b9208781e6124

--- app/capistrano/stages/vincennes.rb
+++
@@ -1,3 +0,0 @@
-server 'demo.v2.cap-collectif.com', user: 'jolicode', roles: [:web, :app]
-set :deploy_to,   "/home/jolicode/htdocs/demo.v2.cap-collectif.com/vincennes"
-set :parameters_yml_file, "app/config/parameters/parameters_vincennes.yml"
