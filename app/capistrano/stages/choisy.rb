1a5889997637c901b3d40c2fbbdbd4dd7bdd868c
sfavot <sfavot@jolicode.com>
2015-07-28 14:22:41 +0200

    Merge pull request #1149 from jolicode/kill-capistrano
    Goodbye capistrano
    
    GitOrigin-RevId: 6513e04a52cbfe9f69aa391cf3479ea44680b988

--- app/capistrano/stages/choisy.rb
+++
@@ -1,3 +0,0 @@
-server 'choisy.cap-collectif.com', user: 'jolicode', roles: [:web, :app]
-set :deploy_to,   "/home/jolicode/htdocs/demo.v2.cap-collectif.com/choisy"
-set :parameters_yml_file, "app/config/parameters/parameters_choisy.yml"
--- app/capistrano/stages/lechesnay.rb
+++
@@ -1,3 +0,0 @@
-server 'lechesnay.cap-collectif.com', user: 'jolicode', roles: [:web, :app]
-set :deploy_to,   "/home/jolicode/htdocs/demo.v2.cap-collectif.com/lechesnay"
-set :parameters_yml_file, "app/config/parameters/parameters_lechesnay.yml"
