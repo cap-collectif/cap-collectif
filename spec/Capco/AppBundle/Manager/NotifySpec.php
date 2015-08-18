da5b081918b9c5e717718cebfca467bd38ae4cdf
sfavot <sfavot@jolicode.com>
2015-08-18 11:12:24 +0200

    fix #1217
    GitOrigin-RevId: 735d22c2eb2c95fb8e5e2558060ba062d7a252e6

--- spec/Capco/AppBundle/Manager/NotifySpec.php
+++ spec/Capco/AppBundle/Manager/NotifySpec.php
@@ -5,7 +5,6 @@ namespace spec\Capco\AppBundle\Manager;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
-use Symfony\Component\Routing\Router;
 use Symfony\Component\Templating\EngineInterface;
 use Symfony\Component\Translation\TranslatorInterface;
 use Capco\AppBundle\SiteParameter\Resolver;
@@ -14,9 +13,9 @@ use Capco\AppBundle\SiteParameter\Resolver;
 class NotifySpec extends ObjectBehavior
 {
 
-    function let(\Swift_Mailer $mailer, EngineInterface $templating, TranslatorInterface $translator, Resolver $resolver, Router $router)
+    function let(\Swift_Mailer $mailer, EngineInterface $templating, TranslatorInterface $translator, Resolver $resolver)
     {
-        $this->beConstructedWith($mailer, $templating, $translator, $resolver, $router, ['confirmation.template' => null, 'resetting.template' => null]);
+        $this->beConstructedWith($mailer, $templating, $translator, $resolver);
     }
 
 
