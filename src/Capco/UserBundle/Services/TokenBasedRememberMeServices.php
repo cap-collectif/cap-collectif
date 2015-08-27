96c8ee396a5ee0465ab98b2b2b2290066ce5a68f
sfavot <sfavot@jolicode.com>
2015-08-27 14:26:53 +0200

    Fix #1204 remember me
    GitOrigin-RevId: 884fc3d5399af39e8b49bc4d83f273fcff93416f

---
+++ src/Capco/UserBundle/Services/TokenBasedRememberMeServices.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Capco\UserBundle\Services;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\Security\Http\RememberMe\TokenBasedRememberMeServices as BaseService;
+
+
+class TokenBasedRememberMeServices extends BaseService
+{
+    protected function onLoginSuccess(Request $request, Response $response, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        $expires = time() + $this->options['lifetime'];
+        $value = $this->generateCookieValue(get_class($user), $user->getEmail(), $expires, $user->getPassword());
+
+        $response->headers->setCookie(
+            new Cookie(
+                $this->options['name'],
+                $value,
+                $expires,
+                $this->options['path'],
+                $this->options['domain'],
+                $this->options['secure'],
+                $this->options['httponly']
+            )
+        );
+    }
+}
