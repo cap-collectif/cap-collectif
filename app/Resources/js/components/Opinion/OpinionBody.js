c6d3e837f52376e34ec3ca8fb599993512a5ea55
sfavot <sfavot@jolicode.com>
2015-09-04 13:00:08 +0200

    Merge pull request #1258 from jolicode/feature/1237_change_version_terms
    Ref #1237 change version terminology
    
    GitOrigin-RevId: 5746e01289851706e648155346ed236f54130e77

--- app/Resources/js/components/Opinion/OpinionBody.js
+++ app/Resources/js/components/Opinion/OpinionBody.js
@@ -1,6 +1,6 @@
 import CustomDiff from '../../services/CustomDiff';
 
-const Panel = ReactBootstrap.Panel;
+const Well = ReactBootstrap.Well;
 
 const OpinionBody = React.createClass({
   propTypes: {
@@ -16,13 +16,18 @@ const OpinionBody = React.createClass({
 
       return (
         <div>
-          <div dangerouslySetInnerHTML={{__html: prettyDiff}} />
           {opinion.comment !== null
-            ? <Panel header={this.getIntlMessage('global.comment')} style={{marginTop: 20}}>
-                <div dangerouslySetInnerHTML={{__html: opinion.comment}} />
-              </Panel>
-            : <span />
+            ? <div>
+                <p className="control-label h5">
+                  {this.getIntlMessage('opinion.version_comment')}
+                </p>
+                <Well bsSize="small" >
+                  <div dangerouslySetInnerHTML={{__html: opinion.comment}} />
+                </Well>
+              </div>
+            : null
           }
+          <div dangerouslySetInnerHTML={{__html: prettyDiff}} />
         </div>
       );
     }
