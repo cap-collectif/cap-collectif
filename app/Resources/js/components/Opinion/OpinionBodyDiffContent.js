b85466542871a7eea7765071ce03462756981a7d
spyl94 <spyl94@gmail.com>
2015-09-21 17:12:14 +0200

    Merge pull request #1334 from jolicode/polish-opinion-diff
    Add content from comparative array
    
    GitOrigin-RevId: 71010e7e398cb16a57e5e7f65f22e952edf5e97e

--- app/Resources/js/components/Opinion/OpinionBodyDiffContent.js
+++ app/Resources/js/components/Opinion/OpinionBodyDiffContent.js
@@ -23,22 +23,32 @@ const OpinionBodyDiffContent = React.createClass({
 
     const parts = [];
     sections.forEach((section) => {
-      parts.push({
-        before: section.slice(0, section.indexOf('<span')),
-        link: section.slice(section.indexOf('>') + 1, section.indexOf('</span>')),
-        after: section.slice(section.indexOf('</span>') + 7),
-        modal: {
-          title: section.substring(section.lastIndexOf('data-diff-title="') + 'data-diff-title="'.length, section.lastIndexOf('" data-diff-before=')),
-          before: section.substring(section.lastIndexOf('data-diff-before="') + 'data-diff-before="'.length, section.lastIndexOf('" data-diff-after=')),
-          after: section.substring(section.lastIndexOf('data-diff-after="') + 'data-diff-after="'.length, section.lastIndexOf('" data-diff-stop')),
-        },
-      });
+      if (section.indexOf('<span') == -1) {
+        parts.push({
+          content: section,
+          link: false,
+        });
+      } else {
+        parts.push({
+          before: section.slice(0, section.indexOf('<span')),
+          link: section.slice(section.indexOf('data-diff-stop="">') + 'data-diff-stop="">'.length, section.indexOf('</span>')),
+          after: section.slice(section.indexOf('</span>') + '</span>'.length),
+          modal: {
+            title: section.substring(section.lastIndexOf('data-diff-title="') + 'data-diff-title="'.length, section.lastIndexOf('" data-diff-before=')),
+            before: section.substring(section.lastIndexOf('data-diff-before="') + 'data-diff-before="'.length, section.lastIndexOf('" data-diff-after=')),
+            after: section.substring(section.lastIndexOf('data-diff-after="') + 'data-diff-after="'.length, section.lastIndexOf('" data-diff-stop')),
+          }
+        });
+      }
     });
 
     return (
       <div>
         {
           parts.map((part) => {
+            if (!part.link) {
+              return <p dangerouslySetInnerHTML={{__html: part.content}}></p>;
+            }
             return (
               <p>
                 <span dangerouslySetInnerHTML={{__html: part.before}} />
