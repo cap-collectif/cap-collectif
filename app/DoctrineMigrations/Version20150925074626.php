87bed747045efd9b055daa65f1eceadc8d8565ee
spyl94 <spyl94@gmail.com>
2015-09-30 18:34:41 +0200

    fix
    GitOrigin-RevId: e414873e0873dc23793b6495ea6f0cbf2eb1a917

--- app/DoctrineMigrations/Version20150925074626.php
+++ app/DoctrineMigrations/Version20150925074626.php
@@ -4,9 +4,18 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class Version20150925074626 extends AbstractMigration
+class Version20150925074626 extends AbstractMigration implements ContainerAwareInterface
 {
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
