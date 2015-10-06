ed77878b2728d6366612465fb6da87e77da25959
sfavot <sfavot@jolicode.com>
2015-10-06 12:39:55 +0200

    fix env
    GitOrigin-RevId: fdf32d84f334f2306da01ebfb0b5eef033b2db71

--- web/app.php
+++ web/app.php
@@ -4,7 +4,7 @@ use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-$env = getenv('SYMFONY_ENV') ?: 'dev';
+$env = getenv('SYMFONY_ENV') ?: 'prod';
 $debug = ('dev' === $env || 'test' === $env);
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
