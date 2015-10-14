23bad4ce0c9e98206ea2640e1bdfe0030f1070cd
sfavot <sfavot@jolicode.com>
2015-10-14 16:35:08 +0200

    Merge pull request #1558 from jolicode/feature/optim-page-consult-contributors
    mass optimize page consultation/participants
    
    GitOrigin-RevId: 7cabb3202342f0a22d7c28e7a8f6206cd41a0480

--- src/Capco/AppBundle/Resolver/ContributionResolver.php
+++ src/Capco/AppBundle/Resolver/ContributionResolver.php
@@ -15,7 +15,7 @@ class ContributionResolver
     }
 
     // Code may looks ugly but in fact it's highly optimized !
-    public function getConsultationContributorsOrdered(Consultation $consultation)
+    public function getConsultationContributorsOrdered(Consultation $consultation, $pagination = 0, $page = 1)
     {
         // Fetch contributors
         $sourcesContributors = $this->repository->findConsultationSourceContributorsWithCount($consultation);
@@ -63,12 +63,6 @@ class ContributionResolver
             $contributors[$sourcesVoter['id']]['sources_votes'] = $sourcesVoter['sources_votes_count'];
         }
 
-        $users = $this->repository->findWithMediaByIds(array_keys($contributors));
-
-        foreach ($users as $user) {
-            $contributors[$user->getId()]['user'] = $user;
-        }
-
         foreach ($contributors as &$contributor) {
             $contributor['total'] = isset($contributor['sources']) ? $contributor['sources'] : 0;
             $contributor['total'] += isset($contributor['arguments']) ? $contributor['arguments'] : 0;
@@ -80,7 +74,17 @@ class ContributionResolver
             $contributor['total'] += isset($contributor['sources_votes']) ? $contributor['sources_votes'] : 0;
         }
 
-        usort($contributors, function ($a, $b) { return $b['total'] - $a['total']; });
+        uasort($contributors, function ($a, $b) { return $b['total'] - $a['total']; });
+
+        if ($pagination && $page) {
+            $contributorsPage = array_slice($contributors, $pagination*$page - $pagination, $pagination, true);
+
+            $users = $this->repository->findWithMediaByIds(array_keys($contributorsPage));
+
+            foreach ($users as $user) {
+                $contributors[$user->getId()]['user'] = $user;
+            }
+        }
 
         return $contributors;
     }
