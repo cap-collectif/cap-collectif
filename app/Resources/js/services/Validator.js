33677ca893afc70aa83138bf4008c9d551a8d6b7
spyl94 <spyl94@gmail.com>
2015-10-19 16:53:58 +0200

    lint
    GitOrigin-RevId: bc925ee726fe46b2f0d3df79f6b8827c62724c4c

--- app/Resources/js/services/Validator.js
+++ app/Resources/js/services/Validator.js
@@ -9,53 +9,53 @@ class Validator {
     const errors = [];
     Object.keys(this.rules).map((rule) => {
       switch (rule) {
-        case 'min':
-          if (!this.min(this.rules[rule].value)) {
-            errors.push(this.rules[rule].message);
-          }
-          break;
-        case 'max':
-          if (!this.max(this.rules[rule].value)) {
-            errors.push(this.rules[rule].message);
-          }
-          break;
-        case 'isEqual':
-          if (!this.isEqual(this.rules[rule].value)) {
-            errors.push(this.rules[rule].message);
-          }
-          break;
-        case 'notEqual':
-          if (!this.notEqual(this.rules[rule].value)) {
-            errors.push(this.rules[rule].message);
-          }
-          break;
-        case 'isEmail':
-          if (!this.isEmail()) {
-            errors.push(this.rules[rule].message);
-          }
-          break;
-        case 'isUrl':
-          if (!this.isUrl()) {
-            errors.push(this.rules[rule].message);
-          }
-          break;
-        case 'notNull':
-          if (!this.notNull()) {
-            errors.push(this.rules[rule].message);
-          }
-          break;
-        case 'notBlank':
-          if (!this.notBlank()) {
-            errors.push(this.rules[rule].message);
-          }
-          break;
-        case 'isTrue':
-          if (!this.isTrue()) {
-            errors.push(this.rules[rule].message);
-          }
-          break;
-        default:
-          throw new Error('The rule ' + rule + ' does not exist in Validator.');
+      case 'min':
+        if (!this.min(this.rules[rule].value)) {
+          errors.push(this.rules[rule].message);
+        }
+        break;
+      case 'max':
+        if (!this.max(this.rules[rule].value)) {
+          errors.push(this.rules[rule].message);
+        }
+        break;
+      case 'isEqual':
+        if (!this.isEqual(this.rules[rule].value)) {
+          errors.push(this.rules[rule].message);
+        }
+        break;
+      case 'notEqual':
+        if (!this.notEqual(this.rules[rule].value)) {
+          errors.push(this.rules[rule].message);
+        }
+        break;
+      case 'isEmail':
+        if (!this.isEmail()) {
+          errors.push(this.rules[rule].message);
+        }
+        break;
+      case 'isUrl':
+        if (!this.isUrl()) {
+          errors.push(this.rules[rule].message);
+        }
+        break;
+      case 'notNull':
+        if (!this.notNull()) {
+          errors.push(this.rules[rule].message);
+        }
+        break;
+      case 'notBlank':
+        if (!this.notBlank()) {
+          errors.push(this.rules[rule].message);
+        }
+        break;
+      case 'isTrue':
+        if (!this.isTrue()) {
+          errors.push(this.rules[rule].message);
+        }
+        break;
+      default:
+        throw new Error('The rule ' + rule + ' does not exist in Validator.');
       }
     });
     return errors;
