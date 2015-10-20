47777c3908dc4123e774f965fbb7dd66fec40d05
welcoMattic <mathieu.santostefano@gmail.com>
2015-10-20 12:01:54 +0200

    add zipcode_at_register to ResetFeatureFlags command
    GitOrigin-RevId: ea7479fc24d025de748d96b54bfd2ee347faffbb

--- src/Capco/AppBundle/Command/ResetFeatureFlagsCommand.php
+++ src/Capco/AppBundle/Command/ResetFeatureFlagsCommand.php
@@ -50,6 +50,7 @@ class ResetFeatureFlagsCommand extends ContainerAwareCommand
         $toggleManager->activate('reporting');
         $toggleManager->activate('search');
 
+        $toggleManager->deactivate('zipcode_at_register');
         $toggleManager->deactivate('shield_mode');
 
         $output->writeln('Feature flags reseted');
