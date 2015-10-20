b253a741093a8d03a341cc19b0d9ab516fae0420
spyl94 <spyl94@gmail.com>
2015-10-20 13:54:05 +0200

    update doc and circle-ci
    GitOrigin-RevId: 5a4a5f94b300ee71e7176457761d6924aaa2bc78

--- fabfile.py
+++ fabfile.py
@@ -1,32 +1,12 @@
 from __future__ import with_statement
 
-import requests
 import os
 
 from fabric.api import *
 from fabric.context_managers import lcd
 from fabric.colors import red, green
-from libsaas.services import github
 
-GH_USER  = 'jolicode'
-REPO     = 'CapCollectif-SF2'
-
-env.circleci  = False
 env.local_dir = env.real_fabfile[:-10]
-
-@task
-def circleci():
-    env.circleci = True
-    env.gh_token = os.environ['GH_TOKEN']
-
-def create_PR_comment(type, result, pr):
-    if os.getenv('GH_TOKEN', True):
-        try:
-            prId      = int(pr.split('/')[-1])
-            gh_client = github.Github(env.gh_token)
-            gh_pr     = gh_client.repo(GH_USER, REPO).issue(prId).comments().create('Warning: You should check your '+type+' files.\n '+result)
-        except ValueError:
-            print 'Can\'t retrieve pr id'
 
 @task
 def build_deps():
@@ -66,7 +46,7 @@ def reinit_data():
         local('php app/console capco:reinit --force')
 
 @task
-def lint(pr=''):
+def lint():
     with lcd(env.local_dir):
         with settings(warn_only=True):
             result = local('./php-cs-fixer fix . --config=sf23 --dry-run', capture=True)
@@ -76,10 +56,7 @@ def lint(pr=''):
             elif result.return_code == 1:
                 print result
                 print(red('PHP: /!\ You should fix your PHP files!'))
-
-                if env.circleci:
-                    create_PR_comment('PHP', result, pr)
-            else: #print error to user
+            else:
                 print result
                 raise SystemExit()
 
