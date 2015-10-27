fe7a12b5f31ebe32a9fddb8ac70caa495641b5ab
welcoMattic <mathieu.santostefano@gmail.com>
2015-10-27 15:52:01 +0100

    add themes api endpoint
    GitOrigin-RevId: be1c8a276a4e92f4d5e875299dee104827d74652

---
+++ src/Capco/AppBundle/Controller/Api/ThemesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Capco\AppBundle\Controller\Api;
+
+use Capco\AppBundle\Entity\ProposalForm;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Put;
+use FOS\RestBundle\Controller\Annotations\Delete;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use FOS\RestBundle\Util\Codes;
+use Symfony\Component\HttpFoundation\Request;
+
+class ThemesController extends FOSRestController
+{
+    /**
+     * Get a themes.
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Get themes",
+     *  statusCodes={
+     *    200 = "Returned when successful",
+     *    404 = "Returned when themes are not found",
+     *  }
+     * )
+     *
+     * @Get("/themes")
+     * @View(statusCode=200, serializerGroups={"Themes"})
+     * @return array
+     */
+    public function getProposalFormAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $themes = $em->getRepository('CapcoAppBundle:Theme')->findAll();
+
+        return $themes;
+    }
+}
