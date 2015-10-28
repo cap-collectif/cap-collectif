870006a7da60ef8f3eeb44a7b8511e8394077ed8
welcoMattic <mathieu.santostefano@gmail.com>
2015-10-28 13:50:04 +0100

    [ci skip] fix section type typo
    GitOrigin-RevId: 7db11767adfb97f481d827619cfe8d05981829c2

--- src/Capco/AdminBundle/Admin/ProposalAdmin.php
+++ src/Capco/AdminBundle/Admin/ProposalAdmin.php
@@ -23,21 +23,20 @@ class ProposalAdmin extends Admin
             ->with('admin.fields.proposal.group_content')
             ->add('title', null, [
                 'label' => 'admin.fields.proposal.title',
-                /*'read_only' => true,
-                'disabled' => true,*/
+                'read_only' => true,
+                'disabled' => true,
             ])
             ->add('body', 'ckeditor', [
                 'label' => 'admin.fields.proposal.body',
                 'config_name' => 'admin_editor',
-                /*'read_only' => true,
-                'disabled' => true,*/
+                'read_only' => true,
+                'disabled' => true,
             ])
-            ->add('author', 'sonata_type_model_autocomplete', [
+            ->add('author', null, [
                 'label' => 'admin.fields.proposal.author',
                 'required' => true,
-                'property' => 'username',
-                /*'read_only' => true,
-                'disabled' => true,*/
+                'read_only' => true,
+                'disabled' => true,
             ])
             ->add('rating', 'choice', [
                 'label' => 'admin.fields.proposal.rating',
