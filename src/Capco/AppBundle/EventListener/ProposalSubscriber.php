de657f5dd32b4f4eb8646670dd03abf83427eb53
welcoMattic <mathieu.santostefano@gmail.com>
2015-10-30 17:06:00 +0100

    real content for delete proposal email + add default status on proposal
    GitOrigin-RevId: 12e658c18b8b3dcda4805aef7460dd466cef6845

---
+++ src/Capco/AppBundle/EventListener/ProposalSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Capco\AppBundle\EventListener;
+
+use Capco\AppBundle\Manager\Notify;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Doctrine\ORM\EntityManager;
+use Capco\AppBundle\CapcoAppBundleEvents;
+use Capco\AppBundle\Event\ProposalEvent;
+
+class ProposalSubscriber implements EventSubscriberInterface
+{
+    protected $em;
+
+    public function __construct(EntityManager $em, Notify $notifier)
+    {
+        $this->em = $em;
+        $this->notifier = $notifier;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            CapcoAppBundleEvents::PROPOSAL_DELETED => 'onProposalDeleted',
+        ];
+    }
+
+    public function onProposalDeleted(ProposalEvent $event)
+    {
+        $proposal = $event->getProposal();
+        $action = $event->getAction();
+
+        if ($action == 'remove') {
+            $this->notifier->notifyProposalDeletion($proposal);
+        }
+    }
+
+}
