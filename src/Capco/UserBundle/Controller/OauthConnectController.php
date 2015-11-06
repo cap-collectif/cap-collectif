26846fd7bb50e470203009789d006d9fc73124c5
spyl94 <spyl94@gmail.com>
2015-11-06 12:56:58 +0100

    add custom oauth form for Nous Citoyens
    GitOrigin-RevId: 8fd0580181aa57499a80a6affc39ec3e85f7e4fc

--- src/Capco/UserBundle/Controller/OauthConnectController.php
+++ src/Capco/UserBundle/Controller/OauthConnectController.php
@@ -101,4 +101,29 @@ class OauthConnectController extends ConnectController
 
         return parent::redirectToServiceAction($request, $service);
     }
+
+    public function nousCitoyensAction()
+    {
+        $ro = $this->getResourceOwnerByName('nous_citoyens');
+        $reflection = new \ReflectionClass($ro);
+        $property = $reflection->getProperty('options');
+        $property->setAccessible(true);
+        $url = $property->getValue($ro)['authorization_url'];
+
+        $params = [
+            "grant_type"    => "authorization_code",
+            "client_id"     => $this->getParameter('nous_citoyens_app_id'),
+            // pas du tout secure mais bon on peut pas redirect
+            // sur une route en POST... en plus cette route a aucune raison d'avoir besoin du secret...
+            "client_secret" => $this->getParameter('nous_citoyens_app_secret'),
+            "redirect_uri"  => $this->container->get('router')->generate('nous_citoyens_login', [], true),
+            "state"         => "123",
+            "response_type" => "code",
+        ];
+
+        return $this->render('CapcoUserBundle:Oauth:nous_citoyens.html.twig', [
+            'url' => $url,
+            'params' => $params,
+        ]);
+    }
 }
