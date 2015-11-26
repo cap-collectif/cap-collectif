e20aed41560f9bacef8089c01a3158cdecab3324
spyl94 <spyl94@gmail.com>
2015-11-26 18:42:35 +0100

    clean
    GitOrigin-RevId: 563fca537b44bea6fbd456f63fecb97bbe31958e

--- infrastructure/services/remote/entrypoint.sh
+++ infrastructure/services/remote/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+# Configure New Relic
 sed -i 's/newrelic.appname = "PHP Application"/newrelic.appname = '"$NR_INSTALL_NAME"'/' /etc/php5/fpm/conf.d/20-newrelic.ini
 sed -i 's/newrelic.license = ""/newrelic.license = '"$NR_INSTALL_KEY"'/' /etc/php5/fpm/conf.d/20-newrelic.ini
 
