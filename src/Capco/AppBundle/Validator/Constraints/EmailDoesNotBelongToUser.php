57f7cbdc125806c4a69705dd9698547b250c9bdf
sfavot <sfavot@jolicode.com>
2015-12-02 11:34:40 +0100

    Ref #1889 Fix error message on idea vote
    GitOrigin-RevId: b5ca9aa78e117ea426459125871e308305060a6b

--- src/Capco/AppBundle/Validator/Constraints/EmailDoesNotBelongToUser.php
+++ src/Capco/AppBundle/Validator/Constraints/EmailDoesNotBelongToUser.php
@@ -9,7 +9,7 @@ use Symfony\Component\Validator\Constraint;
  */
 class EmailDoesNotBelongToUser extends Constraint
 {
-    public $message = 'event_registration.create.email_belongs_to_user';
+    public $message = 'email.email_belongs_to_user';
 
     public function validatedBy()
     {
