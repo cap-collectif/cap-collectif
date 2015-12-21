de9193c51b4635ebfc1a579195627f7000239422
spyl94 <spyl94@gmail.com>
2015-12-21 17:32:46 +0100

    better tests
    GitOrigin-RevId: 7e07ee40dbb5b1e1a540ea988ee1bf684e8aefe4

--- spec/Capco/AppBundle/Command/ImportStructureFromCsvCommandSpec.php
+++ spec/Capco/AppBundle/Command/ImportStructureFromCsvCommandSpec.php
@@ -13,20 +13,20 @@ class ImportStructureFromCsvCommandSpec extends ObjectBehavior
         $this->shouldHaveType('Capco\AppBundle\Command\ImportStructureFromCsvCommand');
     }
 
-    function it_should_get_object_depending_on_type(OpinionType $ot1, OpinionType $ot2, OpinionType $ot3)
+    function it_should_find_opinion_type_by_path(OpinionType $ot1, OpinionType $ot2, OpinionType $ot3)
     {
         $ot1->getTitle()->willReturn('Titre 5');
         $ot1->getChildren()->willReturn([$ot2, $ot3]);
 
         $ot2->getTitle()->willReturn('Chapitre 1');
-        $ot2->getChildren()->willReturn([]);
-
         $ot3->getTitle()->willReturn('Chapitre 2');
-        $ot3->getChildren()->willReturn([]);
 
-        $this->findOpinionTypeByPath('Titre 5|Chapitre 1', [$ot1, $ot2, $ot3])->shouldReturn($ot2);
-        $this->findOpinionTypeByPath('Titre 5|Chapitre 2', [$ot1, $ot2, $ot3])->shouldReturn($ot3);
-        // $this->findOpinionTypeByPath('Chapitre 2', [$ot1, $ot2, $ot3])->shouldReturn(null);
+        $this->findOpinionTypeByPath('Titre 5', [$ot1])->shouldReturn($ot1);
+        $this->findOpinionTypeByPath('Titre 5|Chapitre 1', [$ot1])->shouldReturn($ot2);
+        $this->findOpinionTypeByPath('Titre 5|Chapitre 2', [$ot1])->shouldReturn($ot3);
+
+        $this->shouldThrow(new \Exception('Unknown opinion title: "Chapitre 2"', 1))
+             ->duringFindOpinionTypeByPath('Chapitre 2', [$ot1]);
     }
 
 }
