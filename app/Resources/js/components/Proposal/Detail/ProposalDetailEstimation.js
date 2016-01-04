b375a97d263da4143289eb6c12460da8ba662583
spyl94 <spyl94@gmail.com>
2016-01-04 14:56:08 +0100

    fix NaN issues
    GitOrigin-RevId: 0e404f78fd94a2746e509e19169e05f5c56bfe9a

--- app/Resources/js/components/Proposal/Detail/ProposalDetailEstimation.js
+++ app/Resources/js/components/Proposal/Detail/ProposalDetailEstimation.js
@@ -8,7 +8,9 @@ const ProposalPreviewEstimation = React.createClass({
 
   render() {
     const proposal = this.props.proposal;
-
+    if (!proposal.estimation) {
+      return null;
+    }
     return (
       <span>
         <i className="cap cap-clip-2-1"></i>
