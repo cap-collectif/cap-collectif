98a368a0b561741de015bd2f1b28c1ca4ec49184
spyl94 <spyl94@gmail.com>
2016-01-11 18:39:44 +0100

    create var directory
    GitOrigin-RevId: 053572dd3a54c936f34c27e483fce6844495fe7c

--- infrastructure/services/builder/build.sh
+++ infrastructure/services/builder/build.sh
@@ -4,6 +4,7 @@ if [ "$PRODUCTION" ]; then
   echo "Building for production"
   # Symfony deps
   composer install --no-dev --prefer-dist --no-interaction --optimize-autoloader --no-scripts
+  mkdir -p var
   php vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php var
 
   # Frontend deps
