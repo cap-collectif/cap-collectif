bdd56f9e2c64b275ec2125a6a250d8f58d190b31
sfavot <sfavot@jolicode.com>
2016-01-12 10:00:24 +1100

    Merge pull request #2015 from jolicode/store-jwt-token
    Fix some SonataMedia route generation problems
    
    GitOrigin-RevId: 2c6cf067e1f49956d5dd4500c8cf1a7c4a8e5271

--- src/Capco/AppBundle/EventListener/SerializationListener.php
+++ src/Capco/AppBundle/EventListener/SerializationListener.php
@@ -8,6 +8,7 @@ use JMS\Serializer\Serializer;
 use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
 use JMS\Serializer\EventDispatcher\ObjectEvent;
 use Sonata\MediaBundle\Twig\Extension\MediaExtension;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 class SerializationListener implements EventSubscriberInterface
 {
@@ -33,10 +34,14 @@ class SerializationListener implements EventSubscriberInterface
 
     public function onPostMediaSerialize(ObjectEvent $event)
     {
-        $event->getVisitor()->addData(
-            'url',
-            $this->mediaExtension->path($event->getObject(), 'avatar')
-        );
+        try {
+            $event->getVisitor()->addData(
+                'url',
+                $this->mediaExtension->path($event->getObject(), 'avatar')
+            );
+        } catch(RouteNotFoundException $e) {
+            // Avoid some SonataMedia problems
+        }
     }
 
     public function onPostLogSerialize(ObjectEvent $event)
