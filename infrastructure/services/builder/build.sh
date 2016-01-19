8f48b396369cbb98c42b1a4b40b4b4b4cfe0cdd6
spyl94 <spyl94@gmail.com>
2016-01-19 17:24:21 +0100

    debug builder
    GitOrigin-RevId: 3e31471ad0b8912fca4a27534516daea3cd371a0

--- infrastructure/services/builder/build.sh
+++ infrastructure/services/builder/build.sh
@@ -8,7 +8,7 @@ if [ "$PRODUCTION" ]; then
   php vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php var
 
   # Frontend deps
-  npm install --production
+  npm --quiet install --production
   npm rebuild node-sass
   bower install --config.interactive=false --allow-root --config.storage.cache=/home/capco/.cache/bower
   brunch build --production
