45e45b1271dbe97e17fdc6a36b440111c6c0dafc
sfavot <sfavot@jolicode.com>
2016-02-09 14:33:45 +1100

    fix tests
    GitOrigin-RevId: d1f737b2357f48f44d47583ffaed08d5cfa192da

--- app/Resources/js/components/Utils/PinnedLabel-test.js
+++ app/Resources/js/components/Utils/PinnedLabel-test.js
@@ -4,15 +4,16 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import PinnedLabel from './PinnedLabel';
+import IntlData from '../../translations/FR';
 
 describe('<PinnedLabel />', () => {
   it('should render a blue label if shown', () => {
-    const wrapper = shallow(<PinnedLabel show />);
+    const wrapper = shallow(<PinnedLabel show {...IntlData} />);
     expect(wrapper.find('span.opinion__label.opinion__label--blue')).to.have.length(1);
   });
 
   it('should not render a blue label if not shown', () => {
-    const wrapper = shallow(<PinnedLabel show={false} />);
+    const wrapper = shallow(<PinnedLabel show={false} {...IntlData} />);
     expect(wrapper.find('span.opinion__label.opinion__label--blue')).to.not.exists;
   });
 });
