5eda534dd7bd94192180b5d2cef03ce15135f8f4
spyl94 <spyl94@gmail.com>
2016-02-16 12:06:54 +0100

    refacto of unit tests
    GitOrigin-RevId: 6b522b834f4771e1a103b843e3be69e6ec7670d5

--- app/Resources/js/components/Proposal/Detail/ProposalDetailLikersLabel-test.js
+++ app/Resources/js/components/Proposal/Detail/ProposalDetailLikersLabel-test.js
@@ -7,52 +7,45 @@ import ProposalDetailLikersLabel from './ProposalDetailLikersLabel';
 import IntlData from '../../../translations/FR';
 
 describe('<ProposalDetailLikersLabel />', () => {
-  const proposalWithOneLiker = {
-    likers: [
-      {
-        displayName: 'user',
-      },
-    ],
-  };
-  const proposalWithLongNameLiker = {
-    likers: [
-      {
-        displayName: 'user with a very long name that need to be truncated',
-      },
-    ],
-  };
-  const proposalWithSeveralLikers = {
-    likers: [
-      {
-        displayName: 'user 1',
-      },
-      {
-        displayName: 'user 2',
-      },
-    ],
-  };
-  const proposalWithNoLikers = {
-    likers: [],
-  };
+  const oneLiker = [
+    {
+      displayName: 'user',
+    },
+  ];
 
-  it('should render a string with liker name when proposal has one liker', () => {
-    const wrapper = shallow(<ProposalDetailLikersLabel proposal={proposalWithOneLiker} {...IntlData} />);
+  const oneLongNameLiker = [
+    {
+      displayName: 'user with a very long name that need to be truncated',
+    },
+  ];
+
+  const severalLikers = [
+    {
+      displayName: 'user 1',
+    },
+    {
+      displayName: 'user 2',
+    },
+  ];
+
+  it('should render liker name when only one liker', () => {
+    const wrapper = shallow(<ProposalDetailLikersLabel likers={oneLiker} {...IntlData} />);
     expect(wrapper.text()).to.equal('user');
   });
 
-  it('should render a string with truncated liker name when proposal has one liker with a long name', () => {
-    const wrapper = shallow(<ProposalDetailLikersLabel proposal={proposalWithLongNameLiker} {...IntlData} />);
+  it('should render truncated liker name when the liker has a long name', () => {
+    const wrapper = shallow(<ProposalDetailLikersLabel likers={oneLongNameLiker} {...IntlData} />);
     expect(wrapper.text()).to.equal('user with a very long name tha...');
   });
 
-  it('should render a FormattedMessage when proposal has several likers', () => {
-    const wrapper = shallow(<ProposalDetailLikersLabel proposal={proposalWithSeveralLikers} {...IntlData} />);
+  it('should render a <FormattedMessage/> when several likers', () => {
+    const wrapper = shallow(<ProposalDetailLikersLabel likers={severalLikers} {...IntlData} />);
     expect(wrapper.find('FormattedMessage')).to.have.length(1);
     expect(wrapper.find('FormattedMessage').prop('num')).to.equals(2);
   });
 
-  it('should render nothing when proposal has no likers', () => {
-    const wrapper = shallow(<ProposalDetailLikersLabel proposal={proposalWithNoLikers} {...IntlData} />);
+  it('should render nothing when no likers', () => {
+    const wrapper = shallow(<ProposalDetailLikersLabel likers={[]} {...IntlData} />);
     expect(wrapper.children()).to.have.length(0);
   });
 });
