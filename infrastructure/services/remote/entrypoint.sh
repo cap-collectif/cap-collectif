cfd3bb07c923ee47984d86d963aa0716f95e0270
spyl94 <spyl94@gmail.com>
2016-03-07 18:49:25 +0100

    fix docker perm
    GitOrigin-RevId: 1d0e6fcaafc4f8cbb122970b3c2d03a3f7d26083

--- infrastructure/services/remote/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# Configure New Relic
-sed -i 's/newrelic.appname = "PHP Application"/newrelic.appname = '"$NR_INSTALL_NAME"'/' /etc/php5/fpm/conf.d/20-newrelic.ini
-sed -i 's/newrelic.license = ""/newrelic.license = '"$NR_INSTALL_KEY"'/' /etc/php5/fpm/conf.d/20-newrelic.ini
-
-exec "$@"
