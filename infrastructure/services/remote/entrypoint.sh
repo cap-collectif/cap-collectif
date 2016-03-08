ecb706151677e21ad50152cb07213055f2c14485
spyl94 <spyl94@gmail.com>
2016-03-08 12:13:41 +0100

    fix ESLint issue with estraverse-fb
    GitOrigin-RevId: 146ced7f87d8ec667024e0f613089ec060abb482

---
+++ infrastructure/services/remote/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+# Configure New Relic
+sed -i 's/newrelic.appname = "PHP Application"/newrelic.appname = '"$NR_INSTALL_NAME"'/' /etc/php5/fpm/conf.d/20-newrelic.ini
+sed -i 's/newrelic.license = ""/newrelic.license = '"$NR_INSTALL_KEY"'/' /etc/php5/fpm/conf.d/20-newrelic.ini
+
+exec "$@"
