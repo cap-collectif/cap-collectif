ae5be57fac540f5980c985621e862f3a7bf7a3d7
spyl94 <spyl94@gmail.com>
2016-03-11 11:06:35 +0100

    avoid tests on commands to fail
    GitOrigin-RevId: 42908f767b5a9744c1d486628f1ed13de60d0775

--- infrastructure/deploylib/app.py
+++ infrastructure/deploylib/app.py
@@ -7,7 +7,7 @@ from fabric.api import env
 def deploy(environment='prod'):
     "Deploy"
     env.compose('run builder build')
-    env.service_command('php vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php var', 'application', env.www_app)
+    env.service_command('php vendor/sensio/distribution-bundle/Resources/bin/build_bootstrap.php var', 'application', env.www_app)
     env.service_command('rm -rf var/cache/dev var/cache/prod var/cache/test', 'application', env.www_app, 'root')
     env.service_command('php bin/console cache:warmup --no-optional-warmers --env=' + environment, 'application', env.www_app)
     env.service_command('php bin/console assets:install --symlink', 'application', env.www_app)
