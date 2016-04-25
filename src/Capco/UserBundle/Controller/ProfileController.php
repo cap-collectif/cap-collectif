49f63646caab4f6f607137e9dfed2dc1cc4bc4b8
spyl94 <spyl94@gmail.com>
2016-04-25 17:13:41 +0200

    use security annotation
    GitOrigin-RevId: 1f80a3d6ea4e9142d3ffb9beda6c6cb8b94d5b61

--- src/Capco/UserBundle/Controller/ProfileController.php
+++ src/Capco/UserBundle/Controller/ProfileController.php
@@ -5,6 +5,7 @@ namespace Capco\UserBundle\Controller;
 use Capco\AppBundle\Entity\Argument;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Capco\UserBundle\Entity\User;
 use Sonata\UserBundle\Controller\ProfileFOSUser1Controller as BaseController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -19,13 +20,10 @@ class ProfileController extends BaseController
     /**
      * @Route("/", name="capco_user_profile_show")
      * @Template()
+     * @Security("has_role('ROLE_USER')")
      */
     public function showAction()
     {
-        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            throw $this->createAccessDeniedException();
-        }
-
         $doctrine = $this->getDoctrine();
         $serializer = $this->get('jms_serializer');
         $user = $this->get('security.token_storage')->getToken()->getUser();
