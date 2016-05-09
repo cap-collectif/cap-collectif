913ae6d5e690823b7387bb37770550f40958f1e3
spyl94 <spyl94@gmail.com>
2016-05-09 12:11:18 +0200

    Merge pull request #2443 from jolicode/handle-twilio-failure
    handle twilio failure message
    
    GitOrigin-RevId: 8a723854f14f27603b0065194d9565b5a7e5c5cd

--- src/Capco/AppBundle/Resolver/ProjectDownloadResolver.php
+++ src/Capco/AppBundle/Resolver/ProjectDownloadResolver.php
@@ -164,6 +164,8 @@ class ProjectDownloadResolver
         $headers = [
             'published' => [
                 'id',
+                'author',
+                'phone',
                 'created',
                 'anonymous',
             ],
@@ -660,6 +662,8 @@ class ProjectDownloadResolver
     {
         $item = [
             'id' => $reply->getId(),
+            'author' => $reply->getAuthor()->getUsername(),
+            'phone' => $reply->getAuthor()->getPhone() ? $reply->getAuthor()->getPhone() : '',
             'created' => $this->dateToString($reply->getCreatedAt()),
             'anonymous' => $this->booleanToString($reply->isPrivate()),
         ];
@@ -726,7 +730,7 @@ class ProjectDownloadResolver
                 $values[] = $originalValue['other'];
             }
 
-            return implode(', ', $values);
+            return implode('; ', $values);
         }
 
         return $originalValue;
