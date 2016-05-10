2f3b43c35376d4638778b2a1d46d46854955b722
spyl94 <spyl94@gmail.com>
2016-05-10 14:17:15 +0200

    restaure default number
    GitOrigin-RevId: 0818e585de749a850c518cce1fa7ed7d345d75c4

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -53,4 +53,4 @@ $container->setParameter('recaptcha_private_key', getenv_default('recaptcha_priv
 // Twilio test credentials (no charge)
 $container->setParameter('twilio_sid', getenv_default('twilio_sid', '***REMOVED***'));
 $container->setParameter('twilio_token', getenv_default('twilio_token', '***REMOVED***'));
-$container->setParameter('twilio_number', getenv_default('twilio_number', '+33644600197'));
+$container->setParameter('twilio_number', getenv_default('twilio_number', '+***REMOVED***'));
