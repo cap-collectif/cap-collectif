da93ad968e72396038babdd95e458840c938c65b
sfavot <sfavot@jolicode.com>
2016-05-19 17:24:16 +0200

    add exit codes to builder
    GitOrigin-RevId: a3526e3023907bc28e1e565911292c934733f6ee

--- infrastructure/services/builder/build.sh
+++ infrastructure/services/builder/build.sh
@@ -3,21 +3,21 @@
 if [ "$PRODUCTION" ]; then
   echo "Building for production"
   # We create var directory used by Symfony
-  mkdir -m 777 -p var
+  mkdir -m 777 -p var || exit 1
   # We install vendors with composer
   # We don't use `--no-scripts` or `--no-plugins` because a script in a composer plugin
   # will generate the file vendor/ocramius/package-versions/src/PackageVersions/Versions.php
-  composer install --no-dev --prefer-dist --no-interaction --optimize-autoloader --no-progress --ignore-platform-reqs
+  composer install --no-dev --prefer-dist --no-interaction --optimize-autoloader --no-progress --ignore-platform-reqs || exit 1
   # We build bootstrap.php.cache in the `var` directory
-  php vendor/sensio/distribution-bundle/Resources/bin/build_bootstrap.php var
+  php vendor/sensio/distribution-bundle/Resources/bin/build_bootstrap.php var || exit 1
 
   # Frontend deps
-  npm --quiet --no-color install --production
-  bower install --config.interactive=false --allow-root --config.storage.cache=/home/capco/.cache/bower
-  npm run build:prod
+  npm --quiet --no-color install --production || exit 1
+  bower install --config.interactive=false --allow-root --config.storage.cache=/home/capco/.cache/bower || exit 1
+  npm run build:prod || exit 1
 
   # Server side rendering deps
-  npm run build-server-bundle:prod
+  npm run build-server-bundle:prod || exit 1
 else
   echo "Building for development"
   # Symfony deps
