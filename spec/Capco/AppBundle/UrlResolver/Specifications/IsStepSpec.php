7186a93d6e13ae617620d2a9ac9eb0b8747fa4dc
ptondereau <pierre.tondereau@protonmail.com>
2016-07-25 14:40:23 +0200

    Fix tests
    GitOrigin-RevId: d746609439c20b16accc4cbe770c0b3bfc80c11c

--- spec/Capco/AppBundle/UrlResolver/Specifications/IsStepSpec.php
+++ spec/Capco/AppBundle/UrlResolver/Specifications/IsStepSpec.php
@@ -4,31 +4,20 @@ namespace spec\Capco\AppBundle\UrlResolver\Specifications;
 
 use Capco\AppBundle\Entity\Event;
 use Capco\AppBundle\Entity\Steps\AbstractStep;
-use Capco\AppBundle\UrlResolver\Specifications\HasSlug;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 
 class IsStepSpec extends ObjectBehavior
 {
-    function let(HasSlug $spec)
-    {
-        $this->beConstructedWith($spec);
-    }
-
     function it_is_initializable()
     {
         $this->shouldHaveType('Capco\AppBundle\UrlResolver\Specifications\IsStep');
     }
 
-    function it_should_detect_if_it_is_a_spec(AbstractStep $step, Event $event, $spec)
+    function it_should_detect_if_it_is_a_spec(AbstractStep $step, Event $event)
     {
-        $step->getSlug()->willReturn('test');
-
-        $spec->isSatisfiedBy($step)->shouldBeCalled()->willReturn(true);
         $this->isSatisfiedBy($step)->shouldReturn(true);
-
-        $spec->isSatisfiedBy($step)->shouldBeCalled()->willReturn(true);
         $this->isSatisfiedBy($event)->shouldReturn(false);
     }
 }
