5edddf1842acce000fc2284a06f221df95bc2d4c
spyl94 <spyl94@gmail.com>
2016-08-31 14:53:20 +0200

    rm app_test ip protection
    GitOrigin-RevId: d3674252e8119370a6ea02b80bb6b699dc2ee0e1

--- web/app_test.php
+++ web/app_test.php
@@ -9,27 +9,8 @@ use Symfony\Component\Debug\Debug;
 $loader = require __DIR__ . '/../app/autoload.php';
 
 Debug::enable();
-Request::setTrustedProxies(array(
-    '172.17.0.0/16',
-    '10.10.200.0/16',
-    '127.0.0.1',
-));
 
 $request = Request::createFromGlobals();
-
-if (!\Symfony\Component\HttpFoundation\IpUtils::checkIp($request->getClientIp(), array(
-        '78.192.6.1',
-        '10.1.33.1',
-        '192.168.10.0/16',
-        '10.8.0.0/16',
-        '127.0.0.1',
-        '172.17.0.0/16',
-        'fe80::1',
-        '::1',)
-)) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('Ip ' . $request->getClientIp() . ' is not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
-}
 
 require_once __DIR__.'/../app/AppKernel.php';
 
