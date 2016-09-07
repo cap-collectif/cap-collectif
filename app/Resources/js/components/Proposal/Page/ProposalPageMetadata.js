5be5484e95470bd800a63fb485f7b5b408c21a7d
Laurent Brunet <lbrunet@jolicode.com>
2016-09-07 12:07:26 +0200

    Fix frontend when no district and no category
    GitOrigin-RevId: 0ae87f2a3acf03321ab0052f42880f51319f1214

--- app/Resources/js/components/Proposal/Page/ProposalPageMetadata.js
+++ app/Resources/js/components/Proposal/Page/ProposalPageMetadata.js
@@ -16,30 +16,34 @@ const ProposalPageMetadata = React.createClass({
   render() {
     const { proposal, showCategories, showDistricts, showNullEstimation } = this.props;
     return (
-      <div className="proposal__page__metadata">
-        <div className="proposal__infos">
-          {
-            showCategories && proposal.category
-            && <div className="proposal__info proposal__info--category">
-              <i className="cap cap-tag-1-1 icon--blue"></i>{proposal.category.name}
+        <div>
+          { (showCategories && proposal.category) || (showDistricts && proposal.district)
+          && <div className="proposal__page__metadata">
+            <div className="proposal__infos">
+              {
+                showCategories && proposal.category
+                && <div className="proposal__info proposal__info--category">
+                  <i className="cap cap-tag-1-1 icon--blue"></i>{proposal.category.name}
+                </div>
+              }
+              {
+                showDistricts && proposal.district
+                && <div className="proposal__info proposal__info--district">
+                  <i className="cap cap-marker-1-1 icon--blue"></i>{proposal.district.name}
+                </div>
+              }
+              <ProposalDetailEstimation
+                  proposal={proposal}
+                  showNullEstimation={showNullEstimation}
+              />
+              <ProposalDetailLikers
+                  proposal={proposal}
+                  componentClass="div"
+              />
             </div>
+          </div>
           }
-          {
-            showDistricts && proposal.district
-            && <div className="proposal__info proposal__info--district">
-              <i className="cap cap-marker-1-1 icon--blue"></i>{proposal.district.name}
-            </div>
-          }
-          <ProposalDetailEstimation
-            proposal={proposal}
-            showNullEstimation={showNullEstimation}
-          />
-          <ProposalDetailLikers
-            proposal={proposal}
-            componentClass="div"
-          />
         </div>
-      </div>
     );
   },
 
