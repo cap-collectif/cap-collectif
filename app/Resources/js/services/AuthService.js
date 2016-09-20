3efec65a7fe84c0c63aef433b7d6d0c4e469c7e6
spyl94 <spyl94@gmail.com>
2016-09-20 16:54:02 +0200

    Avoir requesting a token on all api call
    GitOrigin-RevId: 710f172d3bd48c58f89d8d1c624eeb9ffd28a4fe

--- app/Resources/js/services/AuthService.js
+++ app/Resources/js/services/AuthService.js
@@ -14,8 +14,12 @@ function json(response) {
 
 class AuthService {
 
+  constructor() {
+    this.hasRequestedToken = false;
+  }
+
   login() {
-    if (LocalStorageService.isValid('jwt')) {
+    if (LocalStorageService.isValid('jwt') || this.hasRequestedToken) {
       return Promise.resolve();
     }
     return fetch(`${window.location.protocol}//${window.location.host}/get_api_token`, {
@@ -29,6 +33,7 @@ class AuthService {
     .then(status)
     .then(json)
     .then((data) => {
+      this.hasRequestedToken = true;
       return data.token
         ? LoginActions.loginUser(data.token)
         : this.logout()
