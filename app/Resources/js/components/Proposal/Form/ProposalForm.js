14c9f4223381c33de8dad5a9e0f9dc8c12cb125a
spyl94 <spyl94@gmail.com>
2016-09-29 11:22:42 +0200

    Add dev deps in shrinkwrap
    GitOrigin-RevId: 5dcf2e9db402ab29961df339f3372bd26f4484cd

--- app/Resources/js/components/Proposal/Form/ProposalForm.js
+++ app/Resources/js/components/Proposal/Form/ProposalForm.js
@@ -118,9 +118,6 @@ const ProposalForm = React.createClass({
           });
         });
         form.responses = responses;
-        if (responses.length === 0) {
-          delete form.responses;
-        }
         if (!features.themes || !this.props.form.usingThemes || form.theme === -1) {
           delete form.theme;
         }
