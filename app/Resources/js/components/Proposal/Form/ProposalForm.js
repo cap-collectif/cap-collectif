43427e033dfd2a9871ced3fe8fed781e19e59a6b
ptondereau <pierre.tondereau@protonmail.com>
2016-09-29 14:30:18 +0200

    Merge pull request #2998 from jolicode/better-suggestion-collapse
    Better proposal suggestion collapse
    
    GitOrigin-RevId: ad202b8b90b724106a06298c1bd7f8339ee0a3cd

--- app/Resources/js/components/Proposal/Form/ProposalForm.js
+++ app/Resources/js/components/Proposal/Form/ProposalForm.js
@@ -297,7 +297,7 @@ const ProposalForm = React.createClass({
           <Input
             id="proposal_title"
             type="text"
-            autoComplete={false}
+            autoComplete="off"
             onChange={this.handleTitleChange}
             label={this.getIntlMessage('proposal.title')}
             groupClassName={this.getGroupStyle('title')}
