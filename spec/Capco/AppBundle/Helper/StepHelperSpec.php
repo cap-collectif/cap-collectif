129c659c8909680306ccc6415c1a1a0f4139fae1
spyl94 <spyl94@gmail.com>
2016-10-14 11:40:42 +0200

    FIx
    GitOrigin-RevId: 0e4c4fad5a788333dec878d0c7b37bb8430398a4

--- spec/Capco/AppBundle/Helper/StepHelperSpec.php
+++ spec/Capco/AppBundle/Helper/StepHelperSpec.php
@@ -2,7 +2,6 @@
 
 namespace spec\Capco\AppBundle\Helper;
 
-use Capco\AppBundle\Helper\ProjectHelper;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -10,69 +9,51 @@ use Capco\AppBundle\Entity\Steps\AbstractStep;
 
 class StepHelperSpec extends ObjectBehavior
 {
-    function it_is_initializable(ProjectHelper $projectHelper)
+    function it_is_initializable()
     {
-        $this->beConstructedWith($projectHelper);
         $this->shouldHaveType('Capco\AppBundle\Helper\StepHelper');
     }
 
-    function it_can_guess_opening_status(AbstractStep $step, ProjectHelper $projectHelper, AbstractStep $stepA, AbstractStep $stepB)
+    function it_can_guess_opening_status(AbstractStep $step)
     {
-        $this->beConstructedWith($projectHelper);
-
         // if start date is future
         $step->getStartAt()->willReturn((new \DateTime())->modify('+1 weeks'));
         $step->getEndAt()->willReturn((new \DateTime())->modify('+2 weeks'));
         $this->getStatus($step)->shouldReturn('future');
 
-        // if start date is before and end date future
+        // if start date is past and end date future
         $step->getStartAt()->willReturn((new \DateTime())->modify('-1 weeks'));
         $step->getEndAt()->willReturn((new \DateTime())->modify('+1 weeks'));
         $this->getStatus($step)->shouldReturn('open');
 
-        // if end date is past
+        // if start date and end date is past
         $step->getStartAt()->willReturn((new \DateTime())->modify('-2 weeks'));
         $step->getEndAt()->willReturn((new \DateTime())->modify('-1 weeks'));
         $this->getStatus($step)->shouldReturn('closed');
 
-        // if start date is future
+        // if start date is future and end date is null
         $step->getStartAt()->willReturn((new \DateTime())->modify('+1 weeks'));
         $step->getEndAt()->willReturn(null);
         $this->getStatus($step)->shouldReturn('future');
 
-        // if start date is past
+        // if start date is past and end date is null
         $step->getStartAt()->willReturn((new \DateTime())->modify('-1 weeks'));
         $step->getEndAt()->willReturn(null);
-        $this->getStatus($step)->shouldReturn('closed');
+        $this->getStatus($step)->shouldReturn('open');
 
+        // if start date and end date is null
         $step->getStartAt()->willReturn(null);
         $step->getEndAt()->willReturn(null);
+        $this->getStatus($step)->shouldReturn('open');
 
-        // if at least one previous step has future dates
-        $projectHelper->getPreviousSteps($step)->willReturn([$stepA, $stepB]);
-        $stepA->getStartAt()->willReturn((new \DateTime())->modify('+1 weeks'));
-        $stepA->getEndAt()->willReturn(null);
-        $this->getStatus($step)->shouldReturn('future');
+        // if start date is null and end date is future
+        $step->getStartAt()->willReturn(null);
+        $step->getEndAt()->willReturn((new \DateTime())->modify('+1 weeks'));
+        $this->getStatus($step)->shouldReturn('open');
 
-        $stepA->getStartAt()->willReturn(null);
-        $stepA->getEndAt()->willReturn(null);
-        $stepB->getStartAt()->willReturn((new \DateTime())->modify('+1 weeks'));
-        $stepB->getEndAt()->willReturn((new \DateTime())->modify('+1 weeks'));
-        $this->getStatus($step)->shouldReturn('future');
-
-        // if no previous steps 
-        $projectHelper->getPreviousSteps($step)->willReturn([]);
-        $this->getStatus($step)->shouldReturn('closed');
-
-        // if no previous steps has dates
-        $projectHelper->getPreviousSteps($step)->willReturn([$stepA]);
-        $stepA->getStartAt()->willReturn(null);
-        $stepA->getEndAt()->willReturn(null);
-        $this->getStatus($step)->shouldReturn('closed');
-
-        // if previous steps have past dates
-        $stepA->getStartAt()->willReturn((new \DateTime())->modify('-1 weeks'));
-        $stepA->getEndAt()->willReturn((new \DateTime())->modify('-1 weeks'));
+        // if start date is null and end date is past
+        $step->getStartAt()->willReturn(null);
+        $step->getEndAt()->willReturn((new \DateTime())->modify('-1 weeks'));
         $this->getStatus($step)->shouldReturn('closed');
     }
 }
