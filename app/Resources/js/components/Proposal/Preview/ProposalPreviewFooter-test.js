34b7f2c65fab6b2d225ddcb6e9fbd218cd207b19
spyl94 <spyl94@gmail.com>
2016-10-17 14:49:08 +0200

    Merge pull request #3098 from jolicode/fix-nocha-tests
    FIx
    
    GitOrigin-RevId: 5f7476b78b06e3cfc3650f539db0467740667b3c

--- app/Resources/js/components/Proposal/Preview/ProposalPreviewFooter-test.js
+++ app/Resources/js/components/Proposal/Preview/ProposalPreviewFooter-test.js
@@ -9,15 +9,14 @@ import IntlData from '../../../translations/FR';
 describe('<ProposalPreviewFooter />', () => {
   const proposal = {
     comments_count: 3,
-    votesCount: 1,
     votesCountByStepId: {
+      1: 1,
       42: 5,
     },
   };
 
   const props = {
     proposal,
-    votesDelta: 1,
     stepId: 1,
     ...IntlData,
   };
@@ -26,60 +25,33 @@ describe('<ProposalPreviewFooter />', () => {
     const wrapper = shallow(<ProposalPreviewFooter {...props} />);
     const footer = wrapper.find('div.proposal__footer');
     expect(footer).to.have.length(1);
+
     const countersDiv = footer.find('div.proposal__counters');
     expect(countersDiv).to.have.length(1);
     expect(countersDiv.find('div.proposal__counter')).to.have.length(1);
+    expect(countersDiv.find('div.proposal__counter--votes')).to.have.length(0);
+
     const commentsCounter = countersDiv.find('div.proposal__counter--comments');
     expect(commentsCounter).to.have.length(1);
-    expect(commentsCounter.find('.proposal__counter__value').text()).to.equal('3');
-    expect(countersDiv.find('div.proposal__counter--votes')).to.have.length(0);
+    expect(commentsCounter.find('.proposal__counter__value').text()).to.equal(`${proposal.comments_count}`);
   });
 
   it('should render a footer with comment and votes counters', () => {
     const wrapper = shallow(<ProposalPreviewFooter {...props} showVotes />);
-    const footer = wrapper.find('div.proposal__footer');
-    expect(footer).to.have.length(1);
-    const countersDiv = footer.find('div.proposal__counters');
-    expect(countersDiv).to.have.length(1);
-    expect(countersDiv.find('div.proposal__counter')).to.have.length(2);
-    const commentsCounter = countersDiv.find('div.proposal__counter--comments');
-    expect(commentsCounter).to.have.length(1);
-    expect(commentsCounter.find('.proposal__counter__value').text()).to.equal('3');
-    const votesCounter = countersDiv.find('div.proposal__counter--votes');
-    expect(votesCounter).to.have.length(1);
-    const expectedVotes = proposal.votesCount;
-    expect(votesCounter.find('.proposal__counter__value').text()).to.equal(`${expectedVotes}`);
-  });
 
-  it('should render a footer with comment and votes counters when selection step is specified', () => {
-    const wrapper = shallow(<ProposalPreviewFooter {...props} showVotes stepId={42} />);
     const footer = wrapper.find('div.proposal__footer');
     expect(footer).to.have.length(1);
-    const countersDiv = footer.find('div.proposal__counters');
-    expect(countersDiv).to.have.length(1);
-    expect(countersDiv.find('div.proposal__counter')).to.have.length(2);
-    const commentsCounter = countersDiv.find('div.proposal__counter--comments');
-    expect(commentsCounter).to.have.length(1);
-    expect(commentsCounter.find('.proposal__counter__value').text()).to.equal('3');
-    const votesCounter = countersDiv.find('div.proposal__counter--votes');
-    expect(votesCounter).to.have.length(1);
-    const expectedVotes = proposal.votesCountByStepId[42] + props.votesDelta;
-    expect(votesCounter.find('.proposal__counter__value').text()).to.equal(`${expectedVotes}`);
-  });
 
-  it('should render a footer with comment and votes counters when selection step votes are not specified', () => {
-    const wrapper = shallow(<ProposalPreviewFooter {...props} showVotes stepId={43} />);
-    const footer = wrapper.find('div.proposal__footer');
-    expect(footer).to.have.length(1);
     const countersDiv = footer.find('div.proposal__counters');
     expect(countersDiv).to.have.length(1);
     expect(countersDiv.find('div.proposal__counter')).to.have.length(2);
+
     const commentsCounter = countersDiv.find('div.proposal__counter--comments');
     expect(commentsCounter).to.have.length(1);
-    expect(commentsCounter.find('.proposal__counter__value').text()).to.equal('3');
+    expect(commentsCounter.find('.proposal__counter__value').text()).to.equal(`${proposal.comments_count}`);
+
     const votesCounter = countersDiv.find('div.proposal__counter--votes');
     expect(votesCounter).to.have.length(1);
-    const expectedVotes = props.votesDelta;
-    expect(votesCounter.find('.proposal__counter__value').text()).to.equal(`${expectedVotes}`);
+    expect(votesCounter.find('.proposal__counter__value').text()).to.equal(`${proposal.votesCountByStepId[1]}`);
   });
 });
