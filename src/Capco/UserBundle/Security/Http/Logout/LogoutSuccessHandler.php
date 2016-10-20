a5f17e81cc1de22f532c7a40ba6818674f15a340
spyl94 <spyl94@gmail.com>
2016-10-20 14:24:50 +0200

    Do not logout saml if not used (#3140)
    * Do not logout saml is used
    
    * Fix
    
    GitOrigin-RevId: 8571bde266a0c5372a459f73764d4c9bd78b2b95

--- src/Capco/UserBundle/Security/Http/Logout/LogoutSuccessHandler.php
+++ src/Capco/UserBundle/Security/Http/Logout/LogoutSuccessHandler.php
@@ -6,23 +6,29 @@ use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Router;
+use Capco\AppBundle\Toggle\Manager;
 
 class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
 {
     protected $samlAuth;
     protected $router;
+    protected $toggleManager;
 
-    public function __construct(\SimpleSAML_Auth_Simple $samlAuth, Router $router)
+    public function __construct(\SimpleSAML_Auth_Simple $samlAuth, Router $router, Manager $toggleManager)
     {
         $this->samlAuth = $samlAuth;
         $this->router = $router;
+        $this->toggleManager = $toggleManager;
     }
 
     public function onLogoutSuccess(Request $request)
     {
         $returnTo = $request->headers->get('referer', '/');
         $request->getSession()->invalidate();
-        $this->samlAuth->logout($returnTo);
+
+        if ($this->toggleManager->isActive('login_saml')) {
+            $this->samlAuth->logout($returnTo);
+        }
 
         return new RedirectResponse($returnTo);
     }
