39c47b0c8ba394485cff0b71921183e44fab3595
ptondereau <pierre.tondereau@protonmail.com>
2016-10-21 14:35:57 +0200

    move ProposalListener to the right folder
    GitOrigin-RevId: 84c48a36d72dfc2c25532d0e52cc1ef69b6b1e89

---
+++ src/Capco/AppBundle/EventListener/ProposalListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Capco\AppBundle\EventListener;
+
+use Capco\AppBundle\Entity\Answer;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Capco\AppBundle\Entity\Proposal;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ProposalListener implements ContainerAwareInterface
+{
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param PreUpdateEventArgs $args
+     */
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        $changeSet = $args->getEntityChangeSet();
+
+        if ($entity instanceof Proposal) {
+            if ($this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+                $entity->setUpdateAuthor($this->container->get('security.token_storage')->getToken()->getUser());
+            }
+
+            $notifier = $this->container->get('capco.notify_manager');
+
+            if (array_key_exists('status', $changeSet)) {
+                $notifier->notifyProposalStatusChange($entity);
+            }
+
+            if (array_key_exists('answer', $changeSet) && $changeSet['answer'][1] instanceof Answer) {
+                $notifier->notifyProposalAnswer($entity);
+            }
+        }
+    }
+}
