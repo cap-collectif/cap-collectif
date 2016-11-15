b7cea01f7a4e8ff47178824ff12bb2946aa59331
spyl94 <spyl94@gmail.com>
2016-11-15 10:51:42 +0100

    Restore order
    GitOrigin-RevId: 6a0f7228d8ba48fbeb5b761c7939e8f6fd09c822

--- infrastructure/services/builder/build.sh
+++ infrastructure/services/builder/build.sh
@@ -12,7 +12,7 @@ if [ "$PRODUCTION" ]; then
   php vendor/sensio/distribution-bundle/Resources/bin/build_bootstrap.php var || exit 1
 
   # Frontend deps
-  yarn install --pure-lockfile || exit 1
+  yarn install --no-progress --pure-lockfile || exit 1
   bower install --config.interactive=false --allow-root || exit 1
   yarn run build:prod || exit 1
 
@@ -25,7 +25,7 @@ else
   composer dump-autoload
 
   # Frontend deps
-  yarn install --pure-lockfile
+  yarn install --no-progress --pure-lockfile
   bower install --config.interactive=false
   if ./node_modules/node-sass/bin/node-sass | grep --quiet `npm rebuild node-sass` &> /dev/null; then
       echo "Building node-sass binding for the container..."
