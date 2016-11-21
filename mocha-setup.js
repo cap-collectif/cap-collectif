fe1da918077b32c05abd28636709792184645182
spyl94 <spyl94@gmail.com>
2016-11-21 19:11:14 +0100

    Merge pull request #3287 from jolicode/update/sf-2-8-14
    update SF to 2.8.14
    
    GitOrigin-RevId: defbbb6f972cc708b27c68953b167493cb53c716

---
+++ mocha-setup.js
@@ -0,0 +1,29 @@
+import { jsdom } from 'jsdom';
+
+const exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+global.window.__SERVER__ = false;
+global.$ = require('jquery')(window);
+
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+import 'babel-polyfill';
+
+global.navigator = {
+  userAgent: 'node.js',
+};
+
+const throwError = (warning) => {
+  if (!/You are manually calling|Unknown props/.test(warning)) { // fix warnings and remove this
+    throw new Error(warning);
+  }
+};
+console.error = throwError; // eslint-disable-line no-console
+console.warn = throwError; // eslint-disable-line no-console
