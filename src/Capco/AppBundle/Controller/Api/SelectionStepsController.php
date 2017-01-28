2b3e72751129b57d60637111485ab26b038fb8a2
spyl94 <spyl94@gmail.com>
2017-01-28 17:43:32 +0100

    Should fix votes counter indexing
    GitOrigin-RevId: cfa8079dbf5027f67f7bc12e7069a8e2bf66a63e

--- src/Capco/AppBundle/Controller/Api/SelectionStepsController.php
+++ src/Capco/AppBundle/Controller/Api/SelectionStepsController.php
@@ -223,11 +223,10 @@ class SelectionStepsController extends FOSRestController
         $em->persist($vote);
         $em->flush();
 
-        // If not present, es listener will take some time to execute the refresh
-        // and, next time proposals will be fetched, the set of data will be outdated.
-        // Keep in mind that refresh should usually not be triggered manually.
-        $index = $this->get('fos_elastica.index');
-        $index->refresh();
+        $this
+          ->get('fos_elastica.object_persister.app.proposal')
+          ->insertOne($proposal)
+        ;
 
         return $vote;
     }
@@ -270,9 +269,9 @@ class SelectionStepsController extends FOSRestController
 
         $this
           ->get('fos_elastica.object_persister.app.proposal')
-          ->replaceOne($proposal)
+          ->insertOne($proposal)
         ;
-
+        
         return $vote;
     }
 
