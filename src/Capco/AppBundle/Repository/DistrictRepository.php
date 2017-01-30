dbdfa63e9a506c40f5d4758d5b44fc52fddc653d
ptondereau <pierre.tondereau@protonmail.com>
2017-01-30 11:38:11 +0100

    Remove trashed proposals from stats counter (#3582)
    * Remove trashed proposals from stats counter
    
    * Fix stats counter for district and theme
    
    GitOrigin-RevId: 6ef7827be43c74ec47d9c2941a576573cad84935

--- src/Capco/AppBundle/Repository/DistrictRepository.php
+++ src/Capco/AppBundle/Repository/DistrictRepository.php
@@ -22,6 +22,7 @@ class DistrictRepository extends EntityRepository
                 WHERE pf.step = :step
                 AND p.enabled = true
                 AND pd.id = d.id
+                AND p.trashed = false
             ) as value')
             ->setParameter('step', $step)
             ->orderBy('value', 'DESC')
