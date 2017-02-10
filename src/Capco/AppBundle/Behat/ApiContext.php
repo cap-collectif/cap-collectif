373454b657d89abb74093c83772077050381a368
spyl94 <spyl94@gmail.com>
2017-02-10 14:57:02 +0100

    Clean
    GitOrigin-RevId: 49617cc7cc1abcbaeaba36f334438181106a637e

--- src/Capco/AppBundle/Behat/ApiContext.php
+++ src/Capco/AppBundle/Behat/ApiContext.php
@@ -329,7 +329,6 @@ EOF;
         $body = (string) $this->response->getBody();
         $factory = new SimpleFactory();
         $matcher = $factory->createMatcher();
-        var_dump($body);
         PHPUnit::assertTrue($matcher->match($body, $pattern->getRaw()), $matcher->getError());
     }
 
