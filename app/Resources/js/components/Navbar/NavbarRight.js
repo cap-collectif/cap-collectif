0287ead07cd1b88dbda9cbc0b5f6238770836a28
spyl94 <spyl94@gmail.com>
2017-05-23 15:52:23 +0200

    Merge pull request #3969 from cap-collectif/fix-html-diif
    Fix some diff problems
    
    GitOrigin-RevId: 2297e2a25fb9863d10eedf15c50ffa4a4a9d5e65

--- app/Resources/js/components/Navbar/NavbarRight.js
+++ app/Resources/js/components/Navbar/NavbarRight.js
@@ -31,69 +31,72 @@ const NavbarRight = React.createClass({
     const { user, features } = this.props;
     return (
       <Nav pullRight style={{ marginRight: 0 }}>
-        {
-          features.search &&
-            <NavItem eventKey={1} className="navbar__search" href="/search">
-              <i className="cap cap-magnifier"></i> <span
-                className="visible-xs-inline"
-                style={{ whiteSpace: 'nowrap' }}
-                                                    >
-                { this.getIntlMessage('navbar.search') }
-              </span>
-            </NavItem>
-        }
-        {
-          user
-            ? <NavDropdown
+        {features.search &&
+          <NavItem eventKey={1} className="navbar__search" href="/search">
+            <i className="cap cap-magnifier" /> <span
+              className="visible-xs-inline"
+              style={{ whiteSpace: 'nowrap' }}>
+              {this.getIntlMessage('navbar.search')}
+            </span>
+          </NavItem>}
+        {user
+          ? <NavDropdown
               eventKey={3}
               title={
                 <span>
-                  <UserAvatar user={user} size={34} style={{ marginRight: '10px' }} anchor={false} />
+                  <UserAvatar
+                    user={user}
+                    size={34}
+                    style={{ marginRight: '10px' }}
+                    anchor={false}
+                  />
                   <span className="hidden-xs">
-                    { user.username }
+                    {user.username}
                   </span>
                 </span>
               }
               className="navbar__dropdown"
-              id="navbar-username"
-              >
-              {
-                user.isAdmin &&
-                  <MenuItem key={3.1} eventKey={3.1} href="/admin">
-                    { this.getIntlMessage('navbar.admin') }
-                  </MenuItem>
-              }
-              {
-                features.profiles &&
-                  <MenuItem key={3.2} eventKey={3.2} href="/profile">
-                    { this.getIntlMessage('navbar.profile') }
-                  </MenuItem>
-              }
-              <MenuItem key={3.3} eventKey={3.3} href={`/profile/${features.profiles ? 'edit-profile' : 'edit-account'}`}>
-                { this.getIntlMessage('navbar.user_settings') }
+              id="navbar-username">
+              {user.isAdmin &&
+                <MenuItem key={3.1} eventKey={3.1} href="/admin">
+                  {this.getIntlMessage('navbar.admin')}
+                </MenuItem>}
+              {features.profiles &&
+                <MenuItem key={3.2} eventKey={3.2} href="/profile">
+                  {this.getIntlMessage('navbar.profile')}
+                </MenuItem>}
+              <MenuItem
+                key={3.3}
+                eventKey={3.3}
+                href={`/profile/${features.profiles ? 'edit-profile' : 'edit-account'}`}>
+                {this.getIntlMessage('navbar.user_settings')}
               </MenuItem>
               <MenuItem key={3.4} divider />
-              <MenuItem key={3.5} eventKey={3.5} id="logout-button" onClick={this.logout}>
-                { this.getIntlMessage('global.logout') }
+              <MenuItem
+                key={3.5}
+                eventKey={3.5}
+                id="logout-button"
+                onClick={this.logout}>
+                {this.getIntlMessage('global.logout')}
               </MenuItem>
             </NavDropdown>
-          : <li>
-            <RegistrationButton className="navbar-btn" />
-            { ' ' }
-            <LoginButton className="btn-darkest-gray navbar-btn btn--connection" />
-          </li>
-        }
+          : <li style={{ paddingLeft: 15, paddingRight: 15 }}>
+              <RegistrationButton className="navbar-btn" />
+              {' '}
+              <LoginButton className="btn-darkest-gray navbar-btn btn--connection" />
+            </li>}
       </Nav>
     );
   },
-
 });
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     features: state.default.features,
     user: state.user.user,
   };
 };
 
-export default connect(mapStateToProps, null, null, { withRef: true })(NavbarRight);
+export default connect(mapStateToProps, null, null, { withRef: true })(
+  NavbarRight,
+);
