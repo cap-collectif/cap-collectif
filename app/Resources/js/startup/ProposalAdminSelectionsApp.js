a276e0934ee6d39c4baba6bd0f6a6e4bc72dc8e7
spyl94 <spyl94@gmail.com>
2017-07-11 17:40:11 +0200

    Update messages.json format
    GitOrigin-RevId: ac76004f28721721ad8084c9d5b87a2cd54ac426

--- app/Resources/js/startup/ProposalAdminSelectionsApp.js
+++ app/Resources/js/startup/ProposalAdminSelectionsApp.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactOnRails from 'react-on-rails';
+import { IntlProvider } from 'react-intl-redux';
 import ProposalAdminSelections from '../components/Proposal/Admin/ProposalAdminSelections';
 
 export default props =>
- <Provider store={ReactOnRails.getStore('appStore')}>
-   <ProposalAdminSelections {...props} />
- </Provider>
-;
+  <Provider store={ReactOnRails.getStore('appStore')}>
+    <IntlProvider>
+      <ProposalAdminSelections {...props} />
+    </IntlProvider>
+  </Provider>;
