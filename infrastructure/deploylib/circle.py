ee1f2c1b76849b585a1a5e19923ec6506fef4534
spyl94 <spyl94@gmail.com>
2017-07-27 16:42:51 +0200

    Should load images
    GitOrigin-RevId: 0eae87b1149d29fffae4efaa7241a5e091eb34c6

--- infrastructure/deploylib/circle.py
+++ infrastructure/deploylib/circle.py
@@ -5,28 +5,17 @@ import re
 import os
 import yaml
 
-
-@task(environments=['local', 'ci'])
-def fix_environment_variables_with_lxc():
-    with open('app/config/parameters.yml', 'w') as yaml_file:
-        yaml_file.write(yaml.dump({
-            'parameters': {
-                'database_host': 'database',
-                'elasticsearch_host': 'elasticsearch',
-                'redis_host': 'redis',
-            }
-        }, default_flow_style=False))
-
-
-@task(environments=['ci'])
-def clean_cache():
-    "Clean cache of docker images"
-    commit_message = local('git log --format=%B --no-merges -n 1', capture=True)
-    if re.search('\[clean-cache\]', commit_message):
-        local('rm -rf ~/docker')
-        local('rm -rf ~/.composer')
-        local('rm -rf ~/.yarn-cache')
-        local('rm -rf node_modules')
+#
+# @task(environments=['local', 'ci'])
+# def fix_environment_variables_with_lxc():
+#     with open('app/config/parameters.yml', 'w') as yaml_file:
+#         yaml_file.write(yaml.dump({
+#             'parameters': {
+#                 'database_host': 'database',
+#                 'elasticsearch_host': 'elasticsearch',
+#                 'redis_host': 'redis',
+#             }
+#         }, default_flow_style=False))
 
 
 @task(environments=['ci'])
@@ -42,15 +31,7 @@ def load_cache():
     local('docker load -i ~/docker/capcotest_redis.tar || true')
 
 
-@task(environments=['ci'])
-def save_logs():
-    "Save logs"
-    local('docker logs capcotest_application_1 > $CIRCLE_TEST_REPORTS/application.log')
-    local('docker logs capcotest_builder_1 > $CIRCLE_TEST_REPORTS/builder.log')
-    local('docker logs capcotest_elasticsearch_1 > $CIRCLE_TEST_REPORTS/elasticsearch.log')
-
-
-@task(environments=['local'])
+@task(environments=['local', 'ci'])
 def save_fixtures_image(tag='latest'):
     "Publish a new fixtures image"
     env.service_command('php bin/console capco:reinit --force', 'application', env.www_app)
@@ -65,50 +46,77 @@ def save_fixtures_image(tag='latest'):
 @task(environments=['ci'])
 def save_cache():
     "Rebuild infrastructure and save cache"
-    commit_message = local('git log --format=%B --no-merges -n 1', capture=True)
-    create_fresh_cache = re.search('\[fresh-cache\]', commit_message)
+        commit_message = local('git log --format=%B --no-merges -n 1', capture=True)
+
+    if re.search('\[force-rebuild\]', commit_message) or change_detected():
+        build()
+
+        for image, tags in get_images().iteritems():
+            local('docker save capco_%s > ~/.docker-images/capco_%s.tar' % (tags[0], image))
+
+    # if create_fresh_cache:
+    #     print 'Creating a fresh docker cache !'
+    #
+    # if change_in_infrastructure:
+    #     print 'Changes detected in infrastructure, creating an updated cache !'
+    #
+    # if change_in_infrastructure or create_fresh_cache:
+    #     env.compose('build')
+    #     local('docker pull elasticsearch:1.7.3')
+    #     local('docker pull redis:3')
+    #     local('docker pull selenium/hub:2.53.1-beryllium')
+    #     local('docker pull selenium/node-chrome-debug:2.53.1-beryllium')
+    #     local('docker pull jderusse/mailcatcher:latest')
+    #     local('docker save capcotest_application > ~/.docker-images/capcotest_application.tar')
+    #     local('docker save capcotest_applicationdata > ~/.docker-images/capcotest_applicationdata.tar')
+    #     local('docker save capcotest_builder > ~/.docker-images/capcotest_builder.tar')
+    #     local('docker save elasticsearch > ~/.docker-images/capcotest_elasticsearch.tar')
+    #     local('docker save redis > ~/.docker-images/capcotest_redis.tar')
+    #     local('docker save selenium/hub > ~/.docker-images/capcotest_seleniumhub.tar')
+    #     local('docker save selenium/node-chrome-debug > ~/.docker-images/capcotest_chrome.tar')
+    #     local('docker save jderusse/mailcatcher > ~/.docker-images/capcotest_mailcacher.tar')
+
+
+def get_images():
+    return {
+        'application': ['application'],
+        'applicationdata': ['applicationdata'],
+        'builder': ['builder'],
+        # 'elasticsearch': ['elasticsearch'],
+        # 'redis': ['redis'],
+        # 'database': ['database'],
+        # 'selenium/hub': ['selenium/hub'],
+        # 'selenium/node-chrome-debug': ['selenium/node-chrome-debug'],
+        # 'jderusse/mailcatcher': ['jderusse/mailcatcher']
+    }
+
+
+def change_paths():
+    return [
+        'infrastructure/environments/base.yml',
+        'infrastructure/services/(.+?)',
+        '.circleci/.circle-cache',
+    ]
+
+
+def change_detected():
+    compare_url = local('echo $CIRCLE_COMPARE_URL', capture=True)
+
+    simpleMatch = re.search('https://github.com/cap-collectif/platform/commit/([a-z0-9]+?)$', compare_url)
+    match = re.search('https://github.com/cap-collectif/platform/compare/([a-z0-9]+?)\.\.\.([a-z0-9]+?)$', compare_url)
+
+    if simpleMatch is None and match is None:
+        return
+
+    if simpleMatch is not None:
+        changes = local('git diff --name-only %s | cat' % simpleMatch.group(1), capture=True).split("\n")
+    else:
+        changes = local('git diff --name-only %s %s | cat' % (match.group(1), match.group(2)), capture=True).split("\n")
+
     change_in_infrastructure = False
 
-    compare_url = local('echo $CIRCLE_COMPARE_URL', capture=True)
-    simpleMatch = re.search('https://github.com/jolicode/CapCollectif-SF2/compare/([a-z0-9]+?)$', compare_url)
-    match = re.search('https://github.com/jolicode/CapCollectif-SF2/compare/([a-z0-9]+?)\.\.\.([a-z0-9]+?)$', compare_url)
+    for change in changes:
+        for regex in change_paths():
+            change_in_infrastructure = change_in_infrastructure or re.search(regex, change) is not None
 
-    if create_fresh_cache is False:
-        if simpleMatch is None and match is None:
-            return
-        if simpleMatch is not None:
-            changes = local('git diff --name-only %s | cat' % simpleMatch.group(1), capture=True).split("\n")
-        else:
-            changes = local('git diff --name-only %s %s | cat' % (match.group(1), match.group(2)), capture=True).split("\n")
-
-        for change in changes:
-            match = re.search('^docker-compose.yml', change)
-
-            if match is None:
-                match = re.search('^infrastructure/services', change)
-
-            if match is not None:
-                change_in_infrastructure = True
-
-    if create_fresh_cache:
-        print 'Creating a fresh docker cache !'
-
-    if change_in_infrastructure:
-        print 'Changes detected in infrastructure, creating an updated cache !'
-
-    if change_in_infrastructure or create_fresh_cache:
-        env.compose('build')
-        local('docker pull elasticsearch:1.7.3')
-        local('docker pull redis:3')
-        local('docker pull selenium/hub:2.53.1-beryllium')
-        local('docker pull selenium/node-chrome-debug:2.53.1-beryllium')
-        local('docker pull jderusse/mailcatcher:latest')
-        local('mkdir -p ~/docker')
-        local('docker save capcotest_application > ~/docker/capcotest_application.tar')
-        local('docker save capcotest_applicationdata > ~/docker/capcotest_applicationdata.tar')
-        local('docker save capcotest_builder > ~/docker/capcotest_builder.tar')
-        local('docker save elasticsearch > ~/docker/capcotest_elasticsearch.tar')
-        local('docker save redis > ~/docker/capcotest_redis.tar')
-        local('docker save selenium/hub > ~/docker/capcotest_seleniumhub.tar')
-        local('docker save selenium/node-chrome-debug > ~/docker/capcotest_chrome.tar')
-        local('docker save jderusse/mailcatcher > ~/docker/capcotest_mailcacher.tar')
+    return change_in_infrastructure
