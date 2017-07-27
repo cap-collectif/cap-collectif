9fab23c4fb198581a4d43355b4d0595cb5915a55
spyl94 <spyl94@gmail.com>
2017-07-27 19:03:25 +0200

    Test fix builder
    GitOrigin-RevId: eba47d5c16aac23f47808ce4e956c6dad7cd2031

--- infrastructure/deploylib/app.py
+++ infrastructure/deploylib/app.py
@@ -5,11 +5,11 @@ from fabric.api import env
 import os
 
 
-@task
+@task(environments=['local', 'ci'])
 def deploy(environment='dev', user='capco'):
     "Deploy"
     if os.environ.get('CI') == 'true':
-        env.compose('run -u root builder chown capco:capco -R /var/www/web /home/capco/.composer /home/capco/.cache/yarn /home/capco/.cache/bower')
+        env.compose('run -u root builder chown capco:capco -R /var/www /home/capco/.composer /home/capco/.cache/yarn /home/capco/.cache/bower')
     env.compose('run' + ('', ' -e PRODUCTION=true')[environment == 'prod'] + ('', ' -e CI=true')[os.environ.get('CI') == 'true'] + ' builder build')
     env.service_command('php vendor/sensio/distribution-bundle/Resources/bin/build_bootstrap.php var', 'application', env.www_app)
     env.service_command('rm -rf var/cache/dev var/cache/prod var/cache/test', 'application', env.www_app, 'root')
@@ -19,26 +19,26 @@ def deploy(environment='dev', user='capco'):
     env.service_command('php bin/console assets:install --symlink', 'application', env.www_app)
 
 
-@task
+@task(environments=['local'])
 def clean():
     "Clean"
     env.compose('stop chrome || true')
     env.compose('rm -f chrome')
 
 
-@task
+@task(environments=['local'])
 def ssh(user='capco'):
     "Ssh into application container"
     env.ssh_into('application', user)
 
 
-@task
+@task(environments=['local'])
 def clear_cache(environment='dev'):
     "Clear cache"
     env.service_command('rm -rf var/cache/' + environment, 'application', env.www_app, 'root')
 
 
-@task
+@task(environments=['local'])
 def cmd(command='', environment='dev'):
     "Executing Symfony command"
     env.service_command('php -d memory_limit=-1 bin/console ' + command + ' --no-interaction --env=' + environment, 'application', env.www_app, 'root')
