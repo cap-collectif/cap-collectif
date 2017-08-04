e563f2c18bb4df3d894ae351d7d9c604dc990a17
spyl94 <spyl94@gmail.com>
2017-08-04 18:40:42 +0200

    Some refacto
    GitOrigin-RevId: 7bb60c079be3668cba4e9e089f14fe73aa9c07f9

--- app/Resources/js/components/Proposal/Admin/ProposalAdminPage.js
+++ app/Resources/js/components/Proposal/Admin/ProposalAdminPage.js
@@ -1,24 +1,17 @@
 // @flow
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
-import { injectIntl } from 'react-intl';
 import { QueryRenderer, graphql } from 'react-relay';
 import environment, { graphqlError } from '../../../createRelayEnvironment';
-import ProposalAdminSelections from './ProposalAdminSelections';
-import ProposalAdminStatusForm from './ProposalAdminStatusForm';
-import ProposalAdminContentForm from './ProposalAdminContentForm';
-import ProposalAdminNotationForm from './ProposalAdminNotationForm';
-import ProposalAdminNewsForm from './ProposalAdminNewsForm';
+import ProposalAdminPageTabs from './ProposalAdminPageTabs';
 import Loader from '../../Utils/Loader';
 
 type DefaultProps = void;
 type Props = { proposalId: number };
 type State = void;
 
-const renderProposalAdminPage = ({
+const component = ({
   error,
   props,
-  intl,
 }: {
   error: ?string,
   intl: any,
@@ -31,47 +24,12 @@ const renderProposalAdminPage = ({
   if (props) {
     // eslint-disable-next-line
     if (props.proposal !== null) {
-      return (
-        <Tabs defaultActiveKey={2} id="proposal-admin-page-tabs">
-          <Tab
-            eventKey={1}
-            title={intl.formatMessage({ id: 'proposal.admin.activity' })}
-            disabled
-          />
-          <Tab
-            eventKey={2}
-            title={intl.formatMessage({ id: 'proposal.admin.content' })}>
-            <ProposalAdminContentForm {...props} />
-          </Tab>
-          <Tab
-            eventKey={3}
-            title={intl.formatMessage({ id: 'proposal.admin.advancement' })}>
-            <ProposalAdminSelections {...props} />
-          </Tab>
-          <Tab
-            eventKey={4}
-            title={intl.formatMessage({ id: 'proposal.admin.news' })}>
-            <ProposalAdminNewsForm {...props} />
-          </Tab>
-          <Tab
-            eventKey={5}
-            title={intl.formatMessage({ id: 'proposal.admin.notation' })}>
-            <ProposalAdminNotationForm {...props} />
-          </Tab>
-          <Tab
-            eventKey={6}
-            title={intl.formatMessage({ id: 'proposal.admin.publication' })}>
-            <ProposalAdminStatusForm {...props} />
-          </Tab>
-        </Tabs>
-      );
+      return <ProposalAdminPageTabs {...props} />;
     }
     return graphqlError;
   }
   return <Loader />;
 };
-
-const render = injectIntl(renderProposalAdminPage);
 
 export default class ProposalAdminPage extends Component<
   DefaultProps,
@@ -97,7 +55,7 @@ export default class ProposalAdminPage extends Component<
           variables={{
             id: this.props.proposalId,
           }}
-          render={render}
+          render={component}
         />
       </div>
     );
---
+++ app/Resources/js/components/Proposal/Admin/ProposalAdminPageTabs.js
@@ -0,0 +1,59 @@
+// @flow
+import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-bootstrap';
+import { injectIntl } from 'react-intl';
+import ProposalAdminSelections from './ProposalAdminSelections';
+import ProposalAdminStatusForm from './ProposalAdminStatusForm';
+import ProposalAdminContentForm from './ProposalAdminContentForm';
+import ProposalAdminNotationForm from './ProposalAdminNotationForm';
+import ProposalAdminNewsForm from './ProposalAdminNewsForm';
+
+type DefaultProps = void;
+type Props = { proposal: any, intl: Object };
+type State = void;
+
+export class ProposalAdminPageTabs extends Component<
+  DefaultProps,
+  Props,
+  State,
+> {
+  render() {
+    const { intl, proposal } = this.props;
+    return (
+      <Tabs defaultActiveKey={2} id="proposal-admin-page-tabs">
+        <Tab
+          eventKey={1}
+          title={intl.formatMessage({ id: 'proposal.admin.activity' })}
+          disabled
+        />
+        <Tab
+          eventKey={2}
+          title={intl.formatMessage({ id: 'proposal.admin.content' })}>
+          <ProposalAdminContentForm proposal={proposal} />
+        </Tab>
+        <Tab
+          eventKey={3}
+          title={intl.formatMessage({ id: 'proposal.admin.advancement' })}>
+          <ProposalAdminSelections proposal={proposal} />
+        </Tab>
+        <Tab
+          eventKey={4}
+          title={intl.formatMessage({ id: 'proposal.admin.news' })}>
+          <ProposalAdminNewsForm proposal={proposal} />
+        </Tab>
+        <Tab
+          eventKey={5}
+          title={intl.formatMessage({ id: 'proposal.admin.notation' })}>
+          <ProposalAdminNotationForm proposal={proposal} />
+        </Tab>
+        <Tab
+          eventKey={6}
+          title={intl.formatMessage({ id: 'proposal.admin.publication' })}>
+          <ProposalAdminStatusForm proposal={proposal} />
+        </Tab>
+      </Tabs>
+    );
+  }
+}
+
+export default injectIntl(ProposalAdminPageTabs);
