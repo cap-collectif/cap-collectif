c3fe8e513bc855d4ab7d2696a3cc978d127edc3d
spyl94 <spyl94@gmail.com>
2017-09-06 17:25:08 +0200

    Typecheck
    GitOrigin-RevId: ee4f3f1c11b0974e0d2857c0cd2d0b4464efdd2b

--- src/Capco/AppBundle/Helper/GeometryHelper.php
+++ src/Capco/AppBundle/Helper/GeometryHelper.php
@@ -2,11 +2,9 @@
 
 namespace Capco\AppBundle\Helper;
 
-// include_once('../../../vendor/phayes/geophp/geoPHP.inc');
-
 class GeometryHelper
 {
-    public static function isIncluded($lon, $lat, string $geojson): bool
+    public static function isIncluded(float $lon, float $lat, string $geojson): bool
     {
         $polygon = \geoPHP::load($geojson, 'json');
         $point = \geoPHP::load("POINT($lon $lat)", 'wkt');
