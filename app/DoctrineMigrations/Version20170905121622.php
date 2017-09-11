296166898e3d1ca76f46c8b85ec38873f6d2e831
spyl94 <spyl94@gmail.com>
2017-09-11 17:11:02 +0200

    Fix migration
    GitOrigin-RevId: 78ac6a57fdcc6c89311855e55420efc28b018e05

--- app/DoctrineMigrations/Version20170905121622.php
+++ app/DoctrineMigrations/Version20170905121622.php
@@ -9,7 +9,9 @@ class Version20170905121622 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
+        $this->addSql('ALTER TABLE district DROP FOREIGN KEY FK_31C154875FF69B7D');
         $this->addSql('ALTER TABLE district CHANGE form_id form_id INT NOT NULL');
+        $this->addSql('CREATE INDEX IDX_31C154875FF69B7D ON district (form_id)');
     }
 
     public function down(Schema $schema)
