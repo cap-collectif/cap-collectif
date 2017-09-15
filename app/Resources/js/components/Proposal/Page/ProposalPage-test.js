2310bad73bf2dbec7473ce579e97f077a8552876
Amelie52 <amelie.tamisier@gmail.com>
2017-09-15 11:40:15 +0200

    Add text for propositions and comments votes
    GitOrigin-RevId: e161bac9e15e54bd6237b5d1631887bb5ce69e42

--- app/Resources/js/components/Proposal/Page/ProposalPage-test.js
+++ app/Resources/js/components/Proposal/Page/ProposalPage-test.js
@@ -1,13 +1,10 @@
 /* eslint-env jest */
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Tab, Nav, NavItem } from 'react-bootstrap';
+import { Tab } from 'react-bootstrap';
 import { ProposalPage } from './ProposalPage';
 
-import {
-  VOTE_TYPE_SIMPLE,
-  VOTE_TYPE_DISABLED,
-} from '../../../constants/ProposalConstants';
+import { VOTE_TYPE_SIMPLE, VOTE_TYPE_DISABLED } from '../../../constants/ProposalConstants';
 
 describe('<ProposalPage />', () => {
   const props = {
@@ -22,10 +19,7 @@ describe('<ProposalPage />', () => {
       themes: true,
       districts: false,
     },
-    steps: [
-      { id: '1', voteType: VOTE_TYPE_DISABLED },
-      { id: '2', voteType: VOTE_TYPE_SIMPLE },
-    ],
+    steps: [{ id: '1', voteType: VOTE_TYPE_DISABLED }, { id: '2', voteType: VOTE_TYPE_SIMPLE }],
   };
 
   const proposalNoVotes = {
@@ -54,124 +48,17 @@ describe('<ProposalPage />', () => {
   };
 
   it('should render a proposal page', () => {
-    const wrapper = shallow(
-      <ProposalPage {...props} proposal={proposalNoVotes} />,
-    );
+    const wrapper = shallow(<ProposalPage {...props} proposal={proposalNoVotes} />);
     expect(wrapper).toMatchSnapshot();
-
-    const alert = wrapper.find('ProposalPageAlert');
-    expect(alert).toHaveLength(1);
-    expect(alert.prop('proposal')).toEqual(proposalNoVotes);
-
-    const header = wrapper.find('Connect(ProposalPageHeader)');
-    expect(header.props()).toEqual({
-      proposal: proposalNoVotes,
-      className: 'container container--custom',
-    });
-
-    const tabContainer = wrapper.find(Tab.Container);
-    expect(tabContainer).toHaveLength(2);
-    expect(tabContainer.first().props()).toMatchObject({
-      id: 'proposal-page-tabs',
-      defaultActiveKey: 'content',
-      className: 'container--custom',
-    });
-
-    const tabsPills = tabContainer.first().find('div.tabs__pills');
-    expect(tabsPills).toHaveLength(1);
-    expect(tabsPills.find('div.container')).toHaveLength(1);
-    const nav = tabsPills.find(Nav);
-    expect(nav.prop('bsStyle')).toEqual('pills');
-    expect(nav.prop('style')).toEqual({ display: 'inline-block' });
-    const navItems = nav.find(NavItem);
-    expect(navItems).toHaveLength(4);
-    const contentItem = navItems.first();
-    expect(contentItem.prop('eventKey')).toEqual('content');
-    expect(contentItem.prop('className')).toEqual('tabs__pill');
-    const commentsItem = navItems.at(1);
-    expect(commentsItem.prop('eventKey')).toEqual('comments');
-    expect(commentsItem.prop('className')).toEqual('tabs__pill');
-    expect(commentsItem.find('.badge').text()).toEqual(
-      `${proposalNoVotes.comments_count}`,
-    );
-    const voteButtonWrapper = tabsPills.find(
-      'Connect(ProposalVoteButtonWrapper)',
-    );
-    expect(voteButtonWrapper).toHaveLength(1);
-    expect(voteButtonWrapper.props()).toMatchObject({
-      proposal: proposalNoVotes,
-      className: 'pull-right hidden-xs btn-lg',
-    });
-    expect(voteButtonWrapper.prop('style')).toEqual({ marginTop: '10px' });
-    const tabContent = tabContainer.first().find(Tab.Content);
-    expect(tabContent).toHaveLength(2);
-    const tabPanes = tabContent.first().find(Tab.Pane);
-    expect(tabPanes).toHaveLength(4);
-    const contentTabPane = tabPanes.first();
-    expect(contentTabPane.prop('eventKey')).toEqual('content');
-    const proposalContent = contentTabPane.find('Connect(ProposalPageContent)');
-    expect(proposalContent).toHaveLength(1);
-    expect(proposalContent.props()).toMatchObject({
-      proposal: proposalNoVotes,
-      form: props.form,
-      categories: props.categories,
-    });
-    const proposalMetadata = wrapper.find('ProposalPageMetadata');
-    expect(proposalMetadata.props()).toEqual({
-      proposal: proposalNoVotes,
-      showDistricts: false,
-      showCategories: false,
-      showNullEstimation: false,
-      showThemes: true,
-    });
-
-    const proposalAdvancement = contentTabPane.find('ProposalPageAdvancement');
-
-    expect(proposalAdvancement.props()).toEqual({ proposal: proposalNoVotes });
-    const commentsTabPane = tabPanes.at(1);
-    expect(commentsTabPane.props()).toMatchObject({ eventKey: 'comments' });
-
-    const proposalComments = commentsTabPane.find('ProposalPageComments');
-    expect(proposalComments.prop('form')).toEqual(props.form);
-    expect(proposalComments.prop('id')).toEqual(proposalNoVotes.id);
-
-    const proposalBlog = wrapper.find('Connect(ProposalPageBlog)');
-    expect(proposalBlog.props()).toEqual({});
   });
 
   it('should render a vote tab and a vote modal if votable step is specified', () => {
-    const wrapper = shallow(
-      <ProposalPage {...props} proposal={proposalWithVotes} />,
-    );
+    const wrapper = shallow(<ProposalPage {...props} proposal={proposalWithVotes} />);
     expect(wrapper).toMatchSnapshot();
-
-    const tabContainer = wrapper.find(Tab.Container);
-    const tabsPills = tabContainer.find('div.tabs__pills');
-    const nav = tabsPills.find(Nav);
-    const navItems = nav.find(NavItem);
-    expect(navItems).toHaveLength(4);
-    const votesItem = navItems.at(2);
-    expect(votesItem.prop('eventKey')).toEqual('votes');
-    expect(votesItem.prop('className')).toEqual('tabs__pill');
-    expect(votesItem.find('.badge').text()).toEqual(
-      `${proposalWithVotes.votesCountByStepId[2]}`,
-    );
-    const tabContent = tabContainer.find(Tab.Content);
-    const tabPanes = tabContent.find(Tab.Pane);
-    expect(tabPanes).toHaveLength(4);
-    const votesTabPane = tabPanes.at(2);
-    expect(votesTabPane.prop('eventKey')).toEqual('votes');
-    const proposalVoteModal = tabContainer.find('Connect(ProposalVoteModal)');
-    expect(proposalVoteModal).toHaveLength(1);
-    expect(proposalVoteModal.props()).toEqual({
-      proposal: proposalWithVotes,
-    });
   });
 
   it('should not render a vote modal if proposal has no votabledStep', () => {
-    const wrapper = shallow(
-      <ProposalPage {...props} proposal={proposalWithoutVotableStep} />,
-    );
+    const wrapper = shallow(<ProposalPage {...props} proposal={proposalWithoutVotableStep} />);
     const tabContainer = wrapper.find(Tab.Container);
     const proposalVoteModal = tabContainer.find('Connect(ProposalVoteModal)');
     expect(proposalVoteModal).toHaveLength(0);
