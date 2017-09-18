68538e251f9ec13f490fbf0da0059daab8f417ab
spyl94 <spyl94@gmail.com>
2017-09-18 11:05:34 +0200

    Merge pull request #4358 from cap-collectif/some-fixes-for-proposal-bo
    Some fixes for proposal BO
    
    GitOrigin-RevId: 5767ecd39a92cda93fb872a3ddb4f27fd1c989c7

--- src/Capco/AppBundle/Resolver/SearchResolver.php
+++ src/Capco/AppBundle/Resolver/SearchResolver.php
@@ -270,7 +270,6 @@ class SearchResolver
 
         $shouldQuery = new MultiMatch();
         $shouldQuery->setQuery($term);
-        $shouldQuery->setType('phrase_prefix');
         $shouldQuery->setFields([
             'title', 'title.std',
             'body', 'body.std',
