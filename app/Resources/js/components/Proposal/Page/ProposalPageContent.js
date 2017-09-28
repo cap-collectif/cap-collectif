55b522248d89e46c738a48d60a4ee04a1f9c63f4
spyl94 <spyl94@gmail.com>
2017-09-28 18:51:30 +0200

    Merge pull request #4443 from cap-collectif/feat/zoom-leaflet-map
    Map zoom on proposal page content
    
    GitOrigin-RevId: 20a984dfb20421153d3b747eaee4bfa62d31a300

--- app/Resources/js/components/Proposal/Page/ProposalPageContent.js
+++ app/Resources/js/components/Proposal/Page/ProposalPageContent.js
@@ -74,7 +74,7 @@ export const ProposalPageContent = React.createClass({
                   lat: address[0].geometry.location.lat,
                   lng: address[0].geometry.location.lng,
                 }}
-                zoom={13}
+                zoom={16}
                 maxZoom={18}
                 style={{
                   width: '100%',
