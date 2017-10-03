26f7647ea6adf79f7978d3e44ee572aacb014c32
spyl94 <spyl94@gmail.com>
2017-10-03 18:08:03 +0200

    Merge pull request #4471 from cap-collectif/feat-4456-improve-reference
    #4456 update reference in preflush of doctrine event
    
    GitOrigin-RevId: c2fb75ecf04e9ad06259807afb471b136e27a892

--- app/Resources/js/components/Form/Radio.js
+++ app/Resources/js/components/Form/Radio.js
@@ -84,7 +84,7 @@ const Radio = React.createClass({
                 name={fieldName}
                 type="radio"
                 value={choice.label}
-                description={choice.description}
+                help={choice.description}
                 disabled={disabled}
                 image={choice.image ? choice.image.url : null}>
                 {choice.label}
