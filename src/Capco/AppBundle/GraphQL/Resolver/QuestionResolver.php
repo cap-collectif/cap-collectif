c5cd909b8cd68beffcc5e8def3a205ad1cdb0fa7
spyl94 <spyl94@gmail.com>
2017-10-04 16:35:59 +0200

    Merge pull request #4470 from cap-collectif/feat-4452-add-filter
    Remove id filter in proposals list
    
    GitOrigin-RevId: fa91ccdcf0e66fecf09eb777f9d24fe7d0bd5395

--- src/Capco/AppBundle/GraphQL/Resolver/QuestionResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/QuestionResolver.php
@@ -2,9 +2,7 @@
 
 namespace Capco\AppBundle\GraphQL\Resolver;
 
-use Capco\AppBundle\Entity\ProposalForm;
 use Capco\AppBundle\Entity\Questions\AbstractQuestion;
-use Capco\AppBundle\Helper\GeometryHelper;
 use PhpParser\Node\Arg;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
@@ -21,15 +19,6 @@ class QuestionResolver implements ContainerAwareInterface
     public function resolve(Arg $args): AbstractQuestion
     {
         return $this->container->get('capco.abstract_question.repository')->find($args['id']);
-    }
-
-    public function resolveDistrictsForLocalisation(string $proposalFormId, float $latitude, float $longitude): array
-    {
-        $form = $this->container->get('capco.proposal_form.repository')->find($proposalFormId);
-        $districts = $form->getDistricts();
-        return $form->isProposalInAZoneRequired() ? $districts->filter(function ($district) use ($longitude, $latitude) {
-            return $district->getGeojson() && GeometryHelper::isIncluded($longitude, $latitude, $district->getGeojson());
-        }, [])->toArray() : $districts->toArray();
     }
 
     public function resolvePosition(AbstractQuestion $question): int
