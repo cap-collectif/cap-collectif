9ba7652d7fc6ac0ae72c9f63a33bcf182cab5c73
jacquelotjeff <jacquelotjeff@gmail.com>
2017-10-05 16:09:24 +0200

    add evaluation form in proposal form #2969
    GitOrigin-RevId: 2e6a4c8dcf4f72b8942ad409f6404a8d40e1135c

--- src/Capco/AppBundle/Entity/Questionnaire.php
+++ src/Capco/AppBundle/Entity/Questionnaire.php
@@ -92,6 +92,11 @@ class Questionnaire
     private $proposalForm;
 
     /**
+     * @ORM\OneToOne(targetEntity="Capco\AppBundle\Entity\ProposalForm", mappedBy="evaluationForm", cascade={"persist"})
+     */
+    private $proposalForm;
+
+    /**
      * Constructor.
      */
     public function __construct()
@@ -392,6 +397,18 @@ class Questionnaire
     public function setAcknowledgeReplies($acknowledgeReplies)
     {
         $this->acknowledgeReplies = $acknowledgeReplies;
+
+        return $this;
+    }
+
+    public function getProposalForm()
+    {
+        return $this->proposalForm;
+    }
+
+    public function setProposalForm($proposalForm): self
+    {
+        $this->proposalForm = $proposalForm;
 
         return $this;
     }
