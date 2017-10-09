d4873fae78b36e3e5c2cd6ba6c13cfbf9ab8b325
spyl94 <spyl94@gmail.com>
2017-10-09 12:28:29 +0200

    No softdelete for ROLE_ADMIN
    GitOrigin-RevId: fb9ec87629841f764477f19cf0d05e5a3bab7afa

--- src/Capco/AdminBundle/EventListener/FilterConfigurator.php
+++ src/Capco/AdminBundle/EventListener/FilterConfigurator.php
@@ -27,7 +27,7 @@ class FilterConfigurator
             $filters = $this->em->getFilters();
             if (
                 $filters->isEnabled('softdeleted') &&
-                $this->tokenStorage->getToken()->getUser()->hasRole('ROLE_SUPER_ADMIN')
+                $this->tokenStorage->getToken()->getUser()->hasRole('ROLE_ADMIN')
             ) {
                 $this->em->getFilters()->disable('softdeleted');
             }
--- src/Capco/AppBundle/EventListener/ReferenceEventListener.php
+++ src/Capco/AppBundle/EventListener/ReferenceEventListener.php
@@ -38,6 +38,10 @@ class ReferenceEventListener
             if (isset($this->lastProposals[$proposalForm->getId()])) {
                 $lastReference = $this->lastProposals[$proposalForm->getId()];
             } else {
+                $filters = $om->getFilters();
+                if ($filters->isEnabled('softdeleted')) {
+                    $om->getFilters()->disable('softdeleted');
+                }
                 $lastReference = $proposalFormRep->getLastProposalReference($proposalForm->getId());
             }
 
