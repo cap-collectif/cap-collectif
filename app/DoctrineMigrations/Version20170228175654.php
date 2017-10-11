65fb3758c9222e7d730de737c7d40ce1d27ecead
spyl94 <spyl94@gmail.com>
2017-10-11 18:00:00 +0200

    Merge branch 'migrate-question-choice-to-uuid' into develop
    GitOrigin-RevId: 6bac6dbcfa53e5f4df44f06fc72e72c2d956f885

--- app/DoctrineMigrations/Version20170228175654.php
+++ app/DoctrineMigrations/Version20170228175654.php
@@ -15,15 +15,6 @@ class Version20170228175654 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        $this->connection->insert('site_parameter', [
-            'keyname' => 'shield.introduction',
-            'category' => 'pages.shield',
-            'value' => '',
-            'position' => 2,
-            'is_enabled' => true,
-            'type' => 1,
-        ]);
-        $this->connection->insert('site_image', ['keyname' => 'image.shield', 'category' => 'pages.shield', 'Media_id' => null, 'is_enabled' => true, 'position' => 1]);
     }
 
     public function down(Schema $schema)
