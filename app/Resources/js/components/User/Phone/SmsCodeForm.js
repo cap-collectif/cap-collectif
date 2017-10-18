33e906a56c4fa3db5b9e7d2130be1d9bf2041a5f
Amelie52 <amelie.tamisier@gmail.com>
2017-10-18 17:18:31 +0200

    Add of invalid code error
    GitOrigin-RevId: 9ae5a981ac8485132085fd6697c306295205a146

--- app/Resources/js/components/User/Phone/SmsCodeForm.js
+++ app/Resources/js/components/User/Phone/SmsCodeForm.js
@@ -1,12 +1,11 @@
+// @flow
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { reduxForm, Field, SubmissionError } from 'redux-form';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import renderComponent from '../../Form/Field';
-// import Fetcher from '../../../services/Fetcher';
 import UserActions from '../../../actions/UserActions';
-// import FlashMessages from '../../Utils/FlashMessages';
 
 type Props = {
   onSubmitSuccess: Function,
@@ -17,15 +16,13 @@ type Props = {
 const onSubmit = (values, dispatch, props) => {
   const { onSubmitSuccess } = props;
 
-  console.warn(values.code);
-
-  UserActions.sendSmsCode(values)
+  return UserActions.sendSmsCode(values)
     .then(() => {
       onSubmitSuccess();
     })
     .catch(error => {
       if (error && error.response && error.response.message === 'sms_code_invalid') {
-        throw new SubmissionError({ email: 'phone.confirm.code_invalid' });
+        throw new SubmissionError({ code: 'phone.confirm.code_invalid' });
       }
     });
 };
@@ -43,36 +40,6 @@ const validate = ({ code }: Object) => {
 export const formName = 'SmsCodeForm';
 
 export class SmsCodeForm extends React.Component<Props> {
-  // handleSubmit(e) {
-  //   const { onSubmitSuccess } = this.props;
-  //   e.preventDefault();
-  //   if (this.isValid()) {
-  //     const form = JSON.parse(JSON.stringify(this.state.form));
-  //     this.setState({ isSubmitting: true });
-  //     UserActions.sendSmsCode(form)
-  //       .then(() => {
-  //         this.setState(this.getInitialState());
-  //         onSubmitSuccess();
-  //       })
-  //       .catch(error => {
-  //         const response = error.response;
-  //         const errors = this.state.errors;
-  //         if (response.message === 'sms_code_invalid') {
-  //           errors.code = ['phone.confirm.code_invalid'];
-  //           this.setState({ errors, isSubmitting: false });
-  //         }
-  //       });
-  //   }
-  // }
-
-  // renderFormErrors(field) {
-  //   const errors = this.getErrorsMessages(field);
-  //   if (errors.length === 0) {
-  //     return null;
-  //   }
-  //   return <FlashMessages errors={errors} form />;
-  // }
-
   render() {
     const { submitting, handleSubmit } = this.props;
 
