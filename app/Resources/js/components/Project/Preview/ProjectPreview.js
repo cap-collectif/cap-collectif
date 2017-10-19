b56111f94851d35a5603f149459d67a27a784d65
Amelie52 <amelie.tamisier@gmail.com>
2017-10-19 18:06:21 +0200

    Remove of counter when project has no participative step
    GitOrigin-RevId: 88daf951d2e48e706613b09a9fa15acbf68ca60c

--- app/Resources/js/components/Project/Preview/ProjectPreview.js
+++ app/Resources/js/components/Project/Preview/ProjectPreview.js
@@ -1,29 +1,34 @@
 // @flow
 import * as React from 'react';
+// import { connect } from 'react-redux';
 import { Col } from 'react-bootstrap';
 import ProjectType from './ProjectType';
 import ProjectCover from './ProjectCover';
 import ProjectPreviewBody from './ProjectPreviewBody';
 import ProjectPreviewCounters from './ProjectPreviewCounters';
 
-const ProjectPreview = React.createClass({
-  propTypes: {
-    project: React.PropTypes.object.isRequired,
-  },
+type Props = {
+  project: Object,
+  hasNotParticipativeSteps: boolean,
+};
 
+class ProjectPreview extends React.Component<Props> {
   render() {
-    const { project } = this.props;
+    const { project, hasNotParticipativeSteps } = this.props;
+
     return (
       <Col xs={12} sm={6} md={4} lg={3}>
         <div className="thumbnail  thumbnail--custom  block  block--bordered">
           {project.projectType && <ProjectType project={project} />}
           <ProjectCover project={project} />
           <ProjectPreviewBody project={project} />
-          <ProjectPreviewCounters project={project} />
+          {project.hasParticipativeStep && <ProjectPreviewCounters project={project} />}
+          {!hasNotParticipativeSteps &&
+            !project.hasParticipativeStep && <div style={{ height: '55px' }}> </div>}
         </div>
       </Col>
     );
-  },
-});
+  }
+}
 
 export default ProjectPreview;
