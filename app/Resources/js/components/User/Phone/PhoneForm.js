e9b55033c44c3ecb879bd879065f2e2242c1cb89
Amelie52 <amelie.tamisier@gmail.com>
2017-10-20 16:42:47 +0200

    Remove of mixin + add of es6 + add of redux form
    remove of flow error
    
    Add of normalize for phone field
    
    change position of button and action on phonebox to phoneform
    
    GitOrigin-RevId: d9f47eff1ec536bef661311dbac065e132a37517

--- app/Resources/js/components/User/Phone/PhoneForm.js
+++ app/Resources/js/components/User/Phone/PhoneForm.js
@@ -1,125 +1,218 @@
-import React, { PropTypes } from 'react';
-import { FormattedMessage } from 'react-intl';
-import FlashMessages from '../../Utils/FlashMessages';
+// flow to add
+import * as React from 'react';
+import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
+import { reduxForm, Field, SubmissionError } from 'redux-form';
+import { Button, Panel, Alert } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import AppDispatcher from '../../../dispatchers/AppDispatcher';
+import { UPDATE_ALERT } from '../../../constants/AlertConstants';
+import renderComponent from '../../Form/Field';
 import UserActions from '../../../actions/UserActions';
-import DeepLinkStateMixin from '../../../utils/DeepLinkStateMixin';
-import Input from '../../Form/Input';
-import FormMixin from '../../../utils/FormMixin';
+import type { Dispatch } from '../../../types';
 
-const PhoneForm = React.createClass({
-  propTypes: {
-    isSubmitting: PropTypes.bool.isRequired,
-    onSubmit: PropTypes.func.isRequired,
-    onSubmitSuccess: PropTypes.func.isRequired,
-    onSubmitFailure: PropTypes.func.isRequired,
-    initialValue: PropTypes.string,
-  },
+type Props = {
+  user: Object,
+  initialValue: ?string, // n° ou null
+  pristine: boolean,
+  submitting: boolean,
+  handleSubmit: Function,
+};
 
-  mixins: [DeepLinkStateMixin, FormMixin],
+type State = {
+  alert: boolean,
+};
 
-  getDefaultProps() {
-    return {
-      initialValue: null,
-    };
-  },
+const onSubmit = (values: { phone: string }, dispatch: Dispatch, state: State, props: Props) => {
+  const { onSubmitSuccess, onSubmitFailure } = props;
 
-  getInitialState() {
-    const { initialValue } = this.props;
-    return {
-      form: {
-        phone: initialValue || '',
-      },
-      errors: {
-        phone: [],
-      },
-    };
-  },
-
-  componentWillReceiveProps(nextProps) {
-    const { onSubmitFailure, onSubmitSuccess } = this.props;
-    if (nextProps.isSubmitting) {
-      const form = JSON.parse(JSON.stringify(this.state.form));
-      form.phone = form.phone.replace(/((?![0-9]).)/g, '');
-      form.phone = `+33${form.phone.charAt(0) === '0' ? form.phone.substring(1) : form.phone}`;
-      UserActions.update(form)
+  return UserActions.update(values)
+    .then(() => {
+      UserActions.sendConfirmSms()
         .then(() => {
-          UserActions.sendConfirmSms()
-            .then(() => {
-              onSubmitSuccess(form.phone);
-              this.setState(this.getInitialState());
-            })
-            .catch(err => {
-              const errors = this.state.errors;
-              if (err.response.message === 'sms_failed_to_send') {
-                errors.phone = ['phone.confirm.alert.failed_to_send'];
-                this.setState({ errors });
-              } else if (err.response.message === 'sms_already_sent_recently') {
-                errors.phone = ['phone.confirm.alert.wait_for_new'];
-                this.setState({ errors });
-              } else {
-                this.setState(this.getInitialState());
-              }
-              onSubmitFailure();
-            });
+          onSubmitSuccess(values); // à remplacer pas un dispatch(() => {smsSentToNumberAction})
         })
         .catch(error => {
-          const response = error.response;
-          if (response.errors) {
-            const errors = this.state.errors;
-            if (
-              response.errors.children.phone.errors &&
-              response.errors.children.phone.errors.length > 0
-            ) {
-              if (response.errors.children.phone.errors[0] === 'already_used_phone') {
-                errors.phone = ['profile.constraints.phone.already_used'];
-              } else {
-                errors.phone = ['profile.constraints.phone.invalid'];
-              }
-            }
-            this.setState({ errors });
+          if (error.response.message === 'sms_failed_to_send') {
+            throw new SubmissionError({ phone: 'phone.confirm.alert.failed_to_send' });
+          }
+          if (error.response.message === 'sms_already_sent_recently') {
+            throw new SubmissionError({ phone: 'phone.confirm.alert.wait_for_new' });
           }
           onSubmitFailure();
         });
-    }
-  },
+    })
+    .catch(error => {
+      if (
+        error.response.errors.children.phone.errors &&
+        error.response.errors.children.phone.errors.length > 0
+      ) {
+        if (error.response.errors.children.phone.errors[0] === 'already_used_phone') {
+          throw new SubmissionError({ phone: 'profile.constraints.phone.already_used' });
+        } else {
+          throw new SubmissionError({ phone: 'profile.constraints.phone.invalid' });
+        }
+      }
+      onSubmitFailure();
+    });
+};
 
-  formValidationRules: {
-    phone: {
-      notBlank: { message: 'global.constraints.notBlank' },
-    },
-  },
+const validate = ({ phone }: Object) => {
+  const errors = {};
 
-  renderFormErrors(field) {
-    const errors = this.getErrorsMessages(field);
-    if (errors.length === 0) {
-      return null;
-    }
-    return <FlashMessages errors={errors} form />;
-  },
+  if (!phone || phone.length === 0) {
+    errors.phone = 'global.constraints.notBlank';
+  }
+
+  return errors;
+};
+
+const normalizePhone = (values: { phone: string }) => {
+  const onlyNums = values.replace(/[^\d]/g, '');
+
+  if (values.length > 9) {
+    return onlyNums.slice(0, 9);
+  }
+
+  return onlyNums;
+};
+
+export const formName = 'PhoneForm';
+
+export class PhoneForm extends React.Component<Props> {
+  resendSmsCode(e) {
+    e.preventDefault();
+    UserActions.sendConfirmSms()
+      .then(() => {
+        AppDispatcher.dispatch({
+          actionType: UPDATE_ALERT,
+          alert: {
+            bsStyle: 'success',
+            content: 'phone.confirm.alert.received',
+          },
+        });
+      })
+      .catch(err => {
+        let message = err.response.message;
+        if (message === 'sms_already_sent_recently') {
+          message = <FormattedMessage id="phone.confirm.alert.wait_for_new" />;
+        }
+        if (message === 'sms_failed_to_send') {
+          message = <FormattedMessage id="phone.confirm.alert.failed_to_send" />;
+        }
+        this.setState({
+          alert: { type: 'danger', message },
+        });
+      });
+  }
+
+  deletePhone(e) {
+    e.preventDefault();
+    UserActions.update({ phone: null }).then(() => {
+      AppDispatcher.dispatch({
+        actionType: UPDATE_ALERT,
+        alert: { bsStyle: 'success', content: 'alert.success.delete.phone' },
+      });
+    });
+    // this.setState({ isUpdating: true });
+  }
+
+  handleAlertDismiss() {
+    this.setState({ alert: null });
+  }
 
   render() {
-    const { initialValue, onSubmit } = this.props;
-    return (
-      <form
-        style={{ maxWidth: '350px' }}
-        onSubmit={e => {
-          e.preventDefault();
-          onSubmit();
-        }}>
-        <Input
-          type="text"
-          addonBefore="+33"
-          autoFocus
-          valueLink={this.linkState('form.phone')}
-          id="_phone"
-          disabled={this.state.form.phone === initialValue}
-          label={<FormattedMessage id="global.phone" />}
-          groupClassName={this.getGroupStyle('phone')}
-          errors={this.renderFormErrors('phone')}
-        />
-      </form>
+    const { handleSubmit, submitting, pristine, user } = this.props;
+    const { alert } = this.state;
+
+    const header = user.isPhoneConfirm ? (
+      <FormattedMessage id="phone.confirm.check_your_phone" />
+    ) : (
+      <FormattedMessage id="phone.confirm.phone" />
     );
+
+    let footer; // header et footer à faire passer dans le composant parent
+
+    // condition pour sms déjà envoyé
+
+    if (user) {
+      footer = (
+        <span>
+          <Button
+            style={{ paddingLeft: 0, paddingRight: 0 }}
+            onClick={this.resendSmsCode}
+            bsStyle="link">
+            {<FormattedMessage id="phone.confirm.ask_new" />}
+          </Button>
+          {' • '}
+          <Button style={{ paddingLeft: 0 }} onClick={this.askChangeNumber} bsStyle="link">
+            {<FormattedMessage id="phone.confirm.ask_change_number" />}
+          </Button>
+        </span>
+      );
+    } else {
+      footer = (
+        <Button
+          id="confirm-continue"
+          onClick={this.handleSubmit}
+          type="submit"
+          form={formName}
+          disabled={submitting || user.isPhoneConfirmed} // (user.isPhoneConfirmed && !isUpdating)
+          bsStyle="primary">
+          {submitting ? (
+            <FormattedMessage id="global.loading" />
+          ) : (
+            <FormattedMessage id="global.continue" />
+          )}
+        </Button>
+      );
+    }
+
+    return (
+      <Panel header={header} footer={footer}>
+        {alert && (
+          <Alert bsStyle={alert.type} onDismiss={this.handleAlertDismiss} dismissAfter={2000}>
+            {alert.message}
+          </Alert>
+        )}
+        {!user.phone && <FormattedHTMLMessage id="phone.confirm.infos" />}
+        <form style={{ maxWidth: '350px' }} onSubmit={handleSubmit}>
+          <Field
+            type="text"
+            addonBefore="+33"
+            component={renderComponent}
+            autoFocus
+            name="phone"
+            id="_phone"
+            normalize={normalizePhone}
+            props={{ disabled: !!pristine }}
+            label={<FormattedMessage id="global.phone" />}
+          />
+        </form>
+        {user.isPhoneConfirmed && ( // !isUpdating &&
+          <span style={{ color: '#57AD68' }}>
+            {<FormattedMessage id="phone.confirm.ok" />}
+            {' - '}
+            <Button style={{ paddingLeft: 0 }} onClick={this.deletePhone} bsStyle="link">
+              {<FormattedMessage id="phone.ask_delete" />}
+            </Button>
+          </span>
+        )}
+      </Panel>
+    );
+  }
+}
+
+const mapStateToProps = (state, props: Props) => ({
+  initialValues: {
+    phone: props.initialValue,
   },
+  // smsSentToNumber: state.yolo.smsSentToNumber,
 });
 
-export default PhoneForm;
+export default connect(mapStateToProps)(
+  reduxForm({
+    validate,
+    onSubmit,
+    form: formName,
+  })(PhoneForm),
+);
