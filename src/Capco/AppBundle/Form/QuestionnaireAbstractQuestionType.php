04ba988dfc5679266d7c968557e1e912835f0ead
ptondereau <pierre.tondereau@protonmail.com>
2017-10-23 11:15:18 +0200

    fix tests
    GitOrigin-RevId: 92345c3ff09bdb75d720b1c269f6e300504216ad

--- src/Capco/AppBundle/Form/QuestionnaireAbstractQuestionType.php
+++ src/Capco/AppBundle/Form/QuestionnaireAbstractQuestionType.php
@@ -6,16 +6,23 @@ use Capco\AppBundle\Entity\Questions\QuestionnaireAbstractQuestion;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class QuestionnaireAbstractQuestionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('position', IntegerType::class)
-            ->add('question', SimpleQuestionType::class)
-        ;
+        $builder->add('position', IntegerType::class);
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            if (!$data = $event->getData()) {
+                return;
+            }
+            $event->getForm()
+                    ->add('question', 7 === (int) $data['question']['type'] ? MediaQuestionType::class : SimpleQuestionType::class);
+        }, 999);
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Capco/AppBundle/GraphQL/Resolver/QuestionResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/QuestionResolver.php
@@ -3,8 +3,11 @@
 namespace Capco\AppBundle\GraphQL\Resolver;
 
 use Capco\AppBundle\Entity\Questions\AbstractQuestion;
+use Capco\AppBundle\Entity\Questions\MediaQuestion;
 use Capco\AppBundle\Entity\Questions\MultipleChoiceQuestion;
+use Capco\AppBundle\Entity\Questions\SimpleQuestion;
 use Capco\AppBundle\Helper\GeometryHelper;
+use Overblog\GraphQLBundle\Error\UserError;
 use PhpParser\Node\Arg;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
@@ -15,7 +18,7 @@ class QuestionResolver implements ContainerAwareInterface
 
     public function resolveType(AbstractQuestion $question): string
     {
-        return $question->getInputType();
+        return $question->getType();
     }
 
     public function resolve(Arg $args): AbstractQuestion
@@ -70,5 +73,21 @@ class QuestionResolver implements ContainerAwareInterface
         return $this->container
             ->get('capco.questionnaire.repository')
             ->getAvailableQuestionnaires();
+    }
+
+    public function resolveQuestionType(AbstractQuestion $question): string
+    {
+        $typeResolver = $this->container->get('overblog_graphql.type_resolver');
+        if ($question instanceof SimpleQuestion) {
+            return $typeResolver->resolve('SimpleQuestion');
+        }
+        if ($question instanceof MediaQuestion) {
+            return $typeResolver->resolve('MediaQuestion');
+        }
+        if ($question instanceof MultipleChoiceQuestion) {
+            return $typeResolver->resolve('MultipleChoiceQuestion');
+        }
+
+        throw new UserError('Could not resolve type of Question.');
     }
 }
