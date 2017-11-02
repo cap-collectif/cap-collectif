b6d6129add9c07c5a2d9f9ea0dcf348e74f7c787
spyl94 <spyl94@gmail.com>
2017-11-02 18:58:54 +0100

    Merge pull request #4667 from cap-collectif/fix-slug-opinion-list
    Fix slug for opinion list
    
    GitOrigin-RevId: 14bb2705ddfc9adb5e2f104a0dfd6e6322781d96

--- src/Capco/UserBundle/Controller/ChangePasswordFOSUser1Controller.php
+++ src/Capco/UserBundle/Controller/ChangePasswordFOSUser1Controller.php
@@ -2,11 +2,13 @@
 
 namespace Capco\UserBundle\Controller;
 
+use FOS\UserBundle\Model\UserInterface;
 use Sonata\UserBundle\Controller\ChangePasswordFOSUser1Controller as BaseController;
 
-/**
- * Class ChangePasswordFOSUser1Controller.
- */
 class ChangePasswordFOSUser1Controller extends BaseController
 {
+    protected function getRedirectionUrl(UserInterface $user)
+    {
+        return $this->generateUrl('sonata_user_profile_show');
+    }
 }
