9a8196e40688e597331e995048a0e2788ca3e7a6
ptondereau <pierre.tondereau@protonmail.com>
2017-11-17 17:27:58 +0100

    Merge pull request #4745 from cap-collectif/4740-fix-autocomplete-district
    4740 fix autocomplete district
    
    GitOrigin-RevId: 60277bf377a6e8f56df543a8e8acbd7fb1ee9165

--- app/Resources/js/components/Proposal/Form/ProposalForm.js
+++ app/Resources/js/components/Proposal/Form/ProposalForm.js
@@ -250,6 +250,7 @@ export const ProposalForm = React.createClass({
   },
 
   handleAddressChange(address) {
+    const { form } = this.props;
     geocodeByAddress(address)
       .then(results => {
         this.setState(prevState => ({
@@ -260,7 +261,9 @@ export const ProposalForm = React.createClass({
           address: results[0].formatted_address,
           errors: { ...prevState.errors, address: [] },
         }));
-        this.retrieveDistrictForLocation(results[0].geometry.location);
+        if (form.proposalInAZoneRequired) {
+          this.retrieveDistrictForLocation(results[0].geometry.location);
+        }
       })
       .catch(error => {
         this.resetAddressField();
