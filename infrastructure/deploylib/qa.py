99baf71acd68f31f0082028d1a6630c1d0c990f9
spyl94 <spyl94@gmail.com>
2017-11-23 14:19:06 +0100

    Fix
    GitOrigin-RevId: 2ef1f57553ac808b28a25a4165f761150c018369

--- infrastructure/deploylib/qa.py
+++ infrastructure/deploylib/qa.py
@@ -57,7 +57,7 @@ def jest():
 @task(environments=['ci'])
 def perf():
     "Run perf Tests"
-    env.compose('run -e CI=true CIRCLECI CIRCLE_PROJECT_USERNAME CIRCLE_PROJECT_REPONAME CIRCLE_SHA1 CIRCLE_BRANCH builder yarn run bundlesize')
+    env.compose('run -e CI=true -e CIRCLECI -e CIRCLE_PROJECT_USERNAME -e CIRCLE_PROJECT_REPONAME -e CIRCLE_SHA1 -e CIRCLE_BRANCH builder yarn run bundlesize')
 
 
 @task(environments=['local', 'ci'])
