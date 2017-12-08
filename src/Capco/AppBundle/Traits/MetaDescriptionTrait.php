5910125495f7da841d3ffeeeeb365314cf1fd23e
Liinkiing <omar.jbara2@gmail.com>
2017-12-08 12:37:18 +0100

    - Added Length constraint to meta description - Modified migration
    GitOrigin-RevId: bf83a9dd04ca5a05ef279aa818c65309763feedf

--- src/Capco/AppBundle/Traits/MetaDescriptionTrait.php
+++ src/Capco/AppBundle/Traits/MetaDescriptionTrait.php
@@ -3,11 +3,14 @@
 namespace Capco\AppBundle\Traits;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 trait MetaDescriptionTrait
 {
     /**
-     * @ORM\Column(name="meta_description", type="text", nullable=true)
+     * @ORM\Column(name="meta_description", type="string", nullable=true, length=160)
+     * @Assert\Length(max=160,
+     *     maxMessage="argument.metadescription.max_length")
      */
     private $metaDescription;
 
