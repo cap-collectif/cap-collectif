cd35437ac4b98d09cba670f8d1fef73ccfe11eed
spyl94 <spyl94@gmail.com>
2017-12-12 17:28:18 +0100

    Fix cs
    GitOrigin-RevId: d962e4e7717863587e2924e664476655863f45b8

--- src/Capco/AppBundle/Controller/Api/MediasController.php
+++ src/Capco/AppBundle/Controller/Api/MediasController.php
@@ -2,28 +2,11 @@
 
 namespace Capco\AppBundle\Controller\Api;
 
-use Capco\AppBundle\CapcoAppBundleEvents;
-use Capco\AppBundle\Entity\Idea;
-use Capco\AppBundle\Entity\IdeaComment;
-use Capco\AppBundle\Entity\IdeaVote;
-use Capco\AppBundle\Entity\Reporting;
-use Capco\AppBundle\Event\CommentChangedEvent;
-use Capco\AppBundle\Form\CommentType;
-use Capco\AppBundle\Form\IdeaType;
-use Capco\AppBundle\Form\IdeaVoteType;
-use Capco\AppBundle\Form\ReportingType;
-use FOS\RestBundle\Controller\Annotations\Delete;
-use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Post;
-use FOS\RestBundle\Controller\Annotations\QueryParam;
 use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\FOSRestController;
-use FOS\RestBundle\Request\ParamFetcherInterface;
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class MediasController extends FOSRestController
 {
@@ -39,7 +22,7 @@ class MediasController extends FOSRestController
         $em = $this->get('doctrine.orm.entity_manager');
 
         if (!$uploadedMedia) {
-          return;
+            return;
         }
 
         $media = $mediaManager->createFileFromUploadedFile($uploadedMedia);
