df79d3f217a27ab882af519bfe33a6dbb94e09d9
Amelie52 <amelie.tamisier@gmail.com>
2017-12-20 17:01:43 +0100

    Use of other component for valid suppression
    GitOrigin-RevId: 2aee898b9ba406a98d3ac1b3717951efc778a168

--- app/Resources/js/components/Group/Admin/GroupAdminUsers.js
+++ app/Resources/js/components/Group/Admin/GroupAdminUsers.js
@@ -14,6 +14,7 @@ import {
 } from 'redux-form';
 import type { GroupAdminUsers_group } from './__generated__/GroupAdminUsers_group.graphql';
 import AlertForm from '../../Alert/AlertForm';
+import AlertFormSucceededMessage from '../../Alert/AlertFormSucceededMessage';
 import GroupAdminUsersListGroupItem from './GroupAdminUsersListGroupItem';
 import GroupAdminModalAddUsers from './GroupAdminModalAddUsers';
 
@@ -50,7 +51,9 @@ export class GroupAdminUsers extends React.Component<Props, State> {
 
     if (userIsDeleted) {
       return (
-        <AlertForm valid invalid={false} submitSucceeded submitFailed={false} submitting={false} />
+        <div className="d-ib">
+          <AlertFormSucceededMessage />
+        </div>
       );
     }
 
