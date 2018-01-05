0218909650ac8a97f2d3fc13ee735fd1fdd8797d
Amelie52 <amelie.tamisier@gmail.com>
2018-01-05 16:10:37 +0100

    Add of left scroll
    GitOrigin-RevId: 3f5f1ecd0fea8809bb1cb0f606137b5e2497f513

--- app/Resources/js/components/Project/ProjectStepTabs.js
+++ app/Resources/js/components/Project/ProjectStepTabs.js
@@ -8,43 +8,87 @@ type Props = {
   currentStepId: string
 };
 
-export class ProjectStepTabs extends React.Component<Props> {
+type State = {
+  translateX: number,
+  showArrowRight: boolean,
+  showArrowLeft: boolean,
+};
+
+export class ProjectStepTabs extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      translateX: 0,
+      showArrowRight: false,
+      showArrowLeft: false,
+    };
+  }
+
   componentDidMount = () => {
     const stepTabsBar = document.getElementById('step-tabs__list');
-    const allTabs = stepTabsBar.getElementsByTagName('li');
-    const activeTab = stepTabsBar.getElementsByClassName('active')[0];
+    const activeTab = stepTabsBar && stepTabsBar.getElementsByClassName('active')[0];
+    // const stepScrollNav = document.getElementById('step-tabs__scroll-nav');
 
-    const getBoundingBar = stepTabsBar.getBoundingClientRect();
-    const getBoundingActiveTab = activeTab.getBoundingClientRect();
-    const barLeft = getBoundingBar.left;
-    const barRight = getBoundingBar.right;
-    const activeTabLeft = getBoundingActiveTab.left;
-    const navWidth = stepTabsBar.offsetWidth;
-    const activeTabRight = getBoundingActiveTab.right;
+    const getBoundingBar = stepTabsBar && stepTabsBar.getBoundingClientRect();
+    const getBoundingActiveTab = activeTab && activeTab.getBoundingClientRect();
 
-    console.log(barLeft, barRight, activeTabLeft, activeTabRight);
+    const barLeft = getBoundingBar && getBoundingBar.left;
+    const barRight = getBoundingBar && getBoundingBar.right;
+    const activeTabLeft = getBoundingActiveTab && getBoundingActiveTab.left;
+    const activeTabRight = getBoundingActiveTab && getBoundingActiveTab.right;
 
+
+    console.warn(barLeft, barRight, activeTabLeft, activeTabRight);
+
+    // move right
     if(activeTabLeft < barLeft) {
-      // décaler à droite
+      const diffLeft = (activeTabLeft - barLeft);
+      this.setState({
+        translateX: diffLeft + 22,
+      })
     }
 
+    // move left
     if(activeTabRight > barRight) {
-      // décaler à gauche
+      const diffRight = (barRight - activeTabRight);
+      this.setState({
+        translateX: diffRight - 22,
+      })
     }
 
-
-    let totalTabsWidth = 0;
-    for (let i = 0; i < allTabs.length; i++) {
-      const tabsWidth = allTabs[i].offsetWidth;
-      totalTabsWidth += tabsWidth;
+    // if it doesn't move
+    if(activeTabRight <= barRight && activeTabLeft >= barLeft) {
+      // const diffRight = (barRight - activeTabRight);
+      this.setState({
+        showArrowRight: true,
+      })
     }
-    const widthDiff = totalTabsWidth - navWidth;
-    if (widthDiff > 0) {
-      // console.warn(widthDiff);
+
+    // manque flèche droite, à mettre dans didUpdate ? quand scroll nav droite > bar droite après avoir changé
+
+  };
+
+  componentDidUpdate = (prevProps: Props, preState: State) => {
+    // const {} = this.state;
+    if(preState.translateX === 0 && this.state.translateX !== 0) {
+      if(this.state.translateX < 0) {
+        this.setState({
+          showArrowLeft: true,
+        })
+      }
+
+
+      // marche pas
+      if(this.state.translateX > 0) {
+        this.setState({
+          showArrowRight: true,
+        })
+      }
     }
   };
 
-  getClass = (stepId: string) =>{
+  getClass = (stepId: string) => {
     const { currentStepId } = this.props;
 
     if(currentStepId === stepId) {
@@ -52,25 +96,71 @@ export class ProjectStepTabs extends React.Component<Props> {
     }
   };
 
+  getTranslateRight = () => {
+    console.log("riiiight")
+  };
+
+  getTranslateLeft = () => {
+    const { translateX } = this.state;
+
+    // trouver moyen reprendre const du didmount
+    const stepTabsBar = document.getElementById('step-tabs__list');
+    const stepTabsBarWidth = stepTabsBar.offsetWidth;
+    const getBoundingBar = stepTabsBar && stepTabsBar.getBoundingClientRect();
+    const barLeft = getBoundingBar && getBoundingBar.left;
+    const barRight = getBoundingBar && getBoundingBar.right;
+
+    const stepScrollNav = document.getElementById('step-tabs__scroll-nav');
+    const getBoundingScrollNav = stepScrollNav && stepScrollNav.getBoundingClientRect();
+    const scrollNavLeft = getBoundingScrollNav && getBoundingScrollNav.left;
+    const scrollNavRight = getBoundingScrollNav && getBoundingScrollNav.right;
+
+    console.log(barLeft, barRight, scrollNavLeft, scrollNavRight);
+
+    const diffLeft = barLeft - scrollNavLeft;
+    console.warn(translateX);
+    console.log(diffLeft);
+
+    if(diffLeft < stepTabsBarWidth) {
+      this.setState({
+        translateX: translateX + diffLeft,
+      })
+    }
+
+    if(diffLeft > stepTabsBarWidth) {
+      this.setState({
+        translateX: stepTabsBarWidth,
+      })
+    }
+
+  };
+
   render() {
     const { steps } = this.props;
+    const { translateX, showArrowLeft, showArrowRight } = this.state;
+
+    const test = translateX;
+    const translation = `translateX(${test}px)`;
 
     return (
       <div className="step-tabs">
         <div className="step-tabs__bar container">
-          <div className="step-tabs__tab-prev">
-            <a href="">
-              <i className="cap-arrow-65"></i>
-            </a>
-          </div>
-          <div className="step-tabs__tab-next">
-            <a href="">
-              <i className="cap-arrow-66"></i>
-            </a>
-
-          </div>
+          {showArrowLeft &&
+            <div className="step-tabs__tab-prev">
+              <a onClick={this.getTranslateLeft}>
+                <i className="cap-arrow-65"></i>
+              </a>
+            </div>
+          }
+          {showArrowRight &&
+            <div className="step-tabs__tab-next">
+              <a onClick={this.getTranslateRight}>
+                <i className="cap-arrow-66"></i>
+              </a>
+            </div>
+          }
           <div className="step-tabs__list" id="step-tabs__list">
-            <ul className="nav">
+            <ul className="nav" id="step-tabs__scroll-nav" style={{ transform : translation }}>
               {steps.map((step,key) => (
                 <li className={this.getClass(step.id)}>
                   <a href={step._links.show} className="d-flex">
