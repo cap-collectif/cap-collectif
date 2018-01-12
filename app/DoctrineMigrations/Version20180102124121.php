07c4e5de6466129f0c07232ede9f035cb2863ed2
mauriau <maxime.auriau@protonmail.com>
2018-01-12 18:18:35 +0100

    Update doctrine migration to add step's label
    GitOrigin-RevId: bd1a69d4ae3c30be9ae8433b7e498cfebe5e2905

--- app/DoctrineMigrations/Version20180102124121.php
+++ app/DoctrineMigrations/Version20180102124121.php
@@ -31,9 +31,9 @@ class Version20180102124121 extends AbstractMigration
 
      public function postUp(Schema $schema)
      {
-         $posts = $this->connection->fetchAll('SELECT * from step');
-         foreach ($posts as $post) {
-             $this->connection->update('step', ['label' => $post['title']], ['id' => $post['id']]);
+         $steps = $this->connection->fetchAll('SELECT * from step');
+         foreach ($steps as $step) {
+             $this->connection->update('step', ['label' => $steps['title']], ['id' => $steps['id']]);
          }
      }
 }
