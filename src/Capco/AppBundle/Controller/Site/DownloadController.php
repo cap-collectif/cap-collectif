c5c2b7adbf317be43cb65f97698ad532c33a8ac0
spyl94 <spyl94@gmail.com>
2018-01-16 15:27:51 +0100

    Work on file download
    GitOrigin-RevId: 15b426666256396c21af8a41854f296a4ffb1434

--- src/Capco/AppBundle/Controller/Site/DownloadController.php
+++ src/Capco/AppBundle/Controller/Site/DownloadController.php
@@ -4,14 +4,12 @@ namespace Capco\AppBundle\Controller\Site;
 
 use Capco\AppBundle\Entity\Responses\MediaResponse;
 use Capco\MediaBundle\Entity\Media;
-use Gaufrette\Exception\FileNotFound;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sonata\MediaBundle\Model\MediaInterface;
-use Sonata\MediaBundle\Provider\MediaProviderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DownloadController extends Controller
@@ -20,43 +18,30 @@ class DownloadController extends Controller
      * @Route("/download/{responseId}/media/{mediaId}", name="app_media_response_download")
      * @ParamConverter("mediaResponse", options={"mapping": {"responseId": "id"}})
      * @ParamConverter("media", options={"mapping": {"mediaId": "id"}})
-     * @Cache(smaxage="60", public="true")
      */
-    public function downloadAction(MediaResponse $mediaResponse, Media $media)
+    public function downloadAction(MediaResponse $mediaResponse, Media $media, Request $request)
     {
-        try {
-            $response = $this->getProvider($media)
-              ->getDownloadResponse(
-                  $media,
-                  'reference',
-                  $this->get('sonata.media.pool')
-                      ->getDownloadMode($media)
-              );
-        } catch (FileNotFound $exeption) {
-            $this->get('logger')->error('File not found for media : ' . $media->getId());
-
-            return new Response('File not found.');
-        }
-
-        if ($response instanceof BinaryFileResponse) {
-            $response->prepare($this->get('request'));
-        }
-
         if (
             !$mediaResponse->getQuestion()->isPrivate()
             || $this->getUser() === $mediaResponse->getProposal()->getAuthor()
             || $this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')
         ) {
-            ob_get_clean();
+            $type = $media->getContentType();
+            if ('application/pdf' === $type || 'image/jpeg' === $type || 'image/png' === $type) {
+                $url = $request->getUriForPath('/media') . $this->get('sonata.media.twig.extension')->path($media, 'reference');
+
+                return new RedirectResponse($url);
+            }
+
+            $downloadMode = $this->get('sonata.media.pool')->getDownloadMode($media);
+            $response = $this->getProvider($media)->getDownloadResponse($media, 'reference', $downloadMode);
+            if ($response instanceof BinaryFileResponse) {
+                $response->prepare($this->get('request'));
+            }
 
             return $response;
         }
 
-        throw $this->createAccessDeniedException();
-    }
-
-    protected function getProvider(MediaInterface $media): MediaProviderInterface
-    {
-        return $this->get('sonata.media.pool')->getProvider($media->getProviderName());
+        return new Response('Sorry, you are not allowed to see this file.');
     }
 }
