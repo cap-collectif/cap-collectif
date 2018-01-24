8becc672f7d2c9eb8e39bc199e3cee916d4bcaff
spyl94 <spyl94@gmail.com>
2018-01-24 18:48:59 +0100

    Add Trashed Message
    GitOrigin-RevId: c3fce6b08576e586ef44c51fa240bd706229373a

---
+++ src/Capco/AppBundle/Mailer/Message/TrashedArgumentAuthorMessage.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Capco\AppBundle\Mailer\Message;
+
+use Capco\AppBundle\Entity\Argument;
+
+final class TrashedArgumentAuthorMessage extends Message
+{
+    public static function create(Argument $argument, string $argumentLink): self
+    {
+        return new self(
+            $argument->getAuthor()->getEmail(),
+            $argument->getAuthor()->getUsername(),
+            'notification-subject-argument-trashed',
+            static::getMySubjectVars(
+                $argument->getRelated()->getTitle()
+            ),
+            'notification-argument-trashed',
+            static::getMyTemplateVars(
+                $argument->getTrashedReason(),
+                $argument->getBody(),
+                $argument->getTrashedAt()->format('d/m/Y'),
+                $argument->getTrashedAt()->format('H:i:s'),
+                $argumentLink
+            )
+        );
+    }
+
+    private static function getMyTemplateVars(
+        string $trashedReason,
+        string $body,
+        string $trashedDate,
+        string $trashedTime,
+        string $argumentLink
+    ): array {
+        return [
+            '%trashedReason%' => self::escape($trashedReason),
+            '%body%' => self::escape($body),
+            '%trashedDate%' => $trashedDate,
+            '%trashedTime%' => $trashedTime,
+            '%argumentLink%' => $argumentLink,
+        ];
+    }
+
+    private static function getMySubjectVars(
+        string $title
+    ): array {
+        return [
+            '%proposalTitle%' => self::escape($title),
+        ];
+    }
+}
