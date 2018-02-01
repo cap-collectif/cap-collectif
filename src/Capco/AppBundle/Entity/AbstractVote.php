722481d469ab0df0ff0857d7635405cec72aa6c3
spyl94 <spyl94@gmail.com>
2018-02-01 16:01:04 +0100

    Fix profile if vote on deleted
    GitOrigin-RevId: 6dc286135ab1825f04cbdd12f2c4e319d14d9679

--- src/Capco/AppBundle/Entity/AbstractVote.php
+++ src/Capco/AppBundle/Entity/AbstractVote.php
@@ -7,49 +7,11 @@ use Capco\AppBundle\Model\HasAuthorInterface;
 use Capco\AppBundle\Traits\ExpirableTrait;
 use Capco\AppBundle\Traits\IdTrait;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\ORM\Mapping\UniqueConstraint;
 use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
- * @ORM\Table(
- *   name="votes",
- *   indexes={},
- *   uniqueConstraints={
- *        @UniqueConstraint(
- *            name="opinion_vote_unique",
- *            columns={"voter_id", "opinion_id"}
- *        ),
- *        @UniqueConstraint(
- *            name="argument_vote_unique",
- *            columns={"voter_id", "argument_id"}
- *        ),
- *        @UniqueConstraint(
- *            name="opinion_version_vote_unique",
- *            columns={"voter_id", "opinion_version_id"}
- *        ),
- *        @UniqueConstraint(
- *            name="selection_step_vote_unique",
- *            columns={"voter_id", "proposal_id", "selection_step_id"}
- *        ),
- *        @UniqueConstraint(
- *            name="collect_step_vote_unique",
- *            columns={"voter_id", "proposal_id", "collect_step_id"}
- *        ),
- *        @UniqueConstraint(
- *            name="source_vote_unique",
- *            columns={"voter_id", "source_id"}
- *        ),
- *        @UniqueConstraint(
- *            name="comment_vote_unique",
- *            columns={"voter_id", "comment_id"}
- *        ),
- *        @UniqueConstraint(
- *            name="idea_vote_unique",
- *            columns={"voter_id", "idea_id"}
- *        )
- *    }
- * )
  * @ORM\Entity(repositoryClass="Capco\AppBundle\Repository\AbstractVoteRepository")
+ * @ORM\Table(name="votes",indexes={})
  * @ORM\HasLifecycleCallbacks()
  * @ORM\InheritanceType("SINGLE_TABLE")
  * @ORM\DiscriminatorColumn(name = "voteType", type = "string")
