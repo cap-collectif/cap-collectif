75f32c759653eb4b26ae54c66479309d44618a95
Liinkiing <omar.jbara2@gmail.com>
2018-02-01 11:45:30 +0100

    - Switched logic from processor to notifier - Added CommentCreate Message - Added test - Added new fixture (anonymous comment)
    GitOrigin-RevId: 448037305e0c01222f43d72ed653bc52c2de40a4

---
+++ src/Capco/AppBundle/Mailer/Message/Comment/CommentCreateAnonymousMessage.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Capco\AppBundle\Mailer\Message\Comment;
+
+use Capco\AppBundle\Entity\Comment;
+use Capco\AppBundle\Mailer\Message\ExternalMessage;
+
+final class CommentCreateAnonymousMessage extends ExternalMessage
+{
+    public static function create(Comment $comment,
+                                  string $recipentEmail,
+                                  string $recipientName = null,
+                                  string $proposalUrl,
+                                  string $disableNotificationsUrl): self
+    {
+        $message = new self(
+            $recipentEmail,
+            $recipientName,
+            'notification.email.anonymous_comment.to_user.create.subject',
+            static::getMySubjectVars(
+                $comment->getAuthorName()
+            ),
+            'notification.email.anonymous_comment.to_user.create.body',
+            static::getMyTemplateVars(
+                $comment->getAuthorName(),
+                $comment->getRelatedObject()->getTitle(),
+                $comment->getCreatedAt()->format('d/m/Y'),
+                $comment->getCreatedAt()->format('H:i:s'),
+                $comment->getBodyExcerpt(),
+                $proposalUrl,
+                $disableNotificationsUrl
+            )
+        );
+
+        return $message;
+    }
+
+    private static function getMyTemplateVars(
+        string $authorName,
+        string $proposalTitle,
+        string $date,
+        string $time,
+        string $comment,
+        string $proposalUrl,
+        string $disableNotificationsUrl
+    ): array {
+        return [
+            '%username%' => self::escape($authorName),
+            '%proposal%' => self::escape($proposalTitle),
+            '%date%' => $date,
+            '%time%' => $time,
+            '%comment%' => self::escape($comment),
+            '%proposalUrl%' => $proposalUrl,
+            '%disableNotificationsUrl%' => $disableNotificationsUrl,
+        ];
+    }
+
+    private static function getMySubjectVars(
+        string $username
+    ): array {
+        return [
+            '%username%' => self::escape($username),
+        ];
+    }
+}
