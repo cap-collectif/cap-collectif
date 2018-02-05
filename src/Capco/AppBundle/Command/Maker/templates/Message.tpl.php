7350b79d5c97caea599d83ff007f47a1818f483e
Liinkiing <omar.jbara2@gmail.com>
2018-02-05 15:49:33 +0100

    - Added a MakerCommand to generate new message (for #5110)
    GitOrigin-RevId: 69daf88b163c5a2526b3b4a86a9334ffd33158a5

---
+++ src/Capco/AppBundle/Command/Maker/templates/Message.tpl.php
@@ -0,0 +1,73 @@
+<?php echo "<?php\n"; ?>
+
+namespace Capco\AppBundle\Mailer\Message\User\<?php echo $command_entity_name; ?>;
+
+use Capco\AppBundle\Mailer\Message\<?php echo $command_message_type; ?>;
+
+final class <?php echo $command_class_name; ?> extends <?php echo $command_message_type; ?><?php echo PHP_EOL; ?>
+{
+    public static function create(
+        string $recipentEmail,
+        string $recipientName = null
+    ): self
+    {
+        return new self(
+            $recipentEmail,
+            $recipientName,
+            '<?php echo $command_subject_template; ?>',
+            static::getMySubjectVars(),
+            '<?php echo $command_content_template; ?>',
+            static::getMyTemplateVars()
+        );
+    }
+
+    private static function getMyTemplateVars(
+<?php if (isset($command_template_vars)): ?>
+<?php foreach ($command_template_vars as $template_var): ?>
+<?php if (next($command_template_vars)): ?>
+        $<?php echo $template_var; ?>,<?php echo PHP_EOL; ?>
+<?php else: ?>
+        $<?php echo $template_var; ?><?php echo PHP_EOL; ?>
+<?php endif; ?>
+<?php endforeach; ?>
+<?php endif; ?>
+    ): array
+    {
+        return [
+<?php if (isset($command_template_vars)): ?>
+<?php foreach ($command_template_vars as $template_var): ?>
+<?php if (next($command_template_vars)): ?>
+            '{<?php echo $template_var; ?>}' => $<?php echo $template_var; ?>,<?php echo PHP_EOL; ?>
+<?php else: ?>
+            '{<?php echo $template_var; ?>}' => $<?php echo $template_var; ?><?php echo PHP_EOL; ?>
+<?php endif; ?>
+<?php endforeach; ?>
+<?php endif; ?>
+        ];
+    }
+
+    private static function getMySubjectVars(
+<?php if (isset($command_subject_vars)): ?>
+<?php foreach ($command_subject_vars as $subject_var): ?>
+<?php if (next($command_subject_vars)): ?>
+        $<?php echo $subject_var; ?>,<?php echo PHP_EOL; ?>
+<?php else: ?>
+        $<?php echo $subject_var; ?><?php echo PHP_EOL; ?>
+<?php endif; ?>
+<?php endforeach; ?>
+<?php endif; ?>
+    ): array
+    {
+        return [
+<?php if (isset($command_subject_vars)): ?>
+<?php foreach ($command_subject_vars as $subject_var): ?>
+<?php if (next($command_subject_vars)): ?>
+            '{<?php echo $subject_var; ?>}' => $<?php echo $subject_var; ?>,<?php echo PHP_EOL; ?>
+<?php else: ?>
+            '{<?php echo $subject_var; ?>}' => $<?php echo $subject_var; ?><?php echo PHP_EOL; ?>
+<?php endif; ?>
+<?php endforeach; ?>
+<?php endif; ?>
+        ];
+    }
+}
