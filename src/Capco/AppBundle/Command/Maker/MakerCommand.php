c9b16da8ca10bd8ff762249b810fe5260e630037
Liinkiing <omar.jbara2@gmail.com>
2018-02-05 17:10:18 +0100

    - Updated MakerCommand
    GitOrigin-RevId: 227a59e6fb9d831df2db0deb1dca86f357a0d53d

--- src/Capco/AppBundle/Command/Maker/MakerCommand.php
+++ src/Capco/AppBundle/Command/Maker/MakerCommand.php
@@ -2,15 +2,19 @@
 
 namespace Capco\AppBundle\Command\Maker;
 
+use Capco\AppBundle\Utils\Text;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Finder\Finder;
 
 class MakerCommand extends ContainerAwareCommand
 {
+    const TEMPLATE_PATH = __DIR__ . '/templates';
     protected $helper;
+    protected $fqcns;
 
     protected function configure()
     {
@@ -21,13 +25,22 @@ class MakerCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->fqcns = [];
+        $container = $this->getContainer();
+        $finder = new Finder();
+        $directories = [
+            'src/Capco/AppBundle/Entity',
+            'src/Capco/UserBundle/Entity',
+        ];
+        foreach ($finder->files()->name('/\.php$/')->in($directories)->sortByName() as $file) {
+            $this->fqcns[] = NamespaceResolver::getFullQualifiedClassName($file->getRealPath());
+        }
         $this->helper = $this->getHelper('question');
         $messageTypes = ['External', 'Admin', 'Moderator'];
-        $container = $this->getContainer();
         $fs = $container->get('filesystem');
-        $templatePath = __DIR__ . '/templates/Message.tpl.php';
-        $entity = $this->askSimpleQuestion($input, $output, 'Please type the related entity of your message <info>(e.g Proposal)</info>');
-        $name = $this->askSimpleQuestion($input, $output, 'Please type your message name <info>(e.g ProposalCreate)</info>');
+        $messageTemplatePath = self::TEMPLATE_PATH . '/Message.tpl.php';
+        $entity = $this->askEntity($input, $output);
+        $name = $this->askSimpleQuestion($input, $output, 'Please type your message name <info>(e.g ' . $entity->getShortName() . 'Create)</info>');
         $name .= 'Message';
         $type = $this->askChoiceQuestion($input, $output, 'Please select your message type <info>(defaults to External</info>)',
             $messageTypes,
@@ -41,18 +54,39 @@ class MakerCommand extends ContainerAwareCommand
         $templateVars = $this->askQuestionWithArrayResponse($input, $output, 'Please type the names of the variables in the subject <info>(comma-separated list)</info>');
         $subjectVars = $this->askQuestionWithArrayResponse($input, $output, 'Please type the names of the variables in the content <info>(comma-separated list)</info>');
 
-        $parsed = $this->parseTemplate($templatePath, [
+        $parsed = $this->parseTemplate($messageTemplatePath, [
             'command_class_name' => $name,
-            'command_entity_name' => $entity,
+            'command_entity_name' => $entity->getShortName(),
+            'command_entity_name_camelCase' => Text::camelCase($entity->getShortName()),
+            'command_related_entity_fqcn' => $entity->getName(),
             'command_message_type' => $type,
             'command_subject_template' => $subject,
             'command_content_template' => $content,
             'command_template_vars' => $templateVars,
             'command_subject_vars' => $subjectVars,
         ]);
-        $path = $container->get('kernel')->getRootDir() . "/../src/Capco/AppBundle/Mailer/Message/$entity/$name.php";
+        $path = $container->get('kernel')->getRootDir() . '/../src/Capco/AppBundle/Mailer/Message/' . $entity->getShortName() . "/$name.php";
         $fs->dumpFile($path, $parsed);
         $output->writeln("<info>File successfully written at $path</info>");
+    }
+
+    private function askEntity(InputInterface $input, OutputInterface $output): \ReflectionClass
+    {
+        $entity = $this->askSimpleQuestion($input, $output, 'Please type the related entity of your message <info>(e.g Proposal)</info>');
+        $found = null;
+        foreach ($this->fqcns as $fqcn) {
+            if (false !== strpos($fqcn, $entity)) {
+                $found[] = $fqcn;
+            }
+        }
+        if (!$found) {
+            throw new \RuntimeException('The given entity was not found in the application');
+        }
+        if ($found > 1) {
+            return new \ReflectionClass($this->askChoiceQuestion($input, $output, "'$entity' was found in many entities. Please choose one below", $found, null, $found));
+        }
+
+        return new \ReflectionClass($found[0]);
     }
 
     private function askSimpleQuestion(InputInterface $input,
@@ -75,10 +109,9 @@ class MakerCommand extends ContainerAwareCommand
     private function askChoiceQuestion(InputInterface $input,
                                        OutputInterface $output,
                                        string $question,
-                                       array $choices = null,
+                                       array $choices,
                                        $default = null,
-                                       $autocompleteValues = null,
-                                       $capitalize = true): string
+                                       $autocompleteValues = null): string
     {
         $q = new ChoiceQuestion(
             $question . PHP_EOL,
