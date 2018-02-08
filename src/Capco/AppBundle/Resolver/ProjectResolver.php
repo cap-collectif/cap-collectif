0b96c6721cb30302b3623760eb23210f237c25f6
Liinkiing <omar.jbara2@gmail.com>
2018-02-08 14:39:39 +0100

    - Injected only router instead of whole container
    GitOrigin-RevId: babe56631c1102bce02195dae81a22fbd97cf5cb

--- src/Capco/AppBundle/Resolver/ProjectResolver.php
+++ src/Capco/AppBundle/Resolver/ProjectResolver.php
@@ -4,34 +4,29 @@ namespace Capco\AppBundle\Resolver;
 
 use Capco\AppBundle\Entity\Project;
 use Capco\AppBundle\Repository\ProjectRepository;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 
-class ProjectResolver implements ContainerAwareInterface
+class ProjectResolver
 {
-    use ContainerAwareTrait;
-
     protected $projectRepository;
+    protected $router;
 
-    public function __construct(ProjectRepository $projectRepository)
+    public function __construct(ProjectRepository $projectRepository, RouterInterface $router)
     {
         $this->projectRepository = $projectRepository;
+        $this->router = $router;
     }
 
     public function resolveAdminEditUrl(Project $project, $absolute = true)
     {
-        $router = $this->container->get('router');
-
-        return $router->generate('admin_capco_app_project_edit', ['id' => $project->getId()],
+        return $this->router->generate('admin_capco_app_project_edit', ['id' => $project->getId()],
             $absolute ? UrlGeneratorInterface::ABSOLUTE_URL : UrlGeneratorInterface::RELATIVE_PATH);
     }
 
     public function resolveIndexUrl($absolute = true)
     {
-        $router = $this->container->get('router');
-
-        return $router->generate('app_project', [],
+        return $this->router->generate('app_project', [],
             $absolute ? UrlGeneratorInterface::ABSOLUTE_URL : UrlGeneratorInterface::RELATIVE_PATH);
     }
 }
