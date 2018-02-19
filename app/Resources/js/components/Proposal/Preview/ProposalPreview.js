9f8845ba4c58909134f536a086412a30616ca885
mauriau <maxime.auriau@protonmail.com>
2018-02-19 14:10:11 +0100

    Add button follow on proposal preview + test js
    GitOrigin-RevId: 80b86cbd374fd3f3bedbe67edd9dbcb458536793

--- app/Resources/js/components/Proposal/Preview/ProposalPreview.js
+++ app/Resources/js/components/Proposal/Preview/ProposalPreview.js
@@ -1,27 +1,33 @@
-import React, { PropTypes } from 'react';
+// @flow
+import React from 'react';
 import { Col } from 'react-bootstrap';
 import ProposalPreviewHeader from './ProposalPreviewHeader';
 import ProposalPreviewBody from './ProposalPreviewBody';
 import ProposalPreviewFooter from './ProposalPreviewFooter';
 import ProposalStatus from './ProposalStatus';
 import { VOTE_TYPE_DISABLED, VOTE_TYPE_BUDGET } from '../../../constants/ProposalConstants';
+import type { Proposal } from '../../../redux/modules/proposal';
+import { ProposalFollowButton } from '../Follow/ProposalFollowButton';
 import { CardContainer } from '../../Ui/Card/CardContainer';
 
-const ProposalPreview = React.createClass({
-  propTypes: {
-    proposal: PropTypes.object.isRequired,
-    step: PropTypes.object.isRequired,
-    showThemes: PropTypes.bool,
-    showComments: PropTypes.bool,
-  },
+type Step = {
+  id: Uuid,
+  voteType: number,
+  voteThreshold: number,
+};
 
-  getDefaultProps() {
-    return {
-      showThemes: false,
-      showComments: false,
-    };
-  },
+type Props = {
+  proposal: Proposal,
+  step: Step,
+  showThemes: boolean,
+  showComments: boolean,
+};
 
+export class ProposalPreview extends React.Component<Props> {
+  static defaultProps = {
+    showComments: false,
+    showThemes: false,
+  };
   render() {
     const { proposal, step, showThemes, showComments } = this.props;
     const voteType = step.voteType;
@@ -34,12 +40,21 @@ const ProposalPreview = React.createClass({
             proposal.author && proposal.author.vip ? 'bg-vip proposal-preview' : 'proposal-preview'
           }>
           <ProposalPreviewHeader proposal={proposal} />
-          <ProposalPreviewBody
             proposal={proposal}
+          <ProposalPreviewBody
             showNullEstimation={voteType === VOTE_TYPE_BUDGET}
-            showThemes={showThemes}
             step={step}
+            showThemes={showThemes}
           />
+            <div className="proposal__buttons text-center">
+              {step.id === proposal.votableStepId && <ProposalPreviewVote proposal={proposal} />}
+              <ProposalFollowButton proposal={proposal} />
+            </div>
+            {step.voteThreshold > 0 && (
+              <div style={{ marginTop: '20px' }}>
+                <ProposalVoteThresholdProgressBar proposal={proposal} step={step} />
+              </div>
+            )}
           <ProposalPreviewFooter
             proposal={proposal}
             showVotes={voteType !== VOTE_TYPE_DISABLED}
@@ -50,7 +65,7 @@ const ProposalPreview = React.createClass({
         </CardContainer>
       </Col>
     );
-  },
-});
+  }
+}
 
 export default ProposalPreview;
