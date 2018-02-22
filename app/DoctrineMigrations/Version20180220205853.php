031fd9ec71d4b36156e5276bfec9267900220b68
spyl94 <spyl94@gmail.com>
2018-02-22 11:07:04 +0100

    Fix http plug
    GitOrigin-RevId: fc89f17f65262869d8bf3fc00995c6cf78dfc8bc

--- app/DoctrineMigrations/Version20180220205853.php
+++ app/DoctrineMigrations/Version20180220205853.php
@@ -5,32 +5,17 @@ namespace Application\Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-class Version20180216160556 extends AbstractMigration
+class Version20180220205853 extends AbstractMigration
 {
-    /**
-     * @param Schema $schema
-     */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('ALTER TABLE fos_user ADD paris_id VARCHAR(255) DEFAULT NULL');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_957A6479A162E52 ON fos_user (saml_id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_957A6479D6F1A30E ON fos_user (paris_id)');
     }
 
-    /**
-     * @param Schema $schema
-     */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('DROP INDEX UNIQ_957A6479A162E52 ON fos_user');
         $this->addSql('DROP INDEX UNIQ_957A6479D6F1A30E ON fos_user');
         $this->addSql('ALTER TABLE fos_user DROP paris_id');
