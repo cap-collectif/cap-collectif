07dc2b803814f62610f157e871cd6f70c2b88627
mauriau <maxime.auriau@protonmail.com>
2018-03-09 15:40:12 +0100

    Update User followings proposal
    GitOrigin-RevId: b1e509c0853c82cf4de36ec4241bf25608809831

--- app/Resources/js/components/User/Following/FollowingsProposals.js
+++ app/Resources/js/components/User/Following/FollowingsProposals.js
@@ -38,7 +38,6 @@ export class FollowingsProposals extends Component<Props, State> {
     viewer.followingProposals.map(proposal => {
       projectsById[proposal.project.id] = proposal.project;
     });
-
     return (
       <div>
         <h2>
