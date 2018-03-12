d26cf1dd3b8ab8f347d9b807b71713a9ed403ade
spyl94 <spyl94@gmail.com>
2018-03-12 14:03:50 +0100

    Fix migrations
    GitOrigin-RevId: d0808fee7a0157a5b1ea987a970a3f2f786498e4

--- app/DoctrineMigrations/Version20180226163218.php
+++ app/DoctrineMigrations/Version20180226163218.php
@@ -7,6 +7,11 @@ use Doctrine\DBAL\Schema\Schema;
 
 class Version20180226163218 extends IdToUuidMigration
 {
+  public function up(Schema $schema)
+  {
+      $this->addSql('DROP INDEX questionnaire_position_unique ON questionnaire_abstractquestion');
+  }
+
     public function postUp(Schema $schema)
     {
         $this->migrate('questionnaire');
--- app/DoctrineMigrations/Version20180312125200.php
+++ app/DoctrineMigrations/Version20180312125200.php
@@ -5,32 +5,16 @@ namespace Application\Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20180312125200 extends AbstractMigration
 {
-    /**
-     * @param Schema $schema
-     */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('DROP INDEX questionnaire_position_unique ON questionnaire_abstractquestion');
         $this->addSql('CREATE UNIQUE INDEX questionnaire_position_unique ON questionnaire_abstractquestion (questionnaire_id, position)');
         $this->addSql('ALTER TABLE proposal_form DROP INDEX IDX_72E9E8347CE583F7, ADD UNIQUE INDEX UNIQ_72E9E8347CE583F7 (evaluation_form_id)');
     }
 
-    /**
-     * @param Schema $schema
-     */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('ALTER TABLE proposal_form DROP INDEX UNIQ_72E9E8347CE583F7, ADD INDEX IDX_72E9E8347CE583F7 (evaluation_form_id)');
         $this->addSql('DROP INDEX questionnaire_position_unique ON questionnaire_abstractquestion');
         $this->addSql('CREATE UNIQUE INDEX questionnaire_position_unique ON questionnaire_abstractquestion (position)');
