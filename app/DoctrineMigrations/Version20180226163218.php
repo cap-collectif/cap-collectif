e08a91043bd4604e1ef057cee5f0cab185b0c04f
spyl94 <spyl94@gmail.com>
2018-03-12 14:05:50 +0100

    Fix indent
    GitOrigin-RevId: 6b2c9e294f905e8b1cbcace717d66b462c989fcd

--- app/DoctrineMigrations/Version20180226163218.php
+++ app/DoctrineMigrations/Version20180226163218.php
@@ -7,10 +7,10 @@ use Doctrine\DBAL\Schema\Schema;
 
 class Version20180226163218 extends IdToUuidMigration
 {
-  public function up(Schema $schema)
-  {
+    public function up(Schema $schema)
+    {
       $this->addSql('DROP INDEX questionnaire_position_unique ON questionnaire_abstractquestion');
-  }
+    }
 
     public function postUp(Schema $schema)
     {
