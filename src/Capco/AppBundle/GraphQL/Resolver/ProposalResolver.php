2bfa40dca87b5be665091967a90e2ec8bdaa805a
mauriau <maxime.auriau@protonmail.com>
2018-03-15 15:57:13 +0100

    delete unused code
    GitOrigin-RevId: 7e50e8aed58fd22c99012c0aaa5dab5528a33f33

--- src/Capco/AppBundle/GraphQL/Resolver/ProposalResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/ProposalResolver.php
@@ -209,11 +209,4 @@ class ProposalResolver implements ContainerAwareInterface
 
         return null !== $evalForm && (!$evalForm->isFullyPrivate() || $this->resolveViewerIsEvaluer($proposal, $user));
     }
-
-    public function resolveHasEvaluation(Proposal $proposal): bool
-    {
-        $evalForm = $proposal->getProposalForm()->getEvaluationForm();
-
-        return null !== $evalForm && !$evalForm->isFullyPrivate();
-    }
 }
