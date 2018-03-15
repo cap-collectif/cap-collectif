65d4714f83c05ca26def0744205e3d1329c6f7c3
mauriau <maxime.auriau@protonmail.com>
2018-03-15 12:16:23 +0100

    Refactor FollowerProposalNotifier
    GitOrigin-RevId: 2d9f819d7f950220d238e633549bdb51a730e5ab

--- src/Capco/AppBundle/Command/FollowerProposalNotifierCommand.php
+++ src/Capco/AppBundle/Command/FollowerProposalNotifierCommand.php
@@ -3,13 +3,7 @@
 namespace Capco\AppBundle\Command;
 
 use Capco\AppBundle\Entity\Follower;
-use Capco\AppBundle\Entity\Interfaces\FollowerNotifiedOfInterface;
 use Capco\AppBundle\Entity\Proposal;
-use Capco\AppBundle\Entity\ProposalForm;
-use Capco\AppBundle\Entity\Steps\SelectionStep;
-use Capco\AppBundle\Model\UserActivity;
-use Doctrine\ORM\EntityNotFoundException;
-use Monolog\Logger;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -17,11 +11,6 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class FollowerProposalNotifierCommand extends ContainerAwareCommand
 {
-    const NOT_FOLLOWED = 0;
-    const ACTIVITIES = ['isUpdated', 'isDeleted', 'comments', 'votes', 'lastStep'];
-
-    private static $userWithoutValidEmail = [];
-
     protected function configure()
     {
         $this
@@ -34,215 +23,28 @@ class FollowerProposalNotifierCommand extends ContainerAwareCommand
         $container = $this->getContainer();
         $siteName = $container->get('capco.site_parameter.resolver')->getValue('global.site.fullname');
         $notifier = $container->get('capco.follower_notifier');
-        $followersWithActivities = [];
+        $activitiesResolver = $container->get('capco.following.activities.resolver');
+        $followedProposalsByUserId = [];
         try {
-            $followersWithActivities = $this->getFollowersWithActivities();
+            $followedProposalsByUserId = $activitiesResolver->getFollowedProposalsByUserId();
         } catch (\Exception $e) {
             $output->writeln(
                 '<error>' . $e->getMessage() . '</error>'
             );
         }
-        $proposalActivities = $this->getProposalActivities();
-        $followersWithActivities = $this->orderUserProposalActivitiesInProject($followersWithActivities, $proposalActivities['projects'], $proposalActivities['proposals']);
+        $proposalActivities = $activitiesResolver->getYesterdayProposalActivities();
+        $followedProposalsActivitiesByUserId = $activitiesResolver->getMatchingActivitiesByUserId($followedProposalsByUserId, $proposalActivities);
         unset($proposalActivities);
         $sendAt = (new \DateTime('yesterday'))->setTimezone(new \DateTimeZone('Europe/Paris'));
         $siteUrl = $container->get('router')->generate('app_homepage', [], UrlGeneratorInterface::ABSOLUTE_URL);
-        foreach ($followersWithActivities as $userId => $userActivity) {
-            $notifier->onReportActivities($userActivity, $sendAt, $siteName, $siteUrl);
+        foreach ($followedProposalsActivitiesByUserId as $userId => $activities) {
+            $notifier->onReportActivities($activities, $sendAt, $siteName, $siteUrl);
         }
-        $nbNewsletters = count($followersWithActivities);
+        $nbNewsletters = count($followedProposalsActivitiesByUserId);
         $output->writeln(
             '<info>Notification correctly send to ' . $nbNewsletters . ' users</info>'
         );
-        $userWithoutEmail = array_unique(static::$userWithoutValidEmail);
-
-        $output->writeln(
-            '<comment>There are ' . count($userWithoutEmail) . ' users who follow some proposals, without valid email</comment>'
-        );
 
         return 0;
-    }
-
-    private function getFollowersWithActivities(): array
-    {
-        $container = $this->getContainer();
-        /** @var Logger $logger */
-        $logger = $container->get('logger');
-        $em = $container->get('doctrine')->getManager();
-        $followers = $em->getRepository('CapcoAppBundle:Follower')->findAll();
-        $followersWithActivities = [];
-
-        /** @var Follower $follower */
-        foreach ($followers as $follower) {
-            try {
-                $proposalId = $follower->getProposal()->getId();
-                $user = $follower->getUser();
-                $userId = $user->getId();
-            } catch (EntityNotFoundException $e) {
-                $logger->addError(__METHOD__ . $e->getMessage());
-                continue;
-            }
-
-            if (!isset($followersWithActivities[$userId])) {
-                $unfollowingPage = $container->get('router')->generate('capco_profile_followings_login', ['token' => $user->getNotificationsConfiguration()->getUnsubscribeToken()],
-                    UrlGeneratorInterface::ABSOLUTE_URL);
-                $userActivity = new UserActivity();
-                $userActivity->setId($userId);
-                $userActivity->setEmail($user->getEmailCanonical());
-                $userActivity->setUsername($user->getUsername());
-                $userActivity->setFirstname($user->getFirstname());
-                $userActivity->setLastname($user->getLastname());
-                $userActivity->addUserProposal($proposalId);
-                $userActivity->setNotifiedOf($follower->getNotifiedOf());
-                $userActivity->setUrlManagingFollowings($unfollowingPage);
-                /* UserActivity */
-                $followersWithActivities[$userId] = $userActivity;
-                continue;
-            }
-
-        return $followersWithActivities;
-    }
-
-    private function orderUserProposalActivitiesInProject(array $followersWithActivities, array $projects, array $proposalActivities): array
-    {
-        /** @var UserActivity $userActivity * */
-        foreach ($followersWithActivities as $userId => $userActivity) {
-            $userActivity->setUserProjects($projects);
-            if (!$userActivity->hasProposal()) {
-                unset($followersWithActivities[$userId]);
-                continue;
-            }
-
-            foreach ($userActivity->getUserProposals() as $proposalId) {
-                if (isset($proposalActivities[$proposalId])) {
-                    $proposal = $proposalActivities[$proposalId];
-                    if (isset($projects[$proposal['projectId']])) {
-                        if (0 === $proposal['countActivities']) {
-                            continue;
-                        }
-                        $project = $userActivity->getUserProject($proposal['projectId']);
-
-                        if (!isset($project['proposals'])) {
-                            $project['proposals'] = [];
-                        }
-                        if (FollowerNotifiedOfInterface::DEFAULT === $userActivity->getNotifiedOf()) {
-                            $proposal['comments'] = self::NOT_FOLLOWED;
-                            $proposal['votes'] = self::NOT_FOLLOWED;
-                        }
-                        if (FollowerNotifiedOfInterface::DEFAULT_AND_COMMENTS === $userActivity->getNotifiedOf()) {
-                            $proposal['votes'] = self::NOT_FOLLOWED;
-                        }
-
-                        // recount because we get what the user is following
-                        $proposal['countActivities'] = $this->countProposalActivities($proposal);
-                        if (0 === $proposal['countActivities']) {
-                            continue;
-                        }
-                        $project['countActivities'] += $proposal['countActivities'];
-                        $project['proposals'][$proposalId] = $proposal;
-                        $userActivity->addUserProject($project, $proposal['projectId']);
-                    }
-                }
-            }
-            // check if user project got a proposal and remove empty project
-            foreach ($userActivity->getUserProjects() as $projectId => $project) {
-                if (empty($project['proposals'])) {
-                    $userActivity->removeUserProject($projectId);
-                }
-            }
-
-            $userActivity->setUserProposals([]);
-            // check if user got a project and remove user without project
-            if (!$userActivity->hasUserProject()) {
-                unset($followersWithActivities[$userId]);
-                continue;
-            }
-            $followersWithActivities[$userId] = $userActivity;
-        }
-    }
-
-    private function getProposalActivities(): array
-    {
-        $container = $this->getContainer();
-        $em = $container->get('doctrine')->getManager();
-        $proposalRepository = $em->getRepository('CapcoAppBundle:Proposal');
-        $proposalFormRepository = $em->getRepository('CapcoAppBundle:ProposalForm');
-        $yesterdayMidnight = new \DateTime('yesterday midnight');
-        $yesterdayLasTime = (new \DateTime('today midnight'))->modify('-1 second');
-        $twentyFourHoursInterval = new \DateInterval('PT24H');
-        $proposalForms = $proposalFormRepository->findAll();
-        $router = $container->get('router');
-        $pProposals = [];
-        $projects = [];
-
-        /** @var ProposalForm $proposalForm */
-        foreach ($proposalForms as $proposalForm) {
-            $project = $proposalForm->getStep()->getProject();
-            $projectId = $project->getId();
-            $proposals = $proposalForm->getProposals();
-            $projects[$projectId]['projectTitle'] = $project->getTitle();
-            $projects[$projectId]['projectType'] = $project->getProjectType()->getTitle();
-            // init variable, not used yet
-            $projects[$projectId]['proposals'] = [];
-            $projects[$projectId]['countActivities'] = 0;
-
-            /** @var Proposal $proposal */
-            foreach ($proposals as $proposal) {
-                $step = $proposal->getStep();
-                if ($step instanceof SelectionStep && $step->isProposalsHidden()) {
-                    continue;
-                }
-                $currentProposal = [];
-                $proposalId = $proposal->getId();
-                $proposalCommentYesterdays = $proposalRepository->countProposalCommentsCreatedBetween($yesterdayMidnight, $yesterdayLasTime, $proposalId);
-                $proposalVotesInYesterday = $proposalRepository->countProposalVotesCreatedBetween($yesterdayMidnight, $yesterdayLasTime, $proposalId);
-                $proposalStepInYesterday = $proposalRepository->proposalStepChangedBetween($yesterdayMidnight, $yesterdayLasTime, $proposalId);
-                $currentProposal['title'] = $proposal->getTitle();
-                $currentProposal['link'] = $router->generate(
-                    'app_project_show_proposal',
-                    [
-                        'projectSlug' => $project->getSlug(),
-                        'stepSlug' => $proposal->getStep()->getSlug(),
-                        'proposalSlug' => $proposal->getSlug(),
-                    ],
-                    0
-                );
-                $currentProposal['isUpdated'] = $proposal->isUpdatedInLastInterval($yesterdayLasTime, $twentyFourHoursInterval);
-                $currentProposal['isDeleted'] = $proposal->isDeletedInLastInterval($yesterdayLasTime, $twentyFourHoursInterval);
-                $currentProposal['comments'] = (int) $proposalCommentYesterdays[0]['countComment'];
-                $currentProposal['votes'] = $proposalVotesInYesterday[0]['sVotes'] + $proposalVotesInYesterday[0]['cVotes'];
-                $currentProposal['lastStep'] = !empty($proposalStepInYesterday) ? $proposalStepInYesterday[0] : false;
-                $currentProposal['projectId'] = $projectId;
-                $currentProposal['countActivities'] = $this->countProposalActivities($currentProposal);
-                if (0 === $currentProposal['countActivities']) {
-                    unset($currentProposal);
-                } else {
-                    $pProposals[$proposalId] = $currentProposal;
-                }
-            }
-        }
-
-        foreach ($followersWithActivities as $followerWithActivities) {
-            foreach ($followerWithActivities as $follower) {
-            }
-        }
-
-        $output->writeln(
-            '<info>opinions successfully created.</info>'
-        );
-
-        return 0;
-    }
-
-    private function countProposalActivities(array $proposalActivities): int
-    {
-        $nbActivities = count(self::ACTIVITIES);
-        foreach (self::ACTIVITIES as $activity) {
-            if (!$proposalActivities[$activity]) {
-                --$nbActivities;
-            }
-        }
-
-        return $nbActivities;
     }
 }
--- src/Capco/AppBundle/Model/UserActivity.php
+++ src/Capco/AppBundle/Model/UserActivity.php
@@ -17,36 +17,36 @@ class UserActivity implements FollowerNotifiedOfInterface
     protected $notifiedOf;
     protected $urlManagingFollowings;
 
-    public function getUsername()
+    public function getUsername(): string
     {
         return $this->username;
     }
 
-    public function setUsername($username)
+    public function setUsername(string $username): self
     {
         $this->username = $username;
 
         return $this;
     }
 
-    public function getEmail()
+    public function getEmail(): string
     {
         return $this->email;
     }
 
-    public function setEmail($email)
+    public function setEmail(string $email): self
     {
         $this->email = $email;
 
         return $this;
     }
 
-    public function getId()
+    public function getId(): string
     {
         return $this->id;
     }
 
-    public function setId($id)
+    public function setId($id): self
     {
         $this->id = $id;
 
@@ -58,38 +58,38 @@ class UserActivity implements FollowerNotifiedOfInterface
         return $this->userProposals;
     }
 
-    public function setUserProposals(array $userProposals)
+    public function setUserProposals(array $userProposals): self
     {
         $this->userProposals = $userProposals;
 
         return $this;
     }
 
-    public function addUserProposal($userProposal)
+    public function addUserProposal(string $proposalId, string $notifiedOf): self
     {
-        $this->userProposals[] = $userProposal;
+        $this->userProposals[$proposalId] = $notifiedOf;
 
         return $this;
     }
 
-    public function getLastname()
+    public function getLastname(): string
     {
         return $this->lastname;
     }
 
-    public function setLastname($lastname)
+    public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
 
         return $this;
     }
 
-    public function getFirstname()
+    public function getFirstname(): string
     {
         return $this->firstname;
     }
 
-    public function setFirstname($firstname)
+    public function setFirstname(string $firstname): self
     {
         $this->firstname = $firstname;
 
@@ -101,38 +101,42 @@ class UserActivity implements FollowerNotifiedOfInterface
         return $this->userProjects;
     }
 
-    public function removeUserProject($projectId)
+    public function removeUserProject(string $projectId): self
     {
         unset($this->userProjects[$projectId]);
 
         return $this;
     }
 
-    public function getUserProject($project)
+    public function getUserProject(string $projectId): ?array
     {
-        return $this->userProjects[$project];
+        if (isset($this->userProjects[$projectId])) {
+            return $this->userProjects[$projectId];
+        }
+
+        return null;
     }
 
-    public function setUserProjects(array $userProjects)
+    public function setUserProjects(array $userProjects): string
     {
         $this->userProjects = $userProjects;
 
         return $this;
     }
 
-    public function addUserProject($userProject, $projectId)
+    public function addUserProject(array $userProject, string $projectId): self
     {
         $this->userProjects[$projectId] = $userProject;
 
         return $this;
     }
 
-    public function hasUserProject()
+    public function hasUserProject(): bool
     {
         return count($this->userProjects) > 0;
     }
 
-    public function hasProposal()
+    public function hasProposal(): bool
     {
         return count($this->userProposals) > 0;
     }
@@ -140,7 +144,14 @@ class UserActivity implements FollowerNotifiedOfInterface
     public function setNotifiedOf(string $notifiedOf): self
     {
         if (!in_array($notifiedOf, self::NOTIFICATIONS, true)) {
-            throw new InvalidArgumentException(sprintf('%s is not valide value for notified of follower %s -> %s', $notifiedOf, $this->getId(), $this->getUsername()));
+            throw new InvalidArgumentException(
+                sprintf(
+                    '%s is not valide value for notified of follower %s -> %s',
+                    $notifiedOf,
+                    $this->getId(),
+                    $this->getUsername()
+                )
+            );
         }
         $this->notifiedOf = $notifiedOf;
 
