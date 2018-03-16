35c0f5cdf9d04365250f453f6f640f6d719bf8fe
spyl94 <spyl94@gmail.com>
2018-03-16 13:39:45 +0100

    Add some logging
    GitOrigin-RevId: 463efa9ab417462df48540302073877b68a7e727

--- src/Capco/AppBundle/Elasticsearch/IndexationProcessor.php
+++ src/Capco/AppBundle/Elasticsearch/IndexationProcessor.php
@@ -2,20 +2,24 @@
 
 namespace Capco\AppBundle\Elasticsearch;
 
+use Psr\Log\LoggerInterface;
 use Swarrot\Broker\Message;
 use Swarrot\Processor\ProcessorInterface;
 
 class IndexationProcessor implements ProcessorInterface
 {
     private $indexer;
+    private $logger;
 
-    public function __construct($indexer)
+    public function __construct(Indexer $indexer, LoggerInterface $logger)
     {
         $this->indexer = $indexer;
+        $this->logger = $logger;
     }
 
     public function process(Message $message, array $options)
     {
+        $this->logger->info('Asynchronous indexation of: {json}', ['json' => $message->getBody()]);
         $json = json_decode($message->getBody(), true);
         $this->indexer->index($json['class'], $json['id']);
 
