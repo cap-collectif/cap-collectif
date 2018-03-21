9e3fcceeb5045beab6e0e77200ccaabd805ca6cd
spyl94 <spyl94@gmail.com>
2018-03-21 15:57:47 +0100

    Add missing part of migration
    GitOrigin-RevId: f172a5597395ef0381260a7478eb5ecb51edf64a

--- app/DoctrineMigrations/Version20180312125201.php
+++ app/DoctrineMigrations/Version20180312125201.php
@@ -7,6 +7,11 @@ use Doctrine\DBAL\Schema\Schema;
 
 class Version20180312125201 extends IdToUuidMigration
 {
+    public function up(Schema $schema)
+    {
+        $this->addSql('DROP INDEX comment_vote_unique ON votes');
+    }
+
     public function postUp(Schema $schema)
     {
         $this->migrate('comment');
