31030298a67202a308616719dba8c621818fb604
spyl94 <spyl94@gmail.com>
2018-03-26 14:39:13 +0200

    Fix CS
    GitOrigin-RevId: da5b30b9d67088c340aba95b52193d7fa912eb6b

--- src/Capco/AppBundle/EventListener/ShieldListener.php
+++ src/Capco/AppBundle/EventListener/ShieldListener.php
@@ -3,7 +3,6 @@
 namespace Capco\AppBundle\EventListener;
 
 use Capco\AppBundle\Toggle\Manager;
-use Symfony\Bridge\Twig\TwigEngine;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
