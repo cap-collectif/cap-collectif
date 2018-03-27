9d12648b5ba4559fa1fc339f7910f828e361d27f
Liinkiing <omar.jbara2@gmail.com>
2018-03-27 16:32:26 +0200

    - Added Comment Vote repository and resolver - Added CommentVote graphql type - Added CommentVote connection to votes field in comment
    GitOrigin-RevId: 64181c0a41ddbeaec0c0f50d9922f50c298d801d

--- src/Capco/AppBundle/Entity/CommentVote.php
+++ src/Capco/AppBundle/Entity/CommentVote.php
@@ -5,7 +5,7 @@ namespace Capco\AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="Capco\AppBundle\Repository\CommentVoteRepository")
  * @ORM\HasLifecycleCallbacks()
  */
 class CommentVote extends AbstractVote
