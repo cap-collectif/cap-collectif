6332db5a538930ed553e324323643c8d39e97f4b
sanzodown <sanzodown@hotmail.fr>
2018-03-28 18:46:59 +0200

    Add filter least voted
    GitOrigin-RevId: 78ca7ab7f7c5004e5713da34ca046a318ad5b98b

--- app/Resources/js/components/Proposal/List/ProposalListOrderSorting.js
+++ app/Resources/js/components/Proposal/List/ProposalListOrderSorting.js
@@ -25,7 +25,7 @@ type ComponentState = {
 
 export class ProposalListOrderSorting extends React.Component<Props, ComponentState> {
   static defaultProps = {
-    orderByVotes: false,
+    orderByVotes: true,
     orderByComments: false,
     orderByCost: false,
   };
@@ -35,9 +35,9 @@ export class ProposalListOrderSorting extends React.Component<Props, ComponentSt
 
     this.state = {
       // eslint-disable-next-line react/prop-types
-      displayedOrders: PROPOSAL_AVAILABLE_ORDERS.concat(props.orderByVotes ? ['votes'] : [])
-        .concat(props.orderByComments ? ['comments'] : [])
-        .concat(props.orderByCost ? ['expensive', 'cheap'] : []),
+      displayedOrders: PROPOSAL_AVAILABLE_ORDERS.concat(props.orderByComments ? ['comments'] : [])
+        .concat(props.orderByCost ? ['expensive', 'cheap'] : [])
+        .concat(props.orderByVotes ? ['votes', 'least-votes'] : []),
     };
   }
 
--- src/Capco/AppBundle/Entity/Steps/SelectionStep.php
+++ src/Capco/AppBundle/Entity/Steps/SelectionStep.php
@@ -37,6 +37,7 @@ class SelectionStep extends AbstractStep implements ParticipativeStepInterface
         'old' => 'step.sort.old',
         'random' => 'step.sort.random',
         'votes' => 'step.sort.votes',
+        'least-votes' => 'step.sort.least-votes',
         'expensive' => 'step.sort.expensive',
         'cheap' => 'step.sort.cheap',
     ];
@@ -63,7 +64,7 @@ class SelectionStep extends AbstractStep implements ParticipativeStepInterface
 
     /**
      * @ORM\Column(name="default_sort", type="string", nullable=false)
-     * @Assert\Choice(choices={"old","last","votes","comments","random", "cheap", "expensive"})
+     * @Assert\Choice(choices={"old","last","votes","least-votes","comments","random", "cheap", "expensive"})
      */
     private $defaultSort = 'random';
 
