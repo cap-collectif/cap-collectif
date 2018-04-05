f956746825093327e8d3357e123bac8c336da495
sanzodown <sanzodown@hotmail.fr>
2018-04-05 12:10:51 +0200

    test fix
    GitOrigin-RevId: fe93f77d3cd4ac0cad430d16ef18d6cf64ccd1b5

--- app/Resources/js/components/Proposal/Form/ProposalForm.js
+++ app/Resources/js/components/Proposal/Form/ProposalForm.js
@@ -111,8 +111,8 @@ type FormValues = {|
 //   });
 // };
 
-const onUnload = (event: SyntheticEvent<>) => {
-  event.preventDefault();
+const onUnload = event => {
+  return (event.returnValue = true);
 };
 
 const onSubmit = (values: FormValues, dispatch: Dispatch, props: Props) => {
