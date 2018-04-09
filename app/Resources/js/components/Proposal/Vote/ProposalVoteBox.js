056a272bdc640bf4badaf2677b38dea6572bf760
spyl94 <spyl94@gmail.com>
2018-04-09 19:45:10 +0200

    Implement addVote and removeVote mutations
    GitOrigin-RevId: 126703f3e3144f6b83948e023cdcd5dfe07c01e5

--- app/Resources/js/components/Proposal/Vote/ProposalVoteBox.js
+++ app/Resources/js/components/Proposal/Vote/ProposalVoteBox.js
@@ -1,11 +1,59 @@
 // @flow
 import * as React from 'react';
+<<<<<<< HEAD
 import ProposalVoteForm from './ProposalVoteForm';
+=======
+import { FormattedMessage } from 'react-intl';
+import { Row, Col } from 'react-bootstrap';
+import { connect, type MapStateToProps } from 'react-redux';
+import ProposalVoteForm from './ProposalVoteForm';
+import LoginButton from '../../User/Login/LoginButton';
+import ProposalVoteBoxMessage from './ProposalVoteBoxMessage';
+import { VOTE_TYPE_SIMPLE } from '../../../constants/ProposalConstants';
+import RegistrationButton from '../../User/Registration/RegistrationButton';
+import type { State } from '../../../types';
+>>>>>>> Implement addVote and removeVote mutations
 
 type Props = {
   proposal: Object,
   step: Object,
+<<<<<<< HEAD
 };
+=======
+  creditsLeft: number,
+  className: string,
+  formWrapperClassName: string,
+  isSubmitting: boolean,
+  user: ?Object,
+  features: Object,
+};
+
+class ProposalVoteBox extends React.Component<Props> {
+  static defaultProps = {
+    creditsLeft: null,
+    className: '',
+    formWrapperClassName: '',
+    user: null,
+  };
+
+  userHasEnoughCredits = () => {
+    const {
+      creditsLeft,
+      proposal,
+      user,
+      // step,
+    } = this.props;
+    if (user && creditsLeft !== null && proposal.estimation !== null) {
+      return creditsLeft >= proposal.estimation;
+    }
+    return true;
+  };
+
+  displayForm = () => {
+    const { step, user } = this.props;
+    return step.voteType === VOTE_TYPE_SIMPLE || (user && this.userHasEnoughCredits());
+  };
+>>>>>>> Implement addVote and removeVote mutations
 
 class ProposalVoteBox extends React.Component<Props> {
   render() {
@@ -19,5 +67,12 @@ class ProposalVoteBox extends React.Component<Props> {
     );
   }
 }
+<<<<<<< HEAD
+=======
+
+const mapStateToProps: MapStateToProps<*, *, *> = (state: State) => ({
+  features: state.default.features,
+});
+>>>>>>> Implement addVote and removeVote mutations
 
 export default ProposalVoteBox;
--- app/Resources/js/components/Proposal/Vote/ProposalVoteForm.js
+++ app/Resources/js/components/Proposal/Vote/ProposalVoteForm.js
@@ -1,11 +1,16 @@
 // @flow
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
+<<<<<<< HEAD
+=======
+import { connect, type MapStateToProps } from 'react-redux';
+>>>>>>> Implement addVote and removeVote mutations
 import { Alert } from 'react-bootstrap';
 import { reduxForm, Field } from 'redux-form';
 import { vote } from '../../../redux/modules/proposal';
 import { isEmail } from '../../../services/Validator';
 import renderComponent from '../../Form/Field';
+import type { State } from '../../../types';
 
 const form = 'proposalVote';
 const validate = (values, { anonymous }) => {
@@ -29,12 +34,23 @@ type Props = {
   proposal: Object,
   step: Object,
   handleSubmit: () => void,
+<<<<<<< HEAD
   error: string,
+=======
+  isPrivate: boolean,
+  anonymous: boolean,
+  error: string,
+  voteWithoutAccount: boolean,
+>>>>>>> Implement addVote and removeVote mutations
 };
 
 class ProposalVoteForm extends React.Component<Props> {
   render() {
+<<<<<<< HEAD
     const { error, handleSubmit } = this.props;
+=======
+    const { error, handleSubmit, anonymous, voteWithoutAccount } = this.props;
+>>>>>>> Implement addVote and removeVote mutations
     return (
       <form onSubmit={handleSubmit}>
         {error && (
@@ -42,6 +58,7 @@ class ProposalVoteForm extends React.Component<Props> {
             <p>{error}</p>
           </Alert>
         )}
+<<<<<<< HEAD
         <Field
           type="checkbox"
           component={renderComponent}
@@ -50,10 +67,51 @@ class ProposalVoteForm extends React.Component<Props> {
           disableValidation
           children={<FormattedMessage id="proposal.vote.form.private" />}
         />
+=======
+        {anonymous &&
+          voteWithoutAccount && (
+            <Field
+              type="text"
+              component={renderComponent}
+              name="username"
+              id="proposal-vote__username"
+              label={<FormattedMessage id="proposal.vote.form.username" />}
+            />
+          )}
+        {anonymous &&
+          voteWithoutAccount && (
+            <Field
+              type="email"
+              component={renderComponent}
+              name="email"
+              id="proposal-vote__email"
+              label={<FormattedMessage id="proposal.vote.form.email" />}
+            />
+          )}
+        {voteWithoutAccount || !anonymous ? null : (
+          <Field
+            type="checkbox"
+            component={renderComponent}
+            name="private"
+            id="proposal-vote__private"
+            disableValidation
+            children={<FormattedMessage id="proposal.vote.form.private" />}
+          />
+        )}
+>>>>>>> Implement addVote and removeVote mutations
       </form>
     );
   }
 }
+<<<<<<< HEAD
+=======
+
+const mapStateToProps: MapStateToProps<*, *, *> = (state: State) => ({
+  isPrivate: formValueSelector(form)(state, 'private') || false,
+  anonymous: state.user.user === null,
+  voteWithoutAccount: state.default.features.vote_without_account,
+});
+>>>>>>> Implement addVote and removeVote mutations
 
 export default reduxForm({
   form,
