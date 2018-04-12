14c1e037f83804489bc8c6a5901b326681a3cc54
spyl94 <spyl94@gmail.com>
2018-04-12 17:03:35 +0200

    Merge branch 'master' into 5172-searchengine-on-map
    GitOrigin-RevId: 0938b185b30c8a7c66da89d7b041989c92f41e42

--- app/Resources/js/config.js
+++ app/Resources/js/config.js
@@ -6,7 +6,7 @@ export const baseUrl = `${typeof window !== 'undefined' ? window.location.protoc
 export default {
   graphql: `${baseUrl}/graphql/`,
   api: `${baseUrl}/api`,
-  mapsAPIKey: '***REMOVED***',
+  mapsServerKey: '***REMOVED***',
   mapboxApiKey:
     '***REMOVED***',
   // https://github.com/elementalui/elemental/blob/master/src/constants.js
