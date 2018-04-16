af9876a2a364679696cbb90b841cdf3049a3d7d5
spyl94 <spyl94@gmail.com>
2018-04-16 11:52:49 +0200

    Clean
    GitOrigin-RevId: 09e9f84b5be3a92e5e249f277ecff07af9db54a7

--- app/DoctrineMigrations/Version20180410130847.php
+++ app/DoctrineMigrations/Version20180410130847.php
@@ -7,11 +7,6 @@ use Doctrine\DBAL\Schema\Schema;
 
 class Version20180410130847 extends IdToUuidMigration
 {
-    // public function up(Schema $schema)
-    // {
-    //     $this->addSql('DROP INDEX comment_vote_unique ON votes');
-    // }
-
     public function postUp(Schema $schema)
     {
         $this->migrate('event');
