f9137c834ab1772db20af471ded31fdefd535447
Amelie52 <amelie.tamisier@gmail.com>
2018-04-18 18:26:09 +0200

    [ADD] react beautiful dnd & [CHG] style for proposalUserVoteItem
    GitOrigin-RevId: cdbbe2c909cd7e7858a035c9ef6887e7bb33b8f4

--- app/Resources/js/components/Project/Votes/ProposalUserVoteItem.js
+++ app/Resources/js/components/Project/Votes/ProposalUserVoteItem.js
@@ -3,7 +3,8 @@ import React, { PropTypes } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { Button, Row, Col } from 'react-bootstrap';
-import UserLink from '../../User/UserLink';
+import Toggle from 'react-toggle';
+// import UserLink from '../../User/UserLink';
 import { VOTE_TYPE_BUDGET } from '../../../constants/ProposalConstants';
 import ProposalDetailsEstimation from '../../Proposal/Detail/ProposalDetailEstimation';
 import { deleteVote } from '../../../redux/modules/proposal';
@@ -18,41 +19,84 @@ export const ProposalUserVoteItem = React.createClass({
   render() {
     const { step, proposal, dispatch } = this.props;
 
-    const colWidth = proposal.district ? 2 : 3;
+
+    const colWidth = () => {
+      if(step.votesRanking === true && step.voteType === VOTE_TYPE_BUDGET) {
+        return 6;
+      }
+
+      if(step.votesRanking === true) {
+        return 8;
+      }
+
+      if(step.voteType === VOTE_TYPE_BUDGET) {
+        return 7;
+      }
+
+      return 9;
+    };
+
+    console.log(step);
+    console.warn(proposal);
 
     return (
-      <Row className="proposals-user-votes__row" id={`vote-step${step.id}-proposal${proposal.id}`}>
+      <Row className="proposals-user-votes__row d-flex" id={`vote-step${step.id}-proposal${proposal.id}`}>
+        <Col md={1} xs={12} className="proposals-user-votes__col" >
+          <div className="proposals-user-votes__content">
+            <i className="cap cap-android-menu excerpt" />
+            <div className="d-flex">
+              <span className="badge label-primary m-auto">4</span>
+            </div>
+          </div>
+        </Col>
         <Col
           className="proposals-user-votes__col"
-          md={colWidth === 3 ? colWidth + 1 : colWidth + 2}
+          md={colWidth()}
           xs={12}>
-          <a href={proposal.show_url}>{proposal.title}</a>
+          <div className="proposals-user-votes__content">
+            <a href={proposal.show_url}>{proposal.title}</a>
+          </div>
         </Col>
-        <Col className="proposals-user-votes__col" md={colWidth} xs={12}>
-          <i className="cap cap-user-2" />
-          <UserLink user={proposal.author} />
+        <Col className="proposals-user-votes__col" md={2} xs={12}>
+          <div className="proposals-user-votes__content">
+            <div className="d-flex">
+              <FormattedMessage id="public" />
+              <Toggle
+                className="ml-10"
+                // checked={proposal}
+                // onChange={() => {}}
+              />
+            </div>
+          </div>
         </Col>
-        {proposal.district && (
-          <Col className="proposals-user-votes__col" md={colWidth} xs={12}>
-            <i className="cap cap-marker-1" />
-            {proposal.district.name}
+        {/*<Col className="proposals-user-votes__col" md={2} xs={12}>*/}
+          {/*<div className="proposals-user-votes__content">*/}
+            {/*<div>*/}
+              {/*<span className="label label-primary">*/}
+                {/*Soumis au vote*/}
+              {/*</span>*/}
+            {/*</div>*/}
+          {/*</div>*/}
+        {/*</Col>*/}
+        {step.voteType === VOTE_TYPE_BUDGET &&
+          <Col className="proposals-user-votes__col" md={2} xs={12}>
+            <div className="proposals-user-votes__content">
+              <ProposalDetailsEstimation
+                proposal={proposal}
+                showNullEstimation={step.voteType === VOTE_TYPE_BUDGET}
+              />
+            </div>
           </Col>
-        )}
-        <Col className="proposals-user-votes__col" md={colWidth} xs={12}>
-          <ProposalDetailsEstimation
-            proposal={proposal}
-            showNullEstimation={step.voteType === VOTE_TYPE_BUDGET}
-          />
-        </Col>
-        <Col className="proposals-user-votes__col" md={colWidth} xs={12}>
-          <Button
+        }
+        <Col className="proposals-user-votes__col" md={1} xs={12}>
+          <a
             onClick={() => {
               deleteVote(dispatch, step, proposal);
             }}
             className="proposal-vote__delete"
             disabled={!step.open}>
-            <FormattedMessage id="project.votes.delete" />
-          </Button>
+            <i className="cap cap-ios-close" />
+          </a>
         </Col>
       </Row>
     );
