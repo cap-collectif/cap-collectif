46fa2890dba2e2aaae797492bf7b0f2d341a5cc2
Liinkiing <omar.jbara2@gmail.com>
2018-04-30 15:03:09 +0200

    - Removed old 'i' alias in all queries
    GitOrigin-RevId: 477a5fb69b991c2111aa5ce852198c43d94297e5

--- src/Capco/AppBundle/Repository/ThemeRepository.php
+++ src/Capco/AppBundle/Repository/ThemeRepository.php
@@ -43,7 +43,7 @@ class ThemeRepository extends EntityRepository
         }
 
         $qb = $this->getIsEnabledQueryBuilder();
-        $qb->addSelect('c, i')
+        $qb->addSelect('c')
             ->leftJoin('t.projects', 'c')
             ->leftJoin('t.events', 'events')
             ->leftJoin('t.posts', 'posts')
@@ -79,7 +79,7 @@ class ThemeRepository extends EntityRepository
     public function getOneBySlug($slug)
     {
         $qb = $this->getIsEnabledQueryBuilder()
-            ->addSelect('a', 'am', 'm', 'p', 'i', 'post', 'e')
+            ->addSelect('a', 'am', 'm', 'p', 'post', 'e')
             ->leftJoin('t.Author', 'a')
             ->leftJoin('a.Media', 'am')
             ->leftJoin('t.Media', 'm')
