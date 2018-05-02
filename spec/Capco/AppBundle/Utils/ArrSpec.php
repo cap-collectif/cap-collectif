f3ace7580218ddfe2b21a0559568fad69605e8a5
Liinkiing <omar.jbara2@gmail.com>
2018-05-02 09:54:47 +0200

    - Added PHP spec tests for Arr class
    GitOrigin-RevId: 09abfaa3446830aab55ba1ca130fcc3ddd76272a

---
+++ spec/Capco/AppBundle/Utils/ArrSpec.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace spec\Capco\AppBundle\Utils;
+
+use Capco\AppBundle\Utils\Arr;
+use PhpSpec\ObjectBehavior;
+
+class ArrSpec extends ObjectBehavior
+{
+
+    public function it_is_initializable(): void {
+        $this->shouldHaveType(Arr::class);
+    }
+
+    public function it_should_return_correct_value_based_on_simple_path(): void
+    {
+        $array = [
+            'test' => 'My value'
+        ];
+
+        $this::path($array, 'test')->shouldReturn('My value');
+    }
+
+    public function it_should_return_correct_value_based_on_complex_path_when_return_value_is_a_string(): void
+    {
+        $array = [
+            'nested' => [
+                'complex' => [
+                    'path' => [
+                        'value' => 'Que fais tu ici ?'
+                    ]
+                ]
+            ]
+        ];
+
+        $this::path($array, 'nested.complex.path.value')->shouldReturn('Que fais tu ici ?');
+    }
+
+    public function it_should_return_correct_value_based_on_complex_path_when_return_value_is_an_array(): void
+    {
+        $array = [
+            'nested' => [
+                'complex' => [
+                    'path' => [
+                        'value' => [
+                            'test' => 'My value'
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $this::path($array, 'nested.complex.path.value')->shouldReturn([
+            'test' => 'My value'
+        ]);
+    }
+
+    public function it_should_return_null_if_path_not_found(): void
+    {
+        $array = [
+            'nested' => [
+                'complex' => [
+                    'path' => [
+                        'value' => 'Que fais tu ici ?'
+                    ]
+                ]
+            ]
+        ];
+
+        $this::path($array, 'nested.complex.path.inexistant')->shouldReturn(null);
+    }
+}
