ebf51c983c9594ee8125da9d3d26291a3b572110
mauriau <maxime.auriau@protonmail.com>
2018-05-16 10:35:22 +0200

    [UPDATE] - migration file name
    GitOrigin-RevId: 8eeec8ce362ab796422a07cd99b461c57c2651bc

--- app/DoctrineMigrations/Version20180516131916.php
+++ app/DoctrineMigrations/Version20180516131916.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180503131916 extends AbstractMigration
+class Version20180516131916 extends AbstractMigration
 {
     /**
      * @param Schema $schema
