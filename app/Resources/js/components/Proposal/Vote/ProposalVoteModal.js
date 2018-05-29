1ce48dd81eafa72696af2d573e88df0d2446e1d2
ptondereau <pierre.tondereau@protonmail.com>
2018-05-29 12:01:37 +0200

    Merge pull request #5515 from cap-collectif/update-sf-2-8-41
    Update symfony to 2.8.41
    
    GitOrigin-RevId: f074cf794fbd40a3f689e423b1e6ce12bee0e04e

--- app/Resources/js/components/Proposal/Vote/ProposalVoteModal.js
+++ app/Resources/js/components/Proposal/Vote/ProposalVoteModal.js
@@ -3,8 +3,8 @@ import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { graphql, createFragmentContainer, commitLocalUpdate } from 'react-relay';
 import { ConnectionHandler } from 'relay-runtime';
-import { Modal, Panel, Label } from 'react-bootstrap';
-import { submit, isInvalid } from 'redux-form';
+import { Modal } from 'react-bootstrap';
+import { submit } from 'redux-form';
 import { connect, type MapStateToProps } from 'react-redux';
 import CloseButton from '../../Form/CloseButton';
 import SubmitButton from '../../Form/SubmitButton';
@@ -12,7 +12,6 @@ import { closeVoteModal, vote } from '../../../redux/modules/proposal';
 import ProposalsUserVotesTable from '../../Project/Votes/ProposalsUserVotesTable';
 import environment from '../../../createRelayEnvironment';
 import type { State, Dispatch } from '../../../types';
-import RequirementsForm, { formName } from '../../Requirements/RequirementsForm';
 import UpdateProposalVotesMutation from '../../../mutations/UpdateProposalVotesMutation';
 import type { ProposalVoteModal_proposal } from './__generated__/ProposalVoteModal_proposal.graphql';
 import type { ProposalVoteModal_step } from './__generated__/ProposalVoteModal_step.graphql';
@@ -26,7 +25,6 @@ type Props = ParentProps & {
   dispatch: Dispatch,
   showModal: boolean,
   isSubmitting: boolean,
-  invalid: boolean,
 };
 class ProposalVoteModal extends React.Component<Props> {
   componentDidUpdate(prevProps: Props) {
@@ -110,7 +108,7 @@ class ProposalVoteModal extends React.Component<Props> {
   };
 
   render() {
-    const { dispatch, showModal, proposal, step, invalid, isSubmitting } = this.props;
+    const { dispatch, showModal, proposal, step, isSubmitting } = this.props;
     return (
       <Modal
         animation={false}
@@ -126,28 +124,7 @@ class ProposalVoteModal extends React.Component<Props> {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {step.requirements.totalCount > 0 && (
-            <Panel
-              id="required-conditions"
-              defaultExpanded={!!step.requirements.viewerMeetsTheRequirements}>
-              {step.requirements.viewerMeetsTheRequirements && (
-                <Label>
-                  <FormattedMessage id="filled" />
-                </Label>
-              )}
-              {/* <Panel.Heading>
-                <Panel.Title>Conditions requises</Panel.Title>
-                <Panel.Toggle componentClass="a">Arrow</Panel.Toggle>
-              </Panel.Heading>
-              <Panel.Collapse>
-                <Panel.Body> */}
-              {step.requirements.reason}
-              <RequirementsForm step={step} />
-              {/* </Panel.Body> */}
-              {/* </Panel.Collapse> */}
-            </Panel>
-          )}
-          <h3 className="d-ib mr-10 mb-10">
+          <h3 className="d-ib mt-0 mr-10 mb-10">
             <FormattedMessage
               id={step.votesRanking ? 'modal-ranking' : 'proposal.vote.modal.title'}
             />
@@ -176,7 +153,6 @@ class ProposalVoteModal extends React.Component<Props> {
           <CloseButton className="pull-right" onClose={this.onHide} />
           <SubmitButton
             id="confirm-proposal-vote"
-            disabled={step.requirements.totalCount > 0 ? invalid : false}
             onSubmit={() => {
               dispatch(submit(`proposal-user-vote-form-step-${step.id}`));
             }}
@@ -197,7 +173,6 @@ const mapStateToProps: MapStateToProps<*, *, *> = (state: State, props: ParentPr
       state.proposal.currentVoteModal && state.proposal.currentVoteModal === props.proposal.id
     ),
     isSubmitting: !!state.proposal.isVoting,
-    invalid: isInvalid(formName)(state),
   };
 };
 
@@ -216,12 +191,6 @@ export default createFragmentContainer(container, {
       id
       votesRanking
       votesHelpText
-      requirements {
-        viewerMeetsTheRequirements
-        reason
-        totalCount
-      }
-      ...RequirementsForm_step
       ...ProposalsUserVotesTable_step
       viewerVotes(orderBy: { field: POSITION, direction: ASC }) {
         ...ProposalsUserVotesTable_votes
--- src/Capco/AppBundle/DataFixtures/ORM/Dev/LoaderDataFixture.php
+++ src/Capco/AppBundle/DataFixtures/ORM/Dev/LoaderDataFixture.php
@@ -50,8 +50,6 @@ class LoaderDataFixture extends AbstractLoader
             __DIR__ . '/CollectStep.yml',
             __DIR__ . '/SelectionStep.yml',
 
-            __DIR__ . '/Requirement.yml',
-
             __DIR__ . '/QuestionnaireStep.yml',
             __DIR__ . '/ProjectAbstractStep.yml',
 
