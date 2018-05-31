f1a4a25cc57ae3eb7088d5d1b92de3634ad88edb
spyl94 <spyl94@gmail.com>
2018-05-31 10:37:52 +0200

    Fix some errors
    GitOrigin-RevId: b589b6fde67120321506d2c30b3b954ec1208c26

--- src/Capco/AppBundle/GraphQL/Resolver/Proposal/ProposalVotesResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Proposal/ProposalVotesResolver.php
@@ -4,6 +4,7 @@ namespace Capco\AppBundle\GraphQL\Resolver\Proposal;
 
 use Capco\AppBundle\Entity\Proposal;
 use Capco\AppBundle\Entity\Steps\CollectStep;
+use Capco\AppBundle\Entity\Steps\SelectionStep;
 use Capco\AppBundle\Repository\AbstractStepRepository;
 use Capco\AppBundle\Repository\ProposalCollectVoteRepository;
 use Capco\AppBundle\Repository\ProposalSelectionVoteRepository;
@@ -35,30 +36,45 @@ class ProposalVotesResolver
         if ($args->offsetExists('stepId')) {
             try {
                 $step = $this->abstractStepRepository->find($args->offsetGet('stepId'));
+                if (!$step) {
+                    // Maybe throw an exception
+                    return $this->resolveAllVotes($proposal, $args);
+                }
+
                 $field = $args->offsetGet('orderBy')['field'];
                 $direction = $args->offsetGet('orderBy')['direction'];
+
                 if ($step instanceof CollectStep) {
                     $paginator = new Paginator(function (int $offset, int $limit) use ($proposal, $step, $field, $direction) {
                         return $this->proposalCollectVoteRepository->getByProposalAndStep($proposal, $step, $limit, $offset, $field, $direction)->getIterator()->getArrayCopy();
                     });
-
                     $totalCount = $this->proposalCollectVoteRepository->countVotesByProposalAndStep($proposal, $step);
-                } else {
+
+                    return $paginator->auto($args, $totalCount);
+                }
+
+                if ($step instanceof SelectionStep) {
                     $paginator = new Paginator(function (int $offset, int $limit) use ($proposal, $step, $field, $direction) {
                         return $this->proposalSelectionVoteRepository->getByProposalAndStep($proposal, $step, $limit, $offset, $field, $direction)->getIterator()->getArrayCopy();
                     });
-
                     $totalCount = $this->proposalSelectionVoteRepository->countVotesByProposalAndStep($proposal, $step);
+
+                    return $paginator->auto($args, $totalCount);
                 }
 
-                return $paginator->auto($args, $totalCount);
+                throw new \RuntimeException('Unknown step type.');
             } catch (\RuntimeException $exception) {
                 $this->logger->error(__METHOD__ . ' : ' . $exception->getMessage());
                 throw new \RuntimeException($exception->getMessage());
             }
         }
 
-        $paginator = new Paginator(function (int $offset, int $limit) {
+        return $this->resolveAllVotes($proposal, $args);
+    }
+
+    public function resolveAllVotes(Proposal $proposal, Argument $args): Connection
+    {
+        $paginator = new Paginator(function () {
             return [];
         });
 
