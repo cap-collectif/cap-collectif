eff9176282f53d27bbf7a7f601991c26434e1c81
spyl94 <spyl94@gmail.com>
2018-06-04 10:10:53 +0200

    Fix uniqueId with slug
    GitOrigin-RevId: a39c598dee7b2123814b4a410efd98eed9629949

--- app/Resources/js/components/User/Profile/Profile-test.js
+++ app/Resources/js/components/User/Profile/Profile-test.js
@@ -29,37 +29,6 @@ describe('<Profile />', () => {
       userType: 1,
       neighborhood: 'DTC',
     },
-    features: {
-      user_type: true,
-    },
-  };
-
-  const props2 = {
-    ...formMock,
-    intl: intlMock,
-    hasValue: {},
-    profileForm: {},
-    initialValues: {
-      media: {
-        id: 'media1',
-        name: 'media1',
-        size: '128*128',
-        url: 'http://imgur.com/15645613.jpg',
-      },
-      show_url: 'http://linkedin.com/user/dqkfjdsmq',
-      username: 'username',
-      biography: 'I am a fucking customer',
-      website: 'http://perdu.com',
-      facebookUrl: 'https://facebokk.com/mmm',
-      linkedInUrl: 'http://linkedin.com/neurchi',
-      twitterUrl: 'http://twitter.com/cuicui',
-      profilePageIndexed: false,
-      userType: 1,
-      neighborhood: 'DTC',
-    },
-    features: {
-      user_type: false,
-    },
   };
 
   const viewer = {
@@ -84,16 +53,9 @@ describe('<Profile />', () => {
     neighborhood: 'DTC',
   };
 
-  it('should render my profile with features user_type', () => {
+  it('should render my profile', () => {
     const wrapper = shallow(
       <Profile viewer={viewer} userTypes={[{ id: 1, name: 'type_1' }]} {...props} />,
-    );
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it('should render my profile without features user_type', () => {
-    const wrapper = shallow(
-      <Profile viewer={viewer} userTypes={[{ id: 1, name: 'type_1' }]} {...props2} />,
     );
     expect(wrapper).toMatchSnapshot();
   });
