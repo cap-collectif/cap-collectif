e3af4d3e8e18230c81ca542e25fcb39bb9f2152d
ptondereau <pierre.tondereau@protonmail.com>
2018-06-06 13:18:15 +0200

    fix sentry.io/572719841
    GitOrigin-RevId: 168f331c3b6ee13607e49f692690bbd7c5d0e80d

--- flow-typed/Cookies.js
+++
@@ -1,7 +0,0 @@
-declare class CookiesStatic {
-  set: (key: string, value: any, options: Object) => void,
-  getJSON: (key: string) => any,
-  get: (key: string) => any,
-};
-
-declare var Cookies: CookiesStatic;
