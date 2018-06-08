f3b93834f2d0c7762c3213d87516f7f07b84d371
spyl94 <spyl94@gmail.com>
2018-06-08 15:21:36 +0200

    Add a way to disable acl with context
    GitOrigin-RevId: 9e83fd3455da2dada41135acb3a51e97e8c644e2

--- src/Capco/AppBundle/Command/CreateCsvFromUserCommand.php
+++ src/Capco/AppBundle/Command/CreateCsvFromUserCommand.php
@@ -29,11 +29,10 @@ class CreateCsvFromUserCommand extends ContainerAwareCommand
         $writer = WriterFactory::create(Type::CSV);
         $writer->openToFile($this->getPath());
 
-        // TODO disable ACL or give admin rights (to disable access)
         $data = $executor->disabledDebugInfo()->execute([
             'query' => $this->getUserGraphQLQuery($userId),
             'variables' => [],
-        ])->toArray();
+        ], ['disable_acl' => true])->toArray();
 
         if (isset($data['errors'])) {
             var_dump($data['errors']);
