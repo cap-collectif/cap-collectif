9b6276f05fefdc001b9a07b82d68b7150379715a
Amelie52 <amelie.tamisier@gmail.com>
2018-06-12 14:26:13 +0200

    [CHG] es5 to es6 for Reply components
    GitOrigin-RevId: bb36ca71f436f624f7b23895090c283a99e8e1ac

--- app/Resources/js/components/Reply/Form/ButtonBody.js
+++ app/Resources/js/components/Reply/Form/ButtonBody.js
@@ -3,23 +3,25 @@ import * as React from 'react';
 import Linkify from 'react-linkify';
 import { FormattedMessage } from 'react-intl';
 
-const ButtonBody = React.createClass({
-  propTypes: {
-    body: React.PropTypes.string.isRequired,
-  },
+type Props = {
+  body: string,
+};
 
-  getInitialState() {
-    return {
-      expanded: false,
-    };
-  },
+type State = {
+  expanded: boolean,
+};
 
-  textShouldBeTruncated(): boolean {
+class ButtonBody extends React.Component<Props, State> {
+  state = {
+    expanded: false,
+  };
+
+  textShouldBeTruncated = (): boolean => {
     const { body } = this.props;
     return body.length > 700;
-  },
+  };
 
-  generateText(): string {
+  generateText = (): string => {
     const { body } = this.props;
     let text = '';
 
@@ -35,15 +37,15 @@ const ButtonBody = React.createClass({
     }
 
     return text;
-  },
+  };
 
-  expand(expanded): void {
+  expand = (expanded: $FlowFixMe): void => {
     this.setState({
       expanded,
     });
-  },
+  };
 
-  renderReadMoreOrLess() {
+  renderReadMoreOrLess = () => {
     if (this.textShouldBeTruncated() && !this.state.expanded) {
       return (
         <button className="btn-link" onClick={this.expand.bind(this, true)}>
@@ -51,7 +53,7 @@ const ButtonBody = React.createClass({
         </button>
       );
     }
-  },
+  };
 
   render() {
     return (
@@ -62,7 +64,7 @@ const ButtonBody = React.createClass({
         {this.renderReadMoreOrLess()}
       </div>
     );
-  },
-});
+  }
+}
 
 export default ButtonBody;
--- app/Resources/js/components/Reply/Form/ReplyForm-test.js
+++ app/Resources/js/components/Reply/Form/ReplyForm-test.js
@@ -1,84 +1,176 @@
+// @flow
 /* eslint-env jest */
 import React from 'react';
 import { shallow } from 'enzyme';
-import { intlMock, formMock } from '../../../mocks';
+import { intlMock, formMock, $refType } from '../../../mocks';
 import { ReplyForm } from './ReplyForm';
 
 describe('<ReplyForm />', () => {
+  const questionnaireProps = {
+    multipleRepliesAllowed: true,
+    anonymousAllowed: true,
+    description: null,
+    $refType,
+    title: '',
+    id: '2',
+    phoneConfirmationRequired: false,
+    contribuable: true,
+  };
+
   const requiredText = {
-    id: 10,
+    id: '10',
     type: 'text',
-    slug:
-      'etes-vous-satisfait-que-la-ville-de-paris-soit-candidate-a-l-organisation-des-jo-de-2024',
-    question:
+    title:
       "Êtes-vous satisfait que la ville de Paris soit candidate à l'organisation des JO de 2024 ?",
     helpText: '',
+    position: 1,
+    validationRule: null,
+    description: 'description of question',
+    private: false,
     required: true,
+    isOtherAllowed: false,
+    choices: null,
   };
 
   const facultativeCheckbox = {
-    id: 11,
+    id: '11',
     type: 'checkbox',
-    slug: 'pour-quel-type-d-epreuve-etes-vous-pret-a-acheter-des-places',
-    question: "Pour quel type d'épreuve êtes vous prêt à acheter des places",
+    title: "Pour quel type d'épreuve êtes vous prêt à acheter des places",
     helpText: 'Plusieurs choix sont possibles',
+    position: 2,
+    validationRule: null,
+    description: 'description of question',
+    private: false,
     required: false,
     isOtherAllowed: true,
     choices: [
-      { id: 20, label: 'Athlétisme' },
-      { id: 21, label: 'Natation' },
-      { id: 22, label: 'Sports collectifs' },
-      { id: 23, label: 'Sports individuels' },
+      { id: '20', title: 'Athlétisme', color: null, image: null, description: 'description' },
+      { id: '21', title: 'Natation', color: null, image: null, description: 'description' },
+      {
+        id: '22',
+        title: 'Sports collectifs',
+        color: null,
+        image: null,
+        description: 'description',
+      },
+      {
+        id: '23',
+        title: 'Sports individuels',
+        color: null,
+        image: null,
+        description: 'description',
+      },
     ],
   };
 
   const requiredRadio = {
-    id: 12,
+    id: '12',
     type: 'radio',
-    slug: 'quel-est-ton-athlete-favori',
-    question: 'Quel est ton athlète favori ?',
+    title: 'Quel est ton athlète favori ?',
     helpText: 'Un seul choix possible',
+    position: 3,
+    validationRule: null,
+    description: 'description of question',
+    private: false,
     required: true,
     isOtherAllowed: true,
     choices: [
-      { id: 24, label: 'Maxime Arrouard' },
-      { id: 25, label: 'Spylou Super Sayen' },
-      { id: 26, label: 'Cyril Lage' },
-      { id: 27, label: 'Superman' },
+      { id: '24', title: 'Maxime Arrouard', color: null, image: null, description: 'description' },
+      {
+        id: '25',
+        title: 'Spylou Super Sayen',
+        color: null,
+        image: null,
+        description: 'description',
+      },
+      { id: '26', title: 'Cyril Lage', color: null, image: null, description: 'description' },
+      { id: '27', title: 'Superman', color: null, image: null, description: 'description' },
     ],
   };
 
   const facultativeSelect = {
-    id: 13,
+    id: '13',
     type: 'select',
-    slug: 'nelson-monfort-parle-t-il',
-    question: 'Nelson Monfort parle-t-il:',
+    title: 'Nelson Monfort parle-t-il:',
     helpText: 'Merci de répondre sincèrement',
+    position: 4,
+    validationRule: null,
+    description: 'description of question',
+    private: false,
     required: false,
+    isOtherAllowed: false,
     choices: [
-      { id: 28, label: 'Pas assez fort (Mon sonotone est en panne)' },
-      { id: 29, label: 'Assez fort (Mon sonotone est mal réglé)' },
-      { id: 30, label: 'Trop fort (Mon sonotone est tout neuf)' },
+      {
+        id: '28',
+        title: 'Pas assez fort (Mon sonotone est en panne)',
+        color: null,
+        image: null,
+        description: 'description',
+      },
+      {
+        id: '29',
+        title: 'Assez fort (Mon sonotone est mal réglé)',
+        color: null,
+        image: null,
+        description: 'description',
+      },
+      {
+        id: '30',
+        title: 'Trop fort (Mon sonotone est tout neuf)',
+        color: null,
+        image: null,
+        description: 'description',
+      },
     ],
   };
 
   const facultativeRanking = {
-    id: 14,
+    id: '14',
     type: 'ranking',
-    slug: 'nelson-monfort-parle-t-il',
-    question: 'Nelson Monfort parle-t-il:',
+    title: 'Nelson Monfort parle-t-il:',
     helpText: 'Merci de répondre sincèrement',
+    position: 5,
+    validationRule: null,
+    description: 'description of question',
+    private: false,
     required: false,
+    isOtherAllowed: false,
     choices: [
-      { id: 28, label: 'Pas assez fort (Mon sonotone est en panne)' },
-      { id: 29, label: 'Assez fort (Mon sonotone est mal réglé)' },
-      { id: 30, label: 'Trop fort (Mon sonotone est tout neuf)' },
+      {
+        id: '28',
+        title: 'Pas assez fort (Mon sonotone est en panne)',
+        color: null,
+        image: null,
+        description: 'description',
+      },
+      {
+        id: '29',
+        title: 'Assez fort (Mon sonotone est mal réglé)',
+        color: null,
+        image: null,
+        description: 'description',
+      },
+      {
+        id: '30',
+        title: 'Trop fort (Mon sonotone est tout neuf)',
+        color: null,
+        image: null,
+        description: 'description',
+      },
     ],
   };
 
   const props = {
     intl: intlMock,
     ...formMock,
+    responses: [
+      { question: 'Nelson Monfort parle-t-il:', value: 'Trop fort (Mon sonotone est tout neuf)' },
+      {
+        question: "Pour quel type d'épreuve êtes vous prêt à acheter des places",
+        value: { labels: ['Superman'], other: 'Pikachu' },
+      },
+    ],
+    user: null,
   };
 
   it('should render correctly with equal required and facultative fields', () => {
@@ -86,6 +178,7 @@ describe('<ReplyForm />', () => {
       <ReplyForm
         questionnaire={{
           questions: [requiredText, requiredRadio, facultativeSelect, facultativeRanking],
+          ...questionnaireProps,
         }}
         {...props}
       />,
@@ -104,6 +197,7 @@ describe('<ReplyForm />', () => {
             facultativeSelect,
             facultativeRanking,
           ],
+          ...questionnaireProps,
         }}
         {...props}
       />,
@@ -114,7 +208,10 @@ describe('<ReplyForm />', () => {
   it('should render correctly with majority of required fields', () => {
     const wrapper = shallow(
       <ReplyForm
-        questionnaire={{ questions: [requiredText, requiredRadio, facultativeRanking] }}
+        questionnaire={{
+          questions: [requiredText, requiredRadio, facultativeRanking],
+          ...questionnaireProps,
+        }}
         {...props}
       />,
     );
@@ -123,7 +220,10 @@ describe('<ReplyForm />', () => {
 
   it('should render correctly with only required fields', () => {
     const wrapper = shallow(
-      <ReplyForm questionnaire={{ questions: [requiredText, requiredRadio] }} {...props} />,
+      <ReplyForm
+        questionnaire={{ questions: [requiredText, requiredRadio], ...questionnaireProps }}
+        {...props}
+      />,
     );
     expect(wrapper).toMatchSnapshot();
   });
@@ -133,6 +233,7 @@ describe('<ReplyForm />', () => {
       <ReplyForm
         questionnaire={{
           questions: [facultativeCheckbox, facultativeSelect, facultativeRanking],
+          ...questionnaireProps,
         }}
         {...props}
       />,
