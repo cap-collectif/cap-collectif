0b0cbaf27c936149ed6bcc06becbd68d53682b7d
Amelie52 <amelie.tamisier@gmail.com>
2018-06-27 17:28:29 +0200

    [ADD] modal & outline btn
    GitOrigin-RevId: 2aba6c74c21c27854fdbf2e3d7449506bbc0aad9

--- app/Resources/js/stories/Components-stories.js
+++ app/Resources/js/stories/Components-stories.js
@@ -2,7 +2,9 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { ProgressBar } from 'react-bootstrap';
+import { Label, Alert, Button, DropdownButton, MenuItem, ProgressBar, Modal } from 'react-bootstrap';
+import { AlertForm } from "../components/Alert/AlertForm";
+
 import Pagination from '../components/Utils/Pagination';
 import { Progress } from '../components/Ui/Progress';
 import InlineList from '../components/Ui/List/InlineList';
@@ -41,6 +43,18 @@ const toComeProject = {
     className: 'progress-bar_empty'
   },
 };
+
+const labels = ['default','primary','success','warning','danger','info'];
+const buttons = ['default','primary','success','warning','danger','info','link'];
+const buttonsOutline = ['success','warning','danger'];
+const alerts = ['success','warning','danger','info'];
+
+const alertFormProps = [
+  { description: 'Custom error message', values: { valid:false, errorMessage:"My message", invalid:true, submitSucceeded:true, submitFailed:true, submitting:false }},
+  { description: 'Submit succeeded (disappear after 10s)', values: { valid:true, invalid:false, submitSucceeded:true, submitFailed:false, submitting:false}},
+  { description: 'Submit failed', values: { valid:true, invalid:false, submitSucceeded:false, submitFailed:true, submitting:false}},
+  { description: 'Invalid form', values: { valid:false, invalid:true, submitSucceeded:true, submitFailed:false, submitting:false}},
+];
 
 storiesOf('Components', module)
   .add('Progress', withInfo({
@@ -135,7 +149,7 @@ storiesOf('Components', module)
       <div className="ml-30 mr-30 storybook-container">
         <h1>
           List {' '}
-          <a href="https://github.com/cap-collectif/platform/blob/master/app/Resources/js/stories/Style-stories.js">
+          <a href="https://github.com/cap-collectif/platform/blob/master/app/Resources/js/stories/Components-stories.js">
             <i className="small cap cap-setting-gear-1"/>
           </a>
         </h1>
@@ -199,6 +213,197 @@ storiesOf('Components', module)
             current={3}
             onChange={() => {}}
           />
+        </div>
+      </div>
+    );
+  }))
+  .add('Labels', withInfo({
+    source: false,
+    text :`
+      <h2>How to use</h2>
+    
+      ~~~jsx
+      <Label bsStyle={labelStyle}>{label}</Label>
+      ~~~
+    
+    `})(() => {
+    return (
+      <div className="ml-30 mr-30 storybook-container">
+        <h1>
+          Labels {' '}
+          <a href="https://github.com/cap-collectif/platform/blob/master/app/Resources/js/stories/Components-stories.js">
+            <i className="small cap cap-setting-gear-1"/>
+          </a>
+        </h1>
+        <hr/>
+        <div className="row">
+          {labels.map(label => (
+            <div className="col-sm-1 col-xs-6 mb-20">
+              <Label bsStyle={label}>{label}</Label>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }))
+  .add('Alerts', withInfo({
+    source: false,
+    text :`
+      <h2>How to use</h2>
+    
+      ~~~jsx
+      // Basic alert
+      <Alert bsStyle={alertStyle}>{alert}</Alert>
+      
+      // Form alert
+      <Pagination
+        <AlertForm
+          valid
+          invalid={false}
+          submitting={false}
+          submitSucceeded={false}
+          submitFailed={false}
+        />
+      />
+      ~~~
+    
+    `})(() => {
+    return (
+      <div className="ml-30 mr-30 storybook-container">
+        <h1>
+          Alerts {' '}
+          <a href="https://github.com/cap-collectif/platform/blob/master/app/Resources/js/stories/Components-stories.js">
+            <i className="small cap cap-setting-gear-1"/>
+          </a>
+        </h1>
+        <hr/>
+        <h3>Basic alert</h3>
+        <div className="row">
+          {alerts.map(alert => (
+            <div className="col-sm-3 col-xs-12 mb-10">
+              <Alert bsStyle={alert}>{alert} alert</Alert>
+            </div>
+          ))}
+        </div>
+        <h3>Form alert</h3>
+        {alertFormProps.map(props => (
+          <div className="mb-20">
+            <p>{props.description}</p>
+            <AlertForm
+              {...props.values}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }))
+  .add('Buttons', withInfo({
+    source: false,
+    text :`
+      <h2>How to use</h2>
+    
+      ~~~jsx
+      // basic button
+      <Button bsStyle={buttonStyle}>{button}</Button>
+      
+      //dropdown button
+      <DropdownButton
+        open={false}
+        title={buttonTitle}
+      >
+        <MenuItem eventKey="1">Action</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey="2" active>Active Item</MenuItem>
+      </DropdownButton>
+      ~~~
+    
+    `})(() => {
+    return (
+      <div className="ml-30 mr-30 storybook-container">
+        <h1>
+          Buttons {' '}
+          <a href="https://github.com/cap-collectif/platform/blob/master/app/Resources/js/stories/Components-stories.js">
+            <i className="small cap cap-setting-gear-1"/>
+          </a>
+        </h1>
+        <hr/>
+        <div className="row">
+          {buttons.map(button => (
+            <div className="col-md-1 col-sm-2 col-xs-12 mb-20">
+              <Button bsStyle={button}>{button}</Button>
+            </div>
+          ))}
+          {buttonsOutline.map(button => (
+            <div className="col-md-1 col-sm-2 col-xs-12 mb-20">
+              <Button bsStyle={button} className="btn--outline">{button}</Button>
+            </div>
+          ))}
+          <div className="col-md-1 col-sm-2 col-xs-12 mb-20">
+            <DropdownButton
+              bsStyle="primary"
+              title="dropdown button"
+              key="1"
+              id="dropdown-basic"
+            >
+              <MenuItem header>Header</MenuItem>
+              <MenuItem eventKey="1">Action</MenuItem>
+              <MenuItem eventKey="3" active>
+                Active Item
+              </MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey="4">Separated link</MenuItem>
+            </DropdownButton>
+          </div>
+        </div>
+      </div>
+    );
+  }))
+  .add('Modal', withInfo({
+    source: false,
+    propTablesExclude: [Button],
+    text :`
+      <h2>How to use</h2>
+    
+      ~~~jsx
+      <div className="static-modal">
+        <Modal.Dialog>
+          <Modal.Header>
+            <Modal.Title>Modal title</Modal.Title>
+          </Modal.Header>
+      
+          <Modal.Body>body...</Modal.Body>
+      
+          <Modal.Footer>
+            <Button>Close</Button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      </div>;
+      ~~~
+    
+    `})(() => {
+    return (
+      <div className="ml-30 mr-30 storybook-container">
+        <h1>
+          Modal {' '}
+          <a href="https://github.com/cap-collectif/platform/blob/master/app/Resources/js/stories/Components-stories.js">
+            <i className="small cap cap-setting-gear-1"/>
+          </a>
+        </h1>
+        <hr/>
+        <div
+          className="static-modal position-relative"
+        >
+          <Modal.Dialog className="position-relative">
+            <Modal.Header>
+              <Modal.Title>Modal title</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>body...</Modal.Body>
+
+            <Modal.Footer>
+              <Button>Close</Button>
+            </Modal.Footer>
+          </Modal.Dialog>
         </div>
       </div>
     );
--- app/Resources/js/stories/Style-stories.js
+++ app/Resources/js/stories/Style-stories.js
@@ -1,11 +1,8 @@
 // @flow
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { Label, Alert, Button, DropdownButton, MenuItem } from 'react-bootstrap';
 import styled from 'styled-components';
 import { FontStyle } from './FontStyle';
-import { AlertForm } from "../components/Alert/AlertForm";
 
 export const ColorContainer = styled.div`
   height: 50px;
@@ -30,17 +27,6 @@ const colors = [
   { hex: '#0388cc', description:'$primary-color' },
   { hex: '#088A20', description:'$success-color' },
   { hex: '#1D8293', description:'$info-color' },
-];
-
-const labels = ['default','primary','success','warning','danger','info'];
-const buttons = ['default','primary','success','warning','danger','info','link'];
-const alerts = ['success','warning','danger','info'];
-
-const alertFormProps = [
-  { description: 'Custom error message', values: { valid:false, errorMessage:"My message", invalid:true, submitSucceeded:true, submitFailed:true, submitting:false }},
-  { description: 'Submit succeeded (disappear after 10s)', values: { valid:true, invalid:false, submitSucceeded:true, submitFailed:false, submitting:false}},
-  { description: 'Submit failed', values: { valid:true, invalid:false, submitSucceeded:false, submitFailed:true, submitting:false}},
-  { description: 'Invalid form', values: { valid:false, invalid:true, submitSucceeded:true, submitFailed:false, submitting:false}},
 ];
 
 storiesOf('UI', module)
@@ -78,113 +64,6 @@ storiesOf('UI', module)
               </div>
             </div>
           ))}
-        </div>
-      </div>
-    );
-  })
-  .add('Labels',() => {
-    return (
-      <div className="ml-30 mr-30 storybook-container">
-        <h1>
-          Labels {' '}
-          <a href="https://github.com/cap-collectif/platform/blob/master/app/Resources/js/stories/Style-stories.js">
-            <i className="small cap cap-setting-gear-1"/>
-          </a>
-        </h1>
-        <hr/>
-        <div className="row">
-          {labels.map(label => (
-            <div className="col-sm-1 col-xs-6 mb-20">
-              <Label bsStyle={label}>{label}</Label>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  })
-  .add('Alerts',withInfo({
-    source: false,
-    text :`
-      <h2>How to use</h2>
-    
-      ~~~jsx
-      // Basic alert
-      <Alert bsStyle="success">My alert</Alert>
-      
-      // Form alert
-      <Pagination
-        <AlertForm
-          valid
-          invalid={false}
-          submitting={false}
-          submitSucceeded={false}
-          submitFailed={false}
-        />
-      />
-      ~~~
-    
-    `})(() => {
-    return (
-      <div className="ml-30 mr-30 storybook-container">
-        <h1>
-          Alerts {' '}
-          <a href="https://github.com/cap-collectif/platform/blob/master/app/Resources/js/stories/Style-stories.js">
-            <i className="small cap cap-setting-gear-1"/>
-          </a>
-        </h1>
-        <hr/>
-        <h3>Basic alert</h3>
-        <div className="row">
-          {alerts.map(alert => (
-            <div className="col-sm-3 col-xs-12 mb-10">
-              <Alert bsStyle={alert}>{alert} alert</Alert>
-            </div>
-          ))}
-        </div>
-        <h3>Form alert</h3>
-          {alertFormProps.map(props => (
-            <div className="mb-20">
-              <p>{props.description}</p>
-              <AlertForm
-                {...props.values}
-              />
-            </div>
-          ))}
-      </div>
-    );
-  }))
-  .add('Buttons',() => {
-    return (
-      <div className="ml-30 mr-30 storybook-container">
-        <h1>
-          Labels {' '}
-          <a href="https://github.com/cap-collectif/platform/blob/master/app/Resources/js/stories/Style-stories.js">
-            <i className="small cap cap-setting-gear-1"/>
-          </a>
-        </h1>
-        <hr/>
-        <div className="row">
-          {buttons.map(button => (
-            <div className="col-sm-1 col-xs-6 mb-20">
-              <Button bsStyle={button}>{button}</Button>
-            </div>
-          ))}
-          <div>
-            <DropdownButton
-              bsStyle="primary"
-              title="dropdown button"
-              key="1"
-              id="dropdown-basic"
-            >
-              <MenuItem eventKey="1">Action</MenuItem>
-              <MenuItem eventKey="2">Another action</MenuItem>
-              <MenuItem eventKey="3" active>
-                Active Item
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="4">Separated link</MenuItem>
-            </DropdownButton>
-          </div>
         </div>
       </div>
     );
