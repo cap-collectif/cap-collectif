142ecb13569592db33512b34acae188c2b5d8e13
spyl94 <spyl94@gmail.com>
2018-07-03 11:56:16 +0200

    Try
    GitOrigin-RevId: 061d54e4733d67acba1ab5b603cfa6686819b033

--- infrastructure/services/builder/build.sh
+++ infrastructure/services/builder/build.sh
@@ -50,7 +50,7 @@ else
   composer dump-autoload
 
   # Frontend deps
-  yarn install --pure-lockfile
+  yarn install --pure-lockfile --production=false
   bower install --config.interactive=false --allow-root
 
   echo "Testing node-sass binding..."
