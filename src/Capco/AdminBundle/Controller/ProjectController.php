64d105ef8991280ab06daa02aa7e667e9f6eefc4
mauriau <maxime.auriau@protonmail.com>
2018-07-04 16:06:50 +0200

    [ADD] - preview button in BO project
    GitOrigin-RevId: adaa3850516c10a0c277766603681a9839f3c22e

---
+++ src/Capco/AdminBundle/Controller/ProjectController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Capco\AdminBundle\Controller;
+
+use Capco\AppBundle\Entity\Project;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProjectController extends Controller
+{
+    /**
+     * @Security("has_role('ROLE_ADMIN')")
+     * @Route("/admin/capco/app/project/{projectId}/preview", name="capco_admin_project_preview")
+     * @ParamConverter("project", options={"mapping": {"projectId": "id"}})
+     */
+    public function previewAction(Request $request, Project $project): Response
+    {
+        $projectUrlResolver = $this->container->get('Capco\AppBundle\GraphQL\Resolver\Project\ProjectUrlResolver');
+
+        return new RedirectResponse($projectUrlResolver->__invoke($project));
+    }
+}
