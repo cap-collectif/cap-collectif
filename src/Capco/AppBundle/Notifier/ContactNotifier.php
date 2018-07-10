18b3129b78b51661e9566c2a3ea11be11375b7ea
spyl94 <spyl94@gmail.com>
2018-07-10 17:27:47 +0200

    Merge pull request #5731 from cap-collectif/fix-arguments
    Fix arguments refetching
    
    GitOrigin-RevId: 4d323ca270bee1939005630efff7ce20fd81938d

--- src/Capco/AppBundle/Notifier/ContactNotifier.php
+++ src/Capco/AppBundle/Notifier/ContactNotifier.php
@@ -1,19 +1,26 @@
 <?php
-
 namespace Capco\AppBundle\Notifier;
 
 use Capco\AppBundle\Mailer\Message\User\ContactMessage;
 
 final class ContactNotifier extends BaseNotifier
 {
-    public function onContact(string $recipient, string $senderEmail, string $senderName, string $message)
-    {
-        $this->mailer->sendMessage(ContactMessage::create(
-            $recipient,
-            $senderEmail,
-            $senderName,
-            $message,
-            $this->siteParams->getValue('global.site.fullname')
-        ));
+    public function onContact(
+        string $recipient,
+        string $senderEmail,
+        string $senderName,
+        string $message,
+        string $siteUrl
+    ) {
+        $this->mailer->sendMessage(
+            ContactMessage::create(
+                $recipient,
+                $senderEmail,
+                $senderName,
+                $message,
+                $this->siteParams->getValue('global.site.fullname'),
+                $siteUrl
+            )
+        );
     }
 }
