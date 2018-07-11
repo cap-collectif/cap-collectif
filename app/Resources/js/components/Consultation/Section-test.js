3ca6a26d1d8c1ec27a7cc0efd0fabfb0a1c32064
ptondereau <pierre.tondereau@protonmail.com>
2018-07-11 22:17:42 +0200

    Merge pull request #5761 from cap-collectif/5753-fix-comment-related-soft-delete
    Fix comment block with a soft deleted proposal
    
    GitOrigin-RevId: f7f3b9f5c0d035787752e935fdc069fd584512c5

--- app/Resources/js/components/Consultation/Section-test.js
+++ app/Resources/js/components/Consultation/Section-test.js
@@ -3,10 +3,19 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { Section } from './Section';
+import { $refType, $fragmentRefs } from '../../mocks';
 
 describe('<Section />', () => {
   const props = {
-    section: {},
+    section: {
+      $refType,
+      $fragmentRefs,
+      contribuable: true,
+      contributionsCount: 0,
+      slug: 'slug',
+      subtitle: 'subtitle',
+      title: 'title',
+    },
     consultation: {},
     level: 0,
   };
