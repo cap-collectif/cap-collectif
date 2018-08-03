400647d6e747f82608892199b5af0bb23bf1704b
mauriau <maxime.auriau@protonmail.com>
2018-08-03 17:07:49 +0200

    fix projectUrlResolver and map with all project type
    GitOrigin-RevId: e47e4768298b45eb1926ef66fa861d36704a116a

--- src/Capco/AppBundle/GraphQL/Resolver/Project/ProjectUrlResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Project/ProjectUrlResolver.php
@@ -4,6 +4,11 @@ namespace Capco\AppBundle\GraphQL\Resolver\Project;
 use Capco\AppBundle\Entity\Project;
 use Capco\AppBundle\Entity\Steps\CollectStep;
 use Capco\AppBundle\Entity\Steps\ConsultationStep;
+use Capco\AppBundle\Entity\Steps\OtherStep;
+use Capco\AppBundle\Entity\Steps\QuestionnaireStep;
+use Capco\AppBundle\Entity\Steps\RankingStep;
+use Capco\AppBundle\Entity\Steps\SelectionStep;
+use Capco\AppBundle\Entity\Steps\SynthesisStep;
 use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\Router;
@@ -21,16 +26,25 @@ class ProjectUrlResolver implements ResolverInterface
     {
         $projectSlug = $project->getSlug();
         $firstStep = $project->getFirstStep();
-        $routeName = 'app_consultation_show_presentation';
-        if ($firstStep instanceof CollectStep) {
-            $routeName = 'app_project_show_collect';
-        }
-        if ($firstStep instanceof ConsultationStep) {
-            $routeName = 'app_project_show_consultation';
-        }
         // if no step, so we redirect to projects list page
         if (!$firstStep) {
             return $this->router->generate('app_project', UrlGeneratorInterface::ABSOLUTE_URL);
+        }
+        $routeName = 'app_consultation_show_presentation';
+        if ($firstStep instanceof CollectStep) {
+            $routeName = 'app_project_show_collect';
+        } elseif ($firstStep instanceof ConsultationStep) {
+            $routeName = 'app_project_show_consultation';
+        } elseif ($firstStep instanceof QuestionnaireStep) {
+            $routeName = 'app_project_show_questionnaire';
+        } elseif ($firstStep instanceof SelectionStep) {
+            $routeName = 'app_project_show_selection';
+        } elseif ($firstStep instanceof SynthesisStep) {
+            $routeName = 'app_project_show_synthesis';
+        } elseif ($firstStep instanceof OtherStep) {
+            $routeName = 'app_project_show_step';
+        } elseif ($firstStep instanceof RankingStep) {
+            $routeName = 'app_project_show_ranking';
         }
 
         return $this->router->generate(
