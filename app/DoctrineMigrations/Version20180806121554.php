8330929f0c162c564c1744dcd92cd02bf0eb75da
ptondereau <pierre.tondereau@protonmail.com>
2018-08-06 12:06:16 +0200

    up to date migration
    GitOrigin-RevId: cb2abe897f8034582c537cdc6996891b4b1bb8c8

--- app/DoctrineMigrations/Version20180806121554.php
+++ app/DoctrineMigrations/Version20180806121554.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180801091554 extends AbstractMigration
+final class Version20180806121554 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
