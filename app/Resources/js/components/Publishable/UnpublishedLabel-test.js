2e491d58bc48714b2c4504c8467ba87abf660d91
spyl94 <spyl94@gmail.com>
2018-08-07 14:19:47 +0200

    Add unit test
    GitOrigin-RevId: 684cee84f6dad844ae097408ee2a7b781a564ff4

---
+++ app/Resources/js/components/Publishable/UnpublishedLabel-test.js
@@ -0,0 +1,47 @@
+// @flow
+/* eslint-env jest */
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { UnpublishedLabel } from './UnpublishedLabel';
+import { $refType } from '../../mocks';
+
+describe('<UnpublishedLabel />', () => {
+  it('renders when published', () => {
+    const publishablePublished = { $refType, id: 'id1', published: true, notPublishedReason: null };
+    const wrapper = shallow(<UnpublishedLabel publishable={publishablePublished} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders when not published because author must confirm', () => {
+    const publishableUnublishedWaiting = {
+      $refType,
+      id: 'id1',
+      published: false,
+      notPublishedReason: 'WAITING_AUTHOR_CONFIRMATION',
+    };
+    const wrapper = shallow(<UnpublishedLabel publishable={publishableUnublishedWaiting} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders when not published because author is not confirmed', () => {
+    const publishableUnublishedNotConfirmed = {
+      $refType,
+      id: 'id1',
+      published: false,
+      notPublishedReason: 'AUTHOR_NOT_CONFIRMED',
+    };
+    const wrapper = shallow(<UnpublishedLabel publishable={publishableUnublishedNotConfirmed} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders when not published because author confirmed his account too late', () => {
+    const publishableUnublishedTooLate = {
+      $refType,
+      id: 'id1',
+      published: false,
+      notPublishedReason: 'AUTHOR_CONFIRMED_TOO_LATE',
+    };
+    const wrapper = shallow(<UnpublishedLabel publishable={publishableUnublishedTooLate} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
