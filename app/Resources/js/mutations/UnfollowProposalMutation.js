3e848103333c12a8f992abea47121e892c6dcda7
ptondereau <pierre.tondereau@protonmail.com>
2018-08-08 15:25:55 +0200

    add updater logic for unfollow follow proposal mutation
    GitOrigin-RevId: 243350132c874b44a0bceb37dde84d317de47108

--- app/Resources/js/mutations/UnfollowProposalMutation.js
+++ app/Resources/js/mutations/UnfollowProposalMutation.js
@@ -1,5 +1,6 @@
 // @flow
-import { graphql } from 'react-relay';
+import { graphql, type RecordSourceSelectorProxy } from 'react-relay';
+import { ConnectionHandler } from 'relay-runtime';
 import commitMutation from './commitMutation';
 import environnement from '../createRelayEnvironment';
 import type {
@@ -13,12 +14,26 @@ const mutation = graphql`
       proposal {
         id
         ...ProposalFollowButton_proposal
-        ...ProposalPageFollowers_proposal
       }
       unfollowerId
     }
   }
 `;
+
+const decrementFollowerCount = (proposalId: string, store: Object) => {
+  const proposalProxy = store.get(proposalId);
+  if (!proposalProxy) return;
+  const allFollowersProxy = proposalProxy.getLinkedRecord('followers', { first: 0 });
+  if (!allFollowersProxy) return;
+  const previousValue = parseInt(allFollowersProxy.getValue('totalCount'), 10);
+  allFollowersProxy.setValue(previousValue - 1, 'totalCount');
+
+  const connection = ConnectionHandler.getConnection(
+    proposalProxy,
+    'ProposalPageFollowers_followers',
+  );
+  connection.setValue(connection.getValue('totalCount') - 1, 'totalCount');
+};
 
 const commit = (
   variables: UnfollowProposalMutationVariables,
@@ -26,20 +41,28 @@ const commit = (
   commitMutation(environnement, {
     mutation,
     variables,
-    // TODO update in real type @spyl
-    // configs: [
-    //   {
-    //     type: 'RANGE_DELETE',
-    //     parentID: variables.input.proposalId,
-    //     connectionKeys: [
-    //       {
-    //         key: 'ProposalPageFollowers_followerConnection',
-    //       },
-    //     ],
-    //     pathToConnection: ['proposal', 'followerConnection'],
-    //     deletedIDFieldName: 'unfollowerId',
-    //   },
-    // ],
+    configs: [
+      {
+        type: 'NODE_DELETE',
+        deletedIDFieldName: 'unfollowerId',
+      },
+    ],
+    updater: (store: RecordSourceSelectorProxy) => {
+      const payload = store.getRootField('unfollowProposal');
+      if (!payload || !payload.getLinkedRecord('proposal')) {
+        return;
+      }
+
+      if (Array.isArray(variables.input.ids)) {
+        variables.input.ids.map((id: string) => {
+          decrementFollowerCount(id, store);
+        });
+      }
+
+      if (typeof variables.input.proposalId === 'string') {
+        decrementFollowerCount(variables.input.proposalId, store);
+      }
+    },
   });
 
 export default { commit };
