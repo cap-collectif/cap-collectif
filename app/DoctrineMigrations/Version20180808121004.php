25e94a6ea7fc3c6a0312fe86c2776a3df9069705
Liinkiing <omar.jbara2@gmail.com>
2018-08-10 12:16:55 +0200

    - Renamed migration to be the latest after rebase - Removed graphql.config.json
    GitOrigin-RevId: 35f61a651a8e46347e44885a3e74ed204c71e98c

--- app/DoctrineMigrations/Version20180808121004.php
+++ app/DoctrineMigrations/Version20180808121004.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180806143600 extends AbstractMigration
+final class Version20180808121004 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
