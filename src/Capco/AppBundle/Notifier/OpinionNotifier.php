1fddd8df48f5374d9f67d309d96b25ab511500f3
sanzodown <sanzodown@hotmail.fr>
2018-08-22 16:52:47 +0200

    autowire is love, autowire is life
    GitOrigin-RevId: c8c9295967d355f4ee2bc51a9d6c54f92ab24683

--- src/Capco/AppBundle/Notifier/OpinionNotifier.php
+++ src/Capco/AppBundle/Notifier/OpinionNotifier.php
@@ -4,6 +4,7 @@ namespace Capco\AppBundle\Notifier;
 
 use Capco\AppBundle\Entity\Opinion;
 use Capco\AppBundle\GraphQL\Resolver\ConsultationResolver;
+use Capco\AppBundle\GraphQL\Resolver\Opinion\OpinionUrlResolver;
 use Capco\AppBundle\GraphQL\Resolver\UserResolver;
 use Capco\AppBundle\Mailer\MailerService;
 use Capco\AppBundle\Mailer\Message\Opinion\NewOpinionModeratorMessage;
@@ -17,8 +18,13 @@ class OpinionNotifier extends BaseNotifier
     protected $consultationResolver;
     protected $router;
 
-    public function __construct(MailerService $mailer, Resolver $siteParams, UserResolver $userResolver, ConsultationResolver $consultationResolver, RouterInterface $router)
-    {
+    public function __construct(
+        MailerService $mailer,
+        Resolver $siteParams,
+        UserResolver $userResolver,
+        OpinionUrlResolver $consultationResolver,
+        RouterInterface $router
+    ) {
         parent::__construct($mailer, $siteParams, $userResolver);
         $this->consultationResolver = $consultationResolver;
         $this->router = $router;
@@ -29,14 +35,16 @@ class OpinionNotifier extends BaseNotifier
         $step = $opinion->getStep();
 
         if ($step->isModeratingOnCreate()) {
-            $this->mailer->sendMessage(NewOpinionModeratorMessage::create(
-            $opinion,
-            $this->siteParams->getValue('admin.mail.notifications.receive_address'),
-            null,
-            $this->consultationResolver->resolvePropositionUrl($opinion),
-            $this->userResolver->resolveShowUrl($opinion->getAuthor()),
-            $this->router
-          ));
+            $this->mailer->sendMessage(
+                NewOpinionModeratorMessage::create(
+                    $opinion,
+                    $this->siteParams->getValue('admin.mail.notifications.receive_address'),
+                    null,
+                    $this->consultationResolver->__invoke($opinion),
+                    $this->userResolver->resolveShowUrl($opinion->getAuthor()),
+                    $this->router
+                )
+            );
         }
     }
 
@@ -45,22 +53,26 @@ class OpinionNotifier extends BaseNotifier
         $step = $opinion->getStep();
 
         if ($step->isModeratingOnUpdate()) {
-            $this->mailer->sendMessage(UpdateOpinionModeratorMessage::create(
-          $opinion,
-          $this->siteParams->getValue('admin.mail.notifications.receive_address'),
-          null,
-          $this->consultationResolver->resolvePropositionUrl($opinion),
-          $this->userResolver->resolveShowUrl($opinion->getAuthor()),
-          $this->router
-        ));
+            $this->mailer->sendMessage(
+                UpdateOpinionModeratorMessage::create(
+                    $opinion,
+                    $this->siteParams->getValue('admin.mail.notifications.receive_address'),
+                    null,
+                    $this->consultationResolver->__invoke($opinion),
+                    $this->userResolver->resolveShowUrl($opinion->getAuthor()),
+                    $this->router
+                )
+            );
         }
     }
 
     public function onTrash(Opinion $opinion)
     {
-        $this->mailer->sendMessage(TrashedOpinionAuthorMessage::create(
-            $opinion,
-            $this->consultationResolver->resolvePropositionUrl($opinion)
-        ));
+        $this->mailer->sendMessage(
+            TrashedOpinionAuthorMessage::create(
+                $opinion,
+                $this->consultationResolver->__invoke($opinion)
+            )
+        );
     }
 }
