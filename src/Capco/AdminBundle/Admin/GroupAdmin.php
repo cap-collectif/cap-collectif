c2d6c9ee98e0ccafcfb627da1695f08c656987c0
mauriau <maxime.auriau@protonmail.com>
2018-09-05 14:48:22 +0200

    fix : add configureFromFields for GroupAdmin (#5951)
    GitOrigin-RevId: 7d4b3a8874f36283b44fcca2fa5eaff6ffb225b9

--- src/Capco/AdminBundle/Admin/GroupAdmin.php
+++ src/Capco/AdminBundle/Admin/GroupAdmin.php
@@ -2,12 +2,13 @@
 
 namespace Capco\AdminBundle\Admin;
 
-use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 
-class GroupAdmin extends Admin
+class GroupAdmin extends AbstractAdmin
 {
     protected $datagridValues = [
         '_sort_order' => 'DESC',
@@ -19,13 +20,7 @@ class GroupAdmin extends Admin
      */
     public function getExportFields(): array
     {
-        return [
-            'title',
-            'description',
-            'countUserGroups',
-            'createdAt',
-            'updatedAt',
-        ];
+        return ['title', 'description', 'countUserGroups', 'createdAt', 'updatedAt'];
     }
 
     public function getExportFormats(): array
@@ -53,8 +48,7 @@ class GroupAdmin extends Admin
             ])
             ->add('updatedAt', 'datetime', [
                 'label' => 'admin.fields.group.updated_at',
-            ])
-        ;
+            ]);
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
@@ -75,5 +69,16 @@ class GroupAdmin extends Admin
     {
         $collection->remove('create');
         $collection->remove('delete');
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        // Content
+        $formMapper->with('admin.fields.project.group_content', ['class' => 'col-md-12'])->end();
+
+        $formMapper
+            ->with('admin.fields.project.group_content')
+            ->add('title', null, ['label' => 'admin.fields.group.title'])
+            ->end();
     }
 }
