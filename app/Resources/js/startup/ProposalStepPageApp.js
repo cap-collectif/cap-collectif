7822d76171652cccadab19fe4741c723a06c9378
spyl94 <spyl94@gmail.com>
2018-09-12 18:36:16 +0200

    Update to Flow 80 (#6002)
    GitOrigin-RevId: c6e0d60b3841a9406e7416a75dd38c7bf8de9c55

--- app/Resources/js/startup/ProposalStepPageApp.js
+++ app/Resources/js/startup/ProposalStepPageApp.js
@@ -1,10 +1,11 @@
+// @flow
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactOnRails from 'react-on-rails';
 import { IntlProvider } from 'react-intl-redux';
 import ProposalStepPage from '../components/Page/ProposalStepPage';
 
-const mainNode = props => {
+const mainNode = (props: Object) => {
   const store = ReactOnRails.getStore('appStore');
 
   return (
