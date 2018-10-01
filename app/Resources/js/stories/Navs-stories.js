7895b1a00d03e2c052d4d96ce16e38b1b39d2386
Amelie52 <amelie.tamisier@gmail.com>
2018-10-01 16:35:48 +0200

    [ADD] how to use for stackedNav
    GitOrigin-RevId: abbe886645fcb279f4d90031d160ab7469928c6d

--- app/Resources/js/stories/Navs-stories.js
+++ app/Resources/js/stories/Navs-stories.js
@@ -23,13 +23,59 @@ storiesOf('Navs', module).add(
   'StackedNav',
   withInfo({
     source: false,
-    propTablesExclude: [Col, Row],
+    propTablesExclude: [Col, Row, Button],
     text: `
       <h2>Comment l'utiliser</h2>
     
       ~~~jsx
       // StackedNav component
-      
+      <StackedNav>
+        <div className="stacked-nav__header">
+          <p>
+            <i className="cap cap-android-menu mr-5" />
+            <span>My plan</span>
+          </p>
+          <button className="btn-link p-0" href="#">
+            <i className="cap cap-delete-1" />
+          </button>
+        </div>
+        <div className="stacked-nav__list">
+          <Nav>
+            <li>
+              <Button bsStyle="link" className="level--0">
+                Title 1
+              </Button>
+              <Nav>
+                <li>
+                  <Button bsStyle="link" className="level--1 active">
+                    Title 1.1
+                  </Button>
+                </li>
+              </Nav>
+            </li>
+          </Nav>
+          <Nav>
+            <li>
+              <Button bsStyle="link" className="level--0">
+                Title 2
+              </Button>
+              <Nav>
+                <li>
+                  <Button bsStyle="link" className="level--1">
+                    Title 2.2
+                  </Button>
+                </li>
+              </Nav>
+            </li>
+          </Nav>
+        </div>
+        <div className="stacked-nav__footer">
+          <Button bsStyle="link" className="p-0">
+            <i className="cap cap-arrow-68 mr-5" />
+            <span>Back to Top</span>
+          </Button>
+        </div>
+      </StackedNav>
       ~~~
     
     `,
@@ -44,7 +90,7 @@ storiesOf('Navs', module).add(
       <hr />
       <Row>
         <Col>
-          <StackedNav className="w-100">
+          <StackedNav>
             <div className="stacked-nav__header">
               <p>
                 <i className="cap cap-android-menu mr-5" />
