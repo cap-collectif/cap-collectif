454adcb936b139cd82546a8a642ef5f1dacd36dd
spyl94 <spyl94@gmail.com>
2018-10-02 18:59:28 +0200

    Enable some rules on schema
    GitOrigin-RevId: 01f8541e133c08ba3ca87c384c576da535e40073

--- graphql-schema-linter.config.js
+++ graphql-schema-linter.config.js
@@ -1,10 +1,22 @@
 module.exports = {
   rules: [
-    'enum-values-sorted-alphabetically',
-    'enum-values-all-caps',
+    'relay-connection-types-spec',
+    'relay-connection-arguments-spec',
+    'relay-page-info-spec',
+    'types-are-capitalized',
+    'deprecations-have-a-reason',
+    // TODO enable this https://github.com/cap-collectif/platform/issues/6103
+    // 'enum-values-sorted-alphabetically',
+    // TODO enable this https://github.com/cap-collectif/platform/issues/6102
+    // 'enum-values-all-caps',
     // TODO enable this when `show_url` is removed
     //'fields-are-camel-cased',
-    'input-object-values-are-camel-cased',
-    'relay-connection-types-spec',
+    // TODO enable this when `current_password` is removed
+    // 'input-object-values-are-camel-cased',
+    // TODO enable all rules https://github.com/cap-collectif/platform/issues/6104
+    // 'enum-values-have-descriptions',
+    // 'fields-have-descriptions',
+    // 'input-object-values-have-descriptions',
+    // 'types-have-descriptions',
   ],
 };
