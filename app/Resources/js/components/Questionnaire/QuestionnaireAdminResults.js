e63663fe744e121e57a8e5eab8943e0951d06773
Amelie52 <amelie.tamisier@gmail.com>
2018-10-08 18:33:11 +0200

    [ADD] pie chart
    GitOrigin-RevId: 983a563ea8774a1acb667bffed3fee0bf3f1e876

--- app/Resources/js/components/Questionnaire/QuestionnaireAdminResults.js
+++ app/Resources/js/components/Questionnaire/QuestionnaireAdminResults.js
@@ -1,7 +1,10 @@
 // @flow
 import * as React from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
-import {Bar, BarChart, CartesianGrid, ResponsiveContainer, XAxis, YAxis} from 'recharts';
+import {
+  Bar, BarChart, CartesianGrid, Cell, LabelList, Pie, PieChart, ResponsiveContainer, XAxis,
+  YAxis
+} from 'recharts';
 import { FormattedMessage } from 'react-intl';
 import type { QuestionnaireAdminResults_questionnaire } from './__generated__/QuestionnaireAdminResults_questionnaire.graphql';
 
@@ -11,21 +14,22 @@ type Props = {
 
 export class QuestionnaireAdminResults extends React.Component<Props> {
   getBarChart = (choices: Array<Object>) => {
-
-    // console.warn(choices);
-
-    const data = [
-      {name: "tom", value: 23},
-      {name: "tommy", value: 1023},
-      {name: "tom", value: 39},
-    ];
+    const data = choices.reduce((acc, curr) => {
+      acc.push({
+        name: curr.title,
+        value: curr.responses.totalCount
+      });
+      return acc;
+    }, []);
 
     return (
-      <ResponsiveContainer width="100%" height={250}>
+      <ResponsiveContainer height={250}>
         <BarChart data={data} layout="vertical">
-          <XAxis type="number"/>
+          <XAxis type="number" />
           <YAxis dataKey="name" type="category"/>
-          <Bar dataKey='value' maxBarSize={30} fill='#8884d8' />
+          <Bar dataKey='value' maxBarSize={30} fill='#8884d8'>
+            <LabelList dataKey="value" position="right" />
+          </Bar>
         </BarChart>
       </ResponsiveContainer>
     );
@@ -33,14 +37,72 @@ export class QuestionnaireAdminResults extends React.Component<Props> {
   };
 
   getPieChart = (choices: Array<Object>) => {
-    // const data = [
-    //   {name: "tom", value: 23},
-    //   {name: "tommy", value: 87},
-    //   {name: "tom", value: 39},
-    // ];
+
+    const data = choices.reduce((acc, curr) => {
+      acc.push({
+        name: curr.title,
+        value: curr.responses.totalCount
+      });
+      return acc;
+    }, []);
+
+    const RADIAN = Math.PI / 180;
+
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+      const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+      const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+      const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+
+      console.warn(percent);
+
+      return (
+        <text x={x} y={y} fill={percent === 0 ? 'white' : 'white'} textAnchor="middle" dominantBaseline="central">
+          {`${(percent * 100).toFixed(0)}%`}
+          {/* {percent > 0 && */}
+            {/* `${(percent * 100).toFixed(0)}%` */}
+          {/* } */}
+        </text>
+      );
+    };
+
+    const getRandomTintColor = (color: string) => {
+      // const percent = (Math.random() * (0.5 - (-0.5)) - 0.5).toFixed(3);
+      const percent = (Math.random() * ((-0.2) - (0.6)) + 0.2).toFixed(3);
+
+      const f = parseInt(color.slice(1),16);
+      const t = percent<0?0:255;
+      const p = percent<0?percent*-1:percent;
+      const R = f>>16;
+      const G = f>>8&0x00FF;
+      const B = f&0x0000FF;
+      return `#${(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1)}`;
+    };
 
     return (
-      <div>piechart</div>
+      <ResponsiveContainer height={350}>
+        <PieChart>
+          <Pie
+            data={data}
+            // cx={500}
+            // cy={200}
+            innerRadius={35}
+            outerRadius={80}
+            // paddingAngle={2}
+            fill="#82ca9d"
+            stroke="none"
+            fontSize="16px"
+            // fontWeight="bold"
+            isAnimationActive={false}
+            labelLine
+            label={renderCustomizedLabel}
+          >
+             <LabelList dataKey="name" position="outside" stroke="none" offset={31} />
+            {
+              data.map((entry, index) => <Cell key={index} fill={getRandomTintColor('#0088FE')}/>)
+            }
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
     );
   };
 
