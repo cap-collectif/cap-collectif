d56ccab79dac2c73d1d09efc20bbf867fcbabd73
mauriau <maxime.auriau@protonmail.com>
2018-10-10 16:42:21 +0200

    :tada: init new event resolver
    GitOrigin-RevId: c7a658e6b80f86c6a801082d5fc7a668bc8ced25

---
+++ src/Capco/AppBundle/GraphQL/Resolver/Event/EventsResolver.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Capco\AppBundle\GraphQL\Resolver\Event;
+
+
+use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
+
+class EventsResolver implements ResolverInterface
+{
+    public function __construct()
+    {
+    }
+
+    public function __invoke()
+    {
+    }
+}
