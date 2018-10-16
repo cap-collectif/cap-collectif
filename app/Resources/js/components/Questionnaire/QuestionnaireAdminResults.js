9a5d5b6ca0136d56364de97d416e319678c69c8a
Amelie52 <amelie.tamisier@gmail.com>
2018-10-16 09:51:13 +0200

    [ADD] QuestionnaireAdminBarChart file
    GitOrigin-RevId: 4498bc92a97588487d690aac62d7b1133df7fc55

--- app/Resources/js/components/Questionnaire/QuestionnaireAdminResults.js
+++ app/Resources/js/components/Questionnaire/QuestionnaireAdminResults.js
@@ -2,43 +2,27 @@
 import * as React from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
 import {
-  Bar, BarChart, CartesianGrid, Cell, LabelList, Pie, PieChart, ResponsiveContainer, XAxis,
+  Bar, BarChart, Cell, LabelList, Legend, Pie, PieChart, ResponsiveContainer, XAxis,
   YAxis
 } from 'recharts';
+import { Table } from 'react-bootstrap';
 import { FormattedMessage } from 'react-intl';
+import QuestionnaireAdminBarChart from './QuestionnaireAdminBarChart';
 import type { QuestionnaireAdminResults_questionnaire } from './__generated__/QuestionnaireAdminResults_questionnaire.graphql';
+import config from '../../config';
 
 type Props = {
   questionnaire: QuestionnaireAdminResults_questionnaire,
 }
 
 export class QuestionnaireAdminResults extends React.Component<Props> {
-  getBarChart = (choices: Array<Object>) => {
-    const data = choices.reduce((acc, curr) => {
-      acc.push({
-        name: curr.title,
-        value: curr.responses.totalCount
-      });
-      return acc;
-    }, []);
-
-    return (
-      <ResponsiveContainer height={300}>
-        <BarChart data={data} layout="vertical">
-          <XAxis type="number" />
-          <YAxis dataKey="name" type="category" /> {/* allowDataOverflow */}
-          <Bar dataKey='value' maxBarSize={30} fill='#8884d8'>
-            <LabelList dataKey="value" position="right" />
-          </Bar>
-        </BarChart>
-      </ResponsiveContainer>
-    );
-
-  };
-
   getPieChart = (choices: Array<Object>) => {
 
-    const data = choices.reduce((acc, curr) => {
+    // console.warn(choices);
+
+    const data = choices
+      .filter(choice => choice.responses.totalCount > 0)
+      .reduce((acc, curr) => {
       acc.push({
         name: curr.title,
         value: curr.responses.totalCount
@@ -56,18 +40,16 @@ export class QuestionnaireAdminResults extends React.Component<Props> {
       // console.warn(percent);
 
       return (
-        <text x={x} y={y} fill={percent === 0 ? 'white' : 'white'} textAnchor="middle" dominantBaseline="central">
+        <text x={x} y={y} fill='white' textAnchor="middle" dominantBaseline="central">
           {`${(percent * 100).toFixed(0)}%`}
-          {/* {percent > 0 && */}
-            {/* `${(percent * 100).toFixed(0)}%` */}
-          {/* } */}
         </text>
       );
     };
 
-    const getRandomTintColor = (color: string) => {
+    const getRandomTintColor = (color: string, key: number) => {
       // const percent = (Math.random() * (0.5 - (-0.5)) - 0.5).toFixed(3);
-      const percent = (Math.random() * ((-0.2) - (0.6)) + 0.2).toFixed(3);
+      // const percent = (Math.random() * ((-0.2) - (0.6)) + 0.2).toFixed(3);
+      const percent = -0.6 + ((key/10)*2.2);
 
       const f = parseInt(color.slice(1),16);
       const t = percent<0?0:255;
@@ -79,70 +61,89 @@ export class QuestionnaireAdminResults extends React.Component<Props> {
     };
 
     return (
-      <ResponsiveContainer height={350}>
-        <PieChart>
-          <Pie
-            data={data}
-            // cx={500}
-            // cy={200}
-            innerRadius={25}
-            outerRadius={80}
-            paddingAngle={1}
-            fill="#82ca9d"
-            stroke="none"
-            fontSize="16px"
-            // fontWeight="bold"
-            isAnimationActive={false}
-            labelLine
-            label={renderCustomizedLabel}
-          >
-             <LabelList dataKey="name" position="outside" stroke="none" offset={31} />
-            {
-              data.map((entry, index) => <Cell key={index} fill={getRandomTintColor('#0088FE')}/>)
-            }
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
+      <div className="row">
+        <div className="col-xs-12">
+          {/*<ResponsiveContainer width={config.isMobile ? '100%' : 450} height={300} aspect={config.isMobile ? null : 1.5}>*/}
+          <ResponsiveContainer width='100%' height={300}>
+            <PieChart >
+              {config.isMobile &&
+                <Legend verticalAlign="bottom" />
+                // height={36}
+              }
+              <Pie
+                data={data}
+                innerRadius={25}
+                outerRadius={80}
+                paddingAngle={2}
+                // cx="50%"
+                cy="45%"
+                // fill="#82ca9d"
+                stroke="none"
+                fontSize="16px"
+                isAnimationActive={false}
+                labelLine={!config.isMobile}
+                label={renderCustomizedLabel}
+              >
+                {!config.isMobile &&
+                  <LabelList dataKey="name" position="outside" clockWise={0.5} stroke="none" offset={31} scaleToFit="true"/>
+                }
+                {data.map((entry, index) => <Cell key={index} fill={getRandomTintColor('#0088FE', index)} />)} {/* fill={getRandomTintColor('#0088FE')} */}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
     );
   };
 
-  getRankingTable = (choices: Array<Object>) => {
-    const data = choices.reduce((acc, curr) => {
-      acc.push({
-        title: curr.title,
-        ranking: curr.responses.totalCount
-      });
-      return acc;
-    }, []);
-
+  getRankingTable = (question: Array<Object>) => {
     return (
-      <table>
-        {choices.map(choice => {
-          const test = choice.ranking;
-
-          console.log(choice.ranking.length);
-
-          // console.warn(test.length > 0 && test.sort((a, b) => a.position - b.position));
+      <Table reponsive>
+        <thead>
+        <tr>
+          <th>coucou</th>
+        </tr>
+        </thead>
+        <tbody>
+        {question.questionChoices.map(choice => {
+          console.log(choice);
 
           return (
             <tr>
-              <td style={{border: '1px solid black'}}>{choice.title}</td>
+              <td>{choice.title}</td>
               {choice.ranking
-              // .sort((a, b) => a.position - b.position)
-                .map(r => (
-                    <td style={{border: '1px solid black'}}>{r.responses.totalCount}</td>
-                  )
-                )}
+                .map((r, key) => {
+                  if (r.position === key + 1) {
+                    return (
+                      <td>{r.responses.totalCount}</td>
+                    )
+                  }
+                  return <td>0</td>
+                })
+              }
             </tr>
           )
         })}
-      </table>
+        </tbody>
+      </Table>
     )
+
   };
 
   getQuestion = (question: Object) => {
+    // console.log(question.questionChoices.);
+
+    if(question.type === "text"  || question.type === "number" || question.type === "file") {
+      return <span>L'affichage des résultats est pour le moment indisponible</span>
+    }
+
+    if(question.participants && question.participants.totalCount === 0) {
+      return null;
+    }
+
     if(question.type === "checkbox") {
-      return this.getBarChart(question.questionChoices);
+      // return this.getBarChart(question.questionChoices);
+      return <QuestionnaireAdminBarChart multipleChoiceQuestion={question} />
     }
 
     if(question.type === "radio" || question.type === "select" || question.type === "button") {
@@ -150,8 +151,9 @@ export class QuestionnaireAdminResults extends React.Component<Props> {
     }
 
     if(question.type === "ranking") {
-      return this.getRankingTable(question.questionChoices);
+      return this.getRankingTable(question);
     }
+    
   };
   
   render() {
@@ -171,7 +173,15 @@ export class QuestionnaireAdminResults extends React.Component<Props> {
                     <b>{key + 1}. {question.title}</b> <br/>
                     <span className="excerpt">
                     {/* {question.questionChoices && question.questionChoices[0].responses && question.questionChoices[0].responses.totalCount} réponses /{' '} */}
-                      {question.participants.totalCount} participants <br/>
+                      {question.participants && question.participants.totalCount !== 0 ?
+                        <FormattedMessage
+                          id="global.counters.contributors"
+                          values={{ num: question.participants.totalCount }}
+                        />
+                        :
+                        <FormattedMessage id="no-answer" />
+                      }
+                      <br/>
                       {question.required && <b><FormattedMessage id="mandatory-question" /></b>}
                   </span>
                   </p>
@@ -221,6 +231,7 @@ export default createFragmentContainer(
             }
           }
         }
+        ...QuestionnaireAdminBarChart_multipleChoiceQuestion
       }
     }
   `,
