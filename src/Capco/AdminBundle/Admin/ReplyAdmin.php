98094038e38d6a35527ff869587930c1bf072120
mrpandat <mrpandat@hotmail.fr>
2018-10-17 17:09:17 +0200

    [5673-DRAFT] Add missing field to reply admin detail.
    GitOrigin-RevId: 0976aa188dab8927dff4cb482ff23e1d5bd79a41

--- src/Capco/AdminBundle/Admin/ReplyAdmin.php
+++ src/Capco/AdminBundle/Admin/ReplyAdmin.php
@@ -63,8 +63,14 @@ class ReplyAdmin extends AbstractAdmin
     protected function configureShowFields(ShowMapper $showMapper)
     {
         $showMapper
+            ->add('id', null, ['label' => 'admin.fields.reply.id'])
             ->add('author', 'sonata_type_model', ['label' => 'admin.fields.reply.author'])
+            ->add('createdAt', null, ['label' => 'admin.fields.questionnaire.created_at'])
             ->add('updatedAt', null, ['label' => 'admin.fields.reply.updated_at'])
+            ->add('state', null, [
+                'label' => 'admin.fields.proposal.state.label',
+                'template' => 'CapcoAdminBundle:Reply:state_show_field.html.twig',
+            ])
             ->add('responses', null, [
                 'label' => 'admin.fields.reply.responses',
                 'template' => 'CapcoAdminBundle:Reply:responses_show_field.html.twig',
