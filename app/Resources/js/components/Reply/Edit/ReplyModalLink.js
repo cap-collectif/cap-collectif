4c3967535ce9c1793a16205abc586a693d6e8dbf
mrpandat <mrpandat@hotmail.fr>
2018-10-23 09:31:22 +0200

    [5673-DRAFT] Fix multiple bugs on Reply Edit.
    GitOrigin-RevId: 69977c54fd119b80ff76ace27d2dea183ae9fc2b

--- app/Resources/js/components/Reply/Edit/ReplyModalLink.js
+++ app/Resources/js/components/Reply/Edit/ReplyModalLink.js
@@ -64,7 +64,7 @@ export class ReplyModalLink extends React.Component<Props, State> {
               </span>
             )}
             {/* $FlowFixMe $refType */}
-            <ReplyDraftLabel status={reply.draft} />
+            <ReplyDraftLabel draft={reply.draft} />
             <UnpublishedLabel publishable={reply} />
           </div>
           <div>
