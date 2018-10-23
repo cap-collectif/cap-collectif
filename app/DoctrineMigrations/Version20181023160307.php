6e233ea29d17d47772592d04b76d3aaa301b1f69
mrpandat <mrpandat@hotmail.fr>
2018-10-23 16:03:24 +0200

    [5673-DRAFT] Update migration.
    GitOrigin-RevId: 018161bbf80b010b3b4cc1b0f1ef0c8b3e3b3c93

--- app/DoctrineMigrations/Version20181023160307.php
+++ app/DoctrineMigrations/Version20181023160307.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181015111525 extends AbstractMigration
+final class Version20181023160307 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
