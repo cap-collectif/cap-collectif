14a73652b3e0307bc56ac73c04278dc3abefcee0
mrpandat <mrpandat@hotmail.fr>
2018-10-24 14:07:48 +0200

    [5673-DRAFT] Add migration.
    GitOrigin-RevId: d5e75352b3043e79baad2b0d81a74d326af2c0de

---
+++ app/DoctrineMigrations/Version20181024122058.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181024122058 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('ALTER TABLE questionnaire ADD type VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('ALTER TABLE questionnaire DROP type');
+    }
+}
