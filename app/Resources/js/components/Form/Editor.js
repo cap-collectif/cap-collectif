ba7174ca692e27692dd70751760edf09aa5b8abb
Amelie52 <amelie.tamisier@gmail.com>
2018-11-02 14:43:17 +0100

    [ADD] value on editor
    GitOrigin-RevId: 2fb3fcb79555a2b751d7479ce75476b8647edd16

--- app/Resources/js/components/Form/Editor.js
+++ app/Resources/js/components/Form/Editor.js
@@ -1,11 +1,10 @@
 // @flow
 // Todo : ref Quill
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {FormattedMessage, injectIntl, type IntlShape} from 'react-intl';
+import { FormattedMessage, injectIntl, type IntlShape } from 'react-intl';
 import classNames from 'classnames';
 import Quill from 'quill';
-import QuillToolbar from './QuillToolbar';
+// import QuillToolbar from './QuillToolbar';
 
 type Props = {
   intl: IntlShape,
@@ -39,21 +38,52 @@ class Editor extends React.Component<Props> {
   toolbarRef: { current: null | React.ElementRef<'div'> };
 
   componentDidMount() {
-    const {disabled} = this.props;
+    const { disabled, valueLink, onBlur, onChange, value } = this.props;
 
     const options = {
       modules: {
         toolbar: {
-          container: this.toolbarRef.current
+          container: this.toolbarRef.current,
         },
       },
       theme: 'snow',
     };
 
-    if(!disabled) {
+    if (!disabled) {
       const quill = new Quill(this.editorRef.current, options);
-    }
+      quill.keyboard.addBinding({
+        key: '9',
+        shortKey: false,
+        // handler: (range, context) => {
+        //
+        // }
+      });
 
+      if (valueLink) {
+        const defaultValue = valueLink.value;
+
+        if (defaultValue) {
+          quill.clipboard.dangerouslyPasteHTML(defaultValue);
+        }
+
+        quill.on('text-change', () => {
+          valueLink.requestChange(quill.getContents());
+        });
+      } else {
+        const defaultValue = value;
+        if (defaultValue) {
+          quill.clipboard.dangerouslyPasteHTML(defaultValue);
+        }
+        quill.on('selection-change', range => {
+          if (!range) {
+            onBlur(quill.getContents());
+          }
+        });
+        quill.on('text-change', () => {
+          onChange(quill.getContents());
+        });
+      }
+    }
   }
 
   render() {
@@ -67,14 +97,12 @@ class Editor extends React.Component<Props> {
       return <textarea id={id} className={classNames(classes)} disabled />;
     }
     return (
-      <div id={id} className={classNames(classes)} >
-        <div ref={this.toolbarRef} >
+      <div id={id} className={classNames(classes)}>
+        <div ref={this.toolbarRef}>
           <span className="ql-formats">
             <select title="Size" className="ql-size">
               <option value="small">{intl.formatMessage({ id: 'editor.size.small' })}</option>
-              <option selected>
-                {intl.formatMessage({ id: 'editor.size.normal' })}
-              </option>
+              <option selected>{intl.formatMessage({ id: 'editor.size.normal' })}</option>
               <option value="large">{intl.formatMessage({ id: 'editor.size.large' })}</option>
             </select>
           </span>
@@ -82,29 +110,39 @@ class Editor extends React.Component<Props> {
             <button className="ql-bold" />
             <button className="ql-italic" />
             <button className="ql-underline" />
+            <button className="ql-strike" />
           </span>
           <span className="ql-formats">
-            <button className="ql-list" value="ordered" title={<FormattedMessage id="editor.list" />} />
-            <button className="ql-list" value="bullet" title={<FormattedMessage id="editor.bullet" />} />
-            <select
-              title={intl.formatMessage({ id: 'editor.align.title' })}
-              className="ql-align"
-            >
-              <option  label={intl.formatMessage({ id: 'editor.align.left' })} selected />
+            <button
+              className="ql-list"
+              value="ordered"
+              title={<FormattedMessage id="editor.list" />}
+            />
+            <button
+              className="ql-list"
+              value="bullet"
+              title={<FormattedMessage id="editor.bullet" />}
+            />
+            <button
+              className="ql-indent"
+              value="-1"
+              title={<FormattedMessage id="editor.bullet" />}
+            />
+            <button
+              className="ql-indent"
+              value="+1"
+              title={<FormattedMessage id="editor.bullet" />}
+            />
+            <select title={intl.formatMessage({ id: 'editor.align.title' })} className="ql-align">
+              <option label={intl.formatMessage({ id: 'editor.align.left' })} selected />
               <option value="center" label={intl.formatMessage({ id: 'editor.align.center' })} />
               <option value="right" label={intl.formatMessage({ id: 'editor.align.right' })} />
               <option value="justify" label={intl.formatMessage({ id: 'editor.align.justify' })} />
             </select>
           </span>
           <span className="ql-formats">
-            <button
-              title={<FormattedMessage id="editor.link" />}
-              className="ql-link"
-            />
-            <button
-              title={<FormattedMessage id="editor.image" />}
-              className="ql-image"
-            />
+            <button title={<FormattedMessage id="editor.link" />} className="ql-link" />
+            <button title={<FormattedMessage id="editor.image" />} className="ql-image" />
           </span>
         </div>
         <div ref={this.editorRef} />
--- app/Resources/js/components/Form/QuillToolbar.js
+++ app/Resources/js/components/Form/QuillToolbar.js
@@ -1,29 +1,65 @@
 // @flow
 import React from 'react';
-// import { type IntlShape, FormattedMessage, injectIntl } from 'react-intl';
+import { type IntlShape, FormattedMessage, injectIntl } from 'react-intl';
 
 type Props = {
-  // intl: IntlShape,
+  intl: IntlShape,
 };
 
 class QuillToolbar extends React.Component<Props> {
   render() {
-    // const { intl } = this.props;
+    const { intl } = this.props;
 
     return (
       <React.Fragment>
-        <select className="ql-size">
-          <option value="small">frfro</option>
-          <option selected>frkn</option>
-          <option value="large">fjipr</option>
-          <option value="huge">flr</option>
-        </select>
-        <button className="ql-bold" >fre</button>
-        <button className="ql-script" value="sub">fre</button>
-        <button className="ql-script" value="super">ffre</button>
+        <span className="ql-formats">
+          <select title="Size" className="ql-size">
+            <option value="small">{intl.formatMessage({ id: 'editor.size.small' })}</option>
+            <option selected>{intl.formatMessage({ id: 'editor.size.normal' })}</option>
+            <option value="large">{intl.formatMessage({ id: 'editor.size.large' })}</option>
+          </select>
+        </span>
+        <span className="ql-formats">
+          <button className="ql-bold" />
+          <button className="ql-italic" />
+          <button className="ql-underline" />
+          <button className="ql-strike" />
+        </span>
+        <span className="ql-formats">
+          <button
+            className="ql-list"
+            value="ordered"
+            title={<FormattedMessage id="editor.list" />}
+          />
+          <button
+            className="ql-list"
+            value="bullet"
+            title={<FormattedMessage id="editor.bullet" />}
+          />
+          <button
+            className="ql-indent"
+            value="-1"
+            title={<FormattedMessage id="editor.bullet" />}
+          />
+          <button
+            className="ql-indent"
+            value="+1"
+            title={<FormattedMessage id="editor.bullet" />}
+          />
+          <select title={intl.formatMessage({ id: 'editor.align.title' })} className="ql-align">
+            <option label={intl.formatMessage({ id: 'editor.align.left' })} selected />
+            <option value="center" label={intl.formatMessage({ id: 'editor.align.center' })} />
+            <option value="right" label={intl.formatMessage({ id: 'editor.align.right' })} />
+            <option value="justify" label={intl.formatMessage({ id: 'editor.align.justify' })} />
+          </select>
+        </span>
+        <span className="ql-formats">
+          <button title={<FormattedMessage id="editor.link" />} className="ql-link" />
+          <button title={<FormattedMessage id="editor.image" />} className="ql-image" />
+        </span>
       </React.Fragment>
     );
   }
 }
 
-export default QuillToolbar;
+export default injectIntl(QuillToolbar);
