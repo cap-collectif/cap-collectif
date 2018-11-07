b4ea63ab72f6ff4189f1c3544fe1910ef681221f
sanzodown <sanzodown@hotmail.fr>
2018-11-07 18:48:41 +0100

    KILL-JMS total destroy
    GitOrigin-RevId: d6e8852f44f7b3f11672288ab4321ccc08340524

--- src/Capco/AppBundle/EventListener/ProposalSerializationListener.php
+++ src/Capco/AppBundle/EventListener/ProposalSerializationListener.php
@@ -3,30 +3,21 @@
 namespace Capco\AppBundle\EventListener;
 
 use Capco\AppBundle\Entity\Proposal;
-use Capco\AppBundle\GraphQL\Resolver\Commentable\CommentableCommentsResolver;
 use Capco\AppBundle\Repository\ProposalCollectVoteRepository;
 use Capco\AppBundle\Repository\ProposalSelectionVoteRepository;
 use JMS\Serializer\EventDispatcher\ObjectEvent;
-use Overblog\GraphQLBundle\Definition\Argument;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class ProposalSerializationListener extends AbstractSerializationListener
 {
     private $proposalSelectionVoteRepository;
     private $proposalCollectVoteRepository;
-    private $commentableCommentsResolver;
-    private $tokenStorage;
 
     public function __construct(
-        ProposalSelectionVoteRepository $proposalSelectionVoteRepository,
-        ProposalCollectVoteRepository $proposalCollectVoteRepository,
-        CommentableCommentsResolver $commentableCommentsResolver,
-        TokenStorageInterface $tokenStorage
+      ProposalSelectionVoteRepository $proposalSelectionVoteRepository,
+      ProposalCollectVoteRepository $proposalCollectVoteRepository
     ) {
         $this->proposalSelectionVoteRepository = $proposalSelectionVoteRepository;
         $this->proposalCollectVoteRepository = $proposalCollectVoteRepository;
-        $this->commentableCommentsResolver = $commentableCommentsResolver;
-        $this->tokenStorage = $tokenStorage;
     }
 
     public static function getSubscribedEvents(): array
@@ -46,41 +37,26 @@ class ProposalSerializationListener extends AbstractSerializationListener
 
         // For Elasticsearch
         if (isset($this->getIncludedGroups($event)['Elasticsearch'])) {
-            $selectionVotesCount = $this->proposalSelectionVoteRepository->getCountsByProposalGroupedByStepsId(
-                $proposal
-            );
+            $selectionVotesCount = $this->proposalSelectionVoteRepository
+            ->getCountsByProposalGroupedByStepsId($proposal);
 
-            $collectVotesCount = $this->proposalCollectVoteRepository->getCountsByProposalGroupedByStepsId(
-                $proposal
-            );
+            $collectVotesCount = $this->proposalCollectVoteRepository
+            ->getCountsByProposalGroupedByStepsId($proposal);
 
             $data = [];
             foreach ($collectVotesCount as $stepId => $value) {
                 $data[] = [
-                    'step' => ['id' => $stepId],
-                    'count' => $value,
-                ];
+                'step' => ['id' => $stepId],
+                'count' => $value,
+              ];
             }
             foreach ($selectionVotesCount as $stepId => $value) {
                 $data[] = [
-                    'step' => ['id' => $stepId],
-                    'count' => $value,
-                ];
+                'step' => ['id' => $stepId],
+                'count' => $value,
+              ];
             }
-
-            $args = new Argument([
-                'orderBy' => ['field' => 'PUBLISHED_AT', 'direction' => 'DESC'],
-            ]);
-            $commentsConnection = $this->commentableCommentsResolver->__invoke(
-                $event->getObject(),
-                $args,
-                $this->tokenStorage->getToken() ? $this->tokenStorage->getToken()->getUser() : null
-            );
-
             $event->getVisitor()->addData('votesCountByStep', $data);
-            $event
-                ->getVisitor()
-                ->addData('commentsCount', $commentsConnection->{'totalCountWithAnswers'});
         }
     }
 }
