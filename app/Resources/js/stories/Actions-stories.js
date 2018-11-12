74a6ca1b1889a2f62e7073f9017c7c2b21c6404e
Amelie52 <amelie.tamisier@gmail.com>
2018-11-12 19:10:38 +0100

    [CHG] structure of styleguide
    GitOrigin-RevId: 4d07f5fa777829a17684280663c97804a42394cb

---
+++ app/Resources/js/stories/Actions-stories.js
@@ -0,0 +1,118 @@
+// @flow
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, boolean, select, withKnobs } from '@storybook/addon-knobs';
+import { Button, ButtonGroup, ButtonToolbar, DropdownButton, MenuItem } from 'react-bootstrap';
+
+const bsStyleOptions = {
+  Warning: 'warning',
+  Danger: 'danger',
+  Success: 'success',
+  Info: 'info',
+  Primary: 'primary',
+  Link: 'link',
+  Null: null,
+};
+
+const bsSizeOptions = {
+  Large: 'large',
+  Normal: null,
+  Small: 'small',
+  XSmall: 'xsmall',
+};
+
+storiesOf('Actions', module)
+  .addDecorator(withKnobs)
+  .add(
+    'Buttons',
+    () => {
+      const content = text('Content', 'My button');
+      const bsStyle = select('BsStyle', bsStyleOptions, 'primary');
+      const bsSize = select('BsSize', bsSizeOptions, null);
+      const disabled = boolean('Disabled', false);
+      const outline = boolean('Outline', false);
+
+      return (
+        <Button
+          bsStyle={bsStyle}
+          bsSize={bsSize}
+          className={outline ? 'btn--outline' : ''}
+          disabled={disabled}>
+          {content}
+        </Button>
+      );
+    },
+    {
+      info: {
+        text: `
+          Ce composant est utilisé ...
+        `,
+      },
+    },
+  )
+  .add(
+    'Buttons group',
+    () => {
+      const bsSize = select('BsSize', bsSizeOptions, null);
+
+      return (
+        <ButtonGroup bsSize={bsSize}>
+          <Button bsStyle="primary">Left</Button>
+          <Button bsStyle="primary">Middle</Button>
+          <Button bsStyle="primary">Right</Button>
+        </ButtonGroup>
+      );
+    },
+    {
+      info: {
+        text: `
+          Ce composant est utilisé ...
+        `,
+        propTablesExclude: [Button],
+      },
+    },
+  )
+  .add(
+    'Buttons toolbar',
+    () => (
+      <ButtonToolbar>
+        <Button bsStyle="primary">Button</Button>
+        <Button bsStyle="primary">Other button</Button>
+      </ButtonToolbar>
+    ),
+    {
+      info: {
+        text: `
+          Ce composant est utilisé ...
+        `,
+        propTablesExclude: [Button],
+      },
+    },
+  )
+  .add(
+    'Dropdown button',
+    () => {
+      const bsSize = select('BsSize', bsSizeOptions, null);
+      const bsStyle = select('BsStyle', bsStyleOptions, 'primary');
+      const title = text('Title', 'My title');
+      const active = boolean('Active props for first item', false);
+      const header = boolean('Header props for first item', false);
+
+      return (
+        <DropdownButton bsStyle={bsStyle} bsSize={bsSize} title={title}>
+          <MenuItem header={header} active={active}>
+            Item
+          </MenuItem>
+          <MenuItem divider />
+          <MenuItem>Other item</MenuItem>
+        </DropdownButton>
+      );
+    },
+    {
+      info: {
+        text: `
+          Ce composant est utilisé ...
+        `,
+      },
+    },
+  );
--- app/Resources/js/stories/Boxes-stories.js
+++ app/Resources/js/stories/Boxes-stories.js
@@ -6,14 +6,24 @@ import PrivateBox from '../components/Ui/PrivateBox';
 
 storiesOf('Boxes', module)
   .addDecorator(withKnobs)
-  .add('Private box', () => {
-    const message = text('Message', 'my-translation-key');
-    const content = text('Content', 'My content');
-    const show = boolean('Show', true);
+  .add(
+    'Private box',
+    () => {
+      const message = text('Message', 'my-translation-key');
+      const content = text('Content', 'My content');
+      const show = boolean('Show', true);
 
-    return (
-      <PrivateBox show={show} message={message}>
-        <div>{content}</div>
-      </PrivateBox>
-    );
-  });
+      return (
+        <PrivateBox show={show} message={message}>
+          <div>{content}</div>
+        </PrivateBox>
+      );
+    },
+    {
+      info: {
+        text: `
+          Ce composant est utilisé ...
+        `,
+      },
+    },
+  );
