37768ff1383e6f7f028118399f7ebac1f018baec
Amelie52 <amelie.tamisier@gmail.com>
2018-11-13 12:28:52 +0100

    [ADD] medias & overlays stories
    GitOrigin-RevId: 3748b583169ef2e22deac8daf8587bc8934c37aa

--- app/Resources/js/stories/Components-stories.js
+++ app/Resources/js/stories/Components-stories.js
@@ -2,30 +2,12 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import {
-  Label,
-  Button,
-  Modal,
-  Panel,
-  FormControl,
-  ListGroup,
-  ListGroupItem,
-} from 'react-bootstrap';
+import { Label, Button, Panel, FormControl, ListGroup, ListGroupItem } from 'react-bootstrap';
 import Pagination from '../components/Utils/Pagination';
 import InlineList from '../components/Ui/List/InlineList';
-import DarkenGradientMedia from '../components/Ui/DarkenGradientMedia';
-import SixteenNineMedia from '../components/Ui/SixteenNineMedia';
 import { UserAvatar } from '../components/User/UserAvatar';
 
 const labels = ['default', 'primary', 'success', 'warning', 'danger', 'info'];
-
-const author = {
-  username: 'Karim',
-  media: {
-    url: 'https://source.unsplash.com/collection/181462',
-  },
-  _links: {},
-};
 
 storiesOf('Components', module)
   .add(
@@ -88,110 +70,6 @@ storiesOf('Components', module)
             </div>
           ))}
         </div>
-      </div>
-    )),
-  )
-  .add(
-    'Modal',
-    withInfo({
-      source: false,
-      propTablesExclude: [Button],
-      text: `
-      <h2>Comment l'utiliser</h2>
-    
-      ~~~jsx
-      <div className="static-modal">
-        <Modal.Dialog>
-          <Modal.Header>
-            <Modal.Title>Titre</Modal.Title>
-          </Modal.Header>
-      
-          <Modal.Body>Lorem ipsum...</Modal.Body>
-      
-          <Modal.Footer>
-            <Button>Fermer</Button>
-          </Modal.Footer>
-        </Modal.Dialog>
-      </div>;
-      ~~~
-    
-    `,
-    })(() => (
-      <div className="ml-30 mr-30 storybook-container">
-        <h1>
-          Modal{' '}
-          <a href="https://github.com/cap-collectif/platform/blob/master/app/Resources/js/stories/Components-stories.js">
-            <i className="small cap cap-github" />
-          </a>
-        </h1>
-        <hr />
-        <div className="static-modal position-relative">
-          <Modal.Dialog className="position-relative">
-            <Modal.Header>
-              <Modal.Title>Titre</Modal.Title>
-            </Modal.Header>
-
-            <Modal.Body>Lorem ipsum ...</Modal.Body>
-
-            <Modal.Footer>
-              <Button>Fermer</Button>
-            </Modal.Footer>
-          </Modal.Dialog>
-        </div>
-      </div>
-    )),
-  )
-  .add(
-    'Media',
-    withInfo({
-      source: false,
-      propTablesExclude: [Button],
-      text: `
-      <h2>Comment l'utiliser</h2>
-    
-      ~~~jsx
-      // Darken gradient media
-      <DarkenGradientMedia
-        width="600px"
-        height="400px"
-        url="https://source.unsplash.com/collection/1353633"
-        title="title"
-      />
-      
-      // Sixteen  Nine Media
-      <SixteenNineMedia> // depends on parent size
-        <img src="https://source.unsplash.com/collection/1353633" alt="img example"/>
-      </SixteenNineMedia>
-      
-      // User avatar
-      <UserAvatar user={author}/>
-      ~~~
-    
-    `,
-    })(() => (
-      <div className="ml-30 mr-30 storybook-container">
-        <h1>
-          Media{' '}
-          <a href="https://github.com/cap-collectif/platform/blob/master/app/Resources/js/stories/Components-stories.js">
-            <i className="small cap cap-github" />
-          </a>
-        </h1>
-        <hr />
-        <h3>Darken gradient media</h3>
-        <DarkenGradientMedia
-          width="600px"
-          height="400px"
-          url="https://source.unsplash.com/collection/1353633"
-          title="title"
-        />
-        <h3>16:9 media</h3>
-        <div style={{ maxWidth: '400px' }} className="mb-30">
-          <SixteenNineMedia>
-            <img src="https://source.unsplash.com/collection/1353633" alt="img example" />
-          </SixteenNineMedia>
-        </div>
-        <h3>User avatar</h3>
-        <UserAvatar user={author} defaultAvatar={null} />
       </div>
     )),
   )
