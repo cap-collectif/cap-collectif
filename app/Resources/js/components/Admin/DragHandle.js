733cbb9fba938da9bba552c0698fd05ba08efcce
Liinkiing <omar.jbara2@gmail.com>
2018-11-19 17:12:55 +0100

    Merge pull request #6390 from cap-collectif/6277-import-paris-proposals
    Import Paris proposals
    
    GitOrigin-RevId: d683066fff64d68876b1b6fd18db4bd46e7b55cb

--- app/Resources/js/components/Admin/DragHandle.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import * as React from 'react';
-import { SortableHandle } from 'react-sortable-hoc';
-
-export const DragHandle = () => <div className="draghandle" />;
-
-export default SortableHandle(DragHandle);
