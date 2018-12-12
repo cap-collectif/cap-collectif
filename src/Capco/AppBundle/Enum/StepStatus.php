221f18b8286d545ec2d0dada3a9ec7bcda7a08c8
mrpandat <mrpandat@hotmail.fr>
2018-12-12 16:09:38 +0100

    [6563-FIX] Replace step status Enum ENDED by CLOSED.
    GitOrigin-RevId: a4b1bf3a06b8883d650ee1853d86a85ff7f2dc2a

--- src/Capco/AppBundle/Enum/StepStatus.php
+++ src/Capco/AppBundle/Enum/StepStatus.php
@@ -8,5 +8,5 @@ final class StepStatus
 {
     public const FUTURE = AbstractStep::OPENING_STATUS_FUTURE;
     public const OPENED = AbstractStep::OPENING_STATUS_OPENED;
-    public const ENDED = AbstractStep::OPENING_STATUS_ENDED;
+    public const CLOSED = AbstractStep::OPENING_STATUS_CLOSED;
 }
