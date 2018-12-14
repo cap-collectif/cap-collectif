be2a26adc3c1498446dcd71a5e71334d4596f44e
Liinkiing <omar.jbara2@gmail.com>
2018-12-14 14:39:01 +0100

    - Modified MapTokenRepository to get a MapToken given its provider
    GitOrigin-RevId: 78c51f307cebd3f63852eabdc169bfb112fafe47

--- src/Capco/AppBundle/Repository/MapTokenRepository.php
+++ src/Capco/AppBundle/Repository/MapTokenRepository.php
@@ -3,14 +3,35 @@
 namespace Capco\AppBundle\Repository;
 
 use Capco\AppBundle\Entity\MapToken;
+use Capco\AppBundle\Enum\MapProviderEnum;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 
 class MapTokenRepository extends EntityRepository
 {
-    public function getCurrentMapToken(): ?MapToken
+    public function getCurrentMapTokenForProvider(string $provider): ?MapToken
     {
-        return $this->createQueryBuilder('mt')
+        if (!MapProviderEnum::isProviderValid($provider)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    "Invalid provider '%s'. Available providers : %s",
+                    $provider,
+                    implode(', ', MapProviderEnum::getAvailableProviders())
+                )
+            );
+        }
+
+        $qb = $this->getQueryBuilder();
+
+        return $qb
+            ->andWhere($qb->expr()->eq('mt.provider', ':provider'))
+            ->setParameter('provider', $provider)
             ->getQuery()
             ->getOneOrNullResult();
+    }
+
+    private function getQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('mt');
     }
 }
