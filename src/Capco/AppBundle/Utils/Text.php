61b78d6241e5ce0e8bb7010adc2aa8914fef4c88
ptondereau <pierre.tondereau@protonmail.com>
2018-12-17 17:04:22 +0100

    Fix wrong field in users list for email confirmed
    GitOrigin-RevId: 666562a71f2ddd549acd9aa9382e0154b75adc8f

--- src/Capco/AppBundle/Utils/Text.php
+++ src/Capco/AppBundle/Utils/Text.php
@@ -7,7 +7,7 @@ final class Text
     public static function startsWith(string $haystack, string $needle): bool
     {
         // search backwards starting from haystack length characters from the end
-        return '' === $needle || false !== strrpos($haystack, $needle, -\strlen($haystack));
+        return $needle === '' || strrpos($haystack, $needle, -strlen($haystack)) !== false;
     }
 
     public static function escapeHtml($str): string
@@ -49,8 +49,9 @@ final class Text
         // uppercase the first character of each word
         $str = ucwords($str);
         $str = str_replace(' ', '', $str);
+        $str = lcfirst($str);
 
-        return lcfirst($str);
+        return $str;
     }
 
     public static function snakeCase(string $str): string
