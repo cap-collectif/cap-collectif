fe371b1ecdf2fdc24c199b028d35c9e78454a39b
ptondereau <pierre.tondereau@protonmail.com>
2018-12-18 09:59:55 +0100

    Merge pull request #6647 from cap-collectif/5921-fix-wrong-column-confirmed-email
    Fix wrong field in users list for email confirmed
    
    GitOrigin-RevId: e80ff597d0bc0f11870bdd3124aa5d44d0a6f366

--- src/Capco/AdminBundle/Admin/UserAdmin.php
+++ src/Capco/AdminBundle/Admin/UserAdmin.php
@@ -1,10 +1,10 @@
 <?php
+
 namespace Capco\AdminBundle\Admin;
 
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
-use Sonata\CoreBundle\Model\Metadata;
 use Sonata\UserBundle\Admin\Model\UserAdmin as BaseAdmin;
 
 class UserAdmin extends BaseAdmin
@@ -29,6 +29,7 @@ class UserAdmin extends BaseAdmin
             'translation_domain' => 'SonataAdminBundle',
             'ask_confirmation' => false,
         ];
+
         return $actions;
     }
 
@@ -44,7 +45,7 @@ class UserAdmin extends BaseAdmin
             ->addIdentifier('username', null, ['label' => 'registration.username'])
             ->add('email')
             ->add('enabled', null)
-            ->add('confirmationToken', null, [
+            ->add('isEmailConfirmed', null, [
                 'label' => 'confirmed-by-email',
                 'template' => 'CapcoAdminBundle:User:email_confirmed_list_field.html.twig',
             ])
