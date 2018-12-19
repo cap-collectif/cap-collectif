1c2c3e8736fa7cfac4c049e93de82ea77a9aaeff
mrpandat <mrpandat@hotmail.fr>
2018-12-19 14:33:11 +0100

    [5890-FILTER] Fix tests and CSS.
    GitOrigin-RevId: af38a8bee817f7136325c02146ea9967f8063412

---
+++ app/Resources/js/constants/ProjectStatusConstants.js
@@ -0,0 +1,13 @@
+// @flow
+
+export type ProjectType = { id: number, title: string };
+
+const FUTURE = 0;
+const OPENED = 1;
+const CLOSED = 2;
+
+export const PROJECT_STATUSES: ProjectType[] = [
+  { id: FUTURE, title: 'step.status.future' },
+  { id: OPENED, title: 'step.status.open' },
+  { id: CLOSED, title: 'step.status.closed' },
+];
