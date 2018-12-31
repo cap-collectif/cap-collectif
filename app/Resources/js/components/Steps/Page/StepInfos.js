91f738cf4583a62dd7faaf8a5243d1ff27cade29
ptondereau <pierre.tondereau@protonmail.com>
2018-12-31 11:48:09 +0100

    Remove steps body from redux store
    GitOrigin-RevId: afdb216aece9d07b87b33a4bde2d1d6bcf0f7cfa

--- app/Resources/js/components/Steps/Page/StepInfos.js
+++ app/Resources/js/components/Steps/Page/StepInfos.js
@@ -1,7 +1,15 @@
 // @flow
 import React from 'react';
+import { QueryRenderer, graphql, type ReadyState } from 'react-relay';
+import { Row } from 'react-bootstrap';
+import environment, { graphqlError } from '../../../createRelayEnvironment';
 import StepText from './StepText';
 import { CardContainer } from '../../Ui/Card/CardContainer';
+import Loader from '../../Ui/FeedbacksIndicators/Loader';
+import type {
+  StepInfosQueryResponse,
+  StepInfosQueryVariables,
+} from './__generated__/StepInfosQuery.graphql';
 
 type Props = {
   step: Object,
@@ -10,16 +18,45 @@ type Props = {
 class StepInfos extends React.Component<Props> {
   render() {
     const { step } = this.props;
-    const body = step.body;
-    if (!body) {
-      return null;
-    }
 
     return (
       <CardContainer>
-        <div className="card__body">
-          <StepText text={body} />
-        </div>
+        <QueryRenderer
+          environment={environment}
+          query={graphql`
+            query StepInfosQuery($stepId: ID!) {
+              step: node(id: $stepId) {
+                ... on Step {
+                  body
+                }
+              }
+            }
+          `}
+          variables={
+            ({
+              stepId: step.id,
+            }: StepInfosQueryVariables)
+          }
+          render={({ error, props }: { props: ?StepInfosQueryResponse } & ReadyState) => {
+            if (error) {
+              return graphqlError;
+            }
+
+            if (props && props.step) {
+              const { body } = props.step;
+              return (
+                <div className="card__body">
+                  <StepText text={body} />
+                </div>
+              );
+            }
+            return (
+              <Row>
+                <Loader />
+              </Row>
+            );
+          }}
+        />
       </CardContainer>
     );
   }
