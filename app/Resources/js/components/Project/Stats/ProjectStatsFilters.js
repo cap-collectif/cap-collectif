5dc65375ec35b546e10dbdf111885328e27f65cb
Amelie52 <amelie.tamisier@gmail.com>
2019-01-03 16:16:18 +0100

    [ADD] intl for filter
    GitOrigin-RevId: 071d7dc45a9208545656300a5461672a68c27dd3

--- app/Resources/js/components/Project/Stats/ProjectStatsFilters.js
+++ app/Resources/js/components/Project/Stats/ProjectStatsFilters.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl, type IntlShape } from 'react-intl';
 import { Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import Input from '../../Form/Input';
@@ -16,6 +16,7 @@ type Props = {
   onCategoryChange: Function,
   showThemes: boolean,
   showDistricts: boolean,
+  intl: IntlShape,
 };
 
 export class ProjectStatsFilters extends React.Component<Props> {
@@ -30,6 +31,7 @@ export class ProjectStatsFilters extends React.Component<Props> {
       themes,
       showThemes,
       showDistricts,
+      intl,
     } = this.props;
     if (!showFilters) {
       return null;
@@ -56,9 +58,7 @@ export class ProjectStatsFilters extends React.Component<Props> {
         {showThemesFilter && (
           <Col xs={12} md={colWidth}>
             <Input id="stats-filter-themes" type="select" ref="themes" onChange={onThemeChange}>
-              <option value="">
-                <FormattedMessage id="global.select_themes" />
-              </option>
+              <option value="">{intl.formatMessage({ id: 'global.select_themes' })}</option>
               {themes.map(theme => (
                 <option key={theme.id} value={theme.id}>
                   {theme.title}
@@ -70,9 +70,7 @@ export class ProjectStatsFilters extends React.Component<Props> {
         {showCategoriesFilter && (
           <Col xs={12} md={colWidth}>
             <Input id="stats-filter-categories" type="select" onChange={onCategoryChange}>
-              <option value="">
-                <FormattedMessage id="global.select_categories" />
-              </option>
+              <option value="">{intl.formatMessage({ id: 'global.select_categories' })}</option>
               {categories.map(category => (
                 <option key={category.id} value={category.id}>
                   {category.name}
@@ -109,6 +107,6 @@ const mapStateToProps = (state: State, props: Props) => ({
   showDistricts: state.default.features.districts && props.showDistricts,
 });
 
-const container = connect(mapStateToProps)(ProjectStatsFilters);
+const container = connect(mapStateToProps)(injectIntl(ProjectStatsFilters));
 
 export default container;
