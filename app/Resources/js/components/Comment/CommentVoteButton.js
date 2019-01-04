6a9cd3fea4b70523e1ed46b723b1fcc9847c1001
sanzodown <sanzodown@hotmail.fr>
2019-01-04 18:41:21 +0100

    6783-FIX real fix
    GitOrigin-RevId: a61cd942dc8693e8580031fb510626700a252b9c

--- app/Resources/js/components/Comment/CommentVoteButton.js
+++ app/Resources/js/components/Comment/CommentVoteButton.js
@@ -3,17 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { graphql, createFragmentContainer } from 'react-relay';
 import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
 import RemoveCommentVoteMutation from '../../mutations/RemoveCommentVoteMutation';
 import AddCommentVoteMutation from '../../mutations/AddCommentVoteMutation';
 import LoginOverlay from '../Utils/LoginOverlay';
 import UnpublishedTooltip from '../Publishable/UnpublishedTooltip';
 import type { CommentVoteButton_comment } from './__generated__/CommentVoteButton_comment.graphql';
-import type { GlobalState } from '../../types';
 
 type Props = {
   comment: CommentVoteButton_comment,
-  viewer: Object,
 };
 
 class CommentVoteButton extends React.Component<Props> {
@@ -36,8 +33,8 @@ class CommentVoteButton extends React.Component<Props> {
   };
 
   renderFormOrDisabled = () => {
-    const { viewer } = this.props;
-    if (!viewer || !viewer.isEmailConfirmed) {
+    const { comment } = this.props;
+    if (comment.author && comment.author.isViewer) {
       return (
         <button disabled="disabled" className="btn btn-dark-gray btn-sm">
           <i className="cap-hand-like-2" /> {<FormattedMessage id="comment.vote.submit" />}
@@ -90,13 +87,7 @@ class CommentVoteButton extends React.Component<Props> {
   }
 }
 
-const mapStateToProps = (state: GlobalState) => ({
-  viewer: state.user.user,
-});
-
-const container = connect(mapStateToProps)(CommentVoteButton);
-
-export default createFragmentContainer(container, {
+export default createFragmentContainer(CommentVoteButton, {
   comment: graphql`
     fragment CommentVoteButton_comment on Comment
       @argumentDefinitions(isAuthenticated: { type: "Boolean!", defaultValue: true }) {
