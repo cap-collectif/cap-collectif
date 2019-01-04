05d9ad8cb28a032e0563f7296e4f08c482f434be
spyl94 <spyl94@gmail.com>
2019-01-04 14:54:38 +0100

    Merge pull request #6752 from cap-collectif/commentable-dataloader
    COMMENTABLE-DATALOADER add data loader on commentable
    
    GitOrigin-RevId: 574eedcec80d4b1d9c0759a8791a0c40e120f11d

--- spec/Capco/AppBundle/GraphQL/Mutation/AddCommentMutationSpec.php
+++ spec/Capco/AppBundle/GraphQL/Mutation/AddCommentMutationSpec.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace spec\Capco\AppBundle\GraphQL\Mutation;
 
+use Capco\AppBundle\GraphQL\DataLoader\Commentable\CommentableCommentsDataLoader;
 use Prophecy\Argument;
 use PhpSpec\ObjectBehavior;
 use Psr\Log\LoggerInterface;
@@ -16,26 +18,33 @@ use Capco\AppBundle\GraphQL\Resolver\GlobalIdResolver;
 use Overblog\GraphQLBundle\Definition\Argument as Arg;
 use Capco\AppBundle\GraphQL\Mutation\AddCommentMutation;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Capco\AppBundle\Entity\ProposalComment;
 
 class AddCommentMutationSpec extends ObjectBehavior
 {
-    function let(
+    public function let(
         EntityManagerInterface $em,
         FormFactory $formFactory,
         GlobalIdResolver $globalIdResolver,
         LoggerInterface $logger,
-        EventDispatcherInterface $dispatcher
+        EventDispatcherInterface $dispatcher,
+        CommentableCommentsDataLoader $commentableCommentsDataLoader
     ) {
-        $this->beConstructedWith($em, $formFactory, $globalIdResolver, $logger, $dispatcher);
+        $this->beConstructedWith(
+            $em,
+            $formFactory,
+            $globalIdResolver,
+            $logger,
+            $dispatcher,
+            $commentableCommentsDataLoader
+        );
     }
 
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(AddCommentMutation::class);
     }
 
-    function it_returns_userError_if_not_found(
+    public function it_returns_userError_if_not_found(
         GlobalIdResolver $globalIdResolver,
         Arg $arguments,
         User $viewer,
@@ -53,7 +62,7 @@ class AddCommentMutationSpec extends ObjectBehavior
         ]);
     }
 
-    function it_returns_userError_if_not_commentable(
+    public function it_returns_userError_if_not_commentable(
         GlobalIdResolver $globalIdResolver,
         Arg $arguments,
         User $viewer,
@@ -73,7 +82,7 @@ class AddCommentMutationSpec extends ObjectBehavior
         ]);
     }
 
-    function it_returns_userError_if_new_comments_are_not_accepted(
+    public function it_returns_userError_if_new_comments_are_not_accepted(
         GlobalIdResolver $globalIdResolver,
         Arg $arguments,
         User $viewer,
@@ -94,7 +103,8 @@ class AddCommentMutationSpec extends ObjectBehavior
         ]);
     }
 
-    function it_persists_new_comment(
+    public function it_persists_new_comment(
+        $commentableCommentsDataLoader,
         EntityManagerInterface $em,
         FormFactory $formFactory,
         GlobalIdResolver $globalIdResolver,
@@ -106,15 +116,15 @@ class AddCommentMutationSpec extends ObjectBehavior
         Request $request,
         Form $form
     ) {
-        $formData = ["body" => "My body"];
+        $formData = ['body' => 'My body'];
         $form->submit($formData, false)->willReturn(null);
         $form->isValid()->willReturn(true);
         $formFactory
-            ->create("Capco\AppBundle\Form\CommentType", Argument::any())
+            ->create('Capco\\AppBundle\\Form\\CommentType', Argument::any())
             ->willReturn($form);
         $arguments->getRawArguments()->willReturn($formData);
         $commentable
-            ->addComment(Argument::type("Capco\AppBundle\Entity\ProposalComment"))
+            ->addComment(Argument::type('Capco\\AppBundle\\Entity\\ProposalComment'))
             ->willReturn($commentable);
 
         $request->getClientIp()->willReturn('1.1.1.1');
@@ -125,12 +135,13 @@ class AddCommentMutationSpec extends ObjectBehavior
         $arguments->offsetGet('commentableId')->willReturn('123456');
         $globalIdResolver->resolve('123456', $viewer)->willReturn($commentable);
 
-        $em->persist(Argument::type("Capco\AppBundle\Entity\ProposalComment"))->shouldBeCalled();
+        $em->persist(Argument::type('Capco\\AppBundle\\Entity\\ProposalComment'))->shouldBeCalled();
         $em->flush()->shouldBeCalled();
+        $commentableCommentsDataLoader->invalidate('123456')->shouldBeCalled();
         $dispatcher
             ->dispatch(
-                "capco.comment_changed",
-                Argument::type("Capco\AppBundle\Event\CommentChangedEvent")
+                'capco.comment_changed',
+                Argument::type('Capco\\AppBundle\\Event\\CommentChangedEvent')
             )
             ->shouldBeCalled();
 
--- src/Capco/AppBundle/GraphQL/Mutation/DeleteCommentMutation.php
+++ src/Capco/AppBundle/GraphQL/Mutation/DeleteCommentMutation.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace Capco\AppBundle\GraphQL\Mutation;
 
+use Capco\AppBundle\GraphQL\DataLoader\Commentable\CommentableCommentsDataLoader;
 use Psr\Log\LoggerInterface;
 use Capco\UserBundle\Entity\User;
 use Capco\AppBundle\Entity\Comment;
 use Doctrine\ORM\EntityManagerInterface;
-use Overblog\GraphQLBundle\Error\UserError;
 use Capco\AppBundle\Helper\RedisStorageHelper;
 use Capco\AppBundle\Repository\CommentRepository;
 use Overblog\GraphQLBundle\Definition\Argument as Arg;
@@ -17,17 +18,20 @@ class DeleteCommentMutation implements MutationInterface
     private $commentRepo;
     private $redisStorage;
     private $logger;
+    private $commentableCommentsDataLoader;
 
     public function __construct(
         EntityManagerInterface $em,
         CommentRepository $commentRepo,
         RedisStorageHelper $redisStorage,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        CommentableCommentsDataLoader $commentableCommentsDataLoader
     ) {
         $this->em = $em;
         $this->commentRepo = $commentRepo;
         $this->redisStorage = $redisStorage;
         $this->logger = $logger;
+        $this->commentableCommentsDataLoader = $commentableCommentsDataLoader;
     }
 
     public function __invoke(Arg $input, User $user): array
@@ -38,6 +42,7 @@ class DeleteCommentMutation implements MutationInterface
 
         if (!$comment) {
             $this->logger->error('Unknown comment with id: ' . $commentId);
+
             return [
                 'userErrors' => [
                     [
@@ -59,6 +64,9 @@ class DeleteCommentMutation implements MutationInterface
 
         $this->em->remove($comment);
         $this->em->flush();
+        if ($comment->getRelatedObject()) {
+            $this->commentableCommentsDataLoader->invalidate($comment->getRelatedObject()->getId());
+        }
         $this->redisStorage->recomputeUserCounters($user);
 
         return ['deletedCommentId' => $commentId, 'userErrors' => []];
