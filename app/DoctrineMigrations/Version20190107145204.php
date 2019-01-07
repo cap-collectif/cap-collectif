f5c31e7501a18153469ab1dd059cdf3d4fd264cb
spyl94 <spyl94@gmail.com>
2019-01-07 17:09:43 +0100

    Rebase fix
    GitOrigin-RevId: 50acc18d472d9f53b6355215d2a95096f64e6604

--- app/DoctrineMigrations/Version20190107145204.php
+++ app/DoctrineMigrations/Version20190107145204.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181224142858 extends AbstractMigration
+final class Version20190107145204 extends AbstractMigration
 {
     public function postUp(Schema $schema): void
     {
