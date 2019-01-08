d2d2b890f599ae755203e1777d75770705e3af8f
mauriau <maxime.auriau@protonmail.com>
2019-01-08 10:35:20 +0100

    #6789 - WIP restore graphql test
    GitOrigin-RevId: 754c5c98242f3ad0be3a5a06a5a305050eb13853

--- src/Capco/AppBundle/GraphQL/Mutation/DeleteProposalMutation.php
+++ src/Capco/AppBundle/GraphQL/Mutation/DeleteProposalMutation.php
@@ -2,6 +2,7 @@
 
 namespace Capco\AppBundle\GraphQL\Mutation;
 
+use Capco\AppBundle\GraphQL\Resolver\GlobalIdResolver;
 use Swarrot\Broker\Message;
 use Capco\UserBundle\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
@@ -9,38 +10,37 @@ use Capco\AppBundle\Elasticsearch\Indexer;
 use Overblog\GraphQLBundle\Error\UserError;
 use Swarrot\SwarrotBundle\Broker\Publisher;
 use Capco\AppBundle\Helper\RedisStorageHelper;
-use Capco\AppBundle\Repository\ProposalRepository;
 use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
 use Capco\AppBundle\GraphQL\DataLoader\ProposalForm\ProposalFormProposalsDataLoader;
 
 class DeleteProposalMutation implements MutationInterface
 {
     private $em;
-    private $proposalRepo;
     private $redisHelper;
     private $publisher;
     private $indexer;
-    private $dataLoader;
+    private $dataloader;
+    private $globalIdResolver;
 
     public function __construct(
         EntityManagerInterface $em,
-        ProposalRepository $proposalRepo,
         RedisStorageHelper $redisHelper,
         Publisher $publisher,
         Indexer $indexer,
-        ProposalFormProposalsDataLoader $dataLoader
+        ProposalFormProposalsDataLoader $dataloader,
+        GlobalIdResolver $globalIdResolver
     ) {
         $this->em = $em;
-        $this->proposalRepo = $proposalRepo;
         $this->redisHelper = $redisHelper;
         $this->publisher = $publisher;
         $this->indexer = $indexer;
-        $this->dataLoader = $dataLoader;
+        $this->dataloader = $dataloader;
+        $this->globalIdResolver = $globalIdResolver;
     }
 
     public function __invoke(string $proposalId, User $user): array
     {
-        $proposal = $this->proposalRepo->find($proposalId);
+        $proposal = $this->globalIdResolver->resolve($proposalId, $user);
         if (!$proposal) {
             throw new UserError(sprintf('Unknown proposal with id "%s"', $proposalId));
         }
