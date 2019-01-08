824ffc26e43778d95cb2cf6752a21f0d77c8473f
mrpandat <mrpandat@hotmail.fr>
2019-01-08 11:23:35 +0100

    [6705-REQUIREMENT] Add requirements front to source.
    GitOrigin-RevId: 13c252288a15e5abf7b1857fa1107cb629d53363

--- app/Resources/js/components/Opinion/Source/OpinionSourceAdd.js
+++ app/Resources/js/components/Opinion/Source/OpinionSourceAdd.js
@@ -6,24 +6,53 @@ import OpinionSourceAddButton from './OpinionSourceAddButton';
 import OpinionSourceFormModal from './OpinionSourceFormModal';
 import { showSourceCreateModal } from '../../../redux/modules/opinion';
 import type { OpinionSourceAdd_sourceable } from './__generated__/OpinionSourceAdd_sourceable.graphql';
+import RequirementsFormModal from '../../Requirements/RequirementsModal';
 
 type Props = {
   dispatch: Function,
   sourceable: OpinionSourceAdd_sourceable,
 };
 
-class OpinionSourceAdd extends React.Component<Props> {
+type State = {
+  openModal: boolean,
+};
+
+class OpinionSourceAdd extends React.Component<Props, State> {
+  state = { openModal: false };
+
+  openModal = () => {
+    this.setState({ openModal: true });
+  };
+
+  closeModal = () => {
+    this.setState({ openModal: false });
+  };
+
   render() {
     const { dispatch, sourceable } = this.props;
+    const { step } = sourceable;
+    const { openModal } = this.state;
     const disabled = !sourceable.contribuable;
     return (
       <div>
         <OpinionSourceAddButton
           disabled={disabled}
           handleClick={() => {
+            if (step && step.requirements && !step.requirements.viewerMeetsTheRequirements) {
+              this.openModal();
+              return;
+            }
             dispatch(showSourceCreateModal());
           }}
         />
+        {step /* $FlowFixMe */ && (
+          <RequirementsFormModal
+            step={step}
+            reason={step.requirements.reason}
+            handleClose={this.closeModal}
+            show={openModal}
+          />
+        )}
         {!disabled && <OpinionSourceFormModal sourceable={sourceable} source={null} />}
       </div>
     );
@@ -36,6 +65,14 @@ export default createFragmentContainer(container, {
     fragment OpinionSourceAdd_sourceable on Sourceable {
       id
       contribuable
+      step {
+        ...RequirementsForm_step
+        id
+        requirements {
+          viewerMeetsTheRequirements
+          reason
+        }
+      }
       ...OpinionSourceFormModal_sourceable
     }
   `,
