b63791a79584cb8b109bbc6d682a017f5b502d73
spyl94 <spyl94@gmail.com>
2019-01-11 16:06:10 +0100

    Merge pull request #6870 from cap-collectif/6644-fix-classement-trad
    6644 fix classement trad
    
    GitOrigin-RevId: 640d6692c5aa2b49e0c5a47daa573064e7a539ae

--- app/Resources/js/components/Project/Votes/ProposalsUserVotesStep.js
+++ app/Resources/js/components/Project/Votes/ProposalsUserVotesStep.js
@@ -31,6 +31,7 @@ export class ProposalsUserVotesStep extends React.Component<Props> {
 
   render() {
     const { step, dirty, submitting, dispatch } = this.props;
+    const { votesRanking } = step;
     const keyTradProjectCount = step.form.isProposalForm ? 'project.votes.nb' : 'count-questions';
 
     if (!step.viewerVotes) {
@@ -59,7 +60,11 @@ export class ProposalsUserVotesStep extends React.Component<Props> {
         )}
         <div>
           <h3 className="d-ib mr-10 mb-10">
-            <FormattedMessage id="vote-modal-title" />
+            {votesRanking ? (
+              <FormattedMessage id="modal-ranking" />
+            ) : (
+              <FormattedMessage id="vote-modal-title" />
+            )}
           </h3>
           <h4 className="excerpt d-ib">
             <FormattedMessage
@@ -113,6 +118,7 @@ export default createFragmentContainer(container, {
       votesHelpText
       open
       url
+      votesRanking
       viewerVotes(orderBy: { field: POSITION, direction: ASC }) @include(if: $isAuthenticated) {
         totalCount
         ...ProposalsUserVotesTable_votes
