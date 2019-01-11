078999c5319fed737e5cbbb56c3b1166642b9e88
spyl94 <spyl94@gmail.com>
2019-01-11 15:45:04 +0100

    Merge pull request #6867 from cap-collectif/6796-fix-draft-proposal
    [6796-FIX] Add only non-deleted proposal to list.
    
    GitOrigin-RevId: 7fd4a1c6f2aaa1852aef743d7b7ac27b69d3a9ab

--- app/Resources/js/components/Steps/Page/StepPageHeader.js
+++ app/Resources/js/components/Steps/Page/StepPageHeader.js
@@ -26,6 +26,7 @@ class StepPageHeader extends React.Component<Props> {
 
   render() {
     const { step } = this.props;
+
     return (
       <div>
         <h2 className="h2">{step.title}</h2>
@@ -42,7 +43,7 @@ class StepPageHeader extends React.Component<Props> {
             </div>
           )}
         </div>
-        {step.type === 'selection' && step.voteThreshold && step.voteThreshold > 0 ? (
+        {step.type === 'selection' && step.voteThreshold && step.voteThreshold > 0 && (
           <h4 style={{ marginBottom: '20px' }}>
             <i className="cap cap-hand-like-2-1" style={{ fontSize: '22px', color: '#377bb5' }} />{' '}
             <FormattedMessage
@@ -52,7 +53,7 @@ class StepPageHeader extends React.Component<Props> {
               }}
             />
           </h4>
-        ) : null}
+        )}
         {/* $FlowFixMe $refType */}
         <StepInfos step={step} />
       </div>
