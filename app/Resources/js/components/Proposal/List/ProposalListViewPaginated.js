ff3188ca5d922316335eb33e8680260e268afb14
mrpandat <mrpandat@hotmail.fr>
2019-01-11 09:48:13 +0100

    [6812-FIX] Modify trad key to profile vote.
    GitOrigin-RevId: 97c7584f3ddce0df86a34fb6895ccdab9adf8da3

--- app/Resources/js/components/Proposal/List/ProposalListViewPaginated.js
+++ app/Resources/js/components/Proposal/List/ProposalListViewPaginated.js
@@ -13,7 +13,6 @@ type Props = {
   step: ProposalListViewPaginated_step,
   view: 'mosaic' | 'table',
   viewer: ?ProposalListViewPaginated_viewer,
-  count: number,
 };
 type State = {
   loading: boolean,
@@ -25,7 +24,7 @@ export class ProposalListViewPaginated extends React.Component<Props, State> {
   };
 
   render() {
-    const { step, viewer, relay, view, count } = this.props;
+    const { step, viewer, relay, view } = this.props;
     return (
       <div>
         <VisibilityBox enabled={step.private || false}>
@@ -44,11 +43,11 @@ export class ProposalListViewPaginated extends React.Component<Props, State> {
               disabled={this.state.loading}
               onClick={() => {
                 this.setState({ loading: true });
-                relay.loadMore(count, () => {
+                relay.loadMore(50, () => {
                   this.setState({ loading: false });
                 });
               }}>
-              <FormattedMessage id={this.state.loading ? 'global.loading' : 'see-more-proposals'} />
+              <FormattedMessage id="see-more-proposals" />
             </Button>
           )}
         </div>
