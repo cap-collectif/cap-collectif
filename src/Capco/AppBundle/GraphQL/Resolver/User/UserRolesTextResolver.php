9b865b23df050ce05f29a8eed79295b9838d94aa
spyl94 <spyl94@gmail.com>
2019-01-19 21:35:21 +0100

    Fix UserRolesTextResolver
    GitOrigin-RevId: 974d4ba4fd35958ec6538c70b498a3b2a631fe84

--- src/Capco/AppBundle/GraphQL/Resolver/User/UserRolesTextResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/User/UserRolesTextResolver.php
@@ -7,20 +7,29 @@ use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
 
 class UserRolesTextResolver implements ResolverInterface
 {
-    public function __invoke(User $user, User $viewer): ?string
+    public function __invoke(User $user, $viewer, \ArrayObject $context): ?string
     {
-        if ($user === $viewer || $viewer->isAdmin()) {
-            $convertedRoles = array_map(function ($role) {
-                return str_replace(
-                    ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_SUPER_ADMIN'],
-                    ['Utilisateur', 'Administrateur', 'Super Admin'],
-                    $role
-                );
-            }, $user->getRoles());
+        if ($context->offsetExists('disable_acl') && true === $context->offsetGet('disable_acl')) {
+            return $this->resolve($user);
+        }
 
-            return implode('|', $convertedRoles);
+        if ($viewer instanceof User && ($user === $viewer || $viewer->isAdmin())) {
+            return $this->resolve($user);
         }
 
         return null;
+    }
+
+    private function resolve(User $user)
+    {
+        $convertedRoles = array_map(function ($role) {
+            return str_replace(
+                ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_SUPER_ADMIN'],
+                ['Utilisateur', 'Administrateur', 'Super Admin'],
+                $role
+            );
+        }, $user->getRoles());
+
+        return implode('|', $convertedRoles);
     }
 }
