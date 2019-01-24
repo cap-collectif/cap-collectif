1eddd5430f6a6daf1892066af26e5e8bec7a9278
sanzodown <sanzodown@hotmail.fr>
2019-01-24 17:28:03 +0100

    6818-PAGE8 add ariaLabel
    GitOrigin-RevId: a37bf7f4b6ff48bec993723c6ac6a76fa3e72b92

--- app/Resources/js/utils/sizes.js
+++ app/Resources/js/utils/sizes.js
@@ -2,13 +2,11 @@
 export const avatarNb = {
   small: 34,
   normal: 45,
-  tiny: 16,
 };
 
 export const avatarPx = {
   small: `${avatarNb.small}px`,
   normal: `${avatarNb.normal}px`,
-  tiny: `${avatarNb.tiny}px`,
 };
 
 const sizes = {
