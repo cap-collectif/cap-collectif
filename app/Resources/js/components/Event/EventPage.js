fa49bd06bb8d46f3585081a4edc175045431e0e2
spyl94 <spyl94@gmail.com>
2019-01-24 12:17:24 +0100

    Merge pull request #7051 from cap-collectif/7049-add-populate-option
    Stop erasing data after creation of a new index
    
    GitOrigin-RevId: 22828b4ed305421775e8e702059c54ed720c11f3

--- app/Resources/js/components/Event/EventPage.js
+++ app/Resources/js/components/Event/EventPage.js
@@ -35,7 +35,7 @@ export class EventPage extends React.Component<Props> {
               $userType: ID
               $isFuture: Boolean
             ) {
-              ...EventPageHeader_query
+              ...EventRefetch_query
                 @arguments(
                   cursor: $cursor
                   count: $count
@@ -45,7 +45,7 @@ export class EventPage extends React.Component<Props> {
                   userType: $userType
                   isFuture: $isFuture
                 )
-              ...EventRefetch_query
+              ...EventListCounter_query
                 @arguments(
                   cursor: $cursor
                   count: $count
@@ -77,7 +77,7 @@ export class EventPage extends React.Component<Props> {
                 <div>
                   <section className="jumbotron--bg-1 ">
                     {/* $FlowFixMe */}
-                    <EventPageHeader query={props} eventPageTitle={this.props.eventPageTitle} />
+                    <EventPageHeader eventPageTitle={this.props.eventPageTitle} />
                   </section>
                   {this.props.eventPageBody && (
                     <section className="section--custom">
@@ -91,7 +91,7 @@ export class EventPage extends React.Component<Props> {
                       <div
                         id="event-page-filters"
                         className={config.isMobile ? 'event-filters' : null}>
-                        <EventListFilters addToggleViewButton />
+                        <EventListFilters query={props} addToggleViewButton />
                       </div>
                       <div id="event-page-rendered">
                         {/* $FlowFixMe */}
