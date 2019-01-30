21fa8df7e6b79f3911698cdfd016ae75a2d17ac0
mrpandat <mrpandat@hotmail.fr>
2019-01-30 17:08:05 +0100

    [7064-FIX] Add more snapshots to mail behat.
    GitOrigin-RevId: b29b89602226e041753bfb3a95642c8f2978079d

--- src/Capco/AppBundle/GraphQL/Resolver/Project/ProjectAuthorsResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Project/ProjectAuthorsResolver.php
@@ -31,9 +31,7 @@ class ProjectAuthorsResolver implements ResolverInterface
         $orderBy = $args->offsetGet('orderBy');
 
         try {
-            $authorsIds = array_map(function ($item) {
-                return $item['id'];
-            }, $this->projectRepository->getAuthorsId($user, $orderBy));
+            $authorsIds = $this->projectRepository->getAuthors($user, $orderBy);
 
             return $this->getHydratedResults($authorsIds);
         } catch (\RuntimeException $exception) {
@@ -43,17 +41,19 @@ class ProjectAuthorsResolver implements ResolverInterface
         }
     }
 
-    private function getHydratedResults(array $ids): array
+    private function getHydratedResults(array $results): array
     {
         // We can't use findById because we would lost the correct order given by ES
         // https://stackoverflow.com/questions/28563738/symfony-2-doctrine-find-by-ordered-array-of-id/28578750
-        $users = $this->userRepository->hydrateFromIds($ids);
-        // We have to restore the correct order of ids, because Doctrine has lost it, see:
-        // https://stackoverflow.com/questions/28563738/symfony-2-doctrine-find-by-ordered-array-of-id/28578750
-        usort($users, function ($a, $b) use ($ids) {
-            return array_search($a->getId(), $ids, false) > array_search($b->getId(), $ids, false);
-        });
-
-        return $users;
+        return array_values(
+            array_filter(
+                array_map(function ($result) {
+                    return $this->userRepository->findOneBy(['id' => $result['id']]);
+                }, $results),
+                function (?User $user) {
+                    return null !== $user;
+                }
+            )
+        );
     }
 }
