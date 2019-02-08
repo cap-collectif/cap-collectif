6c4cf42479d58a7e43aadd785caf7918ce537d18
sanzodown <sanzodown@hotmail.fr>
2019-02-08 19:09:39 +0100

    FIX-EXPORT create commande for events
    GitOrigin-RevId: b9a88d1275d4520b0bf3ee50bc48d1c266d3163f

---
+++ src/Capco/AppBundle/Command/CreateCsvFromEventsCommand.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Capco\AppBundle\Command;
+
+use Box\Spout\Common\Type;
+use Box\Spout\Writer\WriterFactory;
+use Box\Spout\Writer\WriterInterface;
+use Capco\AppBundle\EventListener\GraphQlAclListener;
+use Capco\AppBundle\GraphQL\ConnectionTraversor;
+use Capco\AppBundle\Toggle\Manager;
+use Capco\AppBundle\Utils\Arr;
+use Overblog\GraphQLBundle\Request\Executor;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateCsvFromEventsCommand extends ContainerAwareCommand
+{
+    public const EVENTS_HEADERS = [
+        'title',
+        'body',
+        'media_id',
+        'media_url',
+        'author_id',
+        'author_email',
+        'created_at',
+        'updated_at',
+        'is_enabled',
+        'start_at',
+        'end_at',
+        'zipCode',
+        'address',
+        'city',
+        'country',
+        'lat',
+        'lng',
+        'link',
+        'registration_enabled',
+        'comments_count',
+        'is_commentable',
+        'meta_description',
+        'custom_code',
+    ];
+    /**
+     * @var WriterInterface
+     */
+    protected $writer;
+    protected $connectionTraversor;
+    protected $listener;
+    protected $executor;
+    protected $projectRootDir;
+    protected $logger;
+
+    public function __construct(
+        GraphQlAclListener $listener,
+        ConnectionTraversor $connectionTraversor,
+        Executor $executor,
+        string $projectRootDir,
+        LoggerInterface $logger
+    ) {
+        $listener->disableAcl();
+        $this->connectionTraversor = $connectionTraversor;
+        $this->executor = $executor;
+        $this->projectRootDir = $projectRootDir;
+        $this->logger = $logger;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setName('capco:export:events')->setDescription('Create csv file from event data');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $container = $this->getContainer();
+        if (!$container->get(Manager::class)->isActive('export')) {
+            return;
+        }
+        $fileName = 'events.csv';
+
+        $data = $this->executor
+            ->execute('internal', [
+                'query' => $this->getEventsGraphQLQuery(),
+                'variables' => [],
+            ])
+            ->toArray();
+
+        if (!isset($data['data'])) {
+            $this->logger->error('GraphQL Query Error: ' . $data['error']);
+            $this->logger->info('GraphQL query: ' . json_encode($data));
+        }
+
+        $this->writer = WriterFactory::create(Type::CSV);
+        $this->writer->openToFile(sprintf('%s/web/export/%s', $this->projectRootDir, $fileName));
+        $this->writer->addRow(self::EVENTS_HEADERS);
+        $writer = $this->writer;
+
+        $totalCount = Arr::path($data, 'data.events.totalCount');
+        $progress = new ProgressBar($output, $totalCount);
+
+        $this->connectionTraversor->traverse(
+            $data,
+            'events',
+            function ($edge) use ($writer, $progress) {
+                $event = $edge['node'];
+                $writer->addRow([
+                    $event['title'],
+                    $event['body'],
+                    $event['media'] ? $event['media']['id'] : null,
+                    $event['media'] ? $event['media']['url'] : null,
+                    $event['author'] ? $event['author']['id'] : null,
+                    $event['author'] ? $event['author']['email'] : null,
+                    $event['createdAt'],
+                    $event['updatedAt'],
+                    $event['enabled'],
+                    $event['startAt'],
+                    $event['endAt'],
+                    $event['zipCode'],
+                    $event['address'],
+                    $event['city'],
+                    $event['country'],
+                    $event['lat'],
+                    $event['lng'],
+                    $event['link'],
+                    $event['registrationEnable'],
+                    $event['comments'] ? $event['comments']['totalCount'] : null,
+                    $event['commentable'],
+                    $event['metaDescription'],
+                    $event['customCode'],
+                ]);
+                $progress->advance();
+            },
+            function ($pageInfo) {
+                return $this->getEventsGraphQLQuery($pageInfo['endCursor']);
+            }
+        );
+        $writer->close();
+
+        $progress->finish();
+        $output->writeln('The export file "' . $fileName . '" has been created.');
+    }
+
+    private function getEventsGraphQLQuery(?string $userCursor = null): string
+    {
+        if ($userCursor) {
+            $userCursor = sprintf(', after: "%s"', $userCursor);
+        }
+
+        return <<<EOF
+        {
+          events(first: 100 ${userCursor}) {
+            totalCount
+            pageInfo {
+              startCursor
+              endCursor
+              hasNextPage
+            }
+            edges {
+              cursor  
+              node {
+                title
+                body
+                media {
+                  id
+                  url
+                }
+                author {
+                  id
+                  email
+                }
+                createdAt
+                updatedAt
+                enabled
+                startAt
+                endAt
+                zipCode
+                address
+                city
+                country
+                lat
+                lng
+                link
+                registrationEnable
+                comments {
+                  totalCount
+                }
+                commentable
+                metaDescription
+                customCode
+              }
+            }
+          }
+        }
+EOF;
+    }
+}
--- src/Capco/AppBundle/Controller/Site/EventController.php
+++ src/Capco/AppBundle/Controller/Site/EventController.php
@@ -6,11 +6,15 @@ use Capco\AppBundle\Entity\Event;
 use Capco\AppBundle\Form\EventRegistrationType;
 use Capco\AppBundle\Helper\EventHelper;
 use Capco\AppBundle\SiteParameter\Resolver;
+use Capco\UserBundle\Security\Exception\ProjectAccessDeniedException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class EventController extends Controller
 {
@@ -26,6 +30,54 @@ class EventController extends Controller
                 'eventPageBody' => $this->get(Resolver::class)->getValue('events.content.body'),
             ],
         ];
+    }
+
+    /**
+     * @Route("/events/download", name="app_events_download")
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function downloadAction(Request $request)
+    {
+        $trans = $this->get('translator');
+
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            throw new ProjectAccessDeniedException(
+                $trans->trans('project.error.not_exportable', [], 'CapcoAppBundle')
+            );
+        }
+
+        $path = sprintf('%s/web/export/', $this->container->getParameter('kernel.project_dir'));
+        $csvFile = 'events.csv';
+
+        if (!file_exists($path . $csvFile)) {
+            // We create a session for flashBag
+            $flashBag = $this->get('session')->getFlashBag();
+
+            $flashBag->add(
+                'danger',
+                $trans->trans('project.download.not_yet_generated', [], 'CapcoAppBundle')
+            );
+
+            return $this->redirect($request->headers->get('referer'));
+        }
+
+        $filename = $csvFile;
+        $contentType = 'text/csv';
+
+        $date = (new \DateTime())->format('Y-m-d');
+
+        $request->headers->set('X-Sendfile-Type', 'X-Accel-Redirect');
+        $response = new BinaryFileResponse($path . $filename);
+        $response->headers->set('X-Accel-Redirect', '/export/' . $filename);
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $date . '_' . $filename
+        );
+        $response->headers->set('Content-Type', $contentType . '; charset=utf-8');
+        $response->headers->set('Pragma', 'public');
+        $response->headers->set('Cache-Control', 'maxage=1');
+
+        return $response;
     }
 
     /**
