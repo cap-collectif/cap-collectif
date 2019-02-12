943f42f3f96650d5c76f6e87850b963da9272fb2
mauriau <maxime.auriau@protonmail.com>
2019-02-12 16:47:50 +0100

    #6984 - fix migration and css
    GitOrigin-RevId: ee5cc47e304edae13687edad07be2b1d3616f1a4

--- app/DoctrineMigrations/Version20190212115338.php
+++ app/DoctrineMigrations/Version20190212115338.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Application\Migrations;
 
@@ -8,25 +9,33 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20190212115338 extends AbstractMigration
+final class Version20190212115338 extends AbstractMigration
 {
     public function postUp(Schema $schema)
     {
-        $this->connection->insert('site_parameter', [
-            'keyname' => 'cookies-list',
-            'category' => 'pages.cookies',
-            'position' => 3,
-            'type' => 1,
-        ]);
-        $id = $this->connection->lastInsertId();
-        $this->connection->update(
-            'site_parameter',
-            [
-                'created_at' => (new \DateTime())->format('Y-m-d H:i:s'),
-                'updated_at' => (new \DateTime())->format('Y-m-d H:i:s'),
-            ],
-            ['id' => $id]
-        );
+        $cookiesList = $this->connection->fetchAll('SELECT id FROM site_parameter where keyname = "cookies-list"');
+        if (count($cookiesList) === 0) {
+            $this->connection->insert(
+                'site_parameter',
+                [
+                    'keyname' => 'cookies-list',
+                    'category' => 'pages.cookies',
+                    'position' => 3,
+                    'type' => 1,
+                    'is_enabled' => true,
+                ]
+            );
+        } else {
+            $this->connection->update(
+                'site_parameter',
+                [
+                    'category' => 'pages.cookies',
+                    'position' => 3,
+                    'type' => 1,
+                ],
+                $cookiesList['id']
+            );
+        }
     }
 
     public function up(Schema $schema)
