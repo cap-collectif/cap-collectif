7b87b2fb7c63b9ccf86898ceb7a2b085bd56d0a9
ptondereau <pierre.tondereau@protonmail.com>
2019-02-13 18:10:51 +0100

    Add optionnal parameter to questionnaire export command to select a given step
    GitOrigin-RevId: 82f0119f9c7a68516703723da3e72f113b210a12

--- src/Capco/AppBundle/Command/CreateCsvFromQuestionnaireStepCommand.php
+++ src/Capco/AppBundle/Command/CreateCsvFromQuestionnaireStepCommand.php
@@ -2,35 +2,55 @@
 
 namespace Capco\AppBundle\Command;
 
+use Capco\AppBundle\Entity\Steps\QuestionnaireStep;
+use Capco\AppBundle\Repository\QuestionnaireStepRepository;
 use Capco\AppBundle\Resolver\ProjectDownloadResolver;
 use Capco\AppBundle\Toggle\Manager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CreateCsvFromQuestionnaireStepCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this->setName('capco:export:questionnaire')->setDescription(
-            'Create csv file from questionnaire step data'
-        );
+        $this->setName('capco:export:questionnaire')
+            ->setDescription('Create csv file from questionnaire step data')
+            ->addArgument(
+                'questionnaireStepId',
+                InputArgument::OPTIONAL,
+                'The ID of the questionnaire step'
+            )
+            ->addOption(
+                'force',
+                false,
+                InputOption::VALUE_NONE,
+                'set this option to force export if feature toggle "export" is disabled'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $container = $this->getContainer();
-        if (!$container->get(Manager::class)->isActive('export')) {
+        if (!$input->getOption('force') && !$container->get(Manager::class)->isActive('export')) {
             $output->writeln('Please enable "export" feature to run this command');
 
             return;
         }
+
         $resolver = $container->get(ProjectDownloadResolver::class);
 
-        $steps = $container
-            ->get('doctrine')
-            ->getRepository('CapcoAppBundle:Steps\QuestionnaireStep')
-            ->findAll();
+        if (($questionnaireStep = $this->getQuestionnaireStep($input))) {
+            $steps = [$questionnaireStep];
+        } else {
+            $steps = $container
+                ->get('doctrine')
+                ->getRepository('CapcoAppBundle:Steps\QuestionnaireStep')
+                ->findAll();
+        }
+
         foreach ($steps as $qs) {
             $writer = $resolver->getContent($qs);
             $filename = '';
@@ -42,5 +62,17 @@ class CreateCsvFromQuestionnaireStepCommand extends ContainerAwareCommand
             $writer->save($path . '/../web/export/' . $filename);
             $output->writeln('The export file "' . $filename . '" has been created.');
         }
+    }
+
+    private function getQuestionnaireStep(InputInterface $input): ?QuestionnaireStep
+    {
+        if (!$input->getArgument('questionnaireStepId')) {
+            return null;
+        }
+
+        return $this->getContainer()
+            ->get('doctrine')
+            ->getRepository(QuestionnaireStepRepository::class)
+            ->find($input->getArgument('questionnaireStepId'));
     }
 }
