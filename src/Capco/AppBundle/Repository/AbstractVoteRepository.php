93bd63c7f89e9802c81390da7999f78fb567e92d
sanzodown <sanzodown@hotmail.fr>
2019-02-14 18:16:11 +0100

    COUNTERS-OPTIMIZE pour faire plaisir a maxime
    GitOrigin-RevId: f29adf7e410c2fe8c99fc60479f20fdafc95d936

--- src/Capco/AppBundle/Repository/AbstractVoteRepository.php
+++ src/Capco/AppBundle/Repository/AbstractVoteRepository.php
@@ -174,64 +174,60 @@ class AbstractVoteRepository extends EntityRepository
 
     public function getVotesFromCollectStep(AbstractStep $step): int
     {
-        $count = 0;
         $id = $step->getId();
 
         $sql = "
-            SELECT COUNT(v.id) as nb , 'opi' as entity
-            FROM votes v
-            LEFT JOIN opinion o ON v.opinion_id = o.id
-            WHERE o.step_id =\"${id}\"
-            AND v.published = true
-            UNION
-            SELECT COUNT(v.id) as nb, 'args' as entity
-            FROM votes v
-            LEFT JOIN argument a ON v.argument_id = a.id
-            LEFT JOIN opinion o ON a.opinion_id = o.id
-            WHERE o.step_id =\"${id}\"
-            AND v.published = true
-            UNION
-            SELECT COUNT(v.id) as nb, 'src' as entity
-            FROM votes v
-            LEFT JOIN source s ON v.source_id = s.id
-            LEFT JOIN opinion o ON s.opinion_id = o.id
-            WHERE o.step_id =\"${id}\"
-            AND v.published = true
-            UNION
-            SELECT COUNT(v.id) as nb, 'opv'as entity
-            FROM votes v
-            LEFT JOIN opinion_version ov ON v.opinion_version_id = ov.id
-            LEFT JOIN opinion o ON ov.opinion_id = o.id
-            WHERE o.step_id =\"${id}\"
-            AND v.published = true
-            UNION
-            SELECT COUNT(v.id) as nb, 'aropv'as entity
-            FROM votes v
-            LEFT JOIN argument a ON v.argument_id = a.id
-            LEFT JOIN opinion_version ov ON a.opinion_version_id = ov.id
-            LEFT JOIN opinion o ON ov.opinion_id = o.id
-            WHERE o.step_id =\"${id}\"
-            AND v.published = true
-            UNION
-            SELECT COUNT(v.id) as nb, 'srcopv'as entity
-            FROM votes v
-            LEFT JOIN source s ON v.source_id = s.id
-            LEFT JOIN opinion_version ov ON s.opinion_version_id = ov.id
-            LEFT JOIN opinion o ON ov.opinion_id = o.id
-            WHERE o.step_id =\"${id}\"
-            AND v.published = true
+            SELECT SUM(nb) as result
+            FROM (
+                SELECT COUNT(v.id) as nb , 'opi' as entity
+                FROM votes v
+                LEFT JOIN opinion o ON v.opinion_id = o.id
+                WHERE o.step_id =\"${id}\"
+                AND v.published = true
+                UNION
+                SELECT COUNT(v.id) as nb, 'args' as entity
+                FROM votes v
+                LEFT JOIN argument a ON v.argument_id = a.id
+                LEFT JOIN opinion o ON a.opinion_id = o.id
+                WHERE o.step_id =\"${id}\"
+                AND v.published = true
+                UNION
+                SELECT COUNT(v.id) as nb, 'src' as entity
+                FROM votes v
+                LEFT JOIN source s ON v.source_id = s.id
+                LEFT JOIN opinion o ON s.opinion_id = o.id
+                WHERE o.step_id =\"${id}\"
+                AND v.published = true
+                UNION
+                SELECT COUNT(v.id) as nb, 'opv'as entity
+                FROM votes v
+                LEFT JOIN opinion_version ov ON v.opinion_version_id = ov.id
+                LEFT JOIN opinion o ON ov.opinion_id = o.id
+                WHERE o.step_id =\"${id}\"
+                AND v.published = true
+                UNION
+                SELECT COUNT(v.id) as nb, 'aropv'as entity
+                FROM votes v
+                LEFT JOIN argument a ON v.argument_id = a.id
+                LEFT JOIN opinion_version ov ON a.opinion_version_id = ov.id
+                LEFT JOIN opinion o ON ov.opinion_id = o.id
+                WHERE o.step_id =\"${id}\"
+                AND v.published = true
+                UNION
+                SELECT COUNT(v.id) as nb, 'srcopv'as entity
+                FROM votes v
+                LEFT JOIN source s ON v.source_id = s.id
+                LEFT JOIN opinion_version ov ON s.opinion_version_id = ov.id
+                LEFT JOIN opinion o ON ov.opinion_id = o.id
+                WHERE o.step_id =\"${id}\"
+                AND v.published = true
+            ) res
         ";
 
-        $stmt = $this->getEntityManager()
+        return $this->getEntityManager()
             ->getConnection()
             ->query($sql)
-            ->fetchAll();
-
-        foreach ($stmt as $result) {
-            $count += $result['nb'];
-        }
-
-        return $count;
+            ->fetchAll()[0]['result'];
     }
 
     protected function getQueryBuilder()
