658012e50963812b43296e7a8530ba88d58cb5be
spyl94 <spyl94@gmail.com>
2019-02-18 16:18:20 +0100

    Merge pull request #7264 from cap-collectif/hot-fix-for-pagination
    Hotfix for API pagination
    
    GitOrigin-RevId: 8fec74f8a50d3525507f3a78cd6759d3e6a07e94

--- src/Capco/AppBundle/Elasticsearch/Indexer.php
+++ src/Capco/AppBundle/Elasticsearch/Indexer.php
@@ -65,6 +65,11 @@ class Indexer
         $this->logger = $logger;
     }
 
+    public function getIndex(): Index
+    {
+        return $this->index;
+    }
+
     /**
      * Fetch ALL the indexable entities and send them to bulks.
      */
