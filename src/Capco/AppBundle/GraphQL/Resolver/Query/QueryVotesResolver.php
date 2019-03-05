7add0b3c8d80b51846d14eb1ef6f5eb216141573
sanzodown <sanzodown@hotmail.fr>
2019-03-05 15:19:56 +0100

    7074-GLOBAL fix votes count
    GitOrigin-RevId: aa5e75107df3fdfa75df781236fa522b4af15ea3

--- src/Capco/AppBundle/GraphQL/Resolver/Query/QueryVotesResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Query/QueryVotesResolver.php
@@ -2,34 +2,82 @@
 
 namespace Capco\AppBundle\GraphQL\Resolver\Query;
 
+use Capco\AppBundle\Entity\Project;
+use Capco\AppBundle\Entity\Steps\AbstractStep;
+use Capco\AppBundle\Entity\Steps\CollectStep;
+use Capco\AppBundle\Entity\Steps\ConsultationStep;
+use Capco\AppBundle\Entity\Steps\SelectionStep;
+use Capco\AppBundle\GraphQL\Resolver\Project\ProjectsResolver;
+use Capco\AppBundle\GraphQL\Resolver\Step\StepVotesCountResolver;
 use Capco\AppBundle\Repository\AbstractVoteRepository;
 use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
 use Overblog\GraphQLBundle\Definition\Argument;
 use Overblog\GraphQLBundle\Relay\Connection\Output\Connection;
 use Overblog\GraphQLBundle\Relay\Connection\Paginator;
+use Overblog\PromiseAdapter\PromiseAdapterInterface;
 
 class QueryVotesResolver implements ResolverInterface
 {
     protected $votesRepository;
+    protected $projectsResolver;
+    protected $stepVotesCountResolver;
+    protected $adapter;
 
-    public function __construct(AbstractVoteRepository $votesRepository)
-    {
+    public function __construct(
+        AbstractVoteRepository $votesRepository,
+        ProjectsResolver $projectsResolver,
+        StepVotesCountResolver $stepVotesCountResolver,
+        PromiseAdapterInterface $adapter
+    ) {
         $this->votesRepository = $votesRepository;
+        $this->projectsResolver = $projectsResolver;
+        $this->stepVotesCountResolver = $stepVotesCountResolver;
+        $this->adapter = $adapter;
     }
 
     public function __invoke(Argument $args): Connection
     {
         $totalCount = 0;
+        foreach ($this->projectsResolver->__invoke($args)->edges as $edge) {
+            $totalCount += $this->countProjectVotes($edge->node);
+        }
 
         $paginator = new Paginator(function (int $offset, int $limit) {
-            $votes = $this->votesRepository->findAllVotes($limit, $offset);
-
-            return $votes->getIterator()->getArrayCopy();
+            return [];
         });
 
         $connection = $paginator->auto($args, $totalCount);
-        $connection->totalCount = $this->votesRepository->countPublished();
+        $connection->totalCount = $totalCount;
 
         return $connection;
+    }
+
+    public function countProjectVotes(Project $project): int
+    {
+        $totalCount = 0;
+
+        foreach ($project->getSteps() as $pas) {
+            $totalCount += $this->countStepVotes($pas->getStep());
+        }
+
+        return $totalCount;
+    }
+
+    public function countStepVotes(AbstractStep $step): int
+    {
+        $count = 0;
+        if ($step instanceof ConsultationStep) {
+            $count = $step->getVotesCount();
+        } elseif ($step instanceof SelectionStep || $step instanceof CollectStep) {
+            $promise = $this->stepVotesCountResolver
+                ->__invoke($step)
+                ->then(function ($value) use (&$count) {
+                    $count += $value;
+                });
+
+            $this->adapter->await($promise);
+        }
+
+        return $count;
     }
 }
