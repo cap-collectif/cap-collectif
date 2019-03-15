6a22e67227de0fdf77fed6ec358b920f839e7c2b
sanzodown <sanzodown@hotmail.fr>
2019-03-15 18:42:02 +0100

    7074-GLOBAL fix counters
    GitOrigin-RevId: 05d96bafa4dcac920a439130ae4f2d51c51a421f

--- src/Capco/AppBundle/GraphQL/Resolver/Query/QueryContributionsResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Query/QueryContributionsResolver.php
@@ -2,21 +2,38 @@
 
 namespace Capco\AppBundle\GraphQL\Resolver\Query;
 
-use Capco\AppBundle\Search\ProjectSearch;
+use Capco\AppBundle\Entity\Project;
+use Capco\AppBundle\Enum\ProjectVisibilityMode;
+use Capco\AppBundle\GraphQL\Resolver\Project\ProjectContributionResolver;
+use Capco\AppBundle\Repository\ProjectRepository;
 use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
 use Overblog\GraphQLBundle\Definition\Argument;
 
 class QueryContributionsResolver implements ResolverInterface
 {
-    protected $projectSearch;
+    protected $projectRepository;
+    protected $projectContributionResolver;
 
-    public function __construct(ProjectSearch $projectSearch)
-    {
-        $this->projectSearch = $projectSearch;
+    public function __construct(
+        ProjectRepository $projectRepository,
+        ProjectContributionResolver $projectContributionResolver
+    ) {
+        $this->projectRepository = $projectRepository;
+        $this->projectContributionResolver = $projectContributionResolver;
     }
 
     public function __invoke(Argument $args): int
     {
-        return $this->projectSearch->getAllContributions();
+        $totalCount = array_sum(
+            array_map(function (Project $project) {
+                if (ProjectVisibilityMode::VISIBILITY_PUBLIC === $project->getVisibility()) {
+                    return $this->projectContributionResolver->__invoke($project);
+                }
+
+                return 0;
+            }, $this->projectRepository->findAll())
+        );
+
+        return $totalCount;
     }
 }
