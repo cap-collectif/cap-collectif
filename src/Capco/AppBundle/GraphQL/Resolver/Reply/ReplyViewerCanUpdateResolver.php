51fbec5aa21a05738c911e4eb680fae8ca07f097
spyl94 <spyl94@gmail.com>
2019-04-04 14:24:45 +0200

    Merge pull request #7511 from cap-collectif/7504-fix-edit-survey
    fix update/delete reply
    
    GitOrigin-RevId: 1abd31b103f6a3bc443a086d8128d9cef03dd8b6

--- src/Capco/AppBundle/GraphQL/Resolver/Reply/ReplyViewerCanUpdateResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Reply/ReplyViewerCanUpdateResolver.php
@@ -10,6 +10,6 @@ class ReplyViewerCanUpdateResolver implements ResolverInterface
 {
     public function __invoke(Reply $reply, User $user): bool
     {
-        return $reply->getAuthor() === $user && $reply->getQuestionnaire()->canContribute();
+        return $reply->getAuthor() === $user && $reply->getQuestionnaire()->canContribute($user);
     }
 }
