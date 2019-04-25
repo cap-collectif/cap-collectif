33884d2f43a08cbd29cfe010e823850322e5991f
Liinkiing <omar.jbara2@gmail.com>
2019-04-25 11:19:42 +0200

    - Usage of EntityRepository instead of ServiceEntityRepository for QuestionnaireStepRepository
    GitOrigin-RevId: a47f16d92bfe584449d255e0a2baa6549113c744

--- src/Capco/AppBundle/Repository/QuestionnaireStepRepository.php
+++ src/Capco/AppBundle/Repository/QuestionnaireStepRepository.php
@@ -3,18 +3,13 @@
 namespace Capco\AppBundle\Repository;
 
 use Capco\AppBundle\Entity\Steps\QuestionnaireStep;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityRepository;
 
 /**
  * @method QuestionnaireStep|null find($id, $lockMode = null, $lockVersion = null)
  * @method QuestionnaireStep|null findOneBy(array $criteria, array $orderBy = null)
  * @method QuestionnaireStep[]    findAll()
  * @method QuestionnaireStep[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null) */
-class QuestionnaireStepRepository extends ServiceEntityRepository
+class QuestionnaireStepRepository extends EntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, QuestionnaireStep::class);
-    }
 }
