4de3cfe1ca66abeaa5ac4caddadfd3cbefb269b5
spyl94 <spyl94@gmail.com>
2019-05-22 18:30:08 +0200

    Update src/Capco/AppBundle/Controller/Site/DeveloperController.php
    Co-Authored-By: Pierre Tondereau <ptondereau@users.noreply.github.com>
    GitOrigin-RevId: f6550bd61f0f7e610d26f0a8c6938d1ad76252e7

--- src/Capco/AppBundle/Controller/Site/DeveloperController.php
+++ src/Capco/AppBundle/Controller/Site/DeveloperController.php
@@ -161,7 +161,6 @@ class DeveloperController extends Controller
 
                     // We hydrate current selection for mutations.
                     if ('mutation' === $category && $type) {
-                        // die();
                         $selection = array_values(
                             array_filter($mutation->getFields(), function (
                                 FieldDefinition $field
