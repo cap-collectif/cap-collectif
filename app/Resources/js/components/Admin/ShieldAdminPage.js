0f82a437632784564dc56f397025f83896a4cced
ptondereau <pierre.tondereau@protonmail.com>
2019-05-31 18:50:03 +0200

    7745-REACT Refactor authenticatipn page to react.
    GitOrigin-RevId: 57502ac90880f693e4174503c2aba2055ae5e28a

---
+++ app/Resources/js/components/Admin/ShieldAdminPage.js
@@ -0,0 +1,49 @@
+// @flow
+import React from 'react';
+import type { ReadyState } from 'react-relay';
+import { graphql, QueryRenderer } from 'react-relay';
+import ShieldAdminForm from './ShieldAdminForm';
+import type { ShieldmAdminPageQueryResponse } from '~relay/ShieldAdminPageQuery.graphql';
+import environment, { graphqlError } from '../../createRelayEnvironment';
+import Loader from '../Ui/FeedbacksIndicators/Loader';
+
+const component = ({
+  error,
+  props,
+}: {
+  props: ?ShieldmAdminPageQueryResponse,
+} & ReadyState) => {
+  if (error) {
+    console.log(error); // eslint-disable-line no-console
+    return graphqlError;
+  }
+  if (props) {
+    // eslint-disable-next-line
+    if (props.shieldAdminForm !== null) {
+      return <ShieldAdminForm {...props} />;
+    }
+    return graphqlError;
+  }
+  return <Loader />;
+};
+
+export class ShieldAdminPage extends React.Component<{}> {
+  render() {
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query ShieldAdminPageQuery {
+            shieldAdminForm {
+              ...ShieldAdminForm_shieldAdminForm
+            }
+          }
+        `}
+        variables={{}}
+        render={component}
+      />
+    );
+  }
+}
+
+export default ShieldAdminPage;
