f53a22e6a9b30889d64936a37f63ba51ae246b6d
ptondereau <pierre.tondereau@protonmail.com>
2019-06-05 15:47:34 +0200

    7745-REACT Add php tests.
    GitOrigin-RevId: 4c65ab4969bc0ed63fca3007dde1b716e6d218c3

---
+++ spec/Capco/AppBundle/GraphQL/Resolver/Query/ShieldAdminFormQueryResolverSpec.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace spec\Capco\AppBundle\GraphQL\Query;
+
+use Capco\AppBundle\Entity\SiteImage;
+use Capco\AppBundle\Entity\SiteParameter;
+use Capco\AppBundle\Repository\SiteImageRepository;
+use Capco\AppBundle\Repository\SiteParameterRepository;
+use Capco\AppBundle\Toggle\Manager;
+use Capco\MediaBundle\Entity\Media;
+use PhpSpec\ObjectBehavior;
+
+class ShieldAdminFormQueryResolverSpec extends ObjectBehavior
+{
+    public function let(
+        Manager $toggleManager,
+        SiteParameterRepository $siteParameterRepository,
+        SiteImageRepository $siteImageRepository
+    ) {
+        $this->beConstructedWith($toggleManager, $siteParameterRepository, $siteImageRepository);
+    }
+
+    public function it should return informations about shield mode(
+        Manager $toggleManager,
+        SiteImageRepository $siteImageRepository,
+        SiteParameterRepository $siteParameterRepository,
+        SiteParameter $siteParameter,
+        SiteImage $siteImage,
+        Media $media
+    ) {
+        $siteParameter->getValue()->willReturn('<p>Introduction text in shield mode</p>');
+        $siteImage->getMedia()->willReturn($media);
+
+        $siteParameterRepository
+            ->findOneBy(['keyname' => 'shield.introduction'])
+            ->willReturn($siteParameter);
+        $siteImageRepository->findOneBy(['keyname' => 'image.shield'])->willReturn($siteImage);
+
+        $toggleManager->isActive('shield_mode')->willReturn(true);
+
+        $this->_invoke()->shouldReturn([
+            'shieldMode' => true,
+            'introduction' => '<p>Introduction text in shield mode</p>',
+            'image' => $media,
+        ]);
+    }
+}
