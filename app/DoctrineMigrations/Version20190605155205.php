129dca429f22d00c808f6ea6e608d67973446315
mrpandat <mrpandat@hotmail.fr>
2019-06-05 13:53:13 +0000

    [7738-MULTI] Test migration.
    GitOrigin-RevId: 784c35068980c6a182a051d403a5f1f448b56e9f

---
+++ app/DoctrineMigrations/Version20190605155205.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190605155205 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            'mysql' !== $this->connection->getDatabasePlatform()->getName(),
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql(
+            'CREATE TABLE project_author (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', user_id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', project_id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', created_at DATETIME NOT NULL, INDEX IDX_AA0B3382A76ED395 (user_id), INDEX IDX_AA0B3382166D1F9C (project_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB'
+        );
+        $this->addSql(
+            'ALTER TABLE project_author ADD CONSTRAINT FK_AA0B3382A76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id) ON DELETE CASCADE'
+        );
+        $this->addSql(
+            'ALTER TABLE project_author ADD CONSTRAINT FK_AA0B3382166D1F9C FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE'
+        );
+        $this->addSql('ALTER TABLE project DROP FOREIGN KEY FK_2FB3D0EEF675F31B');
+        $this->addSql('DROP INDEX IDX_2FB3D0EEF675F31B ON project');
+        $this->addSql('ALTER TABLE project DROP author_id');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            'mysql' !== $this->connection->getDatabasePlatform()->getName(),
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('DROP TABLE project_author');
+        $this->addSql(
+            'ALTER TABLE project ADD author_id CHAR(36) DEFAULT NULL COLLATE utf8_unicode_ci COMMENT \'(DC2Type:guid)\''
+        );
+        $this->addSql(
+            'ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EEF675F31B FOREIGN KEY (author_id) REFERENCES fos_user (id) ON DELETE SET NULL'
+        );
+        $this->addSql('CREATE INDEX IDX_2FB3D0EEF675F31B ON project (author_id)');
+    }
+}
