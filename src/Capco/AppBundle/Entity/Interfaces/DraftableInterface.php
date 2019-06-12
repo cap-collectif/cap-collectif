866ff54886b854382c79421ba43ff5391fd5dba2
mauriau <maxime.auriau@protonmail.com>
2019-06-12 17:46:20 +0200

    #7429-DRAFT add comment on draftableinterface
    GitOrigin-RevId: 59666ba02b3c64ebbb9442ffe139033bb19d1a64

--- src/Capco/AppBundle/Entity/Interfaces/DraftableInterface.php
+++ src/Capco/AppBundle/Entity/Interfaces/DraftableInterface.php
@@ -2,6 +2,7 @@
 
 namespace Capco\AppBundle\Entity\Interfaces;
 
+/** is the entity Draftable ? A draft is an entity not published */
 interface DraftableInterface
 {
     public function isDraft(): bool;
