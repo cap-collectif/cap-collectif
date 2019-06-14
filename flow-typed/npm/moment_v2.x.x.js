752f1b8c11dbff086a1043859e8f1fd761dfd7fa
mauriau <maxime.auriau@protonmail.com>
2019-06-14 15:23:21 +0200

    #8032-FIX fix nl translate
    GitOrigin-RevId: 2f4c37dbd84c75ba0077d34d379bf76363d745ea

--- flow-typed/npm/moment_v2.x.x.js
+++ flow-typed/npm/moment_v2.x.x.js
@@ -254,6 +254,9 @@ declare module 'moment/locale/en-gb' {
 declare module 'moment/locale/es' {
   declare module.exports: any;
 }
+declare module 'moment/locale/nl' {
+  declare module.exports: any;
+}
 declare module 'moment/locale/de' {
   declare module.exports: any;
 }
