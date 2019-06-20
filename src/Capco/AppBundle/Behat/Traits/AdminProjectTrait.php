ba647c93510f5f8f16537eca16cd0086c9eb6f67
psyker <bourgoin.theo1@gmail.com>
2019-06-20 14:47:11 +0200

    Hide the box containing the form for adding additional fields completely
    GitOrigin-RevId: aba7004a4879a214c884f784fddb15d7d283e25f

--- src/Capco/AppBundle/Behat/Traits/AdminProjectTrait.php
+++ src/Capco/AppBundle/Behat/Traits/AdminProjectTrait.php
@@ -5,22 +5,6 @@ namespace Capco\AppBundle\Behat\Traits;
 trait AdminProjectTrait
 {
     /**
-     * @When I go to the admin project list page
-     */
-    public function iGoToTheAdminProjectListPage()
-    {
-        $this->iVisitedPage('AdminProjectListPage');
-    }
-
-    /**
-     * @When I go to the admin project type list page
-     */
-    public function iGoToTheAdminProjectTypeListPage()
-    {
-        $this->iVisitedPage('AdminProjectTypePage');
-    }
-
-    /**
      * @When I click the project add button
      */
     public function iClickAddProjectButton()
@@ -41,53 +25,12 @@ trait AdminProjectTrait
      */
     public function iFillAuthorsField()
     {
-        $node = $this->getCurrentPage()->find('css', '#project-author .react-select__input input');
-
+        $node = $this->getCurrentPage()
+        ->find('css', '#project-author .react-select__input input');
+        
         $node->setValue('admin');
         $node->keyPress(13);
-
+        
         $this->getSession()->wait(10);
-    }
-
-    // TODO: Put each page in a different trait and add specific tests.
-
-    /**
-     * @When I go to the admin source list page
-     */
-    public function iGoToTheAdminSourceListPage()
-    {
-        $this->iVisitedPage('AdminProjectSourcePage');
-    }
-
-    /**
-     * @When I go to the admin proposal list page
-     */
-    public function iGoToTheAdminProposalListPage()
-    {
-        $this->iVisitedPage('AdminProjectProposalPage');
-    }
-
-    /**
-     * @When I go to the admin questionnaire list page
-     */
-    public function iGoToTheAdminQuestionnaireListPage()
-    {
-        $this->iVisitedPage('AdminProjectQuestionnairePage');
-    }
-
-    /**
-     * @When I go to the admin appendix list page
-     */
-    public function iGoToTheAdminAppendixListPage()
-    {
-        $this->iVisitedPage('AdminProjectQuestionnairePage');
-    }
-
-    /**
-     * @When I go to the admin consultation list page
-     */
-    public function iGoToTheAdminConsultationListPage()
-    {
-        $this->iVisitedPage('AdminProjectConsultationPage');
     }
 }
