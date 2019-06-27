ddc5b8bb9141ba1ba85ed96a735d69267b1e29a8
ptondereau <pierre.tondereau@protonmail.com>
2019-06-27 18:17:36 +0200

    7749-SSO Add mutation.
    GitOrigin-RevId: c579e9c1218b617cc3e49d9b9887524799719f06

---
+++ src/Capco/AppBundle/GraphQL/Mutation/DeleteSSOConfigurationMutation.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Capco\AppBundle\GraphQL\Mutation;
+
+use Capco\AppBundle\Repository\AbstractSSOConfigurationRepository;
+use Capco\UserBundle\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Overblog\GraphQLBundle\Definition\Argument as Arg;
+use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
+use Overblog\GraphQLBundle\Relay\Node\GlobalId;
+use Psr\Log\LoggerInterface;
+
+class DeleteSSOConfigurationMutation implements MutationInterface
+{
+    private $em;
+    private $logger;
+    private $repository;
+
+    public function __construct(
+        EntityManagerInterface $em,
+        AbstractSSOConfigurationRepository $repository,
+        LoggerInterface $logger
+    ) {
+        $this->em = $em;
+        $this->repository = $repository;
+        $this->logger = $logger;
+    }
+
+    public function __invoke(Arg $input, User $user): array
+    {
+        $ssoConfigurationId = GlobalId::fromGlobalId($input->offsetGet('id'))['id'];
+
+        $ssoConfiguration = $this->repository->find($ssoConfigurationId);
+
+        if (!$ssoConfiguration) {
+            $this->logger->error("Unknown SSO Configuration with id: ${ssoConfigurationId}");
+
+            return [
+                'userErrors' => [
+                    [
+                        'message' => 'SSO Configuration not found.'
+                    ]
+                ]
+            ];
+        }
+
+        $this->em->remove($ssoConfiguration);
+        $this->em->flush();
+
+        return ['deletedSsoConfigurationId' => $ssoConfigurationId, 'userErrors' => []];
+    }
+}
