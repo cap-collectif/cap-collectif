02e456f6ca2fc47b6c2ccaf760b60fe8ef17f847
spyl94 <spyl94@gmail.com>
2019-07-11 18:27:11 +0200

    Upgrade idee.paris.fr API to 69
    GitOrigin-RevId: 76915bb81230304c1c57dd3ef2c6e9f8ec1b4c65

--- src/Capco/UserBundle/MonCompteParis/OpenAmClient.php
+++ src/Capco/UserBundle/MonCompteParis/OpenAmClient.php
@@ -9,7 +9,7 @@ class OpenAmClient
     public const COOKIE_NAME = 'mcpAuth'; // Iplanetdirectorypro in test env
     public const COOKIE_DOMAIN = '.paris.fr';
     public const API_URL = 'https://moncompte.paris.fr/v69/json';
-    public const API_INFORMATIONS_URL = 'https://moncompte.paris.fr/moncompte/rest/banner/api/1/informations';
+    public const API_INFORMATIONS_URL = 'https://moncompte.paris.fr/v69/json/users/';
 
     protected $cookie;
     protected $client;
@@ -63,17 +63,17 @@ class OpenAmClient
         }
     }
 
-    public function getUserInformations(): array
+    public function getUserInformations(string $email): array
     {
-        $response = $this->client->get(self::API_INFORMATIONS_URL, [
+        $response = $this->client->get(self::API_INFORMATIONS_URL . $email, [
             'Cookie' => self::COOKIE_NAME . '=' . $this->cookie,
         ]);
         $json = json_decode((string) $response->getBody(), true);
-        if ('OK' !== $json['status']) {
+        if ($json) {
             $this->logger->critical('Error returned by moncompte.paris.fr', ['json' => $json]);
             throw new \RuntimeException('Error returned by moncompte.paris.fr.');
         }
 
-        return $json['result'];
+        return $json;
     }
 }
