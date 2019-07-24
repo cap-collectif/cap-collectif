6dcf2f21585510a3161b487a0ae1a679932939a2
Jpec57 <jean.paul.bella@hotmail.fr>
2019-07-24 14:22:12 +0200

    renamed migration
    GitOrigin-RevId: 37f87d29dde503a812c36a7ca1eb45f3e18ab479

--- app/DoctrineMigrations/Version20190723163647.php
+++ app/DoctrineMigrations/Version20190723163647.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190719141326 extends AbstractMigration
+final class Version20190723163647 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
