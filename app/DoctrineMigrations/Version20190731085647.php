79be8d4859ff5dbac9b60e97cd91cf1a2a2e41f5
Liinkiing <omar.jbara2@gmail.com>
2019-07-31 10:35:53 +0200

    - Renamed migration to be after migration of some consultation fields
    GitOrigin-RevId: 0ca288db5606f97be435ef8686cde3c106b224b2

--- app/DoctrineMigrations/Version20190731085647.php
+++ app/DoctrineMigrations/Version20190731085647.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Application\Migrations;
 
@@ -8,25 +10,32 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190717154855 extends AbstractMigration
+final class Version20190731085647 extends AbstractMigration
 {
     protected $consultationsByStep = [];
 
     public function postUp(Schema $schema)
     {
         foreach ($this->consultationsByStep as $consultationByStep) {
-            $this->connection->update('opinion', [
-                'consultation_id' => $consultationByStep['consultation_id']
-            ], [
-                'consultation_id' => $consultationByStep['step_id']
-            ]);
+            $this->connection->update(
+                'opinion',
+                [
+                    'consultation_id' => $consultationByStep['consultation_id']
+                ],
+                [
+                    'consultation_id' => $consultationByStep['step_id']
+                ]
+            );
         }
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf(
+            'mysql' !== $this->connection->getDatabasePlatform()->getName(),
+            'Migration can only be executed safely on \'mysql\'.'
+        );
 
         $this->consultationsByStep = $this->connection->fetchAll('
             SELECT consultation.`id` AS consultation_id, step.id AS step_id
@@ -38,8 +47,12 @@ final class Version20190717154855 extends AbstractMigration
 
         $this->addSql('ALTER TABLE opinion DROP FOREIGN KEY FK_AB02B02773B21E9C');
         $this->addSql('DROP INDEX IDX_AB02B02773B21E9C ON opinion');
-        $this->addSql('ALTER TABLE opinion CHANGE step_id consultation_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\'');
-        $this->addSql('ALTER TABLE opinion ADD CONSTRAINT FK_AB02B02762FF6CDF FOREIGN KEY (consultation_id) REFERENCES consultation (id)');
+        $this->addSql(
+            'ALTER TABLE opinion CHANGE step_id consultation_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\''
+        );
+        $this->addSql(
+            'ALTER TABLE opinion ADD CONSTRAINT FK_AB02B02762FF6CDF FOREIGN KEY (consultation_id) REFERENCES consultation (id)'
+        );
         $this->addSql('CREATE INDEX IDX_AB02B02762FF6CDF ON opinion (consultation_id)');
 
         $this->addSql('SET FOREIGN_KEY_CHECKS=1');
@@ -48,14 +61,21 @@ final class Version20190717154855 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf(
+            'mysql' !== $this->connection->getDatabasePlatform()->getName(),
+            'Migration can only be executed safely on \'mysql\'.'
+        );
 
         $this->addSql('SET FOREIGN_KEY_CHECKS=0');
 
         $this->addSql('ALTER TABLE opinion DROP FOREIGN KEY FK_AB02B02762FF6CDF');
         $this->addSql('DROP INDEX IDX_AB02B02762FF6CDF ON opinion');
-        $this->addSql('ALTER TABLE opinion CHANGE consultation_id step_id CHAR(36) DEFAULT NULL COLLATE utf8_unicode_ci COMMENT \'(DC2Type:guid)\'');
-        $this->addSql('ALTER TABLE opinion ADD CONSTRAINT FK_AB02B02773B21E9C FOREIGN KEY (step_id) REFERENCES step (id)');
+        $this->addSql(
+            'ALTER TABLE opinion CHANGE consultation_id step_id CHAR(36) DEFAULT NULL COLLATE utf8_unicode_ci COMMENT \'(DC2Type:guid)\''
+        );
+        $this->addSql(
+            'ALTER TABLE opinion ADD CONSTRAINT FK_AB02B02773B21E9C FOREIGN KEY (step_id) REFERENCES step (id)'
+        );
         $this->addSql('CREATE INDEX IDX_AB02B02773B21E9C ON opinion (step_id)');
 
         $this->addSql('SET FOREIGN_KEY_CHECKS=1');
