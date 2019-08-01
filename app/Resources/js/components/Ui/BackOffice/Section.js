6521384da96480ef0f36136b2cf25ebd95a6c321
Tichyus <quentin@cap-collectif.com>
2019-08-01 14:05:47 +0200

    creating UI component for section in back office + render it in storybook
    GitOrigin-RevId: 8a4fe73fad978f07971d199bf6b0eb93a6733159

--- app/Resources/js/components/Ui/BackOffice/Section.js
+++ app/Resources/js/components/Ui/BackOffice/Section.js
@@ -1,18 +1,22 @@
+// @flow
 import * as React from 'react';
-import styled from 'styled-components';
 
-type Props= {|
-    title: String,
+type Props = {|
+  title: string,
+  children: any,
 |};
 
 export default class Section extends React.Component<Props> {
-    render() {
+  render() {
+    const { title, children } = this.props;
 
-
-        return (
-            <Section>
-                test
-            </Section>
-        );
-    }
+    return (
+      <div className="box box-primary">
+        <div className="box-header">
+          <h3 className="box-title">{title}</h3>
+        </div>
+        <div className="box-body">{children}</div>
+      </div>
+    );
+  }
 }
