f21bb6d74bf51e79dae3e6306a37c2f06e2b6008
Liinkiing <omar.jbara2@gmail.com>
2019-08-02 16:25:06 +0200

    - Removed unused code
    GitOrigin-RevId: 2216a971a7d64524ddd0d99739d1c4e02e95cf91

--- src/Capco/AppBundle/Repository/ConsultationRepository.php
+++ src/Capco/AppBundle/Repository/ConsultationRepository.php
@@ -5,7 +5,6 @@ namespace Capco\AppBundle\Repository;
 use Capco\AppBundle\Entity\Consultation;
 use Capco\AppBundle\Entity\Steps\ConsultationStep;
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
@@ -18,26 +17,6 @@ use Doctrine\ORM\Tools\Pagination\Paginator;
  */
 class ConsultationRepository extends EntityRepository
 {
-    /**
-     * Get opinion types by id of consultation step type.
-     *
-     * @param $id
-     *
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     *
-     * @return mixed
-     */
-    public function getRelatedTypes($id)
-    {
-        $qb = $this->getIsEnabledQueryBuilder()
-            ->select('ot.id')
-            ->leftJoin('c.opinionTypes', 'ot')
-            ->andWhere('c.id = :id')
-            ->setParameter('id', $id);
-
-        return array_map('current', $qb->getQuery()->getScalarResult());
-    }
-
     public function getByConsultationStepPaginated(
         ConsultationStep $cs,
         int $offset = 0,
@@ -64,15 +43,5 @@ class ConsultationRepository extends EntityRepository
             ->setParameter('cs', $cs)
             ->getQuery()
             ->getSingleScalarResult();
-    }
-
-    /**
-     * @return QueryBuilder
-     */
-    protected function getIsEnabledQueryBuilder()
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.enabled = :enabled')
-            ->setParameter('enabled', true);
     }
 }
