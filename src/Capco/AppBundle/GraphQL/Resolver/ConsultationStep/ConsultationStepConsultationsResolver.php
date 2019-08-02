a18fe7940b534da853d11d783bd61b9c84a76f7f
Liinkiing <omar.jbara2@gmail.com>
2019-08-02 14:08:21 +0200

    - Added consultations field in GraphQL backend
    GitOrigin-RevId: 990360060953575c885846b464a54ce99834bb63

---
+++ src/Capco/AppBundle/GraphQL/Resolver/ConsultationStep/ConsultationStepConsultationsResolver.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Capco\AppBundle\GraphQL\Resolver\ConsultationStep;
+
+use Capco\AppBundle\Entity\Steps\ConsultationStep;
+use Capco\AppBundle\Repository\ConsultationRepository;
+use Overblog\GraphQLBundle\Definition\Argument;
+use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
+use Overblog\GraphQLBundle\Relay\Connection\Output\Connection;
+use Overblog\GraphQLBundle\Relay\Connection\Paginator;
+
+class ConsultationStepConsultationsResolver implements ResolverInterface
+{
+
+    private $repository;
+
+    public function __construct(ConsultationRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function __invoke(ConsultationStep $step, Argument $args): Connection
+    {
+        $paginator = new Paginator(function (int $offset, int $limit) use ($step) {
+
+            return $this->repository
+                ->getByConsultationStepPaginated($step, $offset, $limit)
+                ->getIterator()
+                ->getArrayCopy();
+        });
+
+        $totalCount = $this->repository->countByStep($step);
+
+        return $paginator->auto($args, $totalCount);
+    }
+}
--- src/Capco/AppBundle/Repository/ConsultationRepository.php
+++ src/Capco/AppBundle/Repository/ConsultationRepository.php
@@ -2,11 +2,18 @@
 
 namespace Capco\AppBundle\Repository;
 
+use Capco\AppBundle\Entity\Consultation;
+use Capco\AppBundle\Entity\Steps\ConsultationStep;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
- * ConsultationRepository.
+ * ConsultationRepository. *
+ * @method Consultation|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Consultation|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Consultation[]    findAll()
+ * @method Consultation[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ConsultationRepository extends EntityRepository
 {
@@ -34,13 +41,44 @@ class ConsultationRepository extends EntityRepository
             ->getScalarResult());
     }
 
-    /**
-     * @return QueryBuilder
-     */
-    protected function getIsEnabledQueryBuilder()
+    public function getByConsultationStepPaginated(ConsultationStep $cs, int $offset = 0, int $limit = 100): Paginator
     {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.enabled = :enabled')
-            ->setParameter('enabled', true);
+        $qb = $this->createQueryBuilder('c');
+
+        $query = $qb
+            ->andWhere(
+                $qb->expr()->eq('c.step', ':cs')
+            )
+            ->setParameter('cs', $cs)
+            ->setFirstResult($offset)
+            ->setMaxResults($limit);
+
+        return new Paginator($query);
     }
+
+    public function countByStep(ConsultationStep $cs): int
+    {
+        $qb = $this->createQueryBuilder('c');
+
+        return $qb
+            ->select('count(DISTINCT c)')
+            ->andWhere(
+                $qb->expr()->eq('c.step', ':cs')
+            )
+            ->setParameter('cs', $cs)
+            ->getQuery()
+            ->getSingleScalarResult();
+
+    }
+
+//    /**
+//     * @return QueryBuilder
+//     */
+//    protected function getIsEnabledQueryBuilder()
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.enabled = :enabled')
+//            ->setParameter('enabled', true);
+//    }
+
 }
