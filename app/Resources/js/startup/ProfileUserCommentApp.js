d7c1797fd2da622df63256f1f2dc69e92cca2f72
psyker <bourgoin.theo1@gmail.com>
2019-08-09 15:04:07 +0200

    End comment react component on profile, add pagination
    GitOrigin-RevId: d433cca05310111e573ed5e1de6353bce1a5a8f7

--- app/Resources/js/startup/ProfileUserCommentApp.js
+++ app/Resources/js/startup/ProfileUserCommentApp.js
@@ -2,14 +2,66 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactOnRails from 'react-on-rails';
+import { QueryRenderer, graphql, type ReadyState } from 'react-relay';
 import IntlProvider from './IntlProvider';
-import type { Props } from '../components/Comment/Page/CommentPage';
-import CommentPage from '../components/Comment/Page/CommentPage';
+import environment, { graphqlError } from '../createRelayEnvironment';
+import UserCommentsPaginated, {
+  COMMENT_PAGINATION,
+} from '../components/Comment/UserCommentsPaginated';
+import type { ProfileUserCommentAppQueryResponse } from '~relay/ProfileUserCommentAppQuery.graphql';
 
-export default (props: Props) => (
+export default ({ userId, isAuthenticated }: { userId: string, isAuthenticated: boolean }) => (
   <Provider store={ReactOnRails.getStore('appStore')}>
     <IntlProvider>
-      <CommentPage {...props} />
+      <QueryRenderer
+        variables={{
+          userId,
+          isAuthenticated,
+          count: COMMENT_PAGINATION,
+          cursor: null,
+        }}
+        environment={environment}
+        query={graphql`
+          query ProfileUserCommentAppQuery(
+            $userId: ID!
+            $isAuthenticated: Boolean!
+            $count: Int!
+            $cursor: String
+          ) {
+            user: node(id: $userId) {
+              id
+              ...UserCommentsPaginated_user
+                @arguments(
+                  userId: $userId
+                  count: $count
+                  isAuthenticated: $isAuthenticated
+                  cursor: $cursor
+                )
+            }
+          }
+        `}
+        render={({
+          error,
+          props,
+        }: {
+          ...ReadyState,
+          props: ?ProfileUserCommentAppQueryResponse,
+        }) => {
+          if (error) {
+            return graphqlError;
+          }
+          if (props) {
+            if (props.user && props.user.id) {
+              return (
+                // $FlowFixMe
+                <UserCommentsPaginated user={props.user} />
+              );
+            }
+            return graphqlError;
+          }
+          return null;
+        }}
+      />
     </IntlProvider>
   </Provider>
 );
