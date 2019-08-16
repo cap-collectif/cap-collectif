d7b55e91fb09de54defa17bc4cd3cf8617534b7e
Jpec57 <jean.paul.bella@hotmail.fr>
2019-08-16 16:30:47 +0200

    Injecting zxcvbn instead of creating new instances
    GitOrigin-RevId: e4052212cfa118ee3085fe5303f9fbe9b03949df

--- src/Capco/AppBundle/GraphQL/Resolver/Query/PasswordComplexityScoreResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Query/PasswordComplexityScoreResolver.php
@@ -8,15 +8,21 @@ use ZxcvbnPhp\Zxcvbn;
 
 class PasswordComplexityScoreResolver implements ResolverInterface
 {
+    private $zxcvbn;
+
+    public function __construct(Zxcvbn $zxcvbn)
+    {
+        $this->zxcvbn = $zxcvbn;
+    }
+
     public function __invoke(Argument $args): int
     {
         $password = $args->offsetGet('password');
         $userData = [$args->offsetGet('email')];
         if (!empty($args->offsetGet('username'))) {
-            array_push($userData, $args->offsetGet('username'));
+            $userData[] = $args->offsetGet('username');
         }
-        $zxcvbn = new Zxcvbn();
-        $strength = $zxcvbn->passwordStrength($password, $userData);
+        $strength = $this->zxcvbn->passwordStrength($password, $userData);
 
         return $strength['score'] ?? 0;
     }
