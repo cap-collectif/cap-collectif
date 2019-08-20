e3bdc8fde33116a27e13c55966ee89e4c06c8f89
spyl94 <spyl94@gmail.com>
2019-08-20 09:52:03 +0200

    Merge pull request #8594 from cap-collectif/8418-bug-filter-project
     8418-BUG-FILTER-PROJECT fix
    
    GitOrigin-RevId: 6a05c627fb14f513b2e36198dab706683c4b5fe8

--- features/graphql/query/projects_types.feature
+++ features/graphql/query/projects_types.feature
@@ -1,4 +1,4 @@
-@projects @read-only
+@projects @read-only @projectTypes
 Feature: Projects
 
 Scenario: GraphQL client wants to list projects types 
--- src/Capco/AppBundle/Repository/ProjectRepository.php
+++ src/Capco/AppBundle/Repository/ProjectRepository.php
@@ -287,6 +287,7 @@ class ProjectRepository extends EntityRepository
             ->addSelect('pt.title, pt.slug, pt.color')
             ->leftJoin('p.projectType', 'pt')
             ->where('p IN (:projects)')
+            ->andWhere('p.projectType IS NOT NULL')
             ->setParameter('projects', $projects)
             ->getQuery()
             ->useQueryCache(true)
