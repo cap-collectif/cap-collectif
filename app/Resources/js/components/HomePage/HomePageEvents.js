93a32afb7547ba151037e26244dfb6d677fbf41d
spyl94 <spyl94@gmail.com>
2019-08-21 13:41:57 +0200

    Merge pull request #8596 from cap-collectif/event-card-fix-margin
    Event card fix margin
    
    GitOrigin-RevId: c3b4e9a9a6367c7081dbc30991b4eb6aa44c7e97

--- app/Resources/js/components/HomePage/HomePageEvents.js
+++ app/Resources/js/components/HomePage/HomePageEvents.js
@@ -25,10 +25,19 @@ const EventContainer = styled.div`
   padding-top: 20px;
   width: 100%;
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 5%;
+  grid-template-columns: 50% 50%;
   grid-auto-rows: auto;
+  grid-auto-columns: 1fr;
   margin-bottom: 30px;
+
+  >div{
+    width: 97%;
+    margin-bottom: 5%;
+  }
+  >div:nth-of-type(2n){
+    margin-left: 2%;
+  }
+
 
   >div>div{
     margin-bottom: 0;
