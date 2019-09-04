7c99b5cc7915e8e5dac9bd7d2eeb47bd78eb1c0b
Liinkiing <omar.jbara2@gmail.com>
2019-09-04 12:32:01 +0200

    - Renamed useWindowWidth to a more generic useWindowSize and fixed hook to avoid unnecessary addition of event handlers
    GitOrigin-RevId: 6f55564acfb45905a1a9ab094857d2b7acbabdc0

--- app/Resources/js/utils/hooks/useWindowWidth.js
+++ app/Resources/js/utils/hooks/useWindowWidth.js
@@ -1,15 +1,28 @@
 // @flow
 import { useState, useEffect } from 'react';
 
-export function useWindowWidth() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+type WindowSize = {
+  +width: number,
+  +height: number
+}
+
+export function useWindowWidth(): WindowSize {
+  const [windowWidth, setWindowWidth] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
   useEffect(() => {
-    const handleResize = () => setWindowWidth(window.innerWidth);
+    const handleResize = () => {
+      setWindowWidth({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    };
     window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  });
+  }, []);
 
   return windowWidth;
 }
