bcef78006309159c9b4c00ba5b29565d6f983431
Liinkiing <omar.jbara2@gmail.com>
2019-09-04 11:20:18 +0200

    - Added e2e GraphQL tests and Behat tests
    GitOrigin-RevId: 7b7c6094b74e5b994d34d5208ced669f65d96626

--- app/Resources/js/components/Steps/MetaStepNavigation.js
+++ app/Resources/js/components/Steps/MetaStepNavigation.js
@@ -5,31 +5,34 @@ import styled, { css } from 'styled-components';
 import { FormattedMessage } from 'react-intl';
 import { Button } from 'react-bootstrap';
 import ShareButtonDropdown from '../Utils/ShareButtonDropdown';
-import type { StepPropositionNavigation_step } from '~relay/StepPropositionNavigation_step.graphql';
 import { breakpoint } from '../../utils/mixins';
+import type { MetaStepNavigation_step } from '~relay/MetaStepNavigation_step.graphql';
 
 type RelayProps = {|
-  +step: StepPropositionNavigation_step
-|}
+  +step: MetaStepNavigation_step,
+|};
 
 type Props = {|
-  ...RelayProps
-|}
+  ...RelayProps,
+|};
 
 const SHARE_BUTTON_ID = 'share-button';
 const BACK_TO_LIST_BUTTON_ID = 'back-to-list-button';
 
 const BackIcon = styled.i.attrs({
-  className: 'cap-arrow-65 position-relative'
+  className: 'cap-arrow-65 position-relative',
 })`
-  top: 2px
+  top: 2px;
 `;
 
 const BackMessage = styled.span`
   display: none;
-  ${breakpoint('medium', css`
-    display: inline;
-  `)}
+  ${breakpoint(
+    'medium',
+    css`
+      display: inline;
+    `,
+  )}
 `;
 
 // The consultation span selector is a bit hacky but I have no way to control the ShareButtonDropdown
@@ -37,73 +40,84 @@ const BackMessage = styled.span`
 const ShareButtonDropdownInner = styled.span`
   #${SHARE_BUTTON_ID} > span > span:last-child {
     display: none;
-    ${breakpoint('medium', css`
-      display: inline;
-    `)}
+    ${breakpoint(
+      'medium',
+      css`
+        display: inline;
+      `,
+    )}
   }
 `;
 
-export const MetaStepNavigationBackButton = ({ step }: { step: StepPropositionNavigation_step }) => {
+export const MetaStepNavigationBackButton = ({ step }: { step: MetaStepNavigation_step }) => {
   switch (step.__typename) {
     case 'ConsultationStep':
-      return <Button id={BACK_TO_LIST_BUTTON_ID} href={step.url}>
-        <BackIcon/>&nbsp;
-        <BackMessage>
-          <FormattedMessage id="consultations-list"/>
-        </BackMessage>
-      </Button>;
+      return (
+        <Button id={BACK_TO_LIST_BUTTON_ID} href={step.url}>
+          <BackIcon />
+          &nbsp;
+          <BackMessage>
+            <FormattedMessage id="consultations-list" />
+          </BackMessage>
+        </Button>
+      );
     default:
       return null;
   }
 };
 
-export const MetaStepNavigationTitle = ({ step }: { step: StepPropositionNavigation_step }) => {
+export const MetaStepNavigationTitle = ({ step }: { step: MetaStepNavigation_step }) => {
   switch (step.__typename) {
     case 'ConsultationStep':
-      return <h2><FormattedMessage id="project.types.consultation"/></h2>;
+      return (
+        <h2>
+          <FormattedMessage id="project.types.consultation" />
+        </h2>
+      );
     default:
       return null;
   }
 };
 
-export const MetaStepNavigationShare = ({ step }: { step: StepPropositionNavigation_step }) => {
+export const MetaStepNavigationShare = ({ step }: { step: MetaStepNavigation_step }) => {
   switch (step.__typename) {
     case 'ConsultationStep':
       return step.consultation ? (
-      <ShareButtonDropdownInner>
-        <ShareButtonDropdown
-          id={SHARE_BUTTON_ID}
-          url={step.consultation.url}
-          title={step.consultation.title}
-        />
-      </ShareButtonDropdownInner>) : null;
+        <ShareButtonDropdownInner>
+          <ShareButtonDropdown
+            id={SHARE_BUTTON_ID}
+            url={step.consultation.url}
+            title={step.consultation.title}
+          />
+        </ShareButtonDropdownInner>
+      ) : null;
     default:
       return null;
   }
 };
-
 
 export const MetaStepNavigation = ({ step }: Props) => {
   return (
     <React.Fragment>
-      <MetaStepNavigationBackButton step={step}/>
-      <MetaStepNavigationTitle step={step}/>
-      <MetaStepNavigationShare step={step}/>
+      <MetaStepNavigationBackButton step={step} />
+      <MetaStepNavigationTitle step={step} />
+      <MetaStepNavigationShare step={step} />
     </React.Fragment>
   );
 };
 
 export default createFragmentContainer(MetaStepNavigation, {
   step: graphql`
-      fragment MetaStepNavigation_step on Step @argumentDefinitions(relatedSlug: { type: "String!" }) {
-          __typename
+    fragment MetaStepNavigation_step on Step
+      @argumentDefinitions(relatedSlug: { type: "String!" }) {
+      __typename
+      url
+      ... on ConsultationStep {
+        consultation(slug: $relatedSlug) {
           url
-          ... on ConsultationStep {
-              consultation(slug: $relatedSlug) {
-                  url
-                  title
-              }
-          }
+          title
+        }
       }
+    }
   `,
 });
