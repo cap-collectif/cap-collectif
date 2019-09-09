c5bcb27b59de51c58d25ec997dbd043a0411f162
Liinkiing <omar.jbara2@gmail.com>
2019-09-09 10:07:35 +0200

    - Changed the way we count contributions: usage of the same way we used to in a ConsultationStep
    GitOrigin-RevId: 9bdfb5752f1bc677a7ffe0efe56572352ca60543

--- src/Capco/AppBundle/GraphQL/Resolver/Consultation/ConsultationContributionsResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Consultation/ConsultationContributionsResolver.php
@@ -9,68 +9,30 @@ use Capco\AppBundle\Repository\OpinionVersionRepository;
 use Capco\AppBundle\Repository\SourceRepository;
 use Overblog\GraphQLBundle\Definition\Argument;
 use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
-use Overblog\GraphQLBundle\Relay\Connection\Output\Connection;
+use Overblog\GraphQLBundle\Relay\Connection\ConnectionInterface;
 use Overblog\GraphQLBundle\Relay\Connection\Paginator;
 
 class ConsultationContributionsResolver implements ResolverInterface
 {
     private $opinionRepository;
-    private $sourceRepository;
-    private $argumentRepository;
-    private $opinionVersionRepository;
 
     public function __construct(
-        OpinionRepository $opinionRepository,
-        SourceRepository $sourceRepository,
-        ArgumentRepository $argumentRepository,
-        OpinionVersionRepository $opinionVersionRepository
+        OpinionRepository $opinionRepository
     ) {
         $this->opinionRepository = $opinionRepository;
-        $this->sourceRepository = $sourceRepository;
-        $this->argumentRepository = $argumentRepository;
-        $this->opinionVersionRepository = $opinionVersionRepository;
     }
 
-    public function __invoke(Consultation $consultation, Argument $args): Connection
+    public function __invoke(Consultation $consultation, Argument $args): ConnectionInterface
     {
+        $includeTrashed = $args->offsetGet('includeTrashed');
         $paginator = new Paginator(static function () {
             return [];
         });
 
         return $paginator->auto(
             $args,
-            $this->getConsultationContributionsTotalCount($consultation)
+            $this->opinionRepository->countByConsultation($consultation, $includeTrashed)
         );
     }
 
-    private function getConsultationContributionsTotalCount(Consultation $consultation): int
-    {
-        $totalCount = 0;
-
-        $totalCount += $this->opinionRepository->countPublishedContributionsByConsultation(
-            $consultation
-        );
-        $totalCount += $this->opinionRepository->countTrashedContributionsByConsultation(
-            $consultation
-        );
-
-        $totalCount += $this->argumentRepository->countPublishedArgumentsByConsultation(
-            $consultation
-        );
-        $totalCount += $this->argumentRepository->countTrashedArgumentsByConsultation(
-            $consultation
-        );
-
-        $totalCount += $this->opinionVersionRepository->countPublishedOpinionVersionByConsultation(
-            $consultation
-        );
-        $totalCount += $this->opinionVersionRepository->countTrashedOpinionVersionByConsultation(
-            $consultation
-        );
-
-        $totalCount += $this->sourceRepository->countPublishedSourcesByConsultation($consultation);
-        $totalCount += $this->sourceRepository->countTrashedSourcesByConsultation($consultation);
-
-        return $totalCount;
-    }
 }
