c1838807d519a4d12a57c3f0747f3644b5a140e1
Jpec57 <jean.paul.bella@hotmail.fr>
2019-10-03 16:52:11 +0200

    [CHECK-LOGIN] Added deleted migration and updated phpspec
    GitOrigin-RevId: acf9bb0ce79e435c6d4fad468e6c02bd6bcdaab1

--- spec/Capco/AppBundle/EventListener/AuthenticationHandlerSpec.php
+++ spec/Capco/AppBundle/EventListener/AuthenticationHandlerSpec.php
@@ -31,8 +31,12 @@ class AuthenticationHandlerSpec extends ObjectBehavior
             ->getContent()
             ->shouldBeCalled()
             ->willReturn('{"username": "lbrunet@jolicode.com"}');
+        $request
+            ->getClientIp()
+            ->shouldBeCalled()
+            ->willReturn('192.168.64.2');
         $test = $userConnectionRepository
-            ->countFailedAttemptByEmailInLastHour('lbrunet@jolicode.com')
+            ->countFailedAttemptByEmailAndIPInLastHour('lbrunet@jolicode.com', '192.168.64.2')
             ->shouldBeCalled()
             ->getObjectProphecy();
         $this->onAuthenticationFailure($request, $exception)->shouldBeAnInstanceOf(
