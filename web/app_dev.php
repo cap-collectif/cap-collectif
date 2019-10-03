aab2306f38099084a35a6d0073e2fb904ca63168
Liinkiing <omar.jbara2@gmail.com>
2019-10-03 14:14:38 +0200

    - Added support for .env file in console too
    GitOrigin-RevId: 80340f708efd097eb1ba68c92e682baacd153737

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,59 +1,15 @@
 <?php
 
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-/**
- * @var Composer\Autoload\ClassLoader $loader
- */
-$loader = require __DIR__ . '/../app/autoload.php';
+require dirname(__DIR__).'/config/bootstrap.php';
 
 Debug::enable();
 Request::setTrustedProxies(
     ['172.17.0.0/16', '10.10.200.0/16', '127.0.0.1'],
     Request::HEADER_X_FORWARDED_ALL
 );
-
-// This methode is taken from https://github.com/symfony/dotenv/blob/master/Dotenv.php, because we currently
-// can't use the last version of Dotenv component (needs Symfony > 4), but I just need this method
-function loadEnv(Dotenv $dotenv, string $path, string $varName = 'APP_ENV', string $defaultEnv = 'dev', array $testEnvs = ['test']): void
-{
-    if (file_exists($path) || !file_exists($p = "$path.dist")) {
-        $dotenv->load($path);
-    } else {
-        $dotenv->load($p);
-    }
-    if (null === $env = $_SERVER[$varName] ?? $_ENV[$varName] ?? null) {
-        $dotenv->populate([$varName => $env = $defaultEnv]);
-    }
-    if (!\in_array($env, $testEnvs, true) && file_exists($p = "$path.local")) {
-        $dotenv->load($p);
-        $env = $_SERVER[$varName] ?? $_ENV[$varName] ?? $env;
-    }
-    if ('local' === $env) {
-        return;
-    }
-    if (file_exists($p = "$path.$env")) {
-        $dotenv->load($p);
-    }
-    if (file_exists($p = "$path.$env.local")) {
-        $dotenv->load($p);
-    }
-}
-
-// Load cached env vars if the .env.local.php file exists
-// Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
-    foreach ($env as $k => $v) {
-        $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
-    }
-} elseif (!class_exists(Dotenv::class)) {
-    throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
-} else {
-    // load all the .env files
-    loadEnv(new Dotenv(), dirname(__DIR__).'/.env');
-}
 
 $request = Request::createFromGlobals();
 
