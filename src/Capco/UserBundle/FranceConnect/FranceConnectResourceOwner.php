9b8e907f33cda83fdca39fb36f1cbceb85e7674c
ptondereau <pierre.tondereau@protonmail.com>
2019-10-05 17:05:34 +0200

    Fix France Connect in local dev env.
    GitOrigin-RevId: a2925c49034a227399c019fd3d83906ad525aa6d

--- src/Capco/UserBundle/FranceConnect/FranceConnectResourceOwner.php
+++ src/Capco/UserBundle/FranceConnect/FranceConnectResourceOwner.php
@@ -3,6 +3,7 @@
 namespace Capco\UserBundle\FranceConnect;
 
 use HWI\Bundle\OAuthBundle\OAuth\ResourceOwner\GenericOAuth2ResourceOwner;
+use Symfony\Component\HttpFoundation\Request as HttpRequest;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FranceConnectResourceOwner extends GenericOAuth2ResourceOwner
@@ -14,6 +15,35 @@ class FranceConnectResourceOwner extends GenericOAuth2ResourceOwner
         'lastname' => 'family_name',
         'nickname' => 'preferred_username'
     ];
+
+    public function getAuthorizationUrl($redirectUri, array $extraParameters = []): string
+    {
+        // https://partenaires.franceconnect.gouv.fr/fcp/fournisseur-service#glossary
+        $extraParameters = array_merge(
+            [
+                'nonce' => $this->generateNonce()
+            ],
+            $extraParameters
+        );
+
+        // Uncomment it if you want test France Connect with local mode.
+        // $redirectUri = 'http://localhost:4242/callback';
+
+        return parent::getAuthorizationUrl($redirectUri, $extraParameters);
+    }
+
+    public function getAccessToken(
+        HttpRequest $request,
+        $redirectUri,
+        array $extraParameters = []
+    ): array {
+        // Uncomment it if you want test France Connect with local mode.
+        /* $extraParameters = array_merge([
+            'redirect_uri' => 'http://localhost:4242/callback'
+        ], $extraParameters); */
+
+        return parent::getAccessToken($request, $redirectUri, $extraParameters);
+    }
 
     protected function configureOptions(OptionsResolver $resolver): void
     {
--- src/Capco/UserBundle/Security/Http/Logout/LogoutSuccessHandler.php
+++ src/Capco/UserBundle/Security/Http/Logout/LogoutSuccessHandler.php
@@ -7,39 +7,50 @@ use Capco\AppBundle\Toggle\Manager;
 use Capco\UserBundle\MonCompteParis\OpenAmClient;
 use Capco\UserBundle\OpenID\OpenIDReferrerResolver;
 use HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
 use SimpleSAML\Auth\Simple;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 
 class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
 {
+    protected $tokenStorage;
     protected $samlAuth;
     protected $router;
     protected $toggleManager;
     protected $client;
-    protected $resourceOwner;
+    protected $franceConnectResourceOwner;
+    protected $openIDResourceOwner;
     protected $referrerResolver;
 
     public function __construct(
+        TokenStorageInterface $tokenStorage,
         RouterInterface $router,
         Manager $toggleManager,
         OpenAmClient $client,
-        ResourceOwnerInterface $resourceOwner,
+        ResourceOwnerInterface $openIDResourceOwner,
+        ResourceOwnerInterface $franceConnectResourceOwner,
         OpenIDReferrerResolver $referrerResolver,
         ?Simple $samlAuth = null
     ) {
+        $this->tokenStorage = $tokenStorage;
         $this->samlAuth = $samlAuth;
         $this->router = $router;
         $this->toggleManager = $toggleManager;
         $this->client = $client;
-        $this->resourceOwner = $resourceOwner;
+        $this->franceConnectResourceOwner = $franceConnectResourceOwner;
+        $this->openIDResourceOwner = $openIDResourceOwner;
         $this->referrerResolver = $referrerResolver;
     }
 
     public function onLogoutSuccess(Request $request)
     {
+        $token = $this->tokenStorage->getToken();
+
         $deleteType = $request->get('deleteType');
         $returnTo =
             DeleteAccountType::SOFT === $deleteType || DeleteAccountType::HARD === $deleteType
@@ -54,6 +65,16 @@ class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
 
         $response = new RedirectResponse($returnTo);
 
+        $response = $this->handleParisLogout($request, $response);
+        $response = $this->handleOpenIDLogout($request, $response);
+
+        return $this->handleFranceConnectLogout($response, $token);
+    }
+
+    private function handleParisLogout(
+        Request $request,
+        RedirectResponse $response
+    ): RedirectResponse {
         if (
             $this->toggleManager->isActive('login_paris') &&
             $request->cookies->has(OpenAmClient::COOKIE_NAME)
@@ -67,12 +88,19 @@ class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
             );
         }
 
+        return $response;
+    }
+
+    private function handleOpenIDLogout(
+        Request $request,
+        RedirectResponse $response
+    ): RedirectResponse {
         if (
             $this->toggleManager->isActive('login_openid') &&
             $this->toggleManager->isActive('disconnect_openid') &&
             $request->query->get('ssoSwitchUser')
         ) {
-            $logoutURL = $this->resourceOwner->getOption('logout_url');
+            $logoutURL = $this->openIDResourceOwner->getOption('logout_url');
 
             $homepageUrl = $this->router->generate(
                 'app_homepage',
@@ -81,11 +109,51 @@ class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
             );
             $redirectUri = $homepageUrl . '/login/openid?_destination=' . $homepageUrl;
             $referrerParameter = $this->referrerResolver->getRefererParameterForLogout();
-            $response = new RedirectResponse(
+
+            return new RedirectResponse(
                 $logoutURL . '?' . $referrerParameter . '=' . utf8_encode($redirectUri) ?? '/'
             );
         }
 
         return $response;
+    }
+
+    private function handleFranceConnectLogout(
+        RedirectResponse $response,
+        ?TokenInterface $token = null
+    ): RedirectResponse {
+        if (
+            $token &&
+            $token instanceof OAuthToken &&
+            $this->toggleManager->isActive('login_franceconnect')
+        ) {
+            $logoutURL = $this->franceConnectResourceOwner->getOption('logout_url');
+
+            $homepageUrl = $this->router->generate(
+                'app_homepage',
+                [],
+                RouterInterface::ABSOLUTE_URL
+            );
+
+            // Uncomment it if yo want to test France Connect in localhost.
+            // $homepageUrl = 'http://localhost:4242/logout';
+
+            $parameters = [
+                'post_logout_redirect_uri' => $homepageUrl,
+                'state' => $this->generateNonce(),
+                'id_token_hint' => $token->getRawToken()['id_token']
+            ];
+
+            return new RedirectResponse(
+                $logoutURL . '?' . http_build_query($parameters, '', '&') ?? '/'
+            );
+        }
+
+        return $response;
+    }
+
+    private function generateNonce(): string
+    {
+        return md5(microtime(true) . uniqid('', true));
     }
 }
