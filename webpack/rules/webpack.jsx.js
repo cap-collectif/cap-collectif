5e4e2e00dc2b7fcb2af75ac033cb2836131d3b69
aguilaj <julien.aguilar6@gmail.com>
2019-10-07 13:53:50 +0200

    [7903-UPGRADE] update snapshots
    GitOrigin-RevId: 8c0d72ebe23f561e4f0d5f202ee34da704abe4da

--- webpack/rules/webpack.jsx.js
+++ webpack/rules/webpack.jsx.js
@@ -6,7 +6,7 @@ function getRulesConf() {
       rules: [
         {
           test: [
-            // These dependencies have es6 syntax which ie11 doesn't like.
+            // These dependencies have es6 syntax which ie11 doesn't like. See https://github.com/formatjs/react-intl/blob/master/docs/Upgrade-Guide.md#esm-build
             path.resolve('node_modules/react-intl'),
             path.resolve('node_modules/intl-messageformat'),
             path.resolve('node_modules/intl-messageformat-parser'),
