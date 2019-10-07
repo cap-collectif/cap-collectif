8462dee8f0cf15838558d1b97b537f12957733f1
Liinkiing <omar.jbara2@gmail.com>
2019-10-07 16:32:55 +0200

    - Revert commit 14f905db99c2168e2748c84e558ed5eb88f63f57
    GitOrigin-RevId: c4e5e21e5e07ee1b90b5c0694c2654406b525cb8

--- infrastructure/deploylib/system.py
+++ infrastructure/deploylib/system.py
@@ -177,16 +177,4 @@ def sign_ssl():
     elif _platform == "darwin":
         local('sudo security add-trusted-cert -d -r trustAsRoot -k /Library/Keychains/System.keychain %s' % env.real_fabfile[:-10] + "infrastructure/services/local/nginx/ssl/capco.cer")
         sign_ssl_mac()
-    services = [
-        'mail.cap.co',
-        'cerebro.cap.co',
-        'kibana.cap.co',
-        'rabbitmq.cap.co',
-    ]
-    destination = 'infrastructure/services/local/nginx/ssl/'
-    crt = 'capco_services.crt'
-    key = 'capco_services.key'
-    local('mkcert -cert-file=%s -key-file=%s %s' % (crt, key, ' '.join(services)))
-    local('mv %s %s' % (crt, destination))
-    local('mv %s %s' % (key, destination))
     print cyan('Successfully added HTTPS support !')
