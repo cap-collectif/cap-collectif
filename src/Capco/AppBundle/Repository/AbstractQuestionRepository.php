7222f8b459486bcc46182563e63baf790dabbddb
Liinkiing <omar.jbara2@gmail.com>
2019-10-08 11:58:05 +0200

    - Added a method to `AbstractQuestionRepository` to resolve questions from a `ProposalForm`
    GitOrigin-RevId: b841f4f39b20132a7524e9072ce31364c9fc560a

--- src/Capco/AppBundle/Repository/AbstractQuestionRepository.php
+++ src/Capco/AppBundle/Repository/AbstractQuestionRepository.php
@@ -2,8 +2,25 @@
 
 namespace Capco\AppBundle\Repository;
 
+use Capco\AppBundle\Entity\ProposalForm;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityRepository;
 
 class AbstractQuestionRepository extends EntityRepository
 {
+    public function findByProposalForm(ProposalForm $form, ?array $sort = ['field' => 'position', 'direction' => 'ASC']): iterable
+    {
+        [$field, $direction] = [$sort['field'], $sort['direction']];
+        $qb = $this->createQueryBuilder('aq');
+
+        return $qb
+            ->leftJoin('aq.questionnaireAbstractQuestion', 'qaq')
+            ->andWhere(
+                $qb->expr()->eq('qaq.proposalForm', ':proposalForm')
+            )
+            ->setParameter('proposalForm', $form)
+            ->addOrderBy("qaq.$field", $direction)
+            ->getQuery()
+            ->getResult();
+    }
 }
