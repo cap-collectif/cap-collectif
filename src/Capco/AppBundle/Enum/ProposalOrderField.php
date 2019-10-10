5cab6f66c14fae0e2bf4caa0c6a7fd730dbd1e67
Jpec57 <jean.paul.bella@hotmail.fr>
2019-10-10 14:24:55 +0200

    [8488-FIX] Added created at
    GitOrigin-RevId: 5f8dacb5914cc77522d89fdd0837cd6fa1d1d14b

--- src/Capco/AppBundle/Enum/ProposalOrderField.php
+++ src/Capco/AppBundle/Enum/ProposalOrderField.php
@@ -5,6 +5,7 @@ namespace Capco\AppBundle\Enum;
 final class ProposalOrderField
 {
     public const PUBLISHED_AT = 'PUBLISHED_AT';
+    public const CREATED_AT = 'CREATED_AT';
     public const VOTES = 'VOTES';
     public const COMMENTS = 'COMMENTS';
     public const RANDOM = 'RANDOM';
