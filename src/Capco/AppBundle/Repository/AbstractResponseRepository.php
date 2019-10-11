16d641b15955ace0e1cfdb067d61cc9b343fc2da
mrpandat <mrpandat@hotmail.fr>
2019-10-11 10:30:27 +0200

    [TEST-LESS] Remove some randomly-failing.
    GitOrigin-RevId: add5853f2ccb74c6f4d4939a490793d7872ba7b1

--- src/Capco/AppBundle/Repository/AbstractResponseRepository.php
+++ src/Capco/AppBundle/Repository/AbstractResponseRepository.php
@@ -3,11 +3,13 @@
 namespace Capco\AppBundle\Repository;
 
 use Capco\AppBundle\Entity\Proposal;
+use Capco\AppBundle\Entity\ProposalEvaluation;
+use Capco\AppBundle\Entity\Reply;
 use Doctrine\ORM\EntityRepository;
 
 class AbstractResponseRepository extends EntityRepository
 {
-    public function getByReplyAsArray($replyId)
+    public function getByReplyAsArray($replyId): iterable
     {
         $qb = $this->createQueryBuilder('r')
             ->addSelect('question')
@@ -18,7 +20,7 @@ class AbstractResponseRepository extends EntityRepository
         return $qb->getQuery()->getArrayResult();
     }
 
-    public function getByProposal(Proposal $proposal, bool $showPrivate = false)
+    public function getByProposal(Proposal $proposal, bool $showPrivate = false): iterable
     {
         $qb = $this->createQueryBuilder('r')
             ->addSelect('question')
@@ -27,6 +29,38 @@ class AbstractResponseRepository extends EntityRepository
             ->andWhere('r.proposal = :proposal')
             ->orderBy('questionnaire_abstract_question.position', 'ASC')
             ->setParameter('proposal', $proposal->getId());
+        if (!$showPrivate) {
+            $qb->andWhere('question.private = false');
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function getByReply(Reply $reply, bool $showPrivate = false): iterable
+    {
+        $qb = $this->createQueryBuilder('r')
+            ->addSelect('question')
+            ->leftJoin('r.question', 'question')
+            ->leftJoin('question.questionnaireAbstractQuestion', 'questionnaire_abstract_question')
+            ->andWhere('r.reply = :reply')
+            ->orderBy('questionnaire_abstract_question.position', 'ASC')
+            ->setParameter('reply', $reply->getId());
+        if (!$showPrivate) {
+            $qb->andWhere('question.private = false');
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function getByEvaluation(ProposalEvaluation $evaluation, bool $showPrivate = false): iterable
+    {
+        $qb = $this->createQueryBuilder('r')
+            ->addSelect('question')
+            ->leftJoin('r.question', 'question')
+            ->leftJoin('question.questionnaireAbstractQuestion', 'questionnaire_abstract_question')
+            ->andWhere('r.proposalEvaluation = :evaluation')
+            ->orderBy('questionnaire_abstract_question.position', 'ASC')
+            ->setParameter('evaluation', $evaluation->getId());
         if (!$showPrivate) {
             $qb->andWhere('question.private = false');
         }
