baad64dcc1493f141b250117e6ab028e9d1da8eb
mrpandat <mrpandat@hotmail.fr>
2019-10-14 17:19:41 +0200

    [9034-PROJECT] Separate renderContentAdminForm from ProjectAdminPage.
    GitOrigin-RevId: 077a53c782a924593acad891c136c472fb26e01f

--- app/Resources/js/startup/ProjectAdminApp.js
+++ app/Resources/js/startup/ProjectAdminApp.js
@@ -9,42 +9,43 @@ import environment, { graphqlError } from '../createRelayEnvironment';
 import ProjectContentAdminPage from '../components/Admin/Project/ProjectContentAdminPage';
 import type { ProjectAdminAppQueryResponse } from '~relay/ProjectAdminAppQuery.graphql';
 
+const renderContentAdminForm = (projectId: ?string) =>
+  projectId ? (
+    <QueryRenderer
+      environment={environment}
+      query={graphql`
+        query ProjectAdminAppQuery($projectId: ID!) {
+          project: node(id: $projectId) {
+            ...ProjectContentAdminForm_project
+          }
+        }
+      `}
+      variables={{
+        projectId,
+      }}
+      render={({
+        props,
+        error,
+      }: {
+        ...ReactRelayReadyState,
+        props: ?ProjectAdminAppQueryResponse,
+      }) => {
+        if (error) {
+          return graphqlError;
+        }
+        if (props) {
+          return <ProjectContentAdminPage isEditMode project={props.project} />;
+        }
+        return null;
+      }}
+    />
+  ) : (
+    <ProjectContentAdminPage project={null} isEditMode={false} />
+  );
+
 const ProjectAdminPage = ({ projectId }: { projectId: ?string }) => (
   <Provider store={ReactOnRails.getStore('appStore')}>
-    <IntlProvider>
-      {projectId ? (
-        <QueryRenderer
-          environment={environment}
-          query={graphql`
-            query ProjectAdminAppQuery($projectId: ID!) {
-              project: node(id: $projectId) {
-                ...ProjectContentAdminForm_project
-              }
-            }
-          `}
-          variables={{
-            projectId,
-          }}
-          render={({
-            props,
-            error,
-          }: {
-            ...ReactRelayReadyState,
-            props: ?ProjectAdminAppQueryResponse,
-          }) => {
-            if (error) {
-              return graphqlError;
-            }
-            if (props) {
-              return <ProjectContentAdminPage isEditMode project={props.project} />;
-            }
-            return null;
-          }}
-        />
-      ) : (
-        <ProjectContentAdminPage project={null} isEditMode={false} />
-      )}
-    </IntlProvider>
+    <IntlProvider>{renderContentAdminForm(projectId)}</IntlProvider>
   </Provider>
 );
 
