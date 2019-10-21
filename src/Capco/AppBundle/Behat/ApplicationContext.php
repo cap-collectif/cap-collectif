3ec77fc1ba8ab9a2116fe6d5b32eddab02f9b2ff
Jpec57 <jean.paul.bella@hotmail.fr>
2019-10-21 11:46:02 +0200

    [8410-GENERAL] Deleting personal facebook pixel and refactoring code
    GitOrigin-RevId: 0588fa58c3959e9c8f337eb236daf3c9fb53757b

--- src/Capco/AppBundle/Behat/ApplicationContext.php
+++ src/Capco/AppBundle/Behat/ApplicationContext.php
@@ -1049,14 +1049,6 @@ class ApplicationContext extends UserContext
     }
 
     /**
-     * @When I wait for element :element to appear in dom
-     */
-    public function iCheckPresenceInDom(string $element){
-        $this->waitAndThrowOnFailure(5000, "$('$element').length > 0");
-
-    }
-
-    /**
      * @When I fill the project filter with value :value
      */
     public function iFillProjectFilterWithValue($value)
