a40048867c2427c068e89c0d2bb0388656ff23ac
Liinkiing <omar.jbara2@gmail.com>
2019-10-21 12:18:50 +0200

    [9067-FIX] - Made $context optional in MediaUrlResolver `__invoke` method
    GitOrigin-RevId: 4c2c4622047cec83853439c29c3c8a5aba4e406e

--- src/Capco/AppBundle/GraphQL/Resolver/Media/MediaUrlResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Media/MediaUrlResolver.php
@@ -32,7 +32,7 @@ class MediaUrlResolver implements ResolverInterface
         $this->routerRequestContextHost = $routerRequestContextHost;
     }
 
-    public function __invoke(Media $media, \ArrayObject $context, ?Arg $args = null): string
+    public function __invoke(Media $media, ?\ArrayObject $context = null, ?Arg $args = null): string
     {
         $format = $args && $args['format'] ? $args['format'] : 'reference';
         $isExportContext =
