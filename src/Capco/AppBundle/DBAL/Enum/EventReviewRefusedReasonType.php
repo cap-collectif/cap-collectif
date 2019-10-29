214a53b6e208740c3070e2d5b18d6f5e429d91dc
mauriau <maxime.auriau@protonmail.com>
2019-10-29 11:14:35 +0100

    6552-EXAMINE fix for @ptondereau
    GitOrigin-RevId: 2e0b344a3d3893e198728fbea01954c1fb3f0fe0

--- src/Capco/AppBundle/DBAL/Enum/EventReviewRefusedReasonType.php
+++ src/Capco/AppBundle/DBAL/Enum/EventReviewRefusedReasonType.php
@@ -5,7 +5,7 @@ namespace Capco\AppBundle\DBAL\Enum;
 class EventReviewRefusedReasonType extends AbstractEnumType
 {
     public const SEX = 'sex';
-    public const OFF = 'off';
+    public const OFFENDING = 'offending';
     public const SPAM = 'spam';
     public const ERROR = 'error';
     public const OFF_TOPIC = 'off_topic';
@@ -14,7 +14,7 @@ class EventReviewRefusedReasonType extends AbstractEnumType
     protected $name = 'enum_event_review_refused_reason';
     protected $values = [
         self::SEX,
-        self::OFF,
+        self::OFFENDING,
         self::SPAM,
         self::ERROR,
         self::OFF_TOPIC,
