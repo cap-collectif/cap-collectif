a275e1ee813e6e285427f6068246eefe947f5942
spyl94 <spyl94@gmail.com>
2019-10-31 14:08:11 +0100

    [9070-PERF] - QuestionnaireStep page query (#9125)
    [9070-PERF] - QuestionnaireStep page query
    
    GitOrigin-RevId: 892678e9e1ead93eb875bec4f71f7bbac2aa0026

--- src/Capco/AppBundle/GraphQL/Resolver/Question/QuestionJumpsResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Question/QuestionJumpsResolver.php
@@ -6,31 +6,26 @@ use Capco\AppBundle\Entity\LogicJump;
 use Capco\AppBundle\Entity\Questions\AbstractQuestion;
 use Capco\AppBundle\Enum\JumpsOrderField;
 use Capco\AppBundle\Enum\OrderDirection;
+use Capco\AppBundle\GraphQL\DataLoader\Question\QuestionJumpsDataLoader;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use GraphQL\Executor\Promise\Promise;
 use Overblog\GraphQLBundle\Definition\Argument as Arg;
 use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
 
 class QuestionJumpsResolver implements ResolverInterface
 {
-    public function __invoke(AbstractQuestion $question, Arg $args): Collection
-    {
-        $orderBy = $args->offsetGet('orderBy');
-        list($field, $direction) = [$orderBy['field'], $orderBy['direction']];
-        $iterator = $question->getJumps()->getIterator();
-        $iterator->uasort(static function (LogicJump $a, LogicJump $b) use ($direction, $field) {
-            switch ($field) {
-                case JumpsOrderField::POSITION:
-                    return OrderDirection::ASC === $direction
-                        ? $a->getPosition() <=> $b->getPosition()
-                        : $b->getPosition() <=> $a->getPosition();
-                default:
-                    throw new \InvalidArgumentException(
-                        sprintf('Unknown sort field "%s" for Logic Jump.', $field)
-                    );
-            }
-        });
+    private $dataLoader;
 
-        return new ArrayCollection(iterator_to_array($iterator));
+    public function __construct(QuestionJumpsDataLoader $dataLoader)
+    {
+        $this->dataLoader = $dataLoader;
+    }
+
+    public function __invoke(AbstractQuestion $question, Arg $args): Promise
+    {
+        return $this->dataLoader->load(
+            compact('question', 'args')
+        );
     }
 }
