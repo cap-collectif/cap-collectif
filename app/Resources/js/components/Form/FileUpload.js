50a27c89a32daccc6e9f9fe2a34cb306061d4c5e
aguilaj <julien.aguilar6@gmail.com>
2019-11-15 16:09:46 +0100

    [9284-REACT] upgrade dropzone style
    GitOrigin-RevId: 73a5429ffc9b5a4bab06fa827f05562b884336c3

--- app/Resources/js/components/Form/FileUpload.js
+++ app/Resources/js/components/Form/FileUpload.js
@@ -15,17 +15,19 @@ export class FileUpload extends React.Component<DropzoneProps> {
     return (
       <Row className={classNames(classes)}>
         <Col xs={12} sm={12}>
-          <Dropzone className="image-uploader__dropzone--fullwidth" {...this.props}>
+          <Dropzone {...this.props}>
             {() => (
-              <div className="image-uploader__dropzone-label">
-                <FormattedMessage id="global.image_uploader.file.dropzone" />
-                <br />
-                <FormattedMessage id="global.or" />
-                <p style={{ textAlign: 'center' }}>
-                  <Button className="image-uploader__btn">
-                    <FormattedMessage id="global.image_uploader.file.btn" />
-                  </Button>
-                </p>
+              <div className="image-uploader__dropzone--fullwidth">
+                <div className="image-uploader__dropzone-label">
+                  <FormattedMessage id="global.image_uploader.file.dropzone" />
+                  <br />
+                  <FormattedMessage id="global.or" />
+                  <p style={{ textAlign: 'center' }}>
+                    <Button className="image-uploader__btn">
+                      <FormattedMessage id="global.image_uploader.file.btn" />
+                    </Button>
+                  </p>
+                </div>
               </div>
             )}
           </Dropzone>
