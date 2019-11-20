49bea9fedb44888cfba183b32090d4dd8077864f
aguilaj <julien.aguilar6@gmail.com>
2019-11-20 17:59:41 +0100

    [9222-CHANGE] finish languagechangebutton
    GitOrigin-RevId: 9c059c1c40b87d1021123e7e6c8f32ba5640c650

--- app/Resources/js/components/Ui/Button/LanguageChangeButton.js
+++ app/Resources/js/components/Ui/Button/LanguageChangeButton.js
@@ -1,7 +1,6 @@
 // @flow
 import React, { useState } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { DropdownButton } from 'react-bootstrap';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
 import styled, { type StyledComponent } from 'styled-components';
 
 export type LanguageStatus = {|
@@ -10,9 +9,10 @@ export type LanguageStatus = {|
 |};
 
 type Props = {|
-  onChange: () => void,
+  onChange: string => void,
   languageList: Array<LanguageStatus>,
-  defaultLanguage: LanguageStatus,
+  defaultLanguage: string,
+  pullRight?: boolean,
 |};
 
 const Dot: StyledComponent<{ green: boolean }, {}, HTMLDivElement> = styled.div`
@@ -26,10 +26,25 @@ const Dot: StyledComponent<{ green: boolean }, {}, HTMLDivElement> = styled.div`
 
 const Language: StyledComponent<{}, {}, HTMLDivElement> = styled.div`
   font-family: 'OpenSans', helvetica, arial, sans-serif;
+  font-size: 16px;
 `;
 
 const LanguageContainer: StyledComponent<{}, {}, HTMLDivElement> = styled.div`
   display: flex;
+`;
+
+const DropownLanguageButton: StyledComponent<{}, {}, DropdownButton> = styled(DropdownButton)`
+  display: flex;
+  background: rgba(108, 117, 125, 0.2);
+  border-radius: 4px;
+  .caret {
+    align-self: center;
+    margin-left: 6px;
+  }
+`;
+
+const MenuLanguageItem: StyledComponent<{}, {}, MenuItem> = styled(MenuItem)`
+  margin-bottom: 5px;
 `;
 
 const renderLanguage = (language: LanguageStatus) => {
@@ -41,21 +56,30 @@ const renderLanguage = (language: LanguageStatus) => {
   );
 };
 
-const LanguageChangeButton = ({ onChange, languageList, defaultLanguage }: Props) => {
-  const [currentLanguage, updateLanguage] = useState(defaultLanguage);
+const LanguageChangeButton = ({ onChange, languageList, defaultLanguage, pullRight }: Props) => {
+  const [currentLanguage, updateLanguage] = useState(
+    languageList.find(e => e.language === defaultLanguage),
+  );
+  if (!currentLanguage) return null;
   return (
-    <DropdownButton
+    <DropownLanguageButton
       bsStyle="default"
-      className="btn-outline-warning"
+      pullRight={pullRight}
       onClick={onChange}
       title={renderLanguage(currentLanguage)}>
-      <i className="fa fa-pencil" />
-      {updateLanguage}
-      {languageList}
-      <span className="hidden-xs">
-        <FormattedMessage id="global.edit" className="hidden-xs" />
-      </span>
-    </DropdownButton>
+      {languageList
+        .filter(language => language.language !== currentLanguage.language)
+        .map(language => (
+          <MenuLanguageItem
+            key={language.language}
+            onClick={() => {
+              updateLanguage(language);
+              onChange(language.language);
+            }}>
+            {renderLanguage(language)}
+          </MenuLanguageItem>
+        ))}
+    </DropownLanguageButton>
   );
 };
 
--- app/Resources/js/stories/core/buttons/LanguageChangeButton-stories.js
+++ app/Resources/js/stories/core/buttons/LanguageChangeButton-stories.js
@@ -1,17 +1,23 @@
 // @flow
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
+import { boolean } from 'storybook-addon-knobs';
 
 import LanguageChangeButton from '../../../components/Ui/Button/LanguageChangeButton';
 
 storiesOf('Core|Buttons/LanguageChangeButton', module).add('default', () => {
   const props = {
     onChange: () => {},
-    defaultLanguage: { language: 'Français', translated: true },
+    defaultLanguage: 'Français',
     languageList: [
+      { language: 'Français', translated: false },
+      { language: 'English', translated: false },
+      { language: 'Español', translated: true },
       { language: 'Deutsch', translated: true },
-      { language: 'Nederlander', translated: false },
+      { language: 'Nederlander', translated: true },
     ],
   };
-  return <LanguageChangeButton {...props} />;
+  const pullRight = boolean('pullright', false);
+
+  return <LanguageChangeButton {...props} pullRight={pullRight} />;
 });
