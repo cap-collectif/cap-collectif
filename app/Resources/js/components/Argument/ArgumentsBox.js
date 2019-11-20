89366f3065b3bccd663a66bde5007737f7d1bc90
spyl94 <spyl94@gmail.com>
2019-11-20 15:10:24 +0100

    Do not escape strings in mails template
    GitOrigin-RevId: 3be7fe6c33a2f815e07b8c0ade06c1a31a03446b

--- app/Resources/js/components/Argument/ArgumentsBox.js
+++ app/Resources/js/components/Argument/ArgumentsBox.js
@@ -1,9 +1,7 @@
 // @flow
-import React, { useState } from 'react';
-import { Row, Col, Button } from 'react-bootstrap';
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 import { graphql, createFragmentContainer } from 'react-relay';
-import { FormattedMessage } from 'react-intl';
-import styled, { type StyledComponent } from 'styled-components';
 import { COMMENT_SYSTEM_SIMPLE, COMMENT_SYSTEM_BOTH } from '../../constants/ArgumentConstants';
 import UnpublishedArgumentListRenderer from './UnpublishedArgumentListRenderer';
 import ArgumentList from './ArgumentList';
@@ -15,43 +13,9 @@ type Props = {
   opinion: ArgumentsBox_opinion,
 };
 
-const Switcher: StyledComponent<{}, {}, Col> = styled(Col)`
-  display: none;
-  @media (max-width: 991px) {
-    display: flex;
-  }
-  margin-bottom: 15px;
-  padding: 15px 15px 15px 15px;
-  z-index: 2;
-  top: 50px;
-  background: #f6f6f6;
-  width: 100%;
-  button {
-    width: 50%;
-  }
-  position: sticky;
-  position: -webkit-sticky;
-`;
-
-const ArgumentsCol: StyledComponent<{ hide: boolean }, {}, Col> = styled(Col)`
-  @media (max-width: 991px) {
-    display: ${props => props.hide && 'none'};
-  }
-`;
-
-const scrollToListTop = () => {
-  const top = document.getElementById('argument-list-top-scroll');
-  if (top) {
-    top.scrollIntoView();
-    window.scrollBy(0, -45);
-  }
-};
-
-export const ArgumentsBox = ({ opinion }: Props) => {
-  const [order, setOrder] = useState<ArgumentType>('FOR');
-
-  const renderArgumentsForType = (type: ArgumentType) => {
-    const argumentable = opinion;
+class ArgumentsBox extends React.Component<Props> {
+  renderArgumentsForType = (type: ArgumentType) => {
+    const argumentable = this.props.opinion;
     return (
       <div id={`arguments-col--${type}`}>
         <div className="opinion opinion--add-argument block block--bordered">
@@ -67,60 +31,32 @@ export const ArgumentsBox = ({ opinion }: Props) => {
     );
   };
 
-  const renderSwitcher = () => (
-    <Switcher sm={12} id="arguments-view-switcher" className="btn-group" role="group">
-      <Button
-        bsStyle="default"
-        active={order === 'FOR'}
-        role="checkbox"
-        aria-checked={order === 'FOR'}
-        onClick={() => {
-          setOrder('FOR');
-          scrollToListTop();
-        }}>
-        <FormattedMessage id="argument.show.type.for" />
-      </Button>
-      <Button
-        bsStyle="default"
-        active={order === 'AGAINST'}
-        role="checkbox"
-        aria-checked={order === 'AGAINST'}
-        onClick={() => {
-          setOrder('AGAINST');
-          scrollToListTop();
-        }}>
-        <FormattedMessage id="argument.show.type.against" />
-      </Button>
-    </Switcher>
-  );
+  render() {
+    const { opinion } = this.props;
+    if (!opinion.section) {
+      return null;
+    }
+    const { commentSystem } = opinion.section;
+    if (commentSystem === COMMENT_SYSTEM_BOTH) {
+      return (
+        <Row>
+          <Col sm={12} md={6}>
+            {this.renderArgumentsForType('FOR')}
+          </Col>
+          <Col sm={12} md={6}>
+            {this.renderArgumentsForType('AGAINST')}
+          </Col>
+        </Row>
+      );
+    }
 
-  if (!opinion.section) {
+    if (commentSystem === COMMENT_SYSTEM_SIMPLE) {
+      return this.renderArgumentsForType('SIMPLE');
+    }
+
     return null;
   }
-  const { commentSystem } = opinion.section;
-  if (commentSystem === COMMENT_SYSTEM_BOTH) {
-    return (
-      <>
-        <div id="argument-list-top-scroll" />
-        <Row>
-          {renderSwitcher()}
-          <ArgumentsCol sm={12} md={6} hide={order === 'AGAINST'}>
-            {renderArgumentsForType('FOR')}
-          </ArgumentsCol>
-          <ArgumentsCol sm={12} md={6} hide={order === 'FOR'}>
-            {renderArgumentsForType('AGAINST')}
-          </ArgumentsCol>
-        </Row>
-      </>
-    );
-  }
-
-  if (commentSystem === COMMENT_SYSTEM_SIMPLE) {
-    return renderArgumentsForType('SIMPLE');
-  }
-
-  return null;
-};
+}
 
 export default createFragmentContainer(ArgumentsBox, {
   opinion: graphql`
