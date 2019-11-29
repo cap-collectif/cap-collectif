65a1b5338418b90cf8ff6e5640604176103696cc
spyl94 <spyl94@gmail.com>
2019-11-29 14:18:51 +0100

    Merge pull request #9422 from cap-collectif/9230-fix-remaining-trad
    [9230-FIX-URGENT] fixing small errors due to change in keys
    
    GitOrigin-RevId: b16cfc172f6d46c3a192144c695fac7d092e5523

--- src/Capco/AdminBundle/Admin/PostAdmin.php
+++ src/Capco/AdminBundle/Admin/PostAdmin.php
@@ -144,16 +144,9 @@ class PostAdmin extends CapcoAdmin
         $editMode = $this->getSubject()->getId() ? true : false;
 
         $formMapper
-            ->with('global.contenu', ['class' => 'col-md-12'])
-            ->end()
-            ->with('admin.fields.blog_post.group_linked_content', ['class' => 'col-md-6'])
-            ->end()
-            ->with('admin.fields.blog_post.group_meta', ['class' => 'col-md-6'])
-            ->end();
-        $formMapper
-            ->with('admin.fields.blog_post.group_content')
+            ->with('global.contenu')
             ->add('title', TextType::class, [
-                'label' => 'global.titlee',
+                'label' => 'global.title',
                 'required' => true
             ]);
         if ($editMode) {
