65a7e02f27b25ca64bba2d21987d0bf47a5707c2
ptondereau <pierre.tondereau@protonmail.com>
2019-11-29 16:22:41 +0100

    [Security] Bump simplesamlphp/simplesamlphp from 1.17.7 to 1.18… (#9410)
    Bumps [simplesamlphp/simplesamlphp](https://github.com/simplesamlphp/simplesamlphp) from 1.17.7 to 1.18.2.
    - [Release notes](https://github.com/simplesamlphp/simplesamlphp/releases)
    - [Changelog](https://github.com/simplesamlphp/simplesamlphp/blob/master/docs/simplesamlphp-changelog.md)
    - [Commits](https://github.com/simplesamlphp/simplesamlphp/compare/v1.17.7...v1.18.2)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    GitOrigin-RevId: af8c2affe7d0681101dc09d4a8c10b02c04bac7d

--- features/graphql-public/query/viewer.js
+++ features/graphql-public/query/viewer.js
@@ -28,9 +28,3 @@ describe('Public|Query.viewer', () => {
     await expect(graphql(ViewerQuery, null, 'anonymous')).rejects.toMatchSnapshot();
   });
 });
-
-describe('Internal|Query.viewer', () => {
-  it('fetches admin@test.com when using internal_admin client ', async () => {
-    await expect(graphql(ViewerQuery, null, 'internal_admin')).resolves.toMatchSnapshot();
-  });
-});
