1064a189789d053c0c1970c60e0e6610ee4ee043
Jpec57 <jean.paul.bella@hotmail.fr>
2019-12-12 16:15:28 +0100

    Merge pull request #9515 from cap-collectif/9511-argument-not-vote
    [9511-ARGUMENT] Adding argument vote description
    
    GitOrigin-RevId: f9cc761884ba65c9548765bf6a560828c2c6b5e8

--- frontend/js/components/Vote/VoteItem.js
+++ frontend/js/components/Vote/VoteItem.js
@@ -46,6 +46,8 @@ export class VoteItem extends React.Component<Props> {
 
     if (vote.__typename === 'CommentVote') {
       voteType = 'votes.type.commentary';
+    } else if (vote.__typename === 'ArgumentVote') {
+      voteType = 'votes.type.argument';
     } else if (vote.__typename === 'VersionVote') {
       voteType = 'votes.type.version';
     } else if (vote.value !== null) {
