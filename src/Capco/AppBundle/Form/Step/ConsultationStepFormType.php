aefa77d1b725d6ce802b6d139e444a90f386db2f
Liinkiing <omar.jbara2@gmail.com>
2019-12-13 12:14:34 +0100

    [8054-REFACTO] - Added support for consultations re-ordering in `ConsultationStepFormType`
    GitOrigin-RevId: ffaf6fc62f5a3a558e3e9cdde4f63bc524ff39b3

--- src/Capco/AppBundle/Form/Step/ConsultationStepFormType.php
+++ src/Capco/AppBundle/Form/Step/ConsultationStepFormType.php
@@ -6,6 +6,8 @@ use Capco\AppBundle\Entity\Consultation;
 use Capco\AppBundle\Entity\Steps\ConsultationStep;
 use Capco\AppBundle\Form\Type\RelayNodeType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ConsultationStepFormType extends AbstractStepFormType
@@ -19,6 +21,29 @@ class ConsultationStepFormType extends AbstractStepFormType
             'class' => Consultation::class,
             'choice_label' => 'id'
         ]);
+        $builder->addEventListener(FormEvents::SUBMIT, static function (FormEvent $event) {
+            $normalizedConsultations = $event
+                ->getForm()
+                ->get('consultations')
+                ->getNormData();
+            /** @var ConsultationStep $step */
+            $step = $event->getData();
+            $position = 1;
+            foreach ($normalizedConsultations as $normalizedConsultation) {
+                // And we set in the backend the correct position for the consultation, based on the user order
+                $consultation = $step
+                    ->getConsultations()
+                    ->filter(static function (Consultation $c) use ($normalizedConsultation) {
+                        return $c->getId() === $normalizedConsultation->getId();
+                    })
+                    ->first();
+                /** @var Consultation $consultation */
+                if ($consultation) {
+                    $consultation->setPosition($position);
+                }
+                ++$position;
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
