b01b81d378f2bd1e84b59ae14630b89d79b22010
Jpec57 <jean.paul.bella@hotmail.fr>
2019-12-16 08:20:52 +0100

    [8767-PUBLIC] Updating migration
    GitOrigin-RevId: f23d58503b0a2f74f2fc79e236a77d37acb2fa8e

--- src/DoctrineMigrations/Version20191216080249.php
+++ src/DoctrineMigrations/Version20191216080249.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191213150249 extends AbstractMigration
+final class Version20191216080249 extends AbstractMigration
 {
     public function getDescription() : string
     {
