49a852cf3dadbaa3d8684b673b8051a3405e13eb
Liinkiing <omar.jbara2@gmail.com>
2019-12-17 14:59:05 +0100

    [8054-REFACTO] - Fixed `OrderedCollectionType` propertyAccessor
    GitOrigin-RevId: 04e4b23823b029c8f0fa278b2bbdf4b2a0102df4

--- src/Capco/AppBundle/Form/Type/OrderedCollectionType.php
+++ src/Capco/AppBundle/Form/Type/OrderedCollectionType.php
@@ -37,7 +37,7 @@ class OrderedCollectionType extends CollectionType
             if ($collectionInDb instanceof ArrayCollection) {
                 foreach ($userCollection as $item) {
                     $item->setPosition(++$position);
-                    $this->propertyAccessor->setValue($parent, $field, $item);
+                    $this->propertyAccessor->setValue($parent, $field, [$item]);
                 }
             } else {
                 foreach ($userCollection as $item) {
