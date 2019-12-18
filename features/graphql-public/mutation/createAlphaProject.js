1011b821d6506c503803baa85babb9d3d579b0f9
Liinkiing <omar.jbara2@gmail.com>
2019-12-18 13:24:16 +0100

    [8054-REFACTO] - Added more test cases for createAlphaProject mutation
    GitOrigin-RevId: 5520bd6423b2db3336ebb9bab0822be9d72f9351

--- features/graphql-public/mutation/createAlphaProject.js
+++ features/graphql-public/mutation/createAlphaProject.js
@@ -43,17 +43,28 @@ const CreateAlphaProjectMutation = /* GraphQL */ `
             }
           }
           ... on CollectStep {
-            private 
+            private
             form {
               title
             }
           }
           ... on ProposalStep {
+            requirements {
+              totalCount
+              edges {
+                node {
+                  __typename
+                  ... on CheckboxRequirement {
+                    label
+                  }
+                }
+              }
+            }
             statuses {
               name
               color
-            } 
-            defaultSort 
+            }
+            defaultSort
           }
           ... on QuestionnaireStep {
             questionnaire {
@@ -82,8 +93,45 @@ const BASE_PROJECT = {
   steps: [],
 };
 
-describe('Internal|createAlphaProject simple mutation', () => {
+const BASE_SELECTION_STEP = {
+  type: 'SELECTION',
+  body: "Le beau body de l'étape SelectionStep",
+  requirements: [],
+  statuses: [],
+  voteType: 'DISABLED',
+  defaultSort: 'RANDOM',
+  proposalsHidden: false,
+  timeless: false,
+  isEnabled: true,
+  title: "Le beau titre de l'étape SelectionStep",
+  label: 'SelectionStep',
+};
 
+const BASE_PRESENTATION_STEP = {
+  type: 'PRESENTATION',
+  body: "Le beau body de l'étape PresentationStep",
+  requirements: [],
+  isEnabled: true,
+  title: "Le beau titre de l'étape PresentationStep",
+  label: 'PresentationStep',
+};
+
+const BASE_COLLECT_STEP = {
+  type: 'COLLECT',
+  body: "Le beau body de l'étape CollectStep",
+  requirements: [],
+  statuses: [],
+  voteType: 'DISABLED',
+  defaultSort: 'RANDOM',
+  private: false,
+  proposalForm: 'proposalform13',
+  timeless: false,
+  isEnabled: true,
+  title: "Le beau titre de l'étape CollectStep",
+  label: 'CollectStep',
+};
+
+describe('Internal|createAlphaProject simple mutation', () => {
   it('create a project without any steps', async () => {
     await expect(
       graphql(
@@ -129,16 +177,7 @@ describe('Internal|createAlphaProject simple mutation', () => {
           input: {
             ...BASE_PROJECT,
             title: 'Je suis un projet simple avec une étape de présentation',
-            steps: [
-              {
-                type: 'PRESENTATION',
-                body: "Le beau body de l'étape PresentationStep",
-                requirements: [],
-                isEnabled: true,
-                title: "Le beau titre de l'étape PresentationStep",
-                label: 'PresentationStep',
-              },
-            ],
+            steps: [BASE_PRESENTATION_STEP],
           },
         },
         'internal_admin',
@@ -209,22 +248,7 @@ describe('Internal|createAlphaProject simple mutation', () => {
             ...BASE_PROJECT,
             projectType: '4',
             title: 'Je suis un projet simple avec une étape de dépôt',
-            steps: [
-              {
-                type: 'COLLECT',
-                body: "Le beau body de l'étape CollectStep",
-                requirements: [],
-                statuses: [],
-                voteType: "DISABLED",
-                defaultSort: "RANDOM",
-                private: false,
-                proposalForm: "proposalform13",
-                timeless: false,
-                isEnabled: true,
-                title: "Le beau titre de l'étape CollectStep",
-                label: 'CollectStep',
-              },
-            ],
+            steps: [BASE_COLLECT_STEP],
           },
         },
         'internal_admin',
@@ -241,21 +265,7 @@ describe('Internal|createAlphaProject simple mutation', () => {
             ...BASE_PROJECT,
             projectType: '4',
             title: 'Je suis un projet simple avec une étape de sélection',
-            steps: [
-              {
-                type: 'SELECTION',
-                body: "Le beau body de l'étape SelectionStep",
-                requirements: [],
-                statuses: [],
-                voteType: "DISABLED",
-                defaultSort: "RANDOM",
-                proposalsHidden: false,
-                timeless: false,
-                isEnabled: true,
-                title: "Le beau titre de l'étape SelectionStep",
-                label: 'SelectionStep',
-              },
-            ],
+            steps: [BASE_SELECTION_STEP],
           },
         },
         'internal_admin',
@@ -277,7 +287,7 @@ describe('Internal|createAlphaProject simple mutation', () => {
                 type: 'QUESTIONNAIRE',
                 body: "Le beau body de l'étape QuestionnaireStep",
                 requirements: [],
-                questionnaire: "questionnaire1",
+                questionnaire: 'questionnaire1',
                 isEnabled: true,
                 title: "Le beau titre de l'étape QuestionnaireStep",
                 label: 'QuestionnaireStep',
@@ -291,28 +301,126 @@ describe('Internal|createAlphaProject simple mutation', () => {
   });
 
   it('create a project with only a "RankingStep" step', async () => {
-    await expect(
-      graphql(
-        CreateAlphaProjectMutation,
-        {
-          input: {
-            ...BASE_PROJECT,
-            title: 'Je suis un projet simple avec une étape de classement',
-            steps: [
-              {
-                type: 'RANKING',
-                body: "Le beau body de l'étape RankingStep",
-                requirements: [],
-                isEnabled: true,
-                title: "Le beau titre de l'étape RankingStep",
-                label: 'RankingStep',
-              },
-            ],
-          },
+    const query = graphql(
+      CreateAlphaProjectMutation,
+      {
+        input: {
+          ...BASE_PROJECT,
+          title: 'Je suis un projet simple avec une étape de classement',
+          steps: [
+            {
+              type: 'RANKING',
+              body: "Le beau body de l'étape RankingStep",
+              requirements: [],
+              isEnabled: true,
+              title: "Le beau titre de l'étape RankingStep",
+              label: 'RankingStep',
+            },
+          ],
         },
-        'internal_admin',
-      ),
-    ).resolves.toMatchSnapshot();
+      },
+      'internal_admin',
+    );
+    await expect(query).resolves.toMatchSnapshot();
+    console.log(query.createAlphaProject);
+  });
+});
+
+describe('Internal|createAlphaProject complex mutation', () => {
+  it('create a project with a CollectStep that contains requirements and SelectionStep that contains statuses', async () => {
+    const query = graphql(
+      CreateAlphaProjectMutation,
+      {
+        input: {
+          ...BASE_PROJECT,
+          steps: [
+            {
+              ...BASE_COLLECT_STEP,
+              requirements: [
+                {
+                  label: "Le premier requirement de l'étape de dépôt",
+                  type: 'CHECKBOX',
+                },
+                {
+                  label: "Le deuxième requirement de l'étape de dépôt",
+                  type: 'CHECKBOX',
+                },
+              ],
+            },
+            {
+              ...BASE_SELECTION_STEP,
+              statuses: [
+                {
+                  name: "Le premier statut de l'étape de sélection",
+                  color: 'info',
+                },
+                {
+                  name: "Le deuxième statut de l'étape de sélection",
+                  color: 'info',
+                },
+              ],
+            },
+          ],
+        },
+      },
+      'internal_admin',
+    );
+    await expect(query).resolves.toMatchSnapshot();
   });
 
+  it('create a project with a simple PresentationStep, CollectStep that contains requirements and statuses and SelectionStep that contains statuses', async () => {
+    const query = graphql(
+      CreateAlphaProjectMutation,
+      {
+        input: {
+          ...BASE_PROJECT,
+          steps: [
+            BASE_PRESENTATION_STEP,
+            {
+              ...BASE_COLLECT_STEP,
+              statuses: [
+                {
+                  name: "Le premier statut de l'étape de dépôt",
+                  color: 'info',
+                },
+                {
+                  name: "Le deuxième statut de l'étape de dépôt",
+                  color: 'danger',
+                },
+                {
+                  name: "Le troisième statut de l'étape de dépôt",
+                  color: 'warning',
+                },
+              ],
+              requirements: [
+                {
+                  label: "Le premier requirement de l'étape de dépôt",
+                  type: 'CHECKBOX',
+                },
+                {
+                  label: "Le deuxième requirement de l'étape de dépôt",
+                  type: 'CHECKBOX',
+                },
+              ],
+            },
+            {
+              ...BASE_SELECTION_STEP,
+              statuses: [
+                {
+                  name: "Le premier statut de l'étape de sélection",
+                  color: 'info',
+                },
+                {
+                  name: "Le deuxième statut de l'étape de sélection",
+                  color: 'info',
+                },
+              ],
+            },
+          ],
+        },
+      },
+      'internal_admin',
+    );
+    await expect(query).resolves.toMatchSnapshot();
+  });
 });
