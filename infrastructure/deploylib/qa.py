57abb734f99acb7fde4a03d2b6d923fc7dc8c809
Jpec57 <jean.paul.bella@hotmail.fr>
2019-12-19 11:36:33 +0100

    [CSV-OPTION] Adding step-contributors snapshots in behat
    GitOrigin-RevId: d5ed1a3ae52a345b73f491550bc9852e1b42d3fa

--- infrastructure/deploylib/qa.py
+++ infrastructure/deploylib/qa.py
@@ -59,6 +59,7 @@ def snapshots(tags='false'):
         'capco:export:questionnaire --quiet --updateSnapshot',
         'capco:export:consultation --quiet --updateSnapshot',
         'capco:export:projects-contributors --quiet --updateSnapshot',
+        'capco:export:step-contributors --quiet --updateSnapshot',
         'capco:export:proposalStep --quiet --updateSnapshot',
         'capco:export:events:participants --quiet --updateSnapshot',
     ]
