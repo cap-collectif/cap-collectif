194312013da666c27ac69ad6fab7f6e4a9410809
Jpec57 <jean.paul.bella@hotmail.fr>
2020-03-30 10:12:02 +0200

    [10303-SHORT] Adding a toggle to prevent crash if no project k… (#10382)
    GitOrigin-RevId: 4f2179228910f687152acf25aff290f3085e67e5

--- src/Capco/AppBundle/RedirectionIO/ProjectKeyDataloader.php
+++ src/Capco/AppBundle/RedirectionIO/ProjectKeyDataloader.php
@@ -3,28 +3,36 @@
 namespace Capco\AppBundle\RedirectionIO;
 
 use Capco\AppBundle\Repository\SiteParameterRepository;
+use Capco\AppBundle\Toggle\Manager;
 use RuntimeException;
 
 class ProjectKeyDataloader
 {
     private $siteParamRepository;
     private $projectKey;
+    private $toggle;
 
-
-    public function __construct(SiteParameterRepository $siteParameterRepository)
+    public function __construct(SiteParameterRepository $siteParameterRepository, Manager $toggle)
     {
         $this->siteParamRepository = $siteParameterRepository;
+        $this->toggle = $toggle;
     }
 
-    public function loadKey(): string {
-        if (!$this->projectKey){
-            $projectKey = $this->siteParamRepository->findOneBy(['keyname' => 'redirectionio.project.id']);
-            if (!$projectKey){
+    public function loadKey(): string
+    {
+        if (!$this->toggle->isActive('http_redirects')) {
+            return '';
+        }
+        if (!$this->projectKey) {
+            $projectKey = $this->siteParamRepository->findOneBy([
+                'keyname' => 'redirectionio.project.id',
+            ]);
+            if (!$projectKey) {
                 throw new RuntimeException('No project key is configured in database.');
             }
             $this->projectKey = $projectKey->getValue();
         }
+
         return $this->projectKey;
     }
-
 }
