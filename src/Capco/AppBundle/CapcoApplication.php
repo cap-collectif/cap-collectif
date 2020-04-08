4b9a6d2de0fcfaad1ea119cbb483984abfce7694
mrpandat <mrpandat@hotmail.fr>
2020-04-08 17:50:33 +0200

    10407 infrav2 part2 (#10416)
    * [10407-INFRAV2] Re-Work bin/console to launch command on an instance.
    
    * [10407-INFRAV2] Add .env files in media.
    
    * [10407-INFRAV2] Remove env file.
    
    GitOrigin-RevId: 4913a376428f405d6ed9b4233073b796a998914e

---
+++ src/Capco/AppBundle/CapcoApplication.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Capco\AppBundle;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class CapcoApplication extends Application
+{
+
+    public function __construct(KernelInterface $kernel)
+    {
+        parent::__construct($kernel);
+
+        $inputDefinition = $this->getDefinition();
+        $inputDefinition->addOption(new InputOption('--instance', '-i', InputOption::VALUE_REQUIRED, 'The instance name.'));
+    }
+
+}
