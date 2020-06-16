52a60e24b39989fc3f9ac1e8e1dd029516d54d9a
Jpec57 <jean.paul.bella@hotmail.fr>
2020-06-16 11:15:38 +0200

    [10676-FIX] Changing redirection (#10718)
    GitOrigin-RevId: d0d140905c5f8017b08fb35e68ecea264c4b659e

--- src/Capco/AppBundle/Controller/Api/LocaleController.php
+++ src/Capco/AppBundle/Controller/Api/LocaleController.php
@@ -4,12 +4,11 @@ namespace Capco\AppBundle\Controller\Api;
 
 use Capco\AppBundle\Entity\Locale;
 use Capco\AppBundle\GraphQL\Mutation\Locale\SetUserDefaultLocaleMutation;
+use Capco\AppBundle\Locale\DefaultLocaleCodeDataloader;
 use Capco\AppBundle\Repository\LocaleRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,14 +18,19 @@ class LocaleController extends AbstractFOSRestController
     private $localeRepository;
     private $router;
     private $userDefaultLocaleMutation;
+    private $defaultLocaleCodeDataloader;
 
-    public function __construct(LocaleRepository $localeRepository, RouterInterface $router, SetUserDefaultLocaleMutation $userDefaultLocaleMutation)
-    {
+    public function __construct(
+        LocaleRepository $localeRepository,
+        RouterInterface $router,
+        DefaultLocaleCodeDataloader $defaultLocaleCodeDataloader,
+        SetUserDefaultLocaleMutation $userDefaultLocaleMutation
+    ) {
         $this->localeRepository = $localeRepository;
         $this->router = $router;
         $this->userDefaultLocaleMutation = $userDefaultLocaleMutation;
+        $this->defaultLocaleCodeDataloader = $defaultLocaleCodeDataloader;
     }
-
 
     /**
      * @Route("/api/change-locale/{localeCode}", name="change_locale", defaults={"_feature_flags" = "unstable__multilangue"})
@@ -38,33 +42,38 @@ class LocaleController extends AbstractFOSRestController
         $params = $request->request->get('routeParams', []);
         $keptParams = $params['_route_params'] ?? [];
 
-        if ($user !== null){
+        if (null !== $user) {
             $this->userDefaultLocaleMutation->setUserDefaultLocale($user, $localeCode);
         } else {
-            $locale = $this->localeRepository->findOneBy(['code' => $localeCode, 'published' => true]);
+            $locale = $this->localeRepository->findOneBy([
+                'code' => $localeCode,
+                'published' => true,
+            ]);
             if (!$locale || !($locale instanceof Locale)) {
-                throw new BadRequestHttpException("The locale with code ${localeCode} does not exist or is not enabled.");
+                throw new BadRequestHttpException(
+                    "The locale with code ${localeCode} does not exist or is not enabled."
+                );
             }
         }
         $request->setLocale($localeCode);
         $keptParams['_locale'] = $localeCode;
 
         try {
-            $redirectPath = $this->router->generate(
-                $routeName,
-                $keptParams
-            );
-        }catch (\Exception $exception){
-
-            $redirectPath = $this->router->generate(
-                'app_homepage',
-                ['_locale' => $localeCode]
-            );
+            $redirectPath = $this->router->generate($routeName, $keptParams);
+            $defaultLocaleCode = $this->defaultLocaleCodeDataloader->__invoke();
+            if (
+                $localeCode !== $defaultLocaleCode &&
+                0 !== strpos($redirectPath, '/' . substr($localeCode, 0, 2) . '/')
+            ) {
+                $redirectPath = $this->router->generate('app_homepage', $localeCode);
+            }
+        } catch (\Exception $exception) {
+            $redirectPath = $this->router->generate('app_homepage', ['_locale' => $localeCode]);
         }
 
         return new JsonResponse([
             'locale' => $localeCode,
-            'path' => $redirectPath
+            'path' => $redirectPath,
         ]);
     }
 }
