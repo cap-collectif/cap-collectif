9c7b14dd34d23e6dca7de4d04d83641289ea1f1a
aguilaj <julien.aguilar6@gmail.com>
2021-01-05 12:01:42 +0100

    UI not yet started debate (#11840)
    GitOrigin-RevId: 8e7def7ad8fad9d95c813a99267bc5e023f6067d

--- frontend/js/components/Debate/Page/NotYetStarted/DebateStepPageNotYetStarted.js
+++ frontend/js/components/Debate/Page/NotYetStarted/DebateStepPageNotYetStarted.js
@@ -1,17 +1,57 @@
 // @flow
 import * as React from 'react';
+import moment from 'moment';
 import { createFragmentContainer, graphql } from 'react-relay';
+import { FormattedMessage } from 'react-intl';
 import type { DebateStepPageNotYetStarted_step } from '~relay/DebateStepPageNotYetStarted_step.graphql';
 import AppBox from '~ui/Primitives/AppBox';
+import Flex from '~ui/Primitives/Layout/Flex';
+import Tag from '~ds/Tag/Tag';
+import Heading from '~ui/Primitives/Heading';
+import Button from '~ds/Button/Button';
+import Icon from '~ds/Icon/Icon';
+import Text from '~ui/Primitives/Text';
 
 type Props = {|
   +title: string,
   +step: ?DebateStepPageNotYetStarted_step,
 |};
 
-// WIP
-export const DebateStepPageNotYetStarted = ({ step }: Props) => {
-  return <AppBox id="DebateStepPageNotYetStarted">{step?.timeRange?.startAt}</AppBox>;
+// WIP : Que de l'UI y'a pas l'API donc rien n'est connecté
+export const DebateStepPageNotYetStarted = ({ step, title }: Props) => {
+  const endDate = moment(step?.timeRange?.startAt);
+  const now = moment();
+
+  const daysUntil = endDate.diff(now, 'days');
+  const hoursUntil = endDate.diff(now, 'hours');
+  const minutesUntil = endDate.diff(now, 'minutes');
+
+  let timeUntil = <FormattedMessage id="count.daysUntil" values={{ count: daysUntil }} />;
+  if (daysUntil === 0 && hoursUntil === 0 && minutesUntil !== 0)
+    timeUntil = <FormattedMessage id="count.minutesUntil" values={{ count: minutesUntil }} />;
+  if (daysUntil === 0 && hoursUntil !== 0 && minutesUntil !== 0)
+    timeUntil = <FormattedMessage id="count.hoursUntil" values={{ count: hoursUntil }} />;
+
+  return (
+    <AppBox id="DebateStepPageNotYetStarted">
+      <Flex direction="column" alignItems="center" spacing={4}>
+        <Tag variantType="badge" variant="blue" icon="CLOCK">
+          {timeUntil}
+        </Tag>
+        <Heading as="h2" fontWeight="400" mb={2} textAlign="center" color="gray.900">
+          {title}
+        </Heading>
+        <Text fontWeight="400" mb={16} textAlign="center" size="18" color="gray.700">
+          <FormattedMessage id="activate-debate-notification" />
+        </Text>
+
+        <Button variant="primary" variantColor="primary" variantSize="big" onClick={() => {}}>
+          <Icon name="CLOCK" mr="1" />
+          <FormattedMessage id="launch-mail-alert" />
+        </Button>
+      </Flex>
+    </AppBox>
+  );
 };
 
 export default createFragmentContainer(DebateStepPageNotYetStarted, {
