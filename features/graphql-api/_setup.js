b09e3ded2e811cd6226880cffc43861a1bdca509
mrpandat <mrpandat@hotmail.fr>
2021-01-26 12:06:00 +0100

    Remove Pipenv (#11927)
    * [FIX-PIP] Remove pipenv from CI.
    
    * [FIX-PIP] Add name to containers.
    
    * [FIX-PIP] Container name with capco.
    
    GitOrigin-RevId: 3b5504e112622496b12bacd5a518e966c769cf2c

--- features/graphql-api/_setup.js
+++ features/graphql-api/_setup.js
@@ -6,14 +6,14 @@ const env = process.env.CI ? 'ci' : 'local';
 
 beforeAll(async () => {
   console.log('Saving database...');
-  const { stderrDb } = await exec('pipenv run fab ' + env + '.qa.save_db');
+  const { stderrDb } = await exec('fab ' + env + '.qa.save_db');
 
   if (stderrDb) {
     console.error(`error: ${stderrDb}`);
   }
   console.log('Successfully saved database');
   console.log('Writing ElasticSearch snapshot...');
-  const { stderr } = await exec('pipenv run fab ' + env + '.qa.save_es_snapshot');
+  const { stderr } = await exec('fab ' + env + '.qa.save_es_snapshot');
 
   if (stderr) {
     console.error(`error: ${stderr}`);
@@ -23,7 +23,7 @@ beforeAll(async () => {
 
 afterAll(async () => {
   console.log('Restoring database...');
-  const { stderrDb } = await exec('pipenv run fab ' + env + '.qa.restore_db');
+  const { stderrDb } = await exec('fab ' + env + '.qa.restore_db');
 
   if (stderrDb) {
     console.error(`error: ${stderrDb}`);
@@ -31,7 +31,7 @@ afterAll(async () => {
   console.log('Successfully restored database');
 
   console.log('Restoring ElasticSearch snapshot.');
-  const { stderrEs } = await exec('pipenv run fab ' + env + '.qa.restore_es_snapshot');
+  const { stderrEs } = await exec('fab ' + env + '.qa.restore_es_snapshot');
 
   if (stderrEs) {
     console.error(`error: ${stderrEs}`);
