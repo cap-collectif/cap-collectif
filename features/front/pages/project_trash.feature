37145afb1689f8991c79f78605dcef7a03de06f1
Liinkiing <omar.jbara2@gmail.com>
2021-02-05 09:55:29 +0100

    [11990-FIX] - Corbeille de la consultation ne s'affiche pas (#11991)
    * [11990-FIX] - Fixed SourceVotesResolver
    
    * [11990-FIX] - Added e2e test
    
    GitOrigin-RevId: 2b7756cd0db3ee40be35774fbf1ae7adf0592900

--- features/front/pages/project_trash.feature
+++ features/front/pages/project_trash.feature
@@ -7,3 +7,10 @@ Scenario: User wants to see project's trash (project with one consultStep at lea
   And I visited "project trashed page" with:
     | projectSlug | budget-avec-vote-limite |
   Then I wait ".cap-bubble-attention-6" to appear on current page
+
+Scenario: User wants to see project's trash when a consultation have moderated sources
+  Given feature "project_trash" is enabled
+  And I am logged in as admin
+  And I visited "project trashed page" with:
+    | projectSlug | projet-de-loi-renseignement |
+  Then I should not see "error.500"
--- src/Capco/AppBundle/GraphQL/Resolver/Source/SourceVotesResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Source/SourceVotesResolver.php
@@ -5,6 +5,7 @@ namespace Capco\AppBundle\GraphQL\Resolver\Source;
 use Capco\AppBundle\Elasticsearch\ElasticsearchPaginator;
 use Capco\AppBundle\Search\VoteSearch;
 use Capco\AppBundle\Entity\Source;
+use Overblog\GraphQLBundle\Definition\Argument;
 use Overblog\GraphQLBundle\Definition\Argument as Arg;
 use Overblog\GraphQLBundle\Relay\Connection\ConnectionInterface;
 use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
@@ -18,8 +19,14 @@ class SourceVotesResolver implements ResolverInterface
         $this->voteSearch = $voteSearch;
     }
 
-    public function __invoke(Source $source, Arg $args): ConnectionInterface
+    public function __invoke(Source $source, ?Arg $args = null): ConnectionInterface
     {
+        if (!$args) {
+            $args = new Argument([
+                'first' => 0
+            ]);
+        }
+
         $totalCount = 0;
         $paginator = new ElasticsearchPaginator(function (?string $cursor, int $limit) use (
             $source,
