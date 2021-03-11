76015d976170cc4d56e81f5f6fb0f1513129a4a5
mauriau <maxime.auriau@protonmail.com>
2021-03-11 12:36:48 +0100

    [FIX-FRANCE] rename migration (#12275)
    GitOrigin-RevId: 38b74864503e22be62118624cdbcf681b435708e

--- src/DoctrineMigrations/Version20210311120105.php
+++ src/DoctrineMigrations/Version20210311120105.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210301160105 extends AbstractMigration
+final class Version20210311120105 extends AbstractMigration
 {
     public function getDescription(): string
     {
