01b0ac044809f147cf9e55e059317f41679356cd
Deplais Vincent <vincent.deplais@orange.fr>
2021-04-23 16:12:14 +0200

    [12479-PIE] Add label accessbility for PieChart (#12569)
    GitOrigin-RevId: 7ecd8011770fb021d399c7558e66ceefb8146471

--- frontend/js/components/Ui/Chart/PieChart.js
+++ frontend/js/components/Ui/Chart/PieChart.js
@@ -6,21 +6,22 @@ import ResponsiveContainer from 'recharts/es6/component/ResponsiveContainer';
 import Cell from 'recharts/es6/component/Cell';
 import Pie from 'recharts/es6/polar/Pie';
 import Tooltip from 'recharts/es6/component/Tooltip';
+import { useIntl } from 'react-intl';
 import defaultColors from '~/utils/colors';
 import config from '~/config';
 
-type Data = {
-  name: string,
-  value: number,
-};
+type Data = {|
+  +name: string,
+  +value: number,
+|};
 
 type Props = {
   data: Array<Data>,
-  innerRadius: number,
-  outerRadius: number,
+  innerRadius?: number,
+  outerRadius?: number,
   colors: Array<string>,
-  width: string,
-  height: string,
+  width?: string,
+  height?: string,
 };
 
 const TooltipWrapper: StyledComponent<{}, {}, HTMLDivElement> = styled.div`
@@ -49,81 +50,80 @@ const ContentWrapper: StyledComponent<{}, {}, HTMLDivElement> = styled.div.attrs
   }
 `;
 
-export class PieChart extends React.Component<Props> {
-  static defaultProps = {
-    height: '95px',
-    width: config.isMobile ? '95px' : '115px',
-    innerRadius: 10,
-    outerRadius: 45,
-  };
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  index,
+  value,
+}: Object) => {
+  const RADIAN = Math.PI / 180;
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-  renderCustomizedLabel = ({
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    index,
-    value,
-  }: Object) => {
-    const RADIAN = Math.PI / 180;
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  return (
+    <text x={x} y={y} fill="white" key={index} textAnchor="middle" dominantBaseline="central">
+      {value > 0 ? value : ''}
+    </text>
+  );
+};
 
+const renderTooltip = ({ payload }: { payload: Array<Object> }) => {
+  if (payload && payload.length > 0) {
     return (
-      <text x={x} y={y} fill="white" key={index} textAnchor="middle" dominantBaseline="central">
-        {value > 0 ? value : ''}
-      </text>
-    );
-  };
-
-  renderTooltip = (props: { payload: Array<Object> }) => {
-    const { payload } = props;
-
-    if (payload && payload.length > 0) {
-      return (
-        <TooltipWrapper>
-          {payload[0].name} - {payload[0].value}
-        </TooltipWrapper>
-      );
-    }
-
-    return null;
-  };
-
-  render() {
-    const { data, innerRadius, outerRadius, colors, width, height } = this.props;
-
-    return (
-      <ContentWrapper width={width} height={height}>
-        <ResponsiveContainer>
-          <Chart>
-            <Tooltip content={this.renderTooltip} />
-            <Pie
-              data={data}
-              innerRadius={innerRadius}
-              outerRadius={outerRadius}
-              paddingAngle={2}
-              stroke="none"
-              isAnimationActive={false}
-              percent
-              dataKey="value"
-              labelLine={false}
-              label={this.renderCustomizedLabel}>
-              {data.map((entry, index) => (
-                <Cell
-                  key={index}
-                  aria-labelledby={entry.name}
-                  fill={colors[index % colors.length]}
-                />
-              ))}{' '}
-            </Pie>
-          </Chart>
-        </ResponsiveContainer>
-      </ContentWrapper>
+      <TooltipWrapper>
+        {payload[0].name} - {payload[0].value}
+      </TooltipWrapper>
     );
   }
-}
+
+  return null;
+};
+
+export const PieChart = ({
+  data,
+  innerRadius = 10,
+  outerRadius = 45,
+  colors,
+  width = config.isMobile ? '95px' : '115px',
+  height = '95px',
+}: Props) => {
+  const intl = useIntl();
+
+  return (
+    <ContentWrapper width={width} height={height}>
+      <ResponsiveContainer>
+        <Chart>
+          <Tooltip content={renderTooltip} />
+          <Pie
+            data={data}
+            innerRadius={innerRadius}
+            outerRadius={outerRadius}
+            paddingAngle={2}
+            stroke="none"
+            isAnimationActive={false}
+            percent
+            dataKey="value"
+            labelLine={false}
+            label={renderCustomizedLabel}>
+            {data.map((entry, index) => (
+              <Cell
+                key={index}
+                aria-label={intl.formatMessage(
+                  { id: 'aria-piechart-label-vote' },
+                  { label: entry.name, count: entry.value },
+                )}
+                fill={colors[index % colors.length]}
+              />
+            ))}{' '}
+          </Pie>
+        </Chart>
+      </ResponsiveContainer>
+    </ContentWrapper>
+  );
+};
 
 export default PieChart;
