4519cd61596e39ed331b7ac0642cbcc62efd128b
Alex Tea <alextea2@gmail.com>
2021-06-11 16:07:31 +0200

    [FIXINVITATIONGROUPLABELVISIBILITY] hide pending list title if empty (#12801)
    GitOrigin-RevId: f6c480bb5f26024c4a48d96471f57e4a0ee386e6

--- frontend/js/components/Group/Admin/GroupAdminPendingInvitationsList.js
+++ frontend/js/components/Group/Admin/GroupAdminPendingInvitationsList.js
@@ -1,64 +1,66 @@
 // @flow
 import React from 'react';
-import {FormattedMessage, useIntl} from "react-intl";
-import styled from "styled-components";
-import {graphql, usePaginationFragment} from "react-relay";
-import {Button, ListGroupItem} from "react-bootstrap";
-import Text from "~ui/Primitives/Text";
-import AppBox from "~ui/Primitives/AppBox";
-import colors from "~/styles/modules/colors";
-import type {GroupAdminPendingInvitationsList_group$key} from "~relay/GroupAdminPendingInvitationsList_group.graphql";
-import Loader from "~ui/FeedbacksIndicators/Loader";
+import { FormattedMessage, useIntl } from 'react-intl';
+import styled from 'styled-components';
+import { graphql, usePaginationFragment } from 'react-relay';
+import { Button, ListGroupItem } from 'react-bootstrap';
+import Text from '~ui/Primitives/Text';
+import AppBox from '~ui/Primitives/AppBox';
+import colors from '~/styles/modules/colors';
+import type { GroupAdminPendingInvitationsList_group$key } from '~relay/GroupAdminPendingInvitationsList_group.graphql';
+import Loader from '~ui/FeedbacksIndicators/Loader';
 
 const UserInvitePendingList = styled(AppBox)`
   border: 1px solid ${colors.gray['200']};
   border-radius: 4px;
   padding: 16px;
-`
+`;
 
 const FRAGMENT = graphql`
   fragment GroupAdminPendingInvitationsList_group on Group
     @argumentDefinitions(countInvitations: { type: "Int" }, cursorInvitations: { type: "String" })
-    @refetchable(queryName: "GroupAdminPendingInvitationsListGroup")
-    {
-      pendingInvitations(first: $countInvitations, after: $cursorInvitations)
-      @connection(key: "GroupAdminPendingInvitationsList_pendingInvitations")
-      {
-        edges {
-          node {
-              email
-              isAdmin
-          }
+    @refetchable(queryName: "GroupAdminPendingInvitationsListGroup") {
+    pendingInvitations(first: $countInvitations, after: $cursorInvitations)
+      @connection(key: "GroupAdminPendingInvitationsList_pendingInvitations") {
+      edges {
+        node {
+          email
+          isAdmin
         }
       }
     }
+  }
 `;
 
 type Props = {|
-  +pendingInvitationFragmentRef: GroupAdminPendingInvitationsList_group$key
+  +pendingInvitationFragmentRef: GroupAdminPendingInvitationsList_group$key,
 |};
 
-const GroupAdminPendingInvitationsList = ({pendingInvitationFragmentRef}: Props) => {
-
+const GroupAdminPendingInvitationsList = ({ pendingInvitationFragmentRef }: Props) => {
   const intl = useIntl();
 
-  const { data: group, hasNext, loadNext, isLoadingNext } = usePaginationFragment(FRAGMENT, pendingInvitationFragmentRef);
+  const { data: group, hasNext, loadNext, isLoadingNext } = usePaginationFragment(
+    FRAGMENT,
+    pendingInvitationFragmentRef,
+  );
+
+  if (group.pendingInvitations?.edges && group.pendingInvitations.edges.length <= 0) return null;
 
   return (
     <>
-      <h3 className="box-title">{intl.formatMessage({id: 'invitation.pending'})}</h3>
-      {
-        group.pendingInvitations?.edges?.map(edges => {
-          return (
-            <UserInvitePendingList>
-              <Text>{edges?.node?.email}</Text>
-              <Text color="gray.500">{edges?.node?.isAdmin ?
-                intl.formatMessage({id: 'global.admin'})
-                : intl.formatMessage({id: 'roles.user'})}</Text>
-            </UserInvitePendingList>
-          )
-        })
-      }
+      <h3 className="box-title">{intl.formatMessage({ id: 'invitation.pending' })}</h3>
+      {group.pendingInvitations?.edges?.map(edges => {
+        return (
+          <UserInvitePendingList>
+            <Text>{edges?.node?.email}</Text>
+            <Text color="gray.500">
+              {edges?.node?.isAdmin
+                ? intl.formatMessage({ id: 'global.admin' })
+                : intl.formatMessage({ id: 'roles.user' })}
+            </Text>
+          </UserInvitePendingList>
+        );
+      })}
       {hasNext && (
         <ListGroupItem style={{ textAlign: 'center' }}>
           {isLoadingNext ? (
@@ -74,4 +76,4 @@ const GroupAdminPendingInvitationsList = ({pendingInvitationFragmentRef}: Props)
   );
 };
 
-export default GroupAdminPendingInvitationsList
+export default GroupAdminPendingInvitationsList;
