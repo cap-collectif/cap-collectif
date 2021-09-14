c7da1bafa083c3590bfd460f3b822a2a06af6934
mbuliard <mbuliard@live.fr>
2021-09-14 17:08:54 +0200

    [13284-FIX] fix MailingListUsersConnection (#13294)
    GitOrigin-RevId: 3506da73ffcd6da000d7d29ca13a68045ee1152e

--- src/Capco/AppBundle/GraphQL/Resolver/Emailing/MailingListUsersConnection.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Emailing/MailingListUsersConnection.php
@@ -3,6 +3,7 @@
 namespace Capco\AppBundle\GraphQL\Resolver\Emailing;
 
 use Capco\AppBundle\Entity\MailingList;
+use Capco\AppBundle\Repository\MailingListRepository;
 use Overblog\GraphQLBundle\Definition\Argument;
 use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
 use Overblog\GraphQLBundle\Relay\Connection\Output\Connection;
@@ -10,8 +11,16 @@ use Overblog\GraphQLBundle\Relay\Connection\Paginator;
 
 class MailingListUsersConnection implements ResolverInterface
 {
-    public function __invoke(MailingList $mailingList, Argument $argument): Connection
+    private MailingListRepository $repository;
+
+    public function __construct(MailingListRepository $repository)
     {
+        $this->repository = $repository;
+    }
+
+    public function __invoke($mailingList, Argument $argument): Connection
+    {
+        $mailingList = $this->getMailingListEntity($mailingList);
         $paginator = new Paginator(function (int $offset, int $limit) use (
             $mailingList,
             $argument
@@ -27,5 +36,14 @@ class MailingListUsersConnection implements ResolverInterface
                 ->getUsersWithValidEmail($argument->offsetGet('consentInternalCommunicationOnly'))
                 ->count()
         );
+    }
+
+    private function getMailingListEntity($mailingList): MailingList
+    {
+        if ($mailingList instanceof MailingList) {
+            return $mailingList;
+        }
+
+        return $this->repository->find($mailingList['id']);
     }
 }
