a741a35cbcb1f9f53f05b0c8cb75c31a7e1dd867
mbuliard <mbuliard@live.fr>
2021-11-04 16:47:08 +0100

    [13100-REMOVE] remove reload (#13599)
    GitOrigin-RevId: f54fee1a85926075b4c6e24003576668aa229bbd

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -16,9 +16,5 @@
 import './commands'
 
 beforeEach(() => {
-  // Looks like we need to force reload because of a weird issue causing Cypress to not always clears
-  // cookies, thus persisting PHPSESSID between tests and so the user can sometimes be already connected
-  // see https://github.com/cypress-io/cypress/issues/781
-  cy.reload(true)
   cy.appendOperationToGraphQLFetch()
 })
