c13a8520040db3fb9e6d8165a95b1cf0dda56d1e
Alex Tea <alextea2@gmail.com>
2022-03-21 16:13:39 +0100

    [REMOVE-URL] check n sub domain (#14293)
    GitOrigin-RevId: 7dbd3ccea83d322a11157867cd968225bc7336d0

--- admin-next/components/Domain/Domain.tsx
+++ admin-next/components/Domain/Domain.tsx
@@ -206,7 +206,7 @@ const Domain: FC = () => {
                     rules={{
                       pattern: {
                         // https://stackoverflow.com/questions/10306690/what-is-a-regular-expression-which-will-match-a-valid-domain-name-without-a-subd
-                        value: /^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/i,
+                        value: /^((?!-))((xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.){0,}(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/i,
                         message: intl.formatMessage({id: 'custom.domain.not.valid.error'})
                       }
                     }}
--- src/Capco/AppBundle/Validator/Constraints/CheckCustomDomainValidator.php
+++ src/Capco/AppBundle/Validator/Constraints/CheckCustomDomainValidator.php
@@ -36,7 +36,7 @@ class CheckCustomDomainValidator extends ConstraintValidator
     {
         // https://stackoverflow.com/questions/10306690/what-is-a-regular-expression-which-will-match-a-valid-domain-name-without-a-subd
         $pattern =
-            '/^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/i';
+            '/^((?!-))((xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.){0,}(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/i';
 
         return preg_match($pattern, $customDomain);
     }
