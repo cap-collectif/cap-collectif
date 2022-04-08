b330c026422f0c4afd449d9ff3d3956c76c3494d
lapwat <quentin@cap-collectif.com>
2022-04-08 16:25:18 +0200

    refacto: inject class var from config (#14380)
    * refacto: inject class var from config
    
    * create unique index
    
    * update snapshot
    
    GitOrigin-RevId: 779066ea7026ab231eea4551b23de97c2bd3b650

--- src/Capco/AppBundle/Elasticsearch/IndexBuilder.php
+++ src/Capco/AppBundle/Elasticsearch/IndexBuilder.php
@@ -83,11 +83,7 @@ class IndexBuilder
 
     public function generateIndexName(): string
     {
-        if (empty(getenv('SYMFONY_ELASTICSEARCH_INDEX'))) {
-            return sprintf('capco_%s', date('Y-m-d-H-i-s'));
-        }
-
-        return getenv('SYMFONY_ELASTICSEARCH_INDEX');
+        return sprintf('%s_%s', $this->indexName, date('Y-m-d-H-i-s'));
     }
 
     public function cleanOldIndices(int $afterLiveLimit = 2): array
