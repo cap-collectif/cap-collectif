04bff7799777d52fe9957c7bbb67c09215dd2578
Max Auriau <maxime.auriau@protonmail.com>
2023-01-12 11:09:44 +0100

    [13575-REMOVESONATAPOST] remove legacy Sonata Post (#15157)
    GitOrigin-RevId: 3ccc3fe6d11a1ea7725fadb552219b7185b4b579

--- src/Capco/AdminBundle/Controller/PostController.php
+++ src/Capco/AdminBundle/Controller/PostController.php
@@ -2,38 +2,67 @@
 
 namespace Capco\AdminBundle\Controller;
 
+use Capco\AppBundle\Entity\Post;
 use Capco\AppBundle\Security\PostVoter;
 use Overblog\GraphQLBundle\Relay\Node\GlobalId;
-use Sonata\AdminBundle\Controller\CRUDController as Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
-class PostController extends Controller
+class PostController extends AbstractController
 {
-    public function createAction(Request $request = null)
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * @Route("/admin/capco/app/post/create", name="capco_admin_create_post")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function createAction(?Request $request = null)
     {
         $proposalId = $request->query->get('proposalId', null);
-        return $this->renderWithExtraParams(
-            $this->admin->getTemplate('create'),
+        $post = new Post();
+
+        return $this->render(
+            'CapcoAdminBundle:Post:create.html.twig',
             [
                 'action' => 'create',
-                'object' => $this->admin->getNewInstance(),
-                'proposalId' => $proposalId
+                'object' => $post,
+                'admin' => $post,
+                'proposalId' => $proposalId,
             ],
-            null,
-            $request
+            null
         );
     }
 
-    public function editAction($id = null)
+    /**
+     * @Route("/admin/capco/app/post/{postId}/edit", name="capco_admin_edit_post")
+     * @Entity("post", class="CapcoAppBundle:Post", options={"mapping" = {"postId": "id"}, "repository_method"= "find", "map_method_signature" = true})
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function editAction(?Post $post)
     {
-        $post = $this->admin->getObject($id);
         $this->denyAccessUnlessGranted(PostVoter::EDIT, $post);
-        return $this->renderWithExtraParams(
-            $this->admin->getTemplate('edit'),
+
+        return $this->render(
+            'CapcoAdminBundle:Post:edit.html.twig',
             [
                 'action' => 'edit',
                 'object' => $post,
-                'postId' => GlobalId::toGlobalId('Post', $id)
+                'title' => $this->translator->trans(
+                    'global.edit.title',
+                    ['name' => $post->getTitle()],
+                    'SonataAdminBundle'
+                ),
+                'admin' => $post,
+                'postId' => GlobalId::toGlobalId('Post', $post->getId()),
             ],
             null
         );
