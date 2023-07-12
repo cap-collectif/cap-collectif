2745b31803e2e02ab5149149b5bbf1432a615440
Julien Aguilar <julien.aguilar6@gmail.com>
2023-07-12 15:10:49 +0200

    Add delete modal on new project page BO (#16018)
    GitOrigin-RevId: f4e68ac87d983389de6cbd5f54cf8c893b7e327a

---
+++ admin-next/components/Projects/ProjectConfig/DeleteModal.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+import { Button, CapUIModalSize, Heading, Modal, Text } from '@cap-collectif/ui';
+import { useIntl } from 'react-intl';
+
+type DeleteModalProps = {
+    onClose: () => void,
+    onDelete: () => void,
+};
+
+const DeleteModal: FC<DeleteModalProps> = ({ onDelete, onClose }) => {
+    const intl = useIntl();
+
+    return (
+        <Modal
+            show
+            onClose={onClose}
+            size={CapUIModalSize.Md}
+            ariaLabel={intl.formatMessage({ id: 'group.admin.step.modal.delete.content' })}>
+            <Modal.Header>
+                <Heading>
+                    {intl.formatMessage({ id: 'group.admin.step.modal.delete.content' })}
+                </Heading>
+            </Modal.Header>
+            <Modal.Body>
+                <Text mt={1} mb={2}>
+                    {intl.formatMessage({ id: 'global-action-irreversible' })}
+                </Text>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    type="button"
+                    onClick={onClose}
+                    variant="secondary"
+                    variantSize="big"
+                    variantColor="hierarchy">
+                    {intl.formatMessage({ id: 'cancel' })}
+                </Button>
+                <Button type="button" variantColor="danger" variantSize="big" onClick={onDelete}>
+                    {intl.formatMessage({ id: 'action_delete' })}
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default DeleteModal;
--- admin-next/components/Projects/ProjectConfig/ProjectConfigFormSteps.tsx
+++ admin-next/components/Projects/ProjectConfig/ProjectConfigFormSteps.tsx
@@ -12,6 +12,8 @@ import {
     Text,
 } from '@cap-collectif/ui';
 import { useIntl } from 'react-intl';
+import DeleteModal from './DeleteModal';
+import { useDisclosure } from '@liinkiing/react-hooks';
 
 const getWordingStep = (type: string) =>
     type === 'DebateStep' ? 'global.debate' : `${type.slice(0, -4).toLowerCase()}_step`;
@@ -21,7 +23,8 @@ const getStepUri = (type: string) => `${type.slice(0, -4).toLowerCase()}-step`;
 const ProjectConfigFormSteps: React.FC = () => {
     const intl = useIntl();
     const { control, watch } = useFormContext();
-
+    const { isOpen, onOpen, onClose } = useDisclosure(false);
+    const [indexToDelete, setIndexToDelete] = React.useState(0);
     const stepsValues = watch('steps');
 
     const {
@@ -41,83 +44,110 @@ const ProjectConfigFormSteps: React.FC = () => {
     };
 
     return (
-        <Flex p={6} direction="column" spacing={8} backgroundColor="white" borderRadius="accordion">
-            <Heading as="h4" fontWeight="semibold" color="blue.800">
-                {intl.formatMessage({ id: 'project-header-step-modal-title' })}
-            </Heading>
-            <Flex direction="column" spacing={3}>
-                {steps.length ? (
-                    // @ts-ignore DragnDrop typings not on point yet
-                    <DragnDrop onDragEnd={onDragEnd}>
-                        <DragnDrop.List droppableId="steps">
-                            {steps.map((step, index) => (
-                                <DragnDrop.Item draggableId={step.id} index={index} key={step.id}>
-                                    <ListCard.Item
-                                        bg="gray.100"
-                                        borderRadius="normal"
-                                        borderWidth="1px"
-                                        borderColor="gray.200"
-                                        mb={1}
-                                        mt={1}
-                                        py={2}
-                                        draggable
-                                        width="100%"
-                                        _hover={{ bg: 'gray.100' }}>
-                                        <Flex direction="column">
-                                            <Text color="gray.500" fontSize={1} fontWeight={400}>
-                                                {intl.formatMessage({
-                                                    id: getWordingStep(
-                                                        stepsValues[index].__typename,
-                                                    ),
-                                                })}
-                                            </Text>
-                                            <Text color="blue.900" fontSize={2} fontWeight={600}>
-                                                {stepsValues[index].label}
-                                            </Text>
-                                        </Flex>
-                                        <ButtonGroup>
-                                            <ButtonQuickAction
-                                                variantColor="blue"
-                                                icon={CapUIIcon.Pencil}
-                                                label={intl.formatMessage({
-                                                    id: 'global.edit',
-                                                })}
-                                                onClick={() => {
-                                                    window.location.href += `/update-step/${getStepUri(
-                                                        stepsValues[index].__typename,
-                                                    )}/${stepsValues[index].id}`;
-                                                }}
-                                                type="button"
-                                            />
-                                            <ButtonQuickAction
-                                                // TODO : Modal
-                                                onClick={() => remove(index)}
-                                                variantColor="red"
-                                                icon={CapUIIcon.Trash}
-                                                label={intl.formatMessage({
-                                                    id: 'global.delete',
-                                                })}
-                                                type="button"
-                                            />
-                                        </ButtonGroup>
-                                    </ListCard.Item>
-                                </DragnDrop.Item>
-                            ))}
-                        </DragnDrop.List>
-                    </DragnDrop>
-                ) : (
-                    intl.formatMessage({ id: 'configure-project-steps' })
-                )}
+        <>
+            {isOpen ? (
+                <DeleteModal
+                    onClose={onClose}
+                    onDelete={() => {
+                        onClose();
+                        remove(indexToDelete);
+                    }}
+                />
+            ) : null}
+            <Flex
+                p={6}
+                direction="column"
+                spacing={8}
+                backgroundColor="white"
+                borderRadius="accordion">
+                <Heading as="h4" fontWeight="semibold" color="blue.800">
+                    {intl.formatMessage({ id: 'project-header-step-modal-title' })}
+                </Heading>
+                <Flex direction="column" spacing={3}>
+                    {steps.length ? (
+                        // @ts-ignore DragnDrop typings not on point yet
+                        <DragnDrop onDragEnd={onDragEnd}>
+                            <DragnDrop.List droppableId="steps">
+                                {steps.map((step, index) => (
+                                    <DragnDrop.Item
+                                        draggableId={step.id}
+                                        index={index}
+                                        key={step.id}>
+                                        <ListCard.Item
+                                            bg="gray.100"
+                                            borderRadius="normal"
+                                            borderWidth="1px"
+                                            borderColor="gray.200"
+                                            mb={1}
+                                            mt={1}
+                                            py={2}
+                                            draggable
+                                            width="100%"
+                                            _hover={{ bg: 'gray.100' }}>
+                                            <Flex direction="column">
+                                                <Text
+                                                    color="gray.500"
+                                                    fontSize={1}
+                                                    fontWeight={400}>
+                                                    {intl.formatMessage({
+                                                        id: getWordingStep(
+                                                            stepsValues[index].__typename,
+                                                        ),
+                                                    })}
+                                                </Text>
+                                                <Text
+                                                    color="blue.900"
+                                                    fontSize={2}
+                                                    fontWeight={600}>
+                                                    {stepsValues[index].label}
+                                                </Text>
+                                            </Flex>
+                                            <ButtonGroup>
+                                                <ButtonQuickAction
+                                                    variantColor="blue"
+                                                    icon={CapUIIcon.Pencil}
+                                                    label={intl.formatMessage({
+                                                        id: 'global.edit',
+                                                    })}
+                                                    onClick={() => {
+                                                        window.location.href += `/update-step/${getStepUri(
+                                                            stepsValues[index].__typename,
+                                                        )}/${stepsValues[index].id}`;
+                                                    }}
+                                                    type="button"
+                                                />
+                                                <ButtonQuickAction
+                                                    onClick={() => {
+                                                        setIndexToDelete(index);
+                                                        onOpen();
+                                                    }}
+                                                    variantColor="red"
+                                                    icon={CapUIIcon.Trash}
+                                                    label={intl.formatMessage({
+                                                        id: 'global.delete',
+                                                    })}
+                                                    type="button"
+                                                />
+                                            </ButtonGroup>
+                                        </ListCard.Item>
+                                    </DragnDrop.Item>
+                                ))}
+                            </DragnDrop.List>
+                        </DragnDrop>
+                    ) : (
+                        intl.formatMessage({ id: 'configure-project-steps' })
+                    )}
+                </Flex>
+                <div>
+                    <Button
+                        variant="secondary"
+                        leftIcon={CapUIIcon.Add}
+                        onClick={() => (window.location.href += '/create-step')}>
+                        {intl.formatMessage({ id: 'global.add' })}
+                    </Button>
+                </div>
             </Flex>
-            <div>
-                <Button
-                    variant="secondary"
-                    leftIcon={CapUIIcon.Add}
-                    onClick={() => (window.location.href += '/create-step')}>
-                    {intl.formatMessage({ id: 'global.add' })}
-                </Button>
-            </div>
-        </Flex>
+        </>
     );
 };
 
