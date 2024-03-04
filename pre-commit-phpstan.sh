767d6caaae5bc0c45282a6d9fb847aa6fdc37332
Amelie Le Coz <amelie.lecoz@cap-collectif.com>
2024-03-04 11:35:56 +0100

    add a script to select only files to be committed (#16785)
    GitOrigin-RevId: 52f1918ea65fb36477852adf8a48f9b4e1fbbfc9

---
+++ pre-commit-phpstan.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Récupère les fichiers PHP ajoutés, copiés ou modifiés qui sont prêts à être commités
+FILES=$(git diff --cached --name-only --diff-filter=ACM | grep '\.php$')
+
+if [ "$FILES" ]; then
+    echo "Analyse PHPStan des fichiers PHP modifiés..."
+    php -dmemory_limit=-1 bin/phpstan analyse -c phpstan.neon $FILES
+    if [ $? != 0 ]; then
+        echo "PHPStan a détecté des problèmes."
+        exit 1
+    fi
+fi
+
+exit 0
