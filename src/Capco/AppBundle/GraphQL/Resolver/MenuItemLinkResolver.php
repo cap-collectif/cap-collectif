5b605be5b29008b0208d69b022b30bb2cca47eeb
Alex Tea <alex.tea@cap-collectif.com>
2024-04-22 09:48:25 +0200

    navbar links resolver (#16934)
    * resolve links
    
    * add isEvaluerOnNewTool in viewer
    
    GitOrigin-RevId: 3ca076513a45f965f700f70e4efa4c69a52d4b91

---
+++ src/Capco/AppBundle/GraphQL/Resolver/MenuItemLinkResolver.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Capco\AppBundle\GraphQL\Resolver;
+
+use Capco\AppBundle\Entity\MenuItem;
+use Overblog\GraphQLBundle\Definition\Resolver\QueryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Validator\Constraints\Url;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class MenuItemLinkResolver implements QueryInterface
+{
+    private RouterInterface $router;
+    private ValidatorInterface $validator;
+
+    public function __construct(RouterInterface $router, ValidatorInterface $validator)
+    {
+        $this->router = $router;
+        $this->validator = $validator;
+    }
+
+    public function __invoke(
+        MenuItem $item,
+        RequestStack $requestStack
+    ): string
+    {
+        $request = $requestStack->getCurrentRequest();
+        $locale = $request->getLocale();
+        if ($item->getPage()) {
+            return $this->router->generate('app_page_show', [
+                'slug' => $item
+                    ->getPage()
+                    ->translate()
+                    ->getSlug(),
+                '_locale' => $locale,
+            ]);
+        }
+        $url = $item->getLink($locale, true);
+
+        if ('/' === $url) {
+            return $this->router->generate('app_homepage', ['_locale' => $locale]);
+        }
+
+        $constraint = new Url([
+            'message' => 'not_valid_url',
+            'payload' => ['severity' => 'warning'],
+        ]);
+
+        $errorList = $this->validator->validate($url, $constraint);
+
+        if (0 === \count($errorList)) {
+            return $url ?? '';
+        }
+
+        $routeMatch = $this->router->match("/{$url}");
+        $routeParams = $this->getUrlParams($routeMatch);
+
+        if ('capco_app_cms' === $routeMatch['_route']) {
+            $route = $this->router->generate(
+                'capco_app_cms',
+                array_merge(['url' => $url], $routeParams)
+            );
+        } else {
+            $route = $this->router->generate(
+                $routeMatch['_route'],
+                array_merge(['_locale' => $locale], $routeParams)
+            );
+        }
+
+        return $route;
+    }
+
+    private function getUrlParams(array $routeMatch): array
+    {
+        return array_filter(
+            $routeMatch,
+            function ($value) {
+                return '_' !== $value[0];
+            },
+            \ARRAY_FILTER_USE_KEY
+        );
+    }
+}
