8775e2062857e926d74843c05766d256e648eca4
Alex Tea <alex.tea@cap-collectif.com>
2024-12-04 12:48:46 +0100

    remove NEXT_PUBLIC_SYMFONY_REDIS_PREFIX in condition (#17585)
    GitOrigin-RevId: 345bab3a23adb6a61d63686b433eb0fa73503c16

--- admin-next/utils/feature-flags-resolver.ts
+++ admin-next/utils/feature-flags-resolver.ts
@@ -3,7 +3,7 @@ import { FeatureFlags } from '../types'
 import { defaultFeatureFlags } from '@shared/hooks/useFeatureFlag'
 
 const getRedisFeatureFlagKey = (flag: string) => {
-  const prefix = process.env.NEXT_PUBLIC_SYMFONY_REDIS_PREFIX || process.env.SYMFONY_REDIS_PREFIX
+  const prefix = process.env.SYMFONY_REDIS_PREFIX
   return `${prefix}feature_toggle__TOGGLE__${flag}`
 }
 
