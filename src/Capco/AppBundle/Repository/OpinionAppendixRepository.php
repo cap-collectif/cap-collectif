aff28abe78a55f730678448d598ff573bebc1266
Alex Tea <alex.tea@cap-collectif.com>
2025-02-06 11:40:16 +0100

    put back OpinionTypeAppendixTypeRepository (#17833)
    * put back OpinionTypeAppendixTypeRepository
    
    * put back OpinionAppendixRepository
    
    GitOrigin-RevId: 0d09ed766f757b32c7a2f2dd1f3e383a0aed1d55

---
+++ src/Capco/AppBundle/Repository/OpinionAppendixRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Capco\AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * OpinionAppendixRepository.
+ */
+class OpinionAppendixRepository extends EntityRepository
+{
+}
