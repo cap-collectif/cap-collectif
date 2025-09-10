fe3b5e1022771eb12cec1f739c0ba71939a1e222
myracodes <myriam@cap-collectif.com>
2025-09-10 15:30:30 +0200

    test(emailConfirmation): refactor behat to cypress (#18619)
    GitOrigin-RevId: 57086863102c50a84b6af82bfb3f58c88e7964dc

---
+++ cypress/integration/user/emailConfirmation.cy.ts
@@ -0,0 +1,17 @@
+describe('Email confirmation', () => {
+  before(() => {
+    cy.task('db:restore')
+    cy.task('enable:feature', 'registration')
+  })
+  it('should confirm email for not confirmed user', () => {
+    cy.visit('/account/email_confirmation/azertyuiop')
+    cy.get('#navbar-username').should('exist').and('be.visible').and('contain', 'user_not_confirmed')
+    cy.get('#alert-email-not-confirmed').should('not.exist')
+  })
+
+  it('should update email for user with valid token', () => {
+    cy.visit('/account/new_email_confirmation/check-my-new-email-token')
+    cy.get('#navbar-username').should('exist').and('be.visible').and('contain', 'user_updating_email')
+    cy.get('#alert-email-not-confirmed').should('not.exist')
+  })
+})
