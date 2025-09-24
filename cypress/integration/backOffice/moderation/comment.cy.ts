936f687c347a3b373416003daaf1c77f3b33e93d
myracodes <myriam@cap-collectif.com>
2025-09-24 15:29:00 +0200

    test(BO): refactor behat comments moderation test (#18723)
    GitOrigin-RevId: 6298958ea6d401e69d90add95b18d6e53885bf49

---
+++ cypress/integration/backOffice/moderation/comment.cy.ts
@@ -0,0 +1,18 @@
+describe('Comments moderation', () => {
+  before(() => {
+    cy.task('db:restore')
+  })
+
+  it('should view comments moderation page and its content', () => {
+    cy.interceptGraphQLOperation({ operationName: 'AdminRightNavbarAppQuery' })
+    cy.directLoginAs('admin')
+    cy.visit('/admin/capco/app/comment/list')
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // returning false here prevents Cypress from failing the test
+      return false
+    })
+    cy.wait('@AdminRightNavbarAppQuery', { timeout: 10000 })
+    cy.get('a').contains('global.contenu').should('exist').and('be.visible')
+    cy.get('table tbody tr').should('have.length.greaterThan', 23)
+  })
+})
