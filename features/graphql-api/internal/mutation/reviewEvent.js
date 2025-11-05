f1298eaf495d0ff8c17c13f45a2bf9f0ed9d717b
Adrien <adrien@cap-collectif.com>
2025-11-05 10:18:16 +0100

    Migrate review event folder to jest (#18999)
    * Migrate officialResponse from behat to jest (#18925)
    
    * Migrate reviewEvent from behat to jest (#18925)
    
    GitOrigin-RevId: 29f1782eff55112d5ec4496293da1872abeeddd8

---
+++ features/graphql-api/internal/mutation/reviewEvent.js
@@ -0,0 +1,88 @@
+/* eslint-env jest */
+import '../../resetDatabaseBeforeEach'
+
+const ReviewEventMutation = /* GraphQL*/ `
+    mutation ($input: ReviewEventInput!) {
+      reviewEvent(input: $input) {
+        event {
+          _id
+          review {
+            createdAt
+            status
+            reviewer {
+              _id
+            }
+            refusedReason
+          }
+        }
+      }
+    }
+`
+
+describe('mutations.reviewEventMutation', () => {
+  it('Admin approved an event in awaiting status', async () => {
+    await expect(
+      graphql(
+        ReviewEventMutation,
+        {
+          input: {
+            id: 'RXZlbnQ6ZXZlbnRDcmVhdGVCeUFVc2VyUmV2aWV3QXdhaXRpbmc=',
+            status: 'APPROVED',
+          },
+        },
+        'internal_admin',
+      ),
+    ).resolves.toMatchSnapshot()
+  })
+
+  it('Admin refused an event in awaiting status', async () => {
+    await expect(
+      graphql(
+        ReviewEventMutation,
+        {
+          input: {
+            id: 'RXZlbnQ6ZXZlbnRDcmVhdGVCeUFVc2VyUmV2aWV3QXdhaXRpbmc=',
+            status: 'REFUSED',
+            refusedReason: 'SPAM',
+            comment: 'On se calme sur le SPAM, merci.',
+          },
+        },
+        'internal_admin',
+      ),
+    ).resolves.toMatchSnapshot()
+  })
+
+  it('Admin try to review an event ever approved', async () => {
+    await expect(
+      graphql(
+        ReviewEventMutation,
+        {
+          input: {
+            id: 'RXZlbnQ6ZXZlbnRDcmVhdGVCeUFVc2VyUmV2aWV3QXBwcm92ZWQ=',
+            status: 'REFUSED',
+            refusedReason: 'SPAM',
+            comment: 'On se calme sur le SPAM, merci.',
+          },
+        },
+        'internal_admin',
+      ),
+    ).resolves.toMatchSnapshot()
+  })
+
+  it('SuperAdmin change review of event ever approved', async () => {
+    await expect(
+      graphql(
+        ReviewEventMutation,
+        {
+          input: {
+            id: 'RXZlbnQ6ZXZlbnRDcmVhdGVCeUFVc2VyUmV2aWV3QXBwcm92ZWQ=',
+            status: 'REFUSED',
+            refusedReason: 'SPAM',
+            comment: 'On se calme sur le SPAM, merci.',
+          },
+        },
+        'internal_super_admin',
+      ),
+    ).resolves.toMatchSnapshot()
+  })
+})
