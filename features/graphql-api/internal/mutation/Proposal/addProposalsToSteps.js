41a2a67a54996b4bfcad1d0d0ffeb6377063347a
Adrien <adrien@cap-collectif.com>
2025-11-06 15:40:38 +0100

    Migrate proposal tests folder to jest (#19014)
    * Migrate addProposalsToSteps from behat to jest (#18925)
    
    * Migrate removeProposalsToSteps from behat to jest (#18925)
    
    * Migrate updateProposalVote from behat to jest (#18925)
    
    GitOrigin-RevId: f06cfd7ce1a43f98b7f6f463b89d255ef602b2f6

---
+++ features/graphql-api/internal/mutation/Proposal/addProposalsToSteps.js
@@ -0,0 +1,86 @@
+/* eslint-env jest */
+import '../../../_setup'
+
+const addProposalsToSteps = /* GraphQL */ `
+  mutation ($input: AddProposalsToStepsInput!) {
+    addProposalsToSteps(input: $input) {
+      error
+      steps {
+        id
+      }
+      proposals {
+        edges {
+          node {
+            id
+            selections {
+              step {
+                id
+              }
+              status {
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+describe('Internal|add proposals to steps', () => {
+  it('Admin tries to add an invalid proposal and receives an error', async () => {
+    const response = await graphql(
+      addProposalsToSteps,
+      {
+        input: {
+          proposalIds: ['idonotexist', 'meneither'],
+          stepIds: ['U2VsZWN0aW9uU3RlcDpzZWxlY3Rpb25zdGVwMQ=='],
+        },
+      },
+      'internal_admin',
+    )
+    expect(response).toMatchSnapshot()
+  })
+
+  it('Admin sends step not matching the project or not a collect step and receives an error', async () => {
+    const response = await graphql(
+      addProposalsToSteps,
+      {
+        input: {
+          proposalIds: ['UHJvcG9zYWw6cHJvcG9zYWwx'],
+          stepIds: ['collectstep1', 'selectionQuestionStepVoteClassement'],
+        },
+      },
+      'internal_admin',
+    )
+    expect(response).toMatchSnapshot()
+  })
+
+  it('Admin add an already present step to a proposal', async () => {
+    const response = await graphql(
+      addProposalsToSteps,
+      {
+        input: {
+          proposalIds: ['UHJvcG9zYWw6cHJvcG9zYWwx'],
+          stepIds: ['U2VsZWN0aW9uU3RlcDpzZWxlY3Rpb25zdGVwMQ=='],
+        },
+      },
+      'internal_admin',
+    )
+    expect(response).toMatchSnapshot()
+  })
+
+  it('Admin add a step to two proposals, one already having it. The new step has default status', async () => {
+    const response = await graphql(
+      addProposalsToSteps,
+      {
+        input: {
+          proposalIds: ['UHJvcG9zYWw6cHJvcG9zYWwx', 'UHJvcG9zYWw6cHJvcG9zYWwy'],
+          stepIds: ['U2VsZWN0aW9uU3RlcDpzZWxlY3Rpb25zdGVwMg=='],
+        },
+      },
+      'internal_admin',
+    )
+    expect(response).toMatchSnapshot()
+  })
+})
---
+++ features/graphql-api/internal/mutation/Proposal/removeProposalsToSteps.js
@@ -0,0 +1,83 @@
+/* eslint-env jest */
+import '../../../_setup'
+
+const removeProposalsToSteps = /* GraphQL */ `
+  mutation ($input: RemoveProposalsFromStepsInput!) {
+    removeProposalsFromSteps(input: $input) {
+      error
+      steps {
+        id
+      }
+      proposals {
+        edges {
+          node {
+            id
+            selections {
+              step {
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+describe('Internal|remove proposals to steps', () => {
+  it('Admin tries to remove an invalid proposal and receives an error', async () => {
+    const response = await graphql(
+      removeProposalsToSteps,
+      {
+        input: {
+          proposalIds: ['idonotexist', 'meneither'],
+          stepIds: ['U2VsZWN0aW9uU3RlcDpzZWxlY3Rpb25zdGVwMg=='],
+        },
+      },
+      'internal_admin',
+    )
+    expect(response).toMatchSnapshot()
+  })
+
+  it('Admin sends step not matching the project or not a collect step and receives an error', async () => {
+    const response = await graphql(
+      removeProposalsToSteps,
+      {
+        input: {
+          proposalIds: ['UHJvcG9zYWw6cHJvcG9zYWwx'],
+          stepIds: ['collectstep1', 'selectionQuestionStepVoteClassement'],
+        },
+      },
+      'internal_admin',
+    )
+    expect(response).toMatchSnapshot()
+  })
+
+  it(' Admin wants to remove a proposal not present in a step', async () => {
+    const response = await graphql(
+      removeProposalsToSteps,
+      {
+        input: {
+          proposalIds: ['UHJvcG9zYWw6cHJvcG9zYWwx'],
+          stepIds: ['U2VsZWN0aW9uU3RlcDpzZWxlY3Rpb25zdGVwMg=='],
+        },
+      },
+      'internal_admin',
+    )
+    expect(response).toMatchSnapshot()
+  })
+
+  it(' Admin remove a step from two proposals, only one having it', async () => {
+    const response = await graphql(
+      removeProposalsToSteps,
+      {
+        input: {
+          proposalIds: ['UHJvcG9zYWw6cHJvcG9zYWwx', 'UHJvcG9zYWw6cHJvcG9zYWwy'],
+          stepIds: ['U2VsZWN0aW9uU3RlcDpzZWxlY3Rpb25zdGVwMg=='],
+        },
+      },
+      'internal_admin',
+    )
+    expect(response).toMatchSnapshot()
+  })
+})
