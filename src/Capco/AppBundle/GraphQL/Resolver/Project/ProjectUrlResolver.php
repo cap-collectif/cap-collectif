7d0403908eed56046550a6a00d27e43dcd61828d
Adrien <adrien@cap-collectif.com>
2026-04-27 15:44:25 +0200

    Add new project page url scoped to new_project_page feature flag (#19… (#19652)
    * Add new project page url scoped to new_project_page feature flag (#19505)
    
    * Apply suggestion from @ArthurFalquePierrotin
    
    Co-authored-by: Arthur FP <7476515+ArthurFalquePierrotin@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Arthur FP <7476515+ArthurFalquePierrotin@users.noreply.github.com>
    GitOrigin-RevId: 9dbe192041863522f1b3cb48f00ff0ec5a759cca

--- src/Capco/AppBundle/GraphQL/Resolver/Project/ProjectUrlResolver.php
+++ src/Capco/AppBundle/GraphQL/Resolver/Project/ProjectUrlResolver.php
@@ -3,9 +3,11 @@
 namespace Capco\AppBundle\GraphQL\Resolver\Project;
 
 use Capco\AppBundle\Entity\Project;
+use Capco\AppBundle\Entity\ProjectTab;
 use Capco\AppBundle\Entity\Steps\AbstractStep;
 use Capco\AppBundle\GraphQL\Resolver\Step\StepUrlResolver;
 use Capco\AppBundle\Resolver\LocaleResolver;
+use Capco\AppBundle\Toggle\Manager;
 use Overblog\GraphQLBundle\Definition\Resolver\QueryInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -19,7 +21,8 @@ class ProjectUrlResolver implements QueryInterface
         protected RouterInterface $router,
         protected RequestStack $requestStack,
         LocaleResolver $localeResolver,
-        protected StepUrlResolver $stepUrlResolver
+        protected StepUrlResolver $stepUrlResolver,
+        protected ?Manager $toggleManager = null
     ) {
         $this->defaultLocale = $localeResolver->getDefaultLocaleCodeForRequest();
     }
@@ -29,6 +32,10 @@ class ProjectUrlResolver implements QueryInterface
         $locale = $this->requestStack->getCurrentRequest()->getLocale();
         if (null === $locale || empty($locale)) {
             $locale = $this->defaultLocale;
+        }
+
+        if ($this->toggleManager?->isActive(Manager::new_project_page)) {
+            return $this->resolveProjectPageUrl($project, $locale);
         }
 
         $firstStep = $project->getFirstStep();
@@ -42,5 +49,34 @@ class ProjectUrlResolver implements QueryInterface
         }
 
         return $this->stepUrlResolver->__invoke($firstStep);
+    }
+
+    private function resolveProjectPageUrl(Project $project, string $locale): string
+    {
+        $enabledTabsCount = $project->getTabs()->filter(
+            static fn (ProjectTab $tab): bool => $tab->isEnabled()
+        )->count();
+        $firstStep = $project->getFirstStep();
+
+        if (0 === $enabledTabsCount && $firstStep instanceof AbstractStep && 1 === $project->getSteps()->count()) {
+            return $this->stepUrlResolver->__invoke($firstStep);
+        }
+
+        if (0 === $enabledTabsCount && !$firstStep instanceof AbstractStep) {
+            return $this->router->generate(
+                'app_project',
+                ['_locale' => $locale],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            );
+        }
+
+        return $this->router->generate(
+            'app_project_show_project',
+            [
+                'projectSlug' => $project->getSlug(),
+                '_locale' => $locale,
+            ],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
     }
 }
