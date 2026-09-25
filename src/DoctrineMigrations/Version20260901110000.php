e0e40f234532168dd9d7834a91a357820052a332
Alex Tea <alex.tea@cap-collectif.com>
2026-09-25 11:02:26 +0200

    fix(migrations): resume map visibility migration (#20282)
    GitOrigin-RevId: 61dc5a8bbed20030aa82fc94fe37746c6d096b9c

--- src/DoctrineMigrations/Version20260901110000.php
+++ src/DoctrineMigrations/Version20260901110000.php
@@ -16,8 +16,10 @@ final class Version20260901110000 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE step ADD map_shown_by_default TINYINT(1) DEFAULT \'0\' NOT NULL');
-        $this->addSql("UPDATE step INNER JOIN proposal_form ON step.proposal_form_id = proposal_form.id SET step.map_shown_by_default = 1 WHERE proposal_form.is_map_view_enabled = 1 AND step.main_view IN ('GRID', 'LIST')");
+        if (!$schema->getTable('step')->hasColumn('map_shown_by_default')) {
+            $this->addSql('ALTER TABLE step ADD map_shown_by_default TINYINT(1) DEFAULT \'0\' NOT NULL');
+        }
+        $this->addSql("UPDATE step INNER JOIN proposal_form ON proposal_form.step_id = step.id SET step.map_shown_by_default = 1 WHERE proposal_form.map_view_enabled = 1 AND step.main_view IN ('GRID', 'LIST')");
     }
 
     public function down(Schema $schema): void
